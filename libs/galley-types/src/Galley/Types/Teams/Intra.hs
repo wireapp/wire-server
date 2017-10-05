b11cdf034174e9b246a275543d200be0ca444ba8
jschaul <jschaul@users.noreply.github.com>
2017-10-05 16:37:58 +0200

    Activate team on account activation only
    Add Pending state to Teams, which is their initial state. Upon the first
    user account activation, teams also become active.

--- libs/galley-types/src/Galley/Types/Teams/Intra.hs
+++ libs/galley-types/src/Galley/Types/Teams/Intra.hs
@@ -13,6 +13,7 @@ data TeamStatus
     | PendingDelete
     | Deleted
     | Suspended
+    | PendingActive
     deriving (Eq, Show)
 
 instance ToJSON TeamStatus where
@@ -20,12 +21,14 @@ instance ToJSON TeamStatus where
     toJSON PendingDelete = String "pending_delete"
     toJSON Deleted       = String "deleted"
     toJSON Suspended     = String "suspended"
+    toJSON PendingActive = String "pending_active"
 
 instance FromJSON TeamStatus where
     parseJSON (String "active")         = pure Active
     parseJSON (String "pending_delete") = pure PendingDelete
     parseJSON (String "deleted")        = pure Deleted
     parseJSON (String "suspended")      = pure Suspended
+    parseJSON (String "pending_active") = pure PendingActive
     parseJSON other                     = fail $ "Unknown TeamStatus: " <> show other
 
 data TeamData = TeamData
@@ -56,3 +59,4 @@ instance FromJSON TeamStatusUpdate where
 
 instance ToJSON TeamStatusUpdate where
     toJSON s = object ["status" .= tuStatus s]
+
