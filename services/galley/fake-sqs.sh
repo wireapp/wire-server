0ced5fbf7472532be9d1f67651678f90d7fb5812
jschaul <jschaul@users.noreply.github.com>
2018-01-29 17:48:15 +0100

    Re-add optOrEnvSafe, minor fixes

--- services/galley/fake-sqs.sh
+++ services/galley/fake-sqs.sh
@@ -10,8 +10,17 @@ function cleanup() {
 trap cleanup EXIT
 
 docker run -d --cidfile DOCKER_ID -p 4568:4568 airdock/fake-sqs 
-sleep 2
-curl http://localhost:4568 -d "Action=CreateQueue&QueueName=integration-team-events.fifo" 
+while true
+do
+  curl --silent http://localhost:4568 -d "Action=CreateQueue&QueueName=integration-team-events.fifo"
+  if [ $? -eq 0 ]; then
+    echo "Queue created, all set!"
+    break
+  else
+    echo "Waiting for the queue to be created..."
+  fi
+  sleep 0.5
+done
 
 while true
 do
--- services/galley/test/integration/Main.hs
+++ services/galley/test/integration/Main.hs
@@ -5,6 +5,7 @@ module Main (main) where
 
 import Bilge hiding (header, body)
 import Control.Lens
+import Control.Monad (join)
 import Data.Aeson
 import Data.ByteString.Conversion
 import Data.Monoid
@@ -27,9 +28,10 @@ import Util.Options.Common
 import Util.Test
 
 import qualified API
-import qualified API.Util as Util
-import qualified API.SQS  as SQS
-import qualified Data.ByteString.Char8 as BS
+import qualified API.Util               as Util
+import qualified API.SQS                as SQS
+import qualified Data.ByteString.Char8  as BS
+import qualified System.Posix.Env       as Posix
 
 data IntegrationConfig = IntegrationConfig
   -- internal endpoints
@@ -71,28 +73,31 @@ main :: IO ()
 main = withOpenSSL $ runTests go
   where
     go g i = withResource (getOpts g i) releaseOpts $ API.tests
-
     getOpts gFile iFile = do
-        m <- newManager tlsManagerSettings {
-            managerResponseTimeout = responseTimeoutMicro 300000000
-        }
-        let local p = Endpoint { _epHost = "127.0.0.1", _epPort = p }
+        m <- newManager tlsManagerSettings {managerResponseTimeout = responseTimeoutMicro 300000000}
+        let local p = Endpoint {_epHost = "127.0.0.1", _epPort = p}
         gConf <- handleParseError =<< decodeFileEither gFile
         iConf <- handleParseError =<< decodeFileEither iFile
         g <- mkRequest <$> optOrEnv galley iConf (local . read) "GALLEY_WEB_PORT"
-        b <- mkRequest <$> optOrEnv brig   iConf (local . read) "BRIG_WEB_PORT"
+        b <- mkRequest <$> optOrEnv brig iConf (local . read) "BRIG_WEB_PORT"
         c <- mkRequest <$> optOrEnv cannon iConf (local . read) "CANNON_WEB_PORT"
         -- unset this env variable in galley's config to disable testing SQS team events
-        q <- optOrEnv (handleAws awsQueueName) gConf (Just . pack)              "GALLEY_SQS_TEAM_EVENTS"
-        e <- optOrEnv (handleAws awsEndpoint)  gConf (fromByteString . BS.pack) "GALLEY_SQS_ENDPOINT"
-
+        q <- join <$> optOrEnvSafe queueName gConf (Just . pack) "GALLEY_SQS_TEAM_EVENTS"
+        e <- join <$> optOrEnvSafe endpoint gConf (fromByteString . BS.pack) "GALLEY_SQS_ENDPOINT"
         Util.TestSetup m g b c <$> initAwsEnv e q
 
-    handleAws name = maybe Nothing (Just . view name) . view optJournal
+    queueName = fmap (view awsQueueName) . view optJournal
+    endpoint = fmap (view awsEndpoint) . view optJournal
 
-    initAwsEnv (Just e) (Just q) = SQS.mkAWSEnv (JournalOpts q e) >>= return . Just
-    initAwsEnv _        _        = return Nothing
+    initAwsEnv (Just e) (Just q) = Just <$> SQS.mkAWSEnv (JournalOpts q e)
+    initAwsEnv _ _               = return Nothing
 
     releaseOpts _ = return ()
 
     mkRequest (Endpoint h p) = host (encodeUtf8 h) . port p
+
+-- similar to optOrEnv, except return None if an environment variable is not defined
+optOrEnvSafe :: (a -> b) -> Maybe a -> (String -> b) -> String -> IO (Maybe b)
+optOrEnvSafe getter conf reader var = case conf of
+    Nothing -> fmap reader <$>Posix.getEnv var
+    Just c -> pure $ Just (getter c)
