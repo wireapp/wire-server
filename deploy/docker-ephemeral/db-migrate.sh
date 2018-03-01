ff35ff1caa9217817c5815d873e18385b3edd6e2
jschaul <jschaul@users.noreply.github.com>
2018-03-01 11:31:59 +0100

    reset ES index, 'create' not idempotent.

--- deploy/docker-ephemeral/db-migrate.sh
+++ deploy/docker-ephemeral/db-migrate.sh
@@ -4,7 +4,7 @@ until_ready() {
     until $1; do echo 'service not ready yet'; sleep 5; done
 }
 
-until_ready "brig-index create --elasticsearch-server http://elasticsearch:9200"
+until_ready "brig-index reset --elasticsearch-server http://elasticsearch:9200"
 
 until_ready "brig-schema --host cassandra --keyspace brig_test --replication-factor 1"
 until_ready "galley-schema --host cassandra --keyspace galley_test --replication-factor 1"
