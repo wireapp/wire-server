d30209f18e386517879f7c2e253c4775f5ed7801
jschaul <jschaul@users.noreply.github.com>
2018-03-15 16:07:26 +0100

    bash process management improvements

--- services/integrationtest.sh
+++ services/integrationtest.sh
@@ -4,19 +4,30 @@ set -eo pipefail
 USAGE="$0 <test-executable> [args...]"
 EXE=${1:?$USAGE}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PID=$$
+PARENT_PID=$$
 
 function kill_all() {
-    kill -9 -$(ps -o pgid= $PID | grep -o '[0-9]*')
+    # kill the process tree of the PARENT_PID
+    kill -9 -${PARENT_PID} &> /dev/null
 }
 
-function stop_nicely() {
-    trap "kill_all" INT EXIT TERM ERR
-    kill -2 $(pgrep -f integration.yaml) &> /dev/null
+function list_descendants () {
+  local children=$(ps -o pid= --ppid "$1")
+  for pid in $children
+  do
+    list_descendants "$pid"
+  done
+  echo "$children"
+}
+
+function kill_gracefully() {
+    pkill gundeck
     sleep 1
+    kill $(list_descendants $PARENT_PID) &> /dev/null
 }
 
-trap "stop_nicely" INT EXIT TERM ERR
+trap "kill_gracefully" INT TERM ERR
+trap "kill_all" EXIT
 
 blue=6
 white=7
@@ -29,8 +40,8 @@ function run() {
     service=$1
     colour=$2
     export LOG_LEVEL=$3
-    (cd ${DIR}/${service} && exec dist/${service} -c ${service}.integration.yaml \
-        | sed -e "s/^/$(tput setaf ${colour})[${service}] /" -e "s/$/$(tput sgr0)/" || stop_nicely) &
+    (cd ${DIR}/${service} && dist/${service} -c ${service}.integration.yaml || kill_all) \
+        | sed -e "s/^/$(tput setaf ${colour})[${service}] /" -e "s/$/$(tput sgr0)/" &
 }
 
 # brig,gundeck,galley use the amazonka library's 'Discover', which expects AWS credentials
@@ -47,6 +58,6 @@ run cannon ${orange} Info
 
 sleep 3
 
-(${EXE} "${@:2}" && stop_nicely || stop_nicely) &
+${EXE} "${@:2}" && kill_gracefully || kill_gracefully &
 
 wait
