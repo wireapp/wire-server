f7fa0877c83f9f538c06c04ae610d05b7c209ea6
Tiago Loureiro <tiago.loureiro@gmail.com>
2018-04-03 22:38:29 +0200

    Order TurnURI by scheme and transport

--- libs/brig-types/test/unit/Test/Brig/Types/TURN.hs
+++ libs/brig-types/test/unit/Test/Brig/Types/TURN.hs
@@ -2,16 +2,44 @@
 
 module Test.Brig.Types.TURN where
 
-import Brig.Types.TURN hiding (turnURI)
+import Brig.Types.TURN
 import Data.Aeson
+import Data.List (sort)
+import Data.Misc
+import System.Random.Shuffle
 import Test.Brig.Types.Arbitrary ()
 import Test.Tasty
+import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 
 tests :: TestTree
 tests = testGroup "TURN"
     [ testProperty "TurnURI: decode . encode = id" turnURIid
+    , testGroup "Order"
+        [ testCase "Turn order example" turnURIOrder
+        ]
     ]
 
 turnURIid :: TurnURI -> Property
 turnURIid t = Just t === (decode . encode) t
+
+turnURIOrder :: IO ()
+turnURIOrder = go (100 :: Int)
+  where
+    go 0 = return ()
+    go n = do
+        shuffled <- shuffleM ordered
+        assertEqual "Unexpected shuffle & sorting" (sort shuffled) ordered
+        go (n - 1)
+
+    -- Prefer servers with `SchemeTURN` and then `TransportUDP`
+    ordered = [ turnURI SchemeTurn  host port Nothing
+              , turnURI SchemeTurn  host port (Just TransportUDP)
+              , turnURI SchemeTurn  host port (Just TransportTCP)
+              , turnURI SchemeTurns host port Nothing
+              , turnURI SchemeTurns host port (Just TransportUDP)
+              , turnURI SchemeTurns host port (Just TransportTCP)
+              ]
+
+    host = TurnHost $ IpAddr $ read "127.0.0.1"
+    port = Port 12345
