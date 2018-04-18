24fccdbf2e3720af4a278e14e67b27a9d41e1e0b
Artyom Kazak <yom@artyom.me>
2018-04-18 10:52:15 +0200

    Add Morphing Identity-related migrations

--- services/brig/src/Brig/App.hs
+++ services/brig/src/Brig/App.hs
@@ -118,7 +118,7 @@ import qualified System.Logger            as Log
 import qualified System.Logger.Class      as LC
 
 schemaVersion :: Int32
-schemaVersion = 49
+schemaVersion = 50
 
 -------------------------------------------------------------------------------
 -- Environment
