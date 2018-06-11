f1d2993fac94d904e0be41af7b43fb796e3ffdc9
Matthias Fischmann <mf@zerobuzz.net>
2018-06-11 17:10:20 +0200

    ...  (playing with bilge).

--- services/spar/src/Spar/Brig.hs
+++ services/spar/src/Spar/Brig.hs
@@ -1,15 +1,22 @@
+{-# LANGUAGE ConstraintKinds            #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE ViewPatterns               #-}
 
 module Spar.Brig where
 
+-- TODO: when creating user, we need to be able to provide more
+-- master data (first name, last name, ...)
+
 import Control.Monad.Except
-import Data.String.Conversions (ST, cs)
+import Data.String.Conversions
+import Bilge
+import Network.HTTP.Types.Method
 
 import qualified Brig.Types.User as Brig
 import qualified Data.Id as Brig
@@ -28,16 +35,26 @@ fromUserSSOId (Brig.UserSSOId (cs -> tenant) (cs -> subject)) =
     (_, Left msg)      -> throwError msg
 
 
-class MonadBrigClient (m :: * -> *)  -- no, use runHttpT
-  reqGet :: (ToJSON reqbody, FromJSON respbody) => [ST] -> reqbody -> m respbody
+class Monad m => MonadSparToBrig m where
+  call :: (Request -> Request) -> m (Response (Maybe LBS))
 
 
-getUser :: MonadBrigClient m => SAML.UserId -> m (Maybe Brig.UserId)
-getUser = undefined
+getUser :: (MonadSparToBrig m) => SAML.UserId -> m (Maybe Brig.UserId)
+getUser uid = do
+  resp :: Response (Maybe LBS) <- call
+    $ method GET
+    . path ("/i/user/by-ssoid/" <> cs (show uid))  -- TODO: does anything like this exist?
+  if statusCode resp == 200
+    then do
+      undefined  -- parse body and return user id from brig.  if parsing fails, log a warning and
+                 -- return Nothing.
+        -- (what should the body look like?  a full 'User' object?)
+    else do
+      pure Nothing
 
--- TODO: first name, last name, ...?
-createUser :: MonadBrigClient m => SAML.UserId -> m Brig.UserId
-createUser = undefined
+createUser :: (MonadSparToBrig m) => SAML.UserId -> m Brig.UserId
+createUser _ = undefined
 
-forwardBrigLogin :: MonadBrigClient m => Brig.UserId -> m SAML.Void
+-- | Get session token from brig and redirect user past login process.
+forwardBrigLogin :: (MonadSparToBrig m) => Brig.UserId -> m SAML.Void
 forwardBrigLogin = undefined
