25246c941c2e6a3e20be614e38b0562fd07c3ce7
Matthias Fischmann <mf@zerobuzz.net>
2018-06-12 16:09:55 +0200

    integration testing.

--- services/spar/test-integration/Spec.hs
+++ services/spar/test-integration/Spec.hs
@@ -1,1 +1,14 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
+
+import qualified Test.Spar.APISpec
+import qualified Test.Spar.DataSpec
+import Test.Hspec
+
+main :: IO ()
+main = hspec =<< spec
+
+spec :: IO Spec
+spec = do
+  let specData = describe "Test.Spar.Data" Test.Spar.DataSpec.spec
+  specAPI <- describe "Test.Spar.API" <$> Test.Spar.APISpec.mkspec
+  pure $ specData >> specAPI
