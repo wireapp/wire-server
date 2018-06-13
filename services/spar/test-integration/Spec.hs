48159bec4e0d439339c2557a3cafe411738c822c
Matthias Fischmann <mf@zerobuzz.net>
2018-06-13 17:14:05 +0200

    config file / cli handling.

--- services/spar/test-integration/Spec.hs
+++ services/spar/test-integration/Spec.hs
@@ -1,14 +1,47 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
+
+import Data.Monoid
+import Options.Applicative
+import Spar.Options
+import Test.Hspec
 
 import qualified Test.Spar.APISpec
 import qualified Test.Spar.DataSpec
-import Test.Hspec
+
 
 main :: IO ()
 main = hspec =<< spec
 
 spec :: IO Spec
 spec = do
+  let desc = "Spar - SSO Service Integration Test Suite"
+  (_integrationConfigFilePath, configFilePath) <- execParser (info (helper <*> cliOptsParser) (header desc <> fullDesc))
+  _opts :: Opts <- readOptsFile configFilePath
+  -- intopts :: IntegrationConfig <- readOptsFile integrationConfigFilePath
+
   let specData = describe "Test.Spar.Data" Test.Spar.DataSpec.spec
   specAPI <- describe "Test.Spar.API" <$> Test.Spar.APISpec.mkspec
   pure $ specData >> specAPI
+
+
+-- | Accept config file locations as cli options.
+cliOptsParser :: Parser (String, String)
+cliOptsParser = (,) <$>
+  (strOption $
+    long "integration-config"
+    <> short 'i'
+    <> help "Integration config to load"
+    <> showDefault
+    <> value defaultIntPath)
+  <*>
+  (strOption $
+    long "service-config"
+    <> short 's'
+    <> help "Brig application config to load"
+    <> showDefault
+    <> value defaultBrigPath)
+  where
+    defaultIntPath = "/etc/wire/spar/conf/integration.yaml"
+    defaultBrigPath = "/etc/wire/spar/conf/spar.yaml"
