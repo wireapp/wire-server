b7ef2fd79c0f719d9421c21e561ea6536c4b6b86
Matthias Fischmann <mf@zerobuzz.net>
2018-06-13 12:48:38 +0200

    ...

--- services/spar/test-integration/Test/Spar/APISpec.hs
+++ services/spar/test-integration/Test/Spar/APISpec.hs
@@ -7,35 +7,42 @@ import Bilge
 import Control.Lens
 import Control.Monad
 import Control.Monad.IO.Class
-import Data.Text.Encoding
+import Data.String.Conversions
 import Spar.API ()
 import Spar.Options as Opts
 import Test.Hspec
 import Util.Options
+import SAML2.WebSSO as SAML
 
 
 mkspec :: IO Spec
 mkspec = do
   opts :: Opts <- getOpts
   mgr :: Manager <- newManager defaultManagerSettings
-  let req :: (Request -> Request)
-      req = Bilge.host (opts ^. to Opts.brig . epHost . to encodeUtf8)
-           . Bilge.port (opts ^. to Opts.brig . epPort)
+  let brigreq :: (Request -> Request)
+      brigreq = Bilge.host (opts ^. to Opts.brig . epHost . to cs)
+              . Bilge.port (opts ^. to Opts.brig . epPort)
+      sparreq :: (Request -> Request)
+      sparreq = Bilge.host (opts ^. to Opts.saml . SAML.cfgSPHost . to cs)
+              . Bilge.port (opts ^. to Opts.saml . SAML.cfgSPPort . to fromIntegral)
 
   let shouldRespondWith :: forall a. (HasCallStack, Show a, Eq a) => Http a -> (a -> Bool) -> Expectation
       shouldRespondWith action proper = liftIO (runHttpT mgr action) >>= \resp -> resp `shouldSatisfy` proper
 
   pure $ do
     describe "happy flow" $ do
-      it "/i/status" $ do
-        ping req `shouldRespondWith` (== ())
+      it "brig /i/status" $ do
+        ping brigreq `shouldRespondWith` (== ())
+
+      it "spar /i/status" $ do
+        ping sparreq `shouldRespondWith` (== ())
 
       it "/i/meta" $ do
-        get (req . path "/i/meta" . expect2xx)
+        get (sparreq . path "/i/meta" . expect2xx)
           `shouldRespondWith` (error . show)
 
       it "/i/authreq" $ do
-        get (req . path "/i/authreq/azure-test" . expect2xx)
+        get (sparreq . path "/i/authreq/azure-test" . expect2xx)
           `shouldRespondWith` (error . show)
 
       it "/i/authresp" $ do
