c84aa22f8cc7eeb8813e62639cbbf11b69e0d97c
Matthias Fischmann <mf@zerobuzz.net>
2018-06-13 17:31:39 +0200

    Fixup

--- services/spar/test-integration/Spec.hs
+++ services/spar/test-integration/Spec.hs
@@ -6,23 +6,24 @@ import Data.Monoid
 import Options.Applicative
 import Spar.Options
 import Test.Hspec
+import System.Environment
 
 import qualified Test.Spar.APISpec
 import qualified Test.Spar.DataSpec
 
 
 main :: IO ()
-main = hspec =<< spec
+main = withArgs [] . hspec =<< mkspec
 
-spec :: IO Spec
-spec = do
+mkspec :: IO Spec
+mkspec = do
   let desc = "Spar - SSO Service Integration Test Suite"
   (_integrationConfigFilePath, configFilePath) <- execParser (info (helper <*> cliOptsParser) (header desc <> fullDesc))
-  _opts :: Opts <- readOptsFile configFilePath
+  opts :: Opts <- readOptsFile configFilePath
   -- intopts :: IntegrationConfig <- readOptsFile integrationConfigFilePath
 
   let specData = describe "Test.Spar.Data" Test.Spar.DataSpec.spec
-  specAPI <- describe "Test.Spar.API" <$> Test.Spar.APISpec.mkspec
+  specAPI <- describe "Test.Spar.API" <$> Test.Spar.APISpec.mkspec opts
   pure $ specData >> specAPI
 
 
