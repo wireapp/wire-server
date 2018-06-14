0eb655e0ab41ad234b987f8c9810a6b80437310c
Matthias Fischmann <mf@zerobuzz.net>
2018-06-14 11:33:35 +0200

    ... (fix some integration tests.)

--- services/spar/test-integration/Test/Spar/APISpec.hs
+++ services/spar/test-integration/Test/Spar/APISpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns        #-}
 
 module Test.Spar.APISpec where
 
@@ -7,6 +8,7 @@ import Bilge
 import Control.Lens
 import Control.Monad
 import Control.Monad.IO.Class
+import Data.List (isInfixOf)
 import Data.String.Conversions
 import Spar.API ()
 import Spar.Options as Opts
@@ -25,31 +27,42 @@ mkspec opts = do
       sparreq = Bilge.host (opts ^. to Opts.saml . SAML.cfgSPHost . to cs)
               . Bilge.port (opts ^. to Opts.saml . SAML.cfgSPPort . to fromIntegral)
 
-  let shouldRespondWith :: forall a. (HasCallStack, Show a, Eq a) => Http a -> (a -> Bool) -> Expectation
+      shouldRespondWith :: forall a. (HasCallStack, Show a, Eq a) => Http a -> (a -> Bool) -> Expectation
       shouldRespondWith action proper = liftIO (runHttpT mgr action) >>= \resp -> resp `shouldSatisfy` proper
 
+      ping :: (Request -> Request) -> Http ()
+      ping req = void . get $ req . path "/i/status" . expect2xx
+
   pure $ do
-    xdescribe "happy flow" $ do
+    describe "happy flow" $ do
       it "brig /i/status" $ do
         ping brigreq `shouldRespondWith` (== ())
 
       it "spar /i/status" $ do
         ping sparreq `shouldRespondWith` (== ())
 
-      it "/i/meta" $ do
-        get (sparreq . path "/i/meta" . expect2xx)
-          `shouldRespondWith` (error . show)
+      it "metainfo" $ do
+        get (sparreq . path "/sso/metainfo" . expect2xx)
+          `shouldRespondWith` (\(responseBody -> Just (cs -> bdy)) -> all (`isInfixOf` bdy)
+                                [ "md:SPSSODescriptor"
+                                , "validUntil"
+                                , "WantAssertionsSigned=\"true\""
+                                ])
 
-      it "/i/authreq" $ do
-        get (sparreq . path "/i/authreq/azure-test" . expect2xx)
-          `shouldRespondWith` (error . show)
+      it "authreq" $ do
+        get (sparreq . path "/sso/initiate-login/azure-test" . expect2xx)
+          `shouldRespondWith` (\(responseBody -> Just (cs -> bdy)) -> all (`isInfixOf` bdy)
+                                [ "<html xml:lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">"
+                                , "<body onload=\"document.forms[0].submit()\">"
+                                , "<input name=\"SAMLRequest\" type=\"hidden\" "
+                                ])
 
-      it "/i/authresp" $ do
+      it "authresp" $ do
         pending
         -- (just fake the response from the IdP?)
 
     describe "access denied" $ do
-      it "/i/authresp" $ do
+      it "/sso/authresp" $ do
         pending
 
     it "rejects responses not matching any request" $ do
@@ -57,7 +70,3 @@ mkspec opts = do
 
     it "rejects replayed assertions" $ do
       pending
-
-
-ping :: (Request -> Request) -> Http ()
-ping req = void . get $ req . path "/i/status" . expect2xx
