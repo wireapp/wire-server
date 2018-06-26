e9ff52d502d4811b29b82942c53b16fcc331f392
Matthias Fischmann <mf@zerobuzz.net>
2018-06-26 17:33:15 +0200

    Document maxttl value(s) in config file better.

--- services/spar/test/Test/Spar/DataSpec.hs
+++ services/spar/test/Test/Spar/DataSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
@@ -25,12 +26,12 @@ spec = do
     check (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T08:30:00Z" (Left TTLInPast)
 
 
-check :: HasCallStack => Env -> String -> Either TTLError TTL -> Spec
+check :: HasCallStack => Env -> String -> Either TTLError (TTL "authresp") -> Spec
 check env (parsetm -> endOfLife) expectttl =
-  it (show (env, endOfLife)) $ mkTTL env endOfLife `shouldBe` expectttl
+  it (show (env, endOfLife)) $ mkTTLAssertions env endOfLife `shouldBe` expectttl
 
-mkDataEnv :: HasCallStack => String -> TTL -> Env
-mkDataEnv now = Env (parsetm now)
+mkDataEnv :: HasCallStack => String -> (TTL "authresp") -> Env
+mkDataEnv now maxttl = Env (parsetm now) 0{- will not be looked at -} maxttl{- this one will -}
 
 parsetm :: HasCallStack => String -> UTCTime
 parsetm = fromJust . parseTimeM True defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ"
