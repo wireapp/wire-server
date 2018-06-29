85aa39a9f593b17fac071c63633fdd514605cdab
Matthias Fischmann <mf@zerobuzz.net>
2018-06-29 10:26:35 +0200

    Cleanup.

--- services/spar/src/Spar/Data/Instances.hs
+++ services/spar/src/Spar/Data/Instances.hs
@@ -13,8 +13,8 @@ import Text.XML.DSig (renderKeyInfo, parseKeyInfo)
 import URI.ByteString
 import Text.XML.Util (parseURI')
 
-import qualified Data.Binary.Builder as BB
 import qualified SAML2.WebSSO as SAML
+import qualified Text.XML.Util as SAML
 
 instance Cql (SignedCertificate) where
     ctype = Tagged TextColumn
@@ -25,7 +25,7 @@ instance Cql (SignedCertificate) where
 
 instance Cql (URIRef Absolute) where
     ctype = Tagged TextColumn
-    toCql = CqlText . cs. BB.toLazyByteString . serializeURIRef
+    toCql = CqlText . SAML.renderURI
 
     fromCql (CqlText t) = parseURI' $ t
     fromCql _           = fail "URI: expected CqlBlob"
