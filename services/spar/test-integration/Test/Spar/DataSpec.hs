0f3ccd89f509cb0593c5849c898eb86a27fa5d22
Matthias Fischmann <mf@zerobuzz.net>
2018-07-02 15:21:14 +0200

    tweak integration test context construction.

--- services/spar/test-integration/Test/Spar/DataSpec.hs
+++ services/spar/test-integration/Test/Spar/DataSpec.hs
@@ -1,53 +1,63 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Test.Spar.DataSpec where
 
 import Spar.Data ()
 import Test.Hspec
+import Util
 
 
-spec :: Spec  -- TODO: do we need any of these at all?
+spec :: SpecWith TestEnv
 spec = do
+  describe "TTL" $ do
+    it "works in seconds" $ \_ -> do
+      pending
+
+
+  -- TODO: do we need any of the ones below at all?
+
   describe "cql binding" $ do
     describe "storeRequest" $ do
-      it "stores req ID with end-of-life to disk" $ do
+      it "stores req ID with end-of-life to disk" $ \_ -> do
         pending
 
-      it "sets TTL" $ do
+      it "sets TTL" $ \_ -> do
         pending
 
     describe "checkAgainstRequest" $ do
       context "request exists and is alive" $ do
-        it "returns False" $ do
+        it "returns False" $ \_ -> do
           pending
 
       context "request exists, but is outdated" $ do
-        it "returns False" $ do
+        it "returns False" $ \_ -> do
           pending
 
       context "request does not exist" $ do
-        it "returns True" $ do
+        it "returns True" $ \_ -> do
           pending
 
     describe "storeAssertion" $ do
       context "assertion exists and is alive" $ do
-        it "returns False" $ do
+        it "returns False" $ \_ -> do
           pending
 
-        it "does not write anything" $ do
+        it "does not write anything" $ \_ -> do
           pending
 
       context "assertion exists, but is outdated" $ do
-        it "returns False" $ do
+        it "returns False" $ \_ -> do
           pending
 
-        it "does not write anything" $ do
+        it "does not write anything" $ \_ -> do
           pending
 
       context "assertion does not exist" $ do
-        it "returns True" $ do
+        it "returns True" $ \_ -> do
           pending
 
-        it "stores the new assertion" $ do
+        it "stores the new assertion" $ \_ -> do
           pending
 
-        it "sets new assertion's TTL" $ do
+        it "sets new assertion's TTL" $ \_ -> do
           pending
