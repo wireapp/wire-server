3d55a9661a438d62b82197df3b79a2cac82e078b
Matthias Fischmann <mf@zerobuzz.net>
2018-07-02 17:01:28 +0200

    Fix: request life expectancy option.

--- services/spar/test/Test/Spar/DataSpec.hs
+++ services/spar/test/Test/Spar/DataSpec.hs
@@ -15,6 +15,7 @@ import Data.Time
 import Spar.Data
 import Spar.Options
 import Test.Hspec
+import SAML2.WebSSO (Time(Time), addTime)
 
 
 spec :: Spec
@@ -24,6 +25,11 @@ spec = do
     check (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T09:00:40Z" (Left TTLTooLong)
     check (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T09:00:00Z" (Left TTLNegative)
     check (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T08:30:00Z" (Left TTLNegative)
+
+  describe "ttlToNominalDiffTime" $ do
+    it "" $ do
+      addTime (ttlToNominalDiffTime $ TTL 3) (Time $ parsetm "1924-07-14T08:30:00Z")
+        `shouldBe` (Time $ parsetm "1924-07-14T08:30:03Z")
 
 
 check :: HasCallStack => Env -> String -> Either TTLError (TTL "authresp") -> Spec
