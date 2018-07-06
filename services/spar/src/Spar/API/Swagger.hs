73ce0b27757cbbb33d0a1ad3c708912cc2916641
Artyom Kazak <yom@artyom.me>
2018-07-06 12:10:58 +0200

    Fix Spar schema tests (#397)
    * Fix the NewIdP schema
    
    * Fix the schemas for IDs
    
    * "Fix" the NoContent schema
    
    * Add a comment

--- services/spar/src/Spar/API/Swagger.hs
+++ services/spar/src/Spar/API/Swagger.hs
@@ -18,6 +18,7 @@
 module Spar.API.Swagger where
 
 import Data.Proxy
+import Data.UUID (UUID)
 import "swagger2" Data.Swagger hiding (Header(..))
   -- NB: this package depends on both types-common, swagger2, so there is no away around this name
   -- clash other than -XPackageImports.
@@ -28,11 +29,11 @@ import Spar.API.Instances ()
 import Spar.Types
 import Web.Cookie (SetCookie)
 
-import qualified Data.Aeson as Swagger
-import qualified Data.Scientific as Swagger
+import qualified Data.Swagger.SchemaOptions as Swagger
 import qualified Data.Id as Brig
 import qualified Data.X509 as X509
 import qualified SAML2.WebSSO as SAML
+import qualified SAML2.WebSSO.Config.TH as SAML
 import qualified Servant.Multipart as SM
 import qualified URI.ByteString as URI
 
@@ -48,51 +49,63 @@ instance ToSchema Swagger where
 instance HasSwagger route => HasSwagger (SM.MultipartForm SM.Mem resp :> route) where
   toSwagger _proxy = toSwagger (Proxy @route)
 
-instance ToParamSchema Brig.TeamId
-instance ToParamSchema Brig.UserId
-instance ToParamSchema SAML.IdPId
-instance ToSchema Brig.TeamId
-instance ToSchema Brig.UserId
-instance ToSchema NewIdP
-instance ToSchema SAML.AuthnRequest
-instance ToSchema (SAML.FormRedirect SAML.AuthnRequest)
-instance ToSchema (SAML.IdPConfig Brig.TeamId)  -- TODO: would be nice to add an example here, but that only works for json?
-instance ToSchema SAML.IdPId
-instance ToSchema (SAML.ID SAML.AuthnRequest)
-instance ToSchema SAML.Issuer
-instance ToSchema SAML.Time
-instance ToSchema SAML.Version
+-- | The options to use for schema generation. Must match the options used
+-- for 'ToJSON' instances elsewhere.
+samlSchemaOptions :: SchemaOptions
+samlSchemaOptions = Swagger.fromAesonOptions SAML.deriveJSONOptions
+
+instance ToParamSchema Brig.TeamId where
+  toParamSchema _ = toParamSchema (Proxy @UUID)
+
+instance ToParamSchema Brig.UserId where
+  toParamSchema _ = toParamSchema (Proxy @UUID)
+
+instance ToParamSchema SAML.IdPId where
+  toParamSchema _ = toParamSchema (Proxy @UUID)
+
+instance ToSchema Brig.TeamId where
+  declareNamedSchema _ = declareNamedSchema (Proxy @UUID)
+instance ToSchema Brig.UserId where
+  declareNamedSchema _ = declareNamedSchema (Proxy @UUID)
+instance ToSchema SAML.IdPId where
+  declareNamedSchema _ = declareNamedSchema (Proxy @UUID)
+
+instance ToSchema SAML.AuthnRequest where
+  declareNamedSchema = genericDeclareNamedSchema samlSchemaOptions
+
+instance ToSchema (SAML.FormRedirect SAML.AuthnRequest) where
+  declareNamedSchema = genericDeclareNamedSchema samlSchemaOptions
+  -- TODO: would be nice to add an example here, but that only works for json?
+
+instance ToSchema (SAML.IdPConfig Brig.TeamId) where
+  declareNamedSchema = genericDeclareNamedSchema samlSchemaOptions
+
+instance ToSchema NewIdP where
+  declareNamedSchema = genericDeclareNamedSchema samlSchemaOptions
+
+instance ToSchema (SAML.ID SAML.AuthnRequest) where
+  declareNamedSchema = genericDeclareNamedSchema samlSchemaOptions
+
+instance ToSchema SAML.Issuer where
+  declareNamedSchema _ = declareNamedSchema (Proxy @String)
+
+instance ToSchema SAML.Time where
+  declareNamedSchema = genericDeclareNamedSchema samlSchemaOptions
+
+instance ToSchema SAML.Version where
+  declareNamedSchema = genericDeclareNamedSchema samlSchemaOptions
 
 instance ToSchema X509.SignedCertificate where
-  declareNamedSchema _proxy = genericDeclareNamedSchema defaultSchemaOptions (Proxy @())
+  declareNamedSchema _ = declareNamedSchema (Proxy @String)
 
 instance ToSchema SAML.SPDesc where
-  declareNamedSchema _proxy = genericDeclareNamedSchema defaultSchemaOptions (Proxy @())
+  declareNamedSchema _ = declareNamedSchema (Proxy @String)
 
 instance ToSchema URI.URI where
-  declareNamedSchema _proxy = genericDeclareNamedSchema defaultSchemaOptions (Proxy @())
+  declareNamedSchema _ = declareNamedSchema (Proxy @String)
 
 instance ToParamSchema URI.URI where
-  toParamSchema _proxy = mkEmptyParamSchema SwaggerBoolean
+  toParamSchema _ = toParamSchema (Proxy @String)
 
 instance ToParamSchema SetCookie where
-  toParamSchema _proxy = mkEmptyParamSchema SwaggerBoolean
-
-mkEmptyParamSchema :: SwaggerType t -> ParamSchema t
-mkEmptyParamSchema _paramSchemaType = ParamSchema {..}
-  where
-    _paramSchemaDefault = Nothing :: Maybe Swagger.Value
-    _paramSchemaFormat = Nothing :: Maybe Format
-    _paramSchemaItems = Nothing :: Maybe (SwaggerItems t)
-    _paramSchemaMaximum = Nothing :: Maybe Swagger.Scientific
-    _paramSchemaExclusiveMaximum = Nothing :: Maybe Bool
-    _paramSchemaMinimum = Nothing :: Maybe Swagger.Scientific
-    _paramSchemaExclusiveMinimum = Nothing :: Maybe Bool
-    _paramSchemaMaxLength = Nothing :: Maybe Integer
-    _paramSchemaMinLength = Nothing :: Maybe Integer
-    _paramSchemaPattern = Nothing :: Maybe Pattern
-    _paramSchemaMaxItems = Nothing :: Maybe Integer
-    _paramSchemaMinItems = Nothing :: Maybe Integer
-    _paramSchemaUniqueItems = Nothing :: Maybe Bool
-    _paramSchemaEnum = Nothing :: Maybe [Swagger.Value]
-    _paramSchemaMultipleOf = Nothing :: Maybe Swagger.Scientific
+  toParamSchema _ = toParamSchema (Proxy @Bool)
