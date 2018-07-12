033a7fd7abdfdcac04b6e87816713a0d94e6c4d2
Matthias Fischmann <mf@zerobuzz.net>
2018-07-12 11:33:30 +0200

    Clean up unit test stdout.

--- services/spar/test/Test/Spar/DataSpec.hs
+++ services/spar/test/Test/Spar/DataSpec.hs
@@ -24,10 +24,10 @@ import SAML2.WebSSO (Time(Time), addTime)
 spec :: Spec
 spec = do
   describe "mkTTL" $ do
-    check (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T09:00:15Z" (Right 15)
-    check (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T09:00:40Z" (Left TTLTooLong)
-    check (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T09:00:00Z" (Left TTLNegative)
-    check (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T08:30:00Z" (Left TTLNegative)
+    check 1 (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T09:00:15Z" (Right 15)
+    check 2 (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T09:00:40Z" (Left TTLTooLong)
+    check 3 (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T09:00:00Z" (Left TTLNegative)
+    check 4 (mkDataEnv "1924-07-14T09:00:00Z" 30) "1924-07-14T08:30:00Z" (Left TTLNegative)
 
   describe "ttlToNominalDiffTime" $ do
     it "" $ do
@@ -35,9 +35,9 @@ spec = do
         `shouldBe` (Time $ parsetm "1924-07-14T08:30:03Z")
 
 
-check :: HasCallStack => Env -> String -> Either TTLError (TTL "authresp") -> Spec
-check env (parsetm -> endOfLife) expectttl =
-  it (show (env, endOfLife)) $ mkTTLAssertions env endOfLife `shouldBe` expectttl
+check :: HasCallStack => Int -> Env -> String -> Either TTLError (TTL "authresp") -> Spec
+check testnumber env (parsetm -> endOfLife) expectttl =
+  it (show testnumber) $ mkTTLAssertions env endOfLife `shouldBe` expectttl
 
 mkDataEnv :: HasCallStack => String -> (TTL "authresp") -> Env
 mkDataEnv now maxttl =
