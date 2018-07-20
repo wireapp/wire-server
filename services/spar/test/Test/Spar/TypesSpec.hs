9ba84b98b8f7a4b09627ed65fd4735aa3f66e67b
Matthias Fischmann <mf@zerobuzz.net>
2018-07-20 17:19:59 +0200

    Variable substitution in URIs.

---
+++ services/spar/test/Test/Spar/TypesSpec.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE ViewPatterns               #-}
+
+module Test.Spar.TypesSpec where
+
+import Data.Id
+import Data.UUID
+import Spar.Types
+import Test.Hspec
+import URI.ByteString
+import URI.ByteString.QQ
+import Web.Cookie
+
+
+spec :: Spec
+spec = do
+  describe "mkVerdictGrantedFormatMobile" $ do
+    it "1" $ do
+      mkVerdictGrantedFormatMobile [uri|wire://granted/$cookie/$userid|] def (Id nil)
+        `shouldBe` Right [uri|wire://granted/name=value/00000000-0000-0000-0000-000000000000|]
+    it "2" $ do
+      mkVerdictGrantedFormatMobile ([uri|http://$cookie:1039/granted|] :: URI) def (Id nil)
+        `shouldBe` Right [uri|http://name=value:1039/granted|]
+
+  describe "mkVerdictDeniedFormatMobile" $ do
+    it "1" $ do
+      mkVerdictDeniedFormatMobile [uri|wire://$label|] "forbidden"
+        `shouldBe` Right [uri|wire://forbidden|]
+    it "2" $ do
+      mkVerdictDeniedFormatMobile [uri|http://bad/?label=$label|] "forbidden"
+        `shouldBe` Right [uri|http://bad/?label=forbidden|]
