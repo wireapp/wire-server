0d0ecc5183c9f883649d244090e830310975742c
Artyom Kazak <yom@artyom.me>
2018-08-03 15:25:09 +0200

    Don't parse CLI options in cannon

--- services/cannon/src/Cannon/Options.hs
+++ services/cannon/src/Cannon/Options.hs
@@ -8,8 +8,7 @@
 {-# LANGUAGE FlexibleInstances          #-}
 
 module Cannon.Options
-    ( optsParser
-    , host
+    ( host
     , port
     , cannon
     , gundeck
@@ -21,18 +20,17 @@ where
 
 import Control.Lens (makeFields)
 import Data.Aeson.APIFieldJsonTH
-import Data.Monoid ((<>))
-import Data.Text (Text, pack)
+import Data.Text (Text)
 import Data.Word
 import GHC.Generics
-import Options.Applicative
 
 
 data Cannon = Cannon
-    { _cannonHost             :: !String
-    , _cannonPort             :: !Word16
-    , _cannonExternalHost     :: !(Maybe Text)
-    , _cannonExternalHostFile :: !(Maybe FilePath)
+    { _cannonHost             :: !String            -- ^ Host to listen on
+    , _cannonPort             :: !Word16            -- ^ Port to listen on
+    , _cannonExternalHost     :: !(Maybe Text)      -- ^ Host to report to other services
+    , _cannonExternalHostFile :: !(Maybe FilePath)  -- ^ File containing host address to
+                                                    --   report to other services
     } deriving (Eq, Show, Generic)
 
 makeFields ''Cannon
@@ -47,52 +45,9 @@ makeFields ''Gundeck
 deriveApiFieldJSON ''Gundeck
 
 data Opts = Opts
-    { _optsCannon   :: !Cannon
-    , _optsGundeck  :: !Gundeck
+    { _optsCannon   :: !Cannon   -- ^ Our options
+    , _optsGundeck  :: !Gundeck  -- ^ Gundeck endpoint
     } deriving (Eq, Show, Generic)
 
 makeFields ''Opts
 deriveApiFieldJSON ''Opts
-
-optsParser :: Parser Opts
-optsParser = Opts <$> cParser <*> gParser
-
-gParser :: Parser Gundeck
-gParser = Gundeck
-    <$> (textOption $
-            long "gundeck-host"
-            <> metavar "HOSTNAME"
-            <> help "Gundeck host")
-
-    <*> (option auto $
-            long "gundeck-port"
-            <> metavar "PORT"
-            <> help "Gundeck port")
-
-
-cParser :: Parser Cannon
-cParser = Cannon
-    <$> (strOption $
-            long "host"
-            <> metavar "HOSTNAME"
-            <> help "host to listen on")
-
-    <*> (option auto $
-            long "port"
-            <> short 'p'
-            <> metavar "PORT"
-            <> help "port to listen on")
-
-    <*> (optional $ (textOption $
-            long "external-host"
-            <> metavar "HOSTNAME"
-            <> help "host address to report to other services"))
-
-    <*> (optional $ (strOption $
-            long "external-host-file"
-            <> metavar "HOSTNAME_FILE"
-            <> help "file containing host address to report to other services"))
-
-
-textOption :: Mod OptionFields String -> Parser Text
-textOption = fmap pack . strOption
--- services/cannon/src/Main.hs
+++ services/cannon/src/Main.hs
@@ -1,11 +1,10 @@
 module Main (main) where
 
 import Cannon.API
-import Cannon.Options
 import Util.Options
 
 main :: IO ()
-main = getOptions desc (Just optsParser) defaultPath >>= run
+main = getOptions desc Nothing defaultPath >>= run
   where
     desc = "Cannon - Websocket Push Service"
     defaultPath = "/etc/wire/cannon/conf/cannon.yaml"
