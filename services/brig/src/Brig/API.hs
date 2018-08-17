e0504419e8090c9dbba8d8756371e60ba12f4e50
Artyom Kazak <yom@artyom.me>
2018-08-17 17:03:44 +0200

    Remove unwhitelisting-on-incoming-message

--- services/brig/src/Brig/API.hs
+++ services/brig/src/Brig/API.hs
@@ -17,14 +17,11 @@ import Brig.API.Handler
 import Brig.API.Types
 import Brig.Options hiding (sesQueue, internalEvents)
 import Brig.Types
-import Brig.Types.Provider (ServiceWhitelistStatus (..))
 import Brig.Types.Intra
 import Brig.Types.User (NewUserNoSSO(NewUserNoSSO))
 import Brig.Types.User.Auth
-import Brig.Provider.DB (getServiceWhitelistStatus)
 import Brig.User.Email
 import Brig.User.Phone
-import Control.Concurrent.Async.Lifted.Safe.Extended (forPooled)
 import Control.Error
 import Control.Lens (view, (^.))
 import Control.Monad (liftM2, liftM3, unless, void, when)
@@ -45,7 +42,6 @@ import Data.Text (Text, unpack)
 import Data.Text.Encoding (decodeLatin1)
 import Data.Text.Lazy (pack)
 import Galley.Types (UserClients (..))
-import Galley.Types.Bot (ServiceRef, serviceRefId, serviceRefProvider)
 import Network.HTTP.Types.Status
 import Network.Wai (Request, Response, responseLBS, lazyRequestBody)
 import Network.Wai.Predicate hiding (setStatus, result)
@@ -53,7 +49,6 @@ import Network.Wai.Routing
 import Network.Wai.Utilities hiding (parseJsonBody)
 import Network.Wai.Utilities.Server
 import Network.Wai.Utilities.Swagger (document, mkSwaggerApi)
-import Network.Wai.Utilities.ZAuth ((.&>))
 import Prelude hiding (head)
 import Util.Options
 
@@ -214,15 +209,6 @@ sitemap o = do
     post "/i/clients" (continue internalListClients) $
       accept "application" "json"
       .&. contentType "application" "json"
-      .&. request
-
-    -- provided a list of services, tells you for each whether it's
-    -- whitelisted or not; gets an array of 'ServiceRef's and returns an
-    -- array of 'ServiceWhitelistStatus'es
-    post "/i/teams/:tid/services/whitelisted" (continue internalQueryServiceWhitelist) $
-      accept "application" "json"
-      .&> contentType "application" "json"
-      .&> capture "tid"
       .&. request
 
     -- /users -----------------------------------------------------------------
@@ -1050,15 +1036,6 @@ internalListClients (_ ::: _ ::: req) = do
     UserSet usrs <- parseJsonBody req
     ucs <- Map.fromList <$> lift (API.lookupUsersClientIds $ Set.toList usrs)
     return $ json (UserClients ucs)
-
-internalQueryServiceWhitelist :: TeamId ::: Request -> Handler Response
-internalQueryServiceWhitelist (tid ::: req) = do
-    svcs :: [ServiceRef] <- parseJsonBody req
-    fmap json $ lift $ forPooled 16 svcs $ \svc -> do
-        let pid = svc ^. serviceRefProvider
-            sid = svc ^. serviceRefId
-        status <- getServiceWhitelistStatus tid pid sid
-        return (ServiceWhitelistStatus pid sid status)
 
 getClient :: UserId ::: ClientId ::: JSON -> Handler Response
 getClient (usr ::: clt ::: _) = lift $ do
