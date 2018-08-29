ba4504ff25e1b1b219c020c936208647a3412934
Matthias Fischmann <mf@zerobuzz.net>
2018-08-29 22:45:50 +0200

    Enable TLS for mock IdP: fix client side.

--- services/spar/src/Spar/Run.hs
+++ services/spar/src/Spar/Run.hs
@@ -26,7 +26,6 @@ import Data.String.Conversions
 import Data.String (fromString)
 import Lens.Micro
 import Network.HTTP.Client (responseTimeoutMicro)
-import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.Wai (Application)
 import Network.Wai.Utilities.Request (lookupRequestId)
 import Spar.API
@@ -42,6 +41,8 @@ import Util.Options (epHost, epPort)
 
 import qualified Cassandra.Schema as Cas
 import qualified Cassandra.Settings as Cas
+import qualified Network.Connection as TLS
+import qualified Network.HTTP.Client.TLS as TLS
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.Wai.Utilities.Server as WU
 import qualified SAML2.WebSSO as SAML
@@ -97,9 +98,7 @@ runServer sparCtxOpts = do
   let settings = Warp.defaultSettings
         & Warp.setHost (fromString $ sparCtxOpts ^. to saml . SAML.cfgSPHost)
         . Warp.setPort (sparCtxOpts ^. to saml . SAML.cfgSPPort)
-  sparCtxHttpManager <- newManager tlsManagerSettings
-      { managerResponseTimeout = responseTimeoutMicro (10 * 1000 * 1000)
-      }
+  sparCtxHttpManager <- sparManager (tlsDisableCertValidation sparCtxOpts)
   let sparCtxHttpBrig = Bilge.host (sparCtxOpts ^. to brig . epHost . to cs)
                       . Bilge.port (sparCtxOpts ^. to brig . epPort)
                       $ Bilge.empty
@@ -113,6 +112,24 @@ runServer sparCtxOpts = do
         $ \sparCtxRequestId -> app Env {..}
   WU.runSettingsWithShutdown settings wrappedApp 5
 
+-- | Create a TLS-capabable manager for fetching metadata from IdPs.
+--
+-- NB: In integration tests, we turn certificate validation off.  This is not ideal, but the
+-- alternative would be to register a mock certificate with the production spar service, which is
+-- not trivial and thus not risk-free either:
+--
+-- * https://stackoverflow.com/questions/25833305/accepting-specific-certificate-with-http-client-tls-or-tls
+-- * https://stackoverflow.com/questions/40081508/how-to-provide-a-client-certificate-to-http-client-tls#40082394
+sparManager :: Bool -> IO Manager
+sparManager disableCertificateValidation = newManager (TLS.mkManagerSettings tlss Nothing)
+  { managerResponseTimeout = responseTimeoutMicro (10 * 1000 * 1000)
+  }
+  where
+    tlss = TLS.TLSSettingsSimple
+      { TLS.settingDisableCertificateValidation = disableCertificateValidation
+      , TLS.settingDisableSession = False
+      , TLS.settingUseServerName = False
+      }
 
 lookupRequestIdMiddleware :: (RequestId -> Application) -> Application
 lookupRequestIdMiddleware mkapp req cont = do
