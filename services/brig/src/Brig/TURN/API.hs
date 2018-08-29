aae1554f5db28a815d436a7d28d8669e3b649500
jschaul <jschaul@users.noreply.github.com>
2018-08-29 17:33:10 +0200

    WIP add 'limit' query param

--- services/brig/src/Brig/TURN/API.hs
+++ services/brig/src/Brig/TURN/API.hs
@@ -18,8 +18,10 @@ import Data.ByteString (ByteString)
 import Data.ByteString.Conversion (toByteString')
 import Data.ByteString.Lens
 import Data.Id
+import Data.Range
 import Data.IORef
 import Data.List1 (List1)
+import Data.List ((\\))
 import Data.Foldable (toList)
 import Data.Text.Ascii (AsciiBase64, encodeBase64)
 import Data.Text.Strict.Lens
@@ -59,6 +61,7 @@ routes = do
         accept "application" "json"
         .&. header "Z-User"
         .&. header "Z-Connection"
+        .&. opt (query "limit")
 
     document "GET" "getCallsConfigV2" $ do
         Doc.summary "Retrieve all TURN server addresses and credentials. \
@@ -67,26 +70,28 @@ routes = do
         Doc.returns (Doc.ref Doc.rtcConfiguration)
         Doc.response 200 "RTCConfiguration" Doc.end
 
-getCallsConfigV2 :: JSON ::: UserId ::: ConnId -> Handler Response
-getCallsConfigV2 (_ ::: _ ::: _) = do
+getCallsConfigV2 :: JSON ::: UserId ::: ConnId ::: Maybe (Range 1 10 Int) -> Handler Response
+getCallsConfigV2 (_ ::: _ ::: _ ::: limit ) = do
     env <- liftIO =<< readIORef <$> view turnEnvV2
-    json <$> newConfig env
+    json <$> newConfig env limit
 
 getCallsConfig :: JSON ::: UserId ::: ConnId -> Handler Response
 getCallsConfig (_ ::: _ ::: _) = do
     env <- liftIO =<< readIORef <$> view turnEnv
-    json . dropTransport <$> newConfig env
+    json . dropTransport <$> newConfig env Nothing
   where
     -- In order to avoid being backwards incompatible, remove the `transport` query param from the URIs
     dropTransport :: RTCConfiguration -> RTCConfiguration
     dropTransport = set (rtcConfIceServers . traverse . iceURLs . traverse . turiTransport) Nothing
 
-newConfig :: MonadIO m => TURN.Env -> m RTCConfiguration
-newConfig env = do
-    -- env  <- liftIO =<< readIORef <$> view turnEnv
+newConfig :: MonadIO m => TURN.Env -> Maybe (Range 1 10 Int) -> m RTCConfiguration
+newConfig env limit = do
     let (sha, secret, tTTL, cTTL, prng) = (env^.turnSHA512, env^.turnSecret, env^.turnTokenTTL, env^.turnConfigTTL, env^.turnPrng)
-    uris <- liftIO $ randomize (env^.turnServers)
-    srvs <- for uris $ \uri -> do
+    let uris = case (fromRange <$> limit) of
+                   Nothing     -> env^.turnServers
+                   Just (lim)  -> limitedList (env^.turnServers) lim
+    randomizedUris <- liftIO $ randomize uris
+    srvs <- for randomizedUris $ \uri -> do
                 u <- liftIO $ genUsername tTTL prng
                 pure $ rtcIceServer (List1.singleton uri) u (computeCred sha secret u)
     pure $ rtcConfiguration srvs cTTL
@@ -98,6 +103,22 @@ newConfig env = do
         (f:fs) <- shuffleM (toList xs)
         return $ List1.list1 f fs
 
+    limitedList :: List1 TurnURI -> Int -> List1 TurnURI
+    limitedList servers lim = do
+        let uris = toList servers
+        let udps = filter isUdp uris
+            tcps = filter isTcp uris
+            tlss = filter isTls uris
+            -- is there an easier way to write others?
+            others = ((uris \\ udps) \\ tlss) \\ tcps
+            -- others = filter (liftM not (liftM2 (&&) udp (liftM2 (&&) tcp tls))) ?
+
+            -- if limitHeuristic is safe (returning at least 1 element if limit>=1)
+            -- since the input is List1 and limit in Range 1 10
+            -- this should also be safe.
+            (x:xs) = limitHeuristic [] (udps,tlss,tcps,others) lim
+        List1.list1 x xs
+
     genUsername :: Word32 -> MWC.GenIO -> IO TurnUsername
     genUsername ttl prng = do
         rnd <- view (packedBytes . utf8) <$> replicateM 16 (MWC.uniformR (97, 122) prng)
@@ -106,3 +127,47 @@ newConfig env = do
 
     computeCred :: Digest -> ByteString -> TurnUsername -> AsciiBase64
     computeCred dig secret = encodeBase64 . hmacBS dig secret . toByteString'
+
+
+-- | given a list of URIs and a size, limit URIs
+-- with order priority from highest to lowest: UDP -> TLS -> TCP
+limitHeuristic :: [TurnURI]
+  -> ([TurnURI], [TurnURI], [TurnURI], [TurnURI])
+  -> Int
+  -> [TurnURI]
+-- case at least one of each available
+limitHeuristic xs (udp:udps, tls:tlss, tcp:tcps, o) lim = case lim - length(xs) of
+        x | x >= 3 ->  limitHeuristic (udp:tls:tcp:xs) (udps, tlss, tcps, o) lim
+        2          ->  limitHeuristic (udp:tls:xs) (udps, tlss, tcp:tcps, o) lim
+        1          ->  limitHeuristic (udp:xs) (udps, tls:tlss, tcp:tcps, o) lim
+        _          -> xs
+-- case at least one of udp/tls available
+limitHeuristic xs (udp:udps, tls:tlss, [], o) lim = case lim - length(xs) of
+        x | x >= 2 ->  limitHeuristic (udp:tls:xs) (udps, tlss, [], o) lim
+        1          ->  limitHeuristic (udp:xs) (udps, tls:tlss, [], o) lim
+        _          -> xs
+-- case at least one udp available
+limitHeuristic xs (udp:udps, _tlss, _tcps, o) lim = case lim - length(xs) of
+        x | x >= 1 ->  limitHeuristic (udp:xs) (udps, [], [], o) lim
+        _          -> xs
+-- case at least one tls available
+limitHeuristic xs (_udps, tls:tlss, _tcps, o) lim = case lim - length(xs) of
+        x | x >= 1 ->  limitHeuristic (tls:xs) ([], tlss, [], o) lim
+        _          -> xs
+-- case limit not reached yet, but no more udp/tls: fill with whatever is left
+limitHeuristic xs (udps, tlss, tcps, o) lim = case lim - length(xs) of
+        x | x >= 1 -> (take x (udps++tlss++tcps++o))++xs
+        _          -> xs
+
+isUdp :: TurnURI -> Bool
+isUdp uri = uri^.turiScheme == SchemeTurn
+        && ( uri^.turiTransport == Just(TransportUDP)
+            || uri^.turiTransport == Nothing )
+
+isTcp :: TurnURI -> Bool
+isTcp uri = uri^.turiScheme == SchemeTurn
+        && uri^.turiTransport == Just(TransportTCP)
+
+isTls :: TurnURI -> Bool
+isTls uri = uri^.turiScheme == SchemeTurns
+        && uri^.turiTransport == Just(TransportTCP)
