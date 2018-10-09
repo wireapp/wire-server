61521c6bb384511d85c22553cfefa0d586f0a243
Tiago Loureiro <tiago.loureiro@gmail.com>
2018-10-09 15:55:06 +0200

    Address PR comments

--- services/gen-aws-conf.sh
+++ services/gen-aws-conf.sh
@@ -9,11 +9,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 if [ ! -f "${DIR}/integration-aws.yaml" ]
 then
     echo "Could not find AWS config file to override settings, specify a location on S3 to download the file or add one at ${DIR}/integration-aws.yaml and retry: "
-    read location
-    aws s3 cp s3://$location .
+    read -r location
+    aws s3 cp s3://"$location" "${DIR}/integration-aws.yaml"
 fi
 
 services=( brig cargohold galley gundeck cannon proxy spar )
 for service in "${services[@]}"; do
-    yaml merge ${DIR}/integration-aws.yaml ${DIR}/${service}/${service}.integration.yaml > ${DIR}/${service}/${service}.integration-aws.yaml
+    yaml merge "${DIR}/integration-aws.yaml" "${DIR}/${service}/${service}.integration.yaml" > "${DIR}/${service}/${service}.integration-aws.yaml"
 done
