d9fe66db0070d422565ca4cc767b3604b22e387b
Matthias Fischmann <mf@zerobuzz.net>
2018-10-18 16:29:42 +0200

    haddocks.

--- services/spar/src/Spar/Data.hs
+++ services/spar/src/Spar/Data.hs
@@ -63,6 +63,9 @@ schemaVersion = 3
 ----------------------------------------------------------------------
 -- helpers
 
+-- | Carry some time constants we do not want to pull from Options, IO, respectively.  This way the
+-- functions in this module need fewer effects.  See 'wrapMonadClientWithEnv' (as opposed to
+-- 'wrapMonadClient' where we don't need an 'Env').
 data Env = Env
   { dataEnvNow                :: UTCTime
   , dataEnvMaxTTLAuthRequests :: TTL "authreq"
