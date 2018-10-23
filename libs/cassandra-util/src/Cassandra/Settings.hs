80a9b136e06f427f3ed84c3c134ef830d36f28fd
jschaul <jschaul@users.noreply.github.com>
2018-10-23 17:01:15 +0200

    Remove unused code

--- libs/cassandra-util/src/Cassandra/Settings.hs
+++ libs/cassandra-util/src/Cassandra/Settings.hs
@@ -22,7 +22,6 @@ module Cassandra.Settings
     , setRetrySettings
     , setPolicy
     , initialContactsDisco
-    , initialContactsDNS
     , initialContactsPlain
     ) where
 
@@ -32,13 +31,8 @@ import Data.Aeson.Lens
 import Data.List.NonEmpty (NonEmpty (..))
 import Data.Monoid
 import Data.Text (pack, stripSuffix, unpack, Text)
-import Data.Text.Encoding (encodeUtf8)
 import Database.CQL.IO hiding (values)
-import Network.DNS.Lookup
-import Network.DNS.Resolver
 import Network.Wreq
-
-import qualified Data.List.NonEmpty as NE
 
 -- | This function is likely only useful at Wire, as it is Wire-infra specific.
 -- Given a server name and a url returning a wire-custom "disco" json (AWS describe-instances-like json), e.g.
@@ -61,17 +55,6 @@ initialContactsDisco (pack -> srv) url = liftIO $ do
     case ip of
         i:ii -> return (i :| ii)
         _    -> error "initial-contacts: no IP addresses found."
-
--- | Given a DNS name or single IP, resolve DNS and return a list of IP addresses.
-initialContactsDNS :: MonadIO m => Text -> m (NonEmpty String)
-initialContactsDNS address = liftIO $ do
-    rs  <- makeResolvSeed defaultResolvConf
-    ips <- withResolver rs $ \resolver -> lookupA resolver (encodeUtf8 address)
-    return $ case ips of
-      Right (x:xs) -> NE.map show (x :| xs)
-      _            -> fallback
-  where
-    fallback = unpack address :| [] -- If it's not a valid DNS name, just try using it anyway
 
 -- | Puts the address into a list using the same signature as the other initialContacts
 initialContactsPlain :: MonadIO m => Text -> m (NonEmpty String)
