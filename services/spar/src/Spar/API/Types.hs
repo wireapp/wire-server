d2da4c0352240c48554fb0219151330972277fc8
Matthias Fischmann <mf@zerobuzz.net>
2018-11-13 09:31:27 +0100

    haddocks.

--- services/spar/src/Spar/API/Types.hs
+++ services/spar/src/Spar/API/Types.hs
@@ -74,69 +74,50 @@ type APIAuthReqPrecheck
 --
 -- __Binding existing users to SSO__
 --
--- A user has an existing account with email address and password, and gets idp credentails.  she
+-- A user has an existing account with email address and password, and gets idp credentails.  She
 -- now wants to upgrade her password-based login to sso login, or *bind* the existing user to the
 -- sso credentials.
 --
 -- __The Solution__
 --
---   0. user logs in with password.
---   1. inside the session, she requests `/sso/initiate-login/<idp>` for an idp of her choice (idp
---      must be registered with her team).
---   2. spar notices that she is logged in, decides that this is not about authentication, but about
---      binding, and sends a short-lived bind cookie with the usual response.
---   3. everybody goes through the well-known SAML web sso moves.
+--   0. user logs in with password
+--   1. inside the session, she requests `/sso-initiate-bind/<idp>` for an idp of her choice (idp
+--      must be registered with her team)
+--   2. spar checks the `Z-User` header and sends a short-lived bind cookie with the response that
+--      otherwise is the same as for `/sso/initiate-login/<idp>`
+--   3. everybody goes through the well-known SAML web sso moves, until:
 --   4. the authentication response is sent to `/sso/finalize-login/<idp>` together with the bind
---      cookie.
+--      cookie
 --   5. spar identifies the user both via the bind cookie and via the SAML authentication response,
 --      and performs the binding.
 --
 -- Why a special cookie, and not the session cookie, or the session token?  The wire session cookie
--- gets only sent to /access and we would need to change that; session tokens are hard to handle
+-- gets only sent to `/access`, and we would need to change that; session tokens are hard to handle
 -- while switching between app context and browser context.  Having a separate cookie makes both the
 -- context switching simple and allows us to set a different cookie recipient end-point and short
 -- cookie lifetime.
 --
 -- This solution is very flexible.  UX variants:
 --
---   * team admin posts the initiate-login link in a group in the wire team.  no new UI components
+--   * team admin posts the initiate-bind link in a group in the wire team.  no new UI components
 --     are needed in the frontend.
 --   * we add buttons in the settings page somewhere.
 --   * we send a link containing a token to the user by email.  when the user clicks on the link,
---     she gets authenticated and redirected to the initiate-login end-point in step 1 above.
+--     she gets authenticated and redirected to the initiate-bind end-point in step 1 above.
 --   * ...?
 --
--- __Accidental Creation of new SSO User__
+-- __Corner Case: Accidental Creation of new SSO User__
 --
--- What happens if the user authenticates via SSO first, creates a new user, and then hears about
--- the way old and new user can be connected?
+-- What happens if the user authenticates via SSO first, creates a new user, and then receives the
+-- bind invite?
 --
--- To avoid that, the IdP pushes the list of all users via SCIM, then we can block implicit user
--- creation: every `initiate-login` request outside a wire session will be answered with 403.
+-- Possible solutions:
 --
--- Less ideal alternatives:
---
---   * User needs to delete the SSO user, lose a little bit of user data, and go through the bind
---     protocol.
+--   * The IdP could create all users via SCIM and we block implicit user creation.
+--   * After the fact, the duplicated user deletes the SSO user, loses a little bit of user data,
+--     and goes through the bind process.
 --   * Block implicit creation for a short time window, and ask all existing users to use that time
---   * window to bind.
---
--- __SCIM__
---
--- If a user record hits spar via SCIM with an email address known by brig, spar cannot carry out
--- the bind without authenticating the user.  This is the safest way to prevent the owner of a team
--- `evil.com` to trap user `harmless@oblivious.com` into her rogue team.
---
--- Possible solution (not implemented yet):
---
---   1. Store the entire SCIM user record in a `pending_scim_users` table.
---   2. If the user authenticates via password, establish the session and then redirect her to
---      `initiate-login`.  Proceed as above.
---   3. If the user authenticates via SAML, establish the session, which requires the user to enter
---      the password (email is shown from the SAML authentication response), then proceed as above.
---      This means that the SAML handshake happens twice, but the second time, it should not be
---      visible by the user except for as a flicker of the browser.  And this way it's easy and
---      safe!
+--     window to bind.
 type APIAuthReq
      = Header "Z-User" UserId
     :> QueryParam "success_redirect" URI.URI
