760de55799c7d3c529da37edfa614836f81d41cb
Matthias Fischmann <mf@zerobuzz.net>
2018-11-16 12:50:22 +0100

    haddocks.

--- libs/gundeck-types/src/Gundeck/Types/Presence.hs
+++ libs/gundeck-types/src/Gundeck/Types/Presence.hs
@@ -16,7 +16,7 @@ import qualified Data.ByteString.Lazy as Lazy
 data Presence = Presence
     { userId    :: !UserId
     , connId    :: !ConnId
-    , resource  :: !URI
+    , resource  :: !URI  -- ^ cannon instance hosting the presence
     , clientId  :: !(Maybe ClientId)
     , createdAt :: !Milliseconds
     , __field   :: !Lazy.ByteString -- temp. addition to ease migration
