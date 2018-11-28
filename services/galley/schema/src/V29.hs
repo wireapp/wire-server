2fb66c0cb29ac87f97df2bf0fa44519a86494d05
Tiago Loureiro <tiago.loureiro@gmail.com>
2018-11-28 11:54:01 +0100

    Typo

--- services/galley/schema/src/V29.hs
+++ services/galley/schema/src/V29.hs
@@ -8,5 +8,5 @@ import Cassandra.Schema
 import Text.RawString.QQ
 
 migration :: Migration
-migration = Migration 29 "Add conversation receipt" $
+migration = Migration 29 "Add conversation receipt mode" $
     schema' [r| ALTER TABLE conversation ADD receipt_mode int; |]
