e50054b04d54ab25a7a5f16091ee017d70df3318
Matthias Fischmann <mf@zerobuzz.net>
2018-12-03 18:55:04 +0100

    Deprecate fallback column / column family.

--- services/gundeck/schema/src/V5.hs
+++ services/gundeck/schema/src/V5.hs
@@ -10,3 +10,4 @@ import Text.RawString.QQ
 migration :: Migration
 migration = Migration 5 "Add user_push.fallback column" $
     schema' [r| alter columnfamily user_push add fallback int; |]
+      -- TODO: fallback is deprecated as of https://github.com/wireapp/wire-server/pull/531
