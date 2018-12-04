5423746689f9eaa5341b1ec7df412b178cc12756
Matthias Fischmann <mf@zerobuzz.net>
2018-12-04 10:27:49 +0100

    Improve error info in real-aws integration test script.

--- services/gen-aws-conf.sh
+++ services/gen-aws-conf.sh
@@ -4,7 +4,20 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-which yq >/dev/null || ( echo "*** please install yq ( https://github.com/mikefarah/yq ) in your path."; exit 22 )
+which yq >/dev/null || \
+    ( echo "*** please install yq ( https://github.com/mikefarah/yq ) in your path."; \
+      echo "    e.g.: wget https://github.com/mikefarah/yq/releases/download/2.1.2/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq"; \
+      echo "    NOTE: later versions may not work."; \
+      exit 22 \
+    )
+
+which awscli >/dev/null || \
+    ( echo "*** please install awscli ( https://github.com/aws/aws-cli ) in your path."; \
+      echo "    e.g.: pip install awscli"; \
+      exit 22 \
+    )
+
+echo -e "\nHINT: when prompted for an integration config, consider trying 'z-config/integration/integration-aws.yaml'.\n"
 
 # Ensure that we have a file named integration-aws.yaml in the current
 # dir. If not, fetch it from a known location on S3
