9cc3e7d52b633a5657a4da40ad17c1f0bd7b5b0c
Artyom Kazak <yom@artyom.me>
2019-01-22 19:57:34 +0100

    More renaming

--- services/spar/test/Test/Spar/SCIMSpec.hs
+++ services/spar/test/Test/Spar/SCIMSpec.hs
@@ -8,67 +8,67 @@
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE ViewPatterns               #-}
 
-module Test.Spar.SCIMSpec where
+module Test.Spar.ScimSpec where
 
 import Imports
 import Data.Id
 import Network.URI (parseURI)
-import Spar.SCIM
+import Spar.Scim
 import Test.Hspec
 import URI.ByteString
 
 import qualified Data.UUID as UUID
 import qualified SAML2.WebSSO as SAML
-import qualified Web.SCIM.Class.User as SCIMC
-import qualified Web.SCIM.Schema.Common as SCIM
-import qualified Web.SCIM.Schema.Schema as SCIM
-import qualified Web.SCIM.Schema.Meta as SCIM
-import qualified Web.SCIM.Schema.ResourceType as SCIMR
-import qualified Web.SCIM.Schema.User as SCIM
-import qualified Web.SCIM.Schema.User.Name as SCIMN
+import qualified Web.Scim.Class.User as ScimC
+import qualified Web.Scim.Schema.Common as Scim
+import qualified Web.Scim.Schema.Schema as Scim
+import qualified Web.Scim.Schema.Meta as Scim
+import qualified Web.Scim.Schema.ResourceType as ScimR
+import qualified Web.Scim.Schema.User as Scim
+import qualified Web.Scim.Schema.User.Name as ScimN
 
 
 spec :: Spec
-spec = describe "toSCIMStoredUser'" $ do
+spec = describe "toScimStoredUser'" $ do
   it "works" $ do
-    let usr :: SCIM.User
-        usr = SCIM.User
-          { SCIM.schemas = [SCIM.User20]
-          , SCIM.userName = "02b35298-088f-11e9-b4a4-478635dd0d2b"
-          , SCIM.externalId = Just "c1704a48-0a1e-11e9-9186-9b185fe892e8"
-          , SCIM.name = Just (SCIMN.Name { SCIMN.formatted = Nothing
-                                         , SCIMN.familyName = Just ""
-                                         , SCIMN.givenName = Just ""
-                                         , SCIMN.middleName = Nothing
-                                         , SCIMN.honorificPrefix = Nothing
-                                         , SCIMN.honorificSuffix = Nothing
+    let usr :: Scim.User
+        usr = Scim.User
+          { Scim.schemas = [Scim.User20]
+          , Scim.userName = "02b35298-088f-11e9-b4a4-478635dd0d2b"
+          , Scim.externalId = Just "c1704a48-0a1e-11e9-9186-9b185fe892e8"
+          , Scim.name = Just (ScimN.Name { ScimN.formatted = Nothing
+                                         , ScimN.familyName = Just ""
+                                         , ScimN.givenName = Just ""
+                                         , ScimN.middleName = Nothing
+                                         , ScimN.honorificPrefix = Nothing
+                                         , ScimN.honorificSuffix = Nothing
                                          })
-          , SCIM.displayName = Just "67d0268e-088e-11e9-a400-b71b4d4d2275"
-          , SCIM.nickName = Nothing
-          , SCIM.profileUrl = Nothing
-          , SCIM.title = Nothing
-          , SCIM.userType = Nothing
-          , SCIM.preferredLanguage = Nothing
-          , SCIM.locale = Nothing
-          , SCIM.active = Nothing
-          , SCIM.password = Nothing
-          , SCIM.emails = Nothing
-          , SCIM.phoneNumbers = Nothing
-          , SCIM.ims = Nothing
-          , SCIM.photos = Nothing
-          , SCIM.addresses = Nothing
-          , SCIM.entitlements = Nothing
-          , SCIM.roles = Nothing
-          , SCIM.x509Certificates = Nothing
+          , Scim.displayName = Just "67d0268e-088e-11e9-a400-b71b4d4d2275"
+          , Scim.nickName = Nothing
+          , Scim.profileUrl = Nothing
+          , Scim.title = Nothing
+          , Scim.userType = Nothing
+          , Scim.preferredLanguage = Nothing
+          , Scim.locale = Nothing
+          , Scim.active = Nothing
+          , Scim.password = Nothing
+          , Scim.emails = Nothing
+          , Scim.phoneNumbers = Nothing
+          , Scim.ims = Nothing
+          , Scim.photos = Nothing
+          , Scim.addresses = Nothing
+          , Scim.entitlements = Nothing
+          , Scim.roles = Nothing
+          , Scim.x509Certificates = Nothing
           }
 
-        meta :: SCIM.Meta
-        meta = SCIM.Meta
-          { SCIM.resourceType = SCIMR.UserResource
-          , SCIM.created = now
-          , SCIM.lastModified = now
-          , SCIM.version = SCIM.Strong "e5442c575adce7a7affbb2f744ab0825c553c9e6ce5dafdee8789364d824614e"
-          , SCIM.location = SCIM.URI . fromJust $ Network.URI.parseURI
+        meta :: Scim.Meta
+        meta = Scim.Meta
+          { Scim.resourceType = ScimR.UserResource
+          , Scim.created = now
+          , Scim.lastModified = now
+          , Scim.version = Scim.Strong "e5442c575adce7a7affbb2f744ab0825c553c9e6ce5dafdee8789364d824614e"
+          , Scim.location = Scim.URI . fromJust $ Network.URI.parseURI
                             "https://127.0.0.1/scim/Users/90b5ee1c-088e-11e9-9a16-73f80f483813"
           }
 
@@ -77,8 +77,8 @@ spec = describe "toSCIMStoredUser'" $ do
           URI.ByteString.parseURI laxURIParserOptions "https://127.0.0.1/scim/"
         uid = Id . fromJust . UUID.fromText $ "90b5ee1c-088e-11e9-9a16-73f80f483813"
 
-        result :: SCIMC.StoredUser
-        result = toSCIMStoredUser' now' baseuri uid usr
+        result :: ScimC.StoredUser
+        result = toScimStoredUser' now' baseuri uid usr
 
-    SCIM.meta result `shouldBe` meta
-    SCIM.value (SCIM.thing result) `shouldBe` usr
+    Scim.meta result `shouldBe` meta
+    Scim.value (Scim.thing result) `shouldBe` usr
