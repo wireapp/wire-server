18d14b87eea939c047a9c04086ae395f6673372f
Artyom Kazak <yom@artyom.me>
2019-02-15 16:36:00 +0100

    Move instance Arbitrary PlainTextPassword

--- libs/types-common/src/Data/Misc.hs
+++ libs/types-common/src/Data/Misc.hs
@@ -51,6 +51,9 @@ import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Data.ByteString.Lazy (toStrict)
 import Database.CQL.Protocol hiding (unpack)
 #endif
+#ifdef WITH_ARBITRARY
+import Test.QuickCheck (Arbitrary(..))
+#endif
 import Text.Read (Read (..))
 import URI.ByteString hiding (Port)
 
@@ -258,6 +261,12 @@ instance Show PlainTextPassword where
 instance FromJSON PlainTextPassword where
     parseJSON x = PlainTextPassword . fromRange
                <$> (parseJSON x :: Json.Parser (Range 6 1024 Text))
+
+#ifdef WITH_ARBITRARY
+instance Arbitrary PlainTextPassword where
+    -- TODO: why 6..1024? For tests we might want invalid passwords as well, e.g. 3 chars
+    arbitrary = PlainTextPassword . fromRange <$> genRangeText @6 @1024 arbitrary
+#endif
 
 ----------------------------------------------------------------------
 -- Functor
