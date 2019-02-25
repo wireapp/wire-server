65f06b4d08fde196efb4a8a71a9e435b32fbaa0e
Artyom Kazak <yom@artyom.me>
2019-02-25 14:31:47 +0100

    Another test

--- services/brig/test/integration/API/User/RichInfo.hs
+++ services/brig/test/integration/API/User/RichInfo.hs
@@ -16,15 +16,14 @@ import qualified Galley.Types.Teams          as Team
 tests :: ConnectionLimit -> Opt.Timeout -> Maybe Opt.Opts -> Manager -> Brig -> Cannon -> Galley -> TestTree
 tests _cl _at _conf p b _c g = testGroup "rich info"
     [ test p "there is default empty rich info" $ testDefaultRichInfo b g
+    , test p "non-team members don't have rich info" $ testNonTeamMembersDoNotHaveRichInfo b g
     ]
 
--- TODO Team members can see other team members' rich info
--- TODO Non-team-members don't have rich info at all
 -- TODO Guests can't see rich info
 -- TODO Members of other teams can't see rich info
 -- TODO Empty fields are deleted
 
--- | Test that for team members there is some rich info set by default, but it's empty.
+-- | Test that for team members there is rich info set by default, and that it's empty.
 testDefaultRichInfo :: Brig -> Galley -> Http ()
 testDefaultRichInfo brig galley = do
     -- Create a team with two users
@@ -36,3 +35,20 @@ testDefaultRichInfo brig galley = do
     liftIO $ assertEqual "rich info is not empty, or not present"
         richInfo
         (Just (RichInfo {richInfoFields = []}))
+
+-- | Test that rich info of non-team members can not be queried.
+--
+-- Note: it would be nice to also test that non-team members can not set rich info, but for
+-- now there's no public endpoint for setting it in Brig, so we can't do anything.
+testNonTeamMembersDoNotHaveRichInfo :: Brig -> Galley -> Http ()
+testNonTeamMembersDoNotHaveRichInfo brig galley = do
+    -- Create a user
+    targetUser <- userId <$> randomUser brig
+    -- Another user should get a 'Nothing' when querying their info
+    do nonTeamUser <- userId <$> randomUser brig
+       richInfo <- getRichInfo brig nonTeamUser targetUser
+       liftIO $ assertEqual "rich info is present" richInfo Nothing
+    -- A team member should also get a 'Nothing' when querying their info
+    do (teamUser, _) <- createUserWithTeam brig galley
+       richInfo <- getRichInfo brig teamUser targetUser
+       liftIO $ assertEqual "rich info is present" richInfo Nothing
