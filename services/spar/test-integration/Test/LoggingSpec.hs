5e1d55aa19eb3a5113e3d4ade8b6957fc0bd071b
Matthias Fischmann <mf@zerobuzz.net>
2019-03-06 17:27:57 +0100

    Fixup

--- services/spar/test-integration/Test/LoggingSpec.hs
+++ services/spar/test-integration/Test/LoggingSpec.hs
@@ -13,8 +13,9 @@ spec = describe "logging" $ do
   it "does not log newlines (see haddocks of simpleDefSettings)" $ do
     logger <- asks (^. teSparEnv . to sparCtxLogger)
     liftIO $ do
-      hSetBuffering stdout NoBuffering
-      (out, _) <- capture . Log.fatal logger $ Log.msg ("hrgh\n\nwoaa" :: Text)
+      (out, _) <- capture $ do
+        Log.fatal logger $ Log.msg ("hrgh\n\nwoaa" :: Text)
+        Log.flush logger
       out `shouldContain` "hrgh  woaa"
       out `shouldNotContain` "hrgh\n\nwoaa"
 
@@ -22,17 +23,17 @@ spec = describe "logging" $ do
     let check :: HasCallStack => Bool -> (Settings -> Settings) -> IO String
         check netstr fmt = do
           logger <- Log.new $ Log.simpleDefSettings Log.Debug netstr & fmt
-          (out, _) <- capture . Log.fatal logger $ Log.msg ("blä" :: Text)
+          (out, _) <- capture $ do
+            Log.fatal logger $ Log.msg ("blä" :: Text)
+            Log.flush logger
           pure out
 
     it "netstr renderer" . liftIO $ do
-      hSetBuffering stdout NoBuffering
       out1 <- check True (setFormat Nothing)
       out2 <- check True (setFormat (Just Log.iso8601UTC))
       out1 `shouldBe` out2
 
     it "default renderer" . liftIO $ do
-      hSetBuffering stdout NoBuffering
       out1 <- check False (setFormat Nothing)
       out2 <- check False (setFormat (Just Log.iso8601UTC))
       out1 `shouldBe` out2
