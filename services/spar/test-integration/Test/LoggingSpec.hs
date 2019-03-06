1610e2bdcf5f2b116b7bc4901074473c40171db8
Matthias Fischmann <mf@zerobuzz.net>
2019-03-06 17:17:28 +0100

    Fixup

--- services/spar/test-integration/Test/LoggingSpec.hs
+++ services/spar/test-integration/Test/LoggingSpec.hs
@@ -13,23 +13,26 @@ spec = describe "logging" $ do
   it "does not log newlines (see haddocks of simpleDefSettings)" $ do
     logger <- asks (^. teSparEnv . to sparCtxLogger)
     liftIO $ do
-      (out, _) <- capture $ Log.fatal logger $ Log.msg ("hrgh\n\nwoaa" :: Text)
+      hSetBuffering stdout NoBuffering
+      (out, _) <- capture . Log.fatal logger $ Log.msg ("hrgh\n\nwoaa" :: Text)
       out `shouldContain` "hrgh  woaa"
       out `shouldNotContain` "hrgh\n\nwoaa"
 
-  it "`format = Nothing`, `format = Just iso8601UTC` are equivalent" $ do
+  describe "`format = Nothing`, `format = Just iso8601UTC` are equivalent" $ do
     let check :: HasCallStack => Bool -> (Settings -> Settings) -> IO String
         check netstr fmt = do
           logger <- Log.new $ Log.simpleDefSettings Log.Debug netstr & fmt
           (out, _) <- capture . Log.fatal logger $ Log.msg ("blä" :: Text)
           pure out
 
-    liftIO $ do
+    it "netstr renderer" . liftIO $ do
+      hSetBuffering stdout NoBuffering
       out1 <- check True (setFormat Nothing)
       out2 <- check True (setFormat (Just Log.iso8601UTC))
       out1 `shouldBe` out2
 
-    liftIO $ do
+    it "default renderer" . liftIO $ do
+      hSetBuffering stdout NoBuffering
       out1 <- check False (setFormat Nothing)
       out2 <- check False (setFormat (Just Log.iso8601UTC))
       out1 `shouldBe` out2
