389770157eb2afefb1d57ad0650112d6eb54db8a
Chris Penner <christopher.penner@gmail.com>
2019-03-15 15:45:22 +0100

    REFACTOR: Use StrictData

--- services/galley/src/Galley/Options.hs
+++ services/galley/src/Galley/Options.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE StrictData #-}
 module Galley.Options where
 
 import Imports
@@ -11,15 +12,15 @@ import Data.Misc
 data Settings = Settings
     {
     -- | Number of connections for the HTTP client pool
-      _setHttpPoolSize          :: !Int
+      _setHttpPoolSize          :: Int
     -- | Max number of members in a team. NOTE: This must be in sync with Brig
-    , _setMaxTeamSize           :: !Word16
+    , _setMaxTeamSize           :: Word16
     -- | Max number of members in a conversation. NOTE: This must be in sync with Brig
-    , _setMaxConvSize           :: !Word16
+    , _setMaxConvSize           :: Word16
     -- | Whether to call Brig for device listing
-    , _setIntraListing          :: !Bool
+    , _setIntraListing          :: Bool
     -- | URI prefix for conversations with access mode @code@
-    , _setConversationCodeURI   :: !HttpsUrl
+    , _setConversationCodeURI   :: HttpsUrl
     } deriving (Show, Generic)
 
 deriveFromJSON toOptionFieldName ''Settings
@@ -34,18 +35,18 @@ deriveFromJSON toOptionFieldName ''JournalOpts
 makeLenses ''JournalOpts
 
 data Opts = Opts
-    { _optGalley    :: !Endpoint             -- ^ Host and port to bind to
-    , _optCassandra :: !CassandraOpts        -- ^ Cassandra settings
-    , _optBrig      :: !Endpoint             -- ^ Brig endpoint
-    , _optGundeck   :: !Endpoint             -- ^ Gundeck endpoint
-    , _optSpar      :: !Endpoint             -- ^ Spar endpoint
-    , _optDiscoUrl  :: !(Maybe Text)         -- ^ Disco URL
-    , _optSettings  :: !Settings             -- ^ Other settings
-    , _optJournal   :: !(Maybe JournalOpts)  -- ^ Journaling options ('Nothing'
+    { _optGalley    :: Endpoint             -- ^ Host and port to bind to
+    , _optCassandra :: CassandraOpts        -- ^ Cassandra settings
+    , _optBrig      :: Endpoint             -- ^ Brig endpoint
+    , _optGundeck   :: Endpoint             -- ^ Gundeck endpoint
+    , _optSpar      :: Endpoint             -- ^ Spar endpoint
+    , _optDiscoUrl  :: (Maybe Text)         -- ^ Disco URL
+    , _optSettings  :: Settings             -- ^ Other settings
+    , _optJournal   :: (Maybe JournalOpts)  -- ^ Journaling options ('Nothing'
                                              --   disables journaling)
     -- Logging
-    , _optLogLevel      :: !Level            -- ^ Log level (Debug, Info, etc)
-    , _optLogNetStrings :: !Bool             -- ^ Use netstrings encoding:
+    , _optLogLevel      :: Level            -- ^ Log level (Debug, Info, etc)
+    , _optLogNetStrings :: Bool             -- ^ Use netstrings encoding:
                                              --   <http://cr.yp.to/proto/netstrings.txt>
     }
 
