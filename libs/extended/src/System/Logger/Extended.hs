7b5af2666f41e2de82b38339ca8f4e60f805e445
Chris Penner <christopher.penner@gmail.com>
2019-03-20 15:11:30 +0100

    Add LoggerT with needed dependencies

--- libs/extended/src/System/Logger/Extended.hs
+++ libs/extended/src/System/Logger/Extended.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- | Tinylog convenience things.
 module System.Logger.Extended
     ( mkLogger
     , mkLogger'
+    , LoggerT(..)
     ) where
 
 import Imports
 
 import qualified System.Logger as Log
+import qualified System.Logger.Class as LC
+import Database.CQL.IO
+import Control.Monad.Catch
 
 mkLogger :: Log.Level -> Bool -> IO Log.Logger
 mkLogger lvl netstr = Log.new'
@@ -23,3 +28,21 @@ mkLogger' = Log.new
     . Log.setOutput Log.StdOut
     . Log.setFormat Nothing
     $ Log.defSettings
+
+newtype LoggerT m a = LoggerT {runLoggerT :: ReaderT Log.Logger m a}
+    deriving newtype
+        (Functor
+        , Applicative
+        , Monad
+        , MonadIO
+        , MonadThrow
+        , MonadCatch
+        , MonadMask
+        , MonadClient
+        )
+
+instance (MonadIO m) => LC.MonadLogger (LoggerT m) where
+    log :: LC.Level -> (LC.Msg -> LC.Msg) -> LoggerT m ()
+    log l m = LoggerT $ do
+        logger <- ask
+        Log.log logger l m
