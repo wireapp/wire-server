4a22d0fa83226ae7693d24fa5137e7b4f281f671
Julia Longtin <julia.longtin@wire.com>
2019-03-21 09:53:39 +0000

    be more explicit about cassandra imports.

--- libs/cassandra-util/src/Cassandra.hs
+++ libs/cassandra-util/src/Cassandra.hs
@@ -1,4 +1,4 @@
 module Cassandra (module C) where
 
-import Cassandra.CQL  as C
-import Cassandra.Exec as C
+import Cassandra.CQL as C (Keyspace(Keyspace), Tagged(Tagged), TimeUuid(TimeUuid), Blob(Blob), Ascii(Ascii), Set(Set), QueryString(QueryString), Consistency(One, Quorum), BatchType(BatchLogged, BatchUnLogged), Value(CqlInt, CqlBlob, CqlText), ColumnType(IntColumn, BlobColumn, TextColumn), Version(V3), R, W, Cql, unKeyspace, ctype, toCql, fromCql, fromSet, fromBlob,  fromTimeUuid)
+import Cassandra.Exec as C (MonadClient, ClientState, Client, Page, PrepQuery, BatchM, query, retry, query1, batch, emptyPage, hasMore, nextPage, localState, liftClient, paginateC, result, setConsistency, setType, addPrepQuery, runClient, params, paramsP, paginate, shutdown, init, write, x5, x1)
