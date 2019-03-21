b284f756b00bf24a79159e8253529768618d7b09
Chris Penner <christopher.penner@gmail.com>
2019-03-21 16:30:27 +0100

    Add prometheus middleware to proxy

---
+++ services/proxy/src/Proxy/Run.hs
@@ -0,0 +1,27 @@
+module Proxy.Run (run) where
+
+import Imports hiding (head)
+import Control.Monad.Catch
+import Control.Lens hiding ((.=))
+import Data.Metrics.Middleware hiding (path)
+import Data.Metrics.Middleware.Prometheus (waiPrometheusMiddleware)
+import Data.Metrics.WaiRoute (treeToPaths)
+import Network.Wai.Utilities.Server hiding (serverPort)
+import Network.Wai.Handler.Warp (runSettings)
+import Proxy.Env
+import Proxy.Proxy
+import Proxy.Options
+import Proxy.API (sitemap)
+
+run :: Opts -> IO ()
+run o = do
+    m <- metrics
+    e <- createEnv m o
+    s <- newSettings $ defaultServer (o^.host) (o^.port) (e^.applog) m
+    let rtree    = compile (sitemap e)
+    let measured = measureRequests m (treeToPaths rtree)
+    let app r k  = runProxy e r (route rtree r k)
+    let middleware = waiPrometheusMiddleware (sitemap e)
+                   . measured
+                   . catchErrors (e^.applog) m
+    runSettings s (middleware app) `finally` destroyEnv e
