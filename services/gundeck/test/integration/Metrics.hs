1f5dd7cf4af63a46323015f280a83db036ed62c0
Chris Penner <christopher.penner@gmail.com>
2019-03-25 15:17:22 +0100

    fixup! Add prometheus middleware to gundeck

--- services/gundeck/test/integration/Metrics.hs
+++ services/gundeck/test/integration/Metrics.hs
@@ -10,12 +10,22 @@ import Control.Lens (view)
 type TestSignature a = GundeckR -> Http a
 
 tests :: IO TestSetup -> TestTree
-tests s = testGroup "Metrics" [test s "prometheus" testPrometheusMetrics]
+tests s = testGroup "Metrics" [ test s "prometheus gundeck" testPrometheusMetricsGundeck
+                              , test s "prometheus cannon" testPrometheusMetricsCannon
+                              ]
 
-testPrometheusMetrics :: TestM ()
-testPrometheusMetrics = do
+testPrometheusMetricsGundeck :: TestM ()
+testPrometheusMetricsGundeck = do
     gundeck <- view tsGundeck
     get (runGundeckR gundeck . path "/i/metrics") !!! do
+        const 200 === statusCode
+        -- Should contain the request duration metric in its output
+        const (Just "TYPE http_request_duration_seconds histogram") =~= responseBody
+
+testPrometheusMetricsCannon :: TestM ()
+testPrometheusMetricsCannon = do
+    cannon <- view tsCannon
+    get (runCannonR cannon . path "/i/metrics") !!! do
         const 200 === statusCode
         -- Should contain the request duration metric in its output
         const (Just "TYPE http_request_duration_seconds histogram") =~= responseBody
