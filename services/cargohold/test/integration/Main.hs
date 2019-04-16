75f0a52f0eb8dd453af8be08d2299931b5676774
Chris Penner <christopher.penner@gmail.com>
2019-04-16 16:53:30 +0200

    Revert changes to cargohold

--- services/cargohold/test/integration/Main.hs
+++ services/cargohold/test/integration/Main.hs
@@ -13,12 +13,10 @@ import Options.Applicative
 import Util.Options
 import Util.Options.Common
 import Util.Test
--- import Test.Tasty
--- import Test.Tasty.Options
+import Test.Tasty
+import Test.Tasty.Options
 
-import Forecastle
-import Forecastle.HUnit
--- import TestSetup
+import TestSetup
 import qualified API.V3
 import qualified Metrics
 
