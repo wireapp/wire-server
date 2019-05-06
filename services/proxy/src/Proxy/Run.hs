48be78e2bd086ff39e85d8214c6157c3d88dc7dc
jschaul <jschaul@users.noreply.github.com>
2019-05-06 17:35:18 +0200

    remove all middlewares from proxy

--- services/proxy/src/Proxy/Run.hs
+++ services/proxy/src/Proxy/Run.hs
@@ -3,11 +3,9 @@ module Proxy.Run (run) where
 import Imports hiding (head)
 import Control.Monad.Catch
 import Control.Lens hiding ((.=))
-import Data.Metrics.Middleware hiding (path)
-import Data.Metrics.Middleware.Prometheus (waiPrometheusMiddleware)
-import Data.Metrics.WaiRoute (treeToPaths)
 import Network.Wai.Utilities.Server hiding (serverPort)
 import Network.Wai.Handler.Warp (runSettings)
+import Data.Metrics.Middleware hiding (path)
 import Proxy.Env
 import Proxy.Proxy
 import Proxy.Options
@@ -19,8 +17,6 @@ run o = do
     e <- createEnv m o
     s <- newSettings $ defaultServer (o^.host) (o^.port) (e^.applog) m
     let rtree    = compile (sitemap e)
-    let measured = measureRequests m (treeToPaths rtree)
     let app r k  = runProxy e r (route rtree r k)
-    let middleware = waiPrometheusMiddleware (sitemap e)
-                   . measured
+    let middleware = catchErrors (e^.applog) [Right m]
     runSettings s (middleware app) `finally` destroyEnv e
