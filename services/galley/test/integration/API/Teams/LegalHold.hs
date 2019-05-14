b9dfd046e1b8e8cc311709c682a4ca0da052d892
jschaul <jschaul@users.noreply.github.com>
2019-05-14 19:05:48 +0200

    redundant imports

--- services/galley/test/integration/API/Teams/LegalHold.hs
+++ services/galley/test/integration/API/Teams/LegalHold.hs
@@ -2,45 +2,38 @@ module API.Teams.LegalHold (tests) where
 
 import Imports
 import API.Util
-import Bilge hiding (timeout)
-import Bilge.Assert
-import Control.Lens hiding ((#), (.=))
-import Data.Aeson hiding (json)
-import Data.Aeson.Lens
-import Data.ByteString.Conversion
-import Data.Id
-import Data.List1
-import Data.Misc (PlainTextPassword (..))
-import Data.Range
-import Galley.Types hiding (EventType (..), EventData (..), MemberUpdate (..))
-import Galley.Types.Teams
-import Galley.Types.Teams.Intra
-import Gundeck.Types.Notification
+-- import Bilge hiding (timeout)
+-- import Bilge.Assert
+-- import Control.Lens hiding ((#), (.=))
+-- import Data.Aeson hiding (json)
+-- import Data.Aeson.Lens
+-- import Data.ByteString.Conversion
+-- import Data.Id
+-- import Data.List1
+-- import Data.Misc (PlainTextPassword (..))
+-- import Data.Range
+-- import Galley.Types hiding (EventType (..), EventData (..), MemberUpdate (..))
+-- import Galley.Types.Teams
+-- import Galley.Types.Teams.Intra
+-- import Gundeck.Types.Notification
 import Test.Tasty
-import Test.Tasty.Cannon (TimeoutUnit (..), (#))
-import Test.Tasty.HUnit
-import TestSetup (test,  TestSetup, TestM, tsCannon, tsGalley)
+import TestSetup
 import API.SQS
-import UnliftIO (mapConcurrently, mapConcurrently_)
 import Brig.Types.Client
 
 import qualified API.Util as Util
-import qualified Data.Currency as Currency
-import qualified Data.List1 as List1
-import qualified Data.Set as Set
-import qualified Data.Text as T
-import qualified Data.UUID as UUID
-import qualified Galley.Types as Conv
-import qualified Network.Wai.Utilities.Error as Error
-import qualified Test.Tasty.Cannon as WS
+-- import qualified Data.Currency as Currency
+-- import qualified Data.List1 as List1
+-- import qualified Data.Set as Set
+-- import qualified Data.Text as T
+-- import qualified Data.UUID as UUID
+-- import qualified Galley.Types as Conv
+-- import qualified Network.Wai.Utilities.Error as Error
 
 tests :: IO TestSetup -> TestTree
 tests s = testGroup "Teams LegalHold API"
     [ test s "create client" testDisallowLegalHoldDeviceCreation
     ]
-
-timeout :: WS.Timeout
-timeout = 3 # Second
 
 testDisallowLegalHoldDeviceCreation :: TestM ()
 testDisallowLegalHoldDeviceCreation = do
@@ -53,7 +46,7 @@ testDisallowLegalHoldDeviceCreation = do
 
     -- team users cannot create LegalHoldClients
     owner <- Util.randomUser
-    tid   <- Util.createTeamInternal "foo" owner
+    _tid   <- Util.createTeamInternal "foo" owner
     assertQueue "create team" tActivate
 
     -- TODO: requests to /clients with type=LegalHoldClientType should fail (400 instead of 201)
