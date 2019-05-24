57faa2c673a47557072095450e361acd5a16f9c9
Chris Penner <christopher.penner@gmail.com>
2019-05-24 12:27:26 +0200

    Separate out external legalhold calls

--- services/galley/src/Galley/API/LegalHold.hs
+++ services/galley/src/Galley/API/LegalHold.hs
@@ -98,56 +98,18 @@ removeSettings (zusr ::: tid ::: _) = do
     LegalHoldData.removeSettings tid
     pure $ responseLBS status204 [] mempty
 
--- | Get /status from legal hold service; throw 'Wai.Error' with 400 if things go wrong.
-checkLegalHoldServiceStatus :: Fingerprint Rsa -> HttpsUrl -> Galley ()
-checkLegalHoldServiceStatus fpr (HttpsUrl url) = do
-    (mgr, verifyFingerprints) <- view (extEnv . extGetManager)
-    let verified = verifyFingerprints [fpr]
-    rs <- extHandleAll (const $ throwM legalHoldServiceUnavailable) $ do
-        recovering x3 httpHandlers $ const $ liftIO $
-            withVerifiedSslConnection verified mgr reqBuilder $ \req ->
-                Http.httpLbs req mgr
-    if | Bilge.statusCode rs < 400 -> pure ()
-       | otherwise -> throwM legalHoldServiceBadResponse
-  where
-    reqBuilder :: Http.Request -> Http.Request
-    reqBuilder
-        = maybe id Bilge.host (Bilge.extHost url)
-        . Bilge.port (fromMaybe 443 (Bilge.extPort url))
-        . Bilge.paths ["status"]
-        . Bilge.method GET
-        . Bilge.secure
-        . Bilge.expect2xx
 
-    x3 :: RetryPolicy
-    x3 = limitRetries 3 <> exponentialBackoff 100000
+-- | Request to provision a device on the legal hold service for a user
+requestDevice :: UserId ::: TeamId ::: UserId ::: JSON -> Galley Response
+requestDevice (zusr ::: tid ::: uid ::: _) = do
+    membs <- Data.teamMembers tid
+    void $ permissionCheck zusr ChangeLegalHoldUserSettings membs
+    assertLegalHoldEnabled tid
 
-    extHandleAll :: MonadCatch m => (SomeException -> m a) -> m a -> m a
-    extHandleAll f ma = catches ma
-        [ Handler $ \(ex :: SomeAsyncException) -> throwM ex
-        , Handler $ \(ex :: SomeException)      -> f ex
-        ]
+    mresult <- LegalHoldData.getSettings tid
+    legalHoldService <- case mresult of
+        Nothing -> throwM legalHoldNotRegistered
+        Just lhs -> pure lhs
+    undefined
 
--- | Copied unchanged from "Brig.Provider.API".  It would be nice to move (part of) this to
--- ssl-util, but it has types from brig-types and types-common.
-validateServiceKey :: MonadIO m => ServiceKeyPEM -> m (Maybe (ServiceKey, Fingerprint Rsa))
-validateServiceKey pem = liftIO $ readPublicKey >>= \pk ->
-    case join (SSL.toPublicKey <$> pk) of
-        Nothing  -> return Nothing
-        Just pk' -> do
-            Just sha <- SSL.getDigestByName "SHA256"
-            let size = SSL.rsaSize (pk' :: SSL.RSAPubKey)
-            if size < minRsaKeySize
-                then return Nothing
-                else do
-                    fpr <- Fingerprint <$> SSL.rsaFingerprint sha pk'
-                    let bits = fromIntegral size * 8
-                    let key = ServiceKey RsaServiceKey bits pem
-                    return $ Just (key, fpr)
-  where
-    readPublicKey = handleAny
-        (const $ return Nothing)
-        (SSL.readPublicKey (LC8.unpack (toByteString pem)) >>= return . Just)
 
-    minRsaKeySize :: Int
-    minRsaKeySize = 256 -- Bytes (= 2048 bits)
