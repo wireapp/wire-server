cb923e3a8f9aff74ae7f1f41791ed86675173c6d
Chris Penner <christopher.penner@gmail.com>
2019-06-12 11:36:47 +0200

    Fix broken schema migration

--- services/galley/schema/src/V32.hs
+++ services/galley/schema/src/V32.hs
@@ -10,7 +10,7 @@ migration = Migration 32 "Migrate User Legal Hold Status to Team Members Table" 
         ALTER TABLE team_member
           ADD
         (
-            legalhold_status             int,
+            legalhold_status             int
         )
     |]
 
