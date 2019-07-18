c2f0a022e38f312029983832270c0f55fa731cc1
fisx <mf@zerobuzz.net>
2019-07-18 13:02:36 +0200

    More scripts (#801)
    * create teams with admins
    
    * populate teams with generated members
    
    * populate teams with users from csv input

---
+++ deploy/services-demo/create_test_team_admins.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -e
+
+COUNT="1"
+BRIG_HOST="http://localhost:8082"
+CSV="false"
+
+USAGE="
+This bash script can be used to create active team admin users and
+their teams.
+
+Note that this uses an internal brig endpoint.  It is not exposed over
+nginz and can only be used if you have direct access to brig.
+
+USAGE: $0
+    -n <N>: Create <N> users. default: ${COUNT}
+    -h <host>: Base URI of brig. default: ${BRIG_HOST}
+    -c: Output as headerless CSV in format 'User-Id,Email,Password'. default: ${CSV}
+"
+
+# Option parsing:
+# https://sookocheff.com/post/bash/parsing-bash-script-arguments-with-shopts/
+while getopts ":n:h:c" opt; do
+  case ${opt} in
+    n ) COUNT="$OPTARG"
+      ;;
+    h ) BRIG_HOST="$OPTARG"
+      ;;
+    c ) CSV="true"
+      ;;
+    : ) echo "-$OPTARG" requires an argument 1>&2
+        exit 1
+      ;;
+    \? ) echo "$USAGE" 1>&2
+         exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+if [ "$#" -ne 0 ]; then
+  echo "$USAGE" 1>&2
+  exit 1
+fi
+
+# Generate users
+
+for i in $(seq 1 "$COUNT")
+do
+    EMAIL=$(cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 8)"@example.com"
+    PASSWORD=$(cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 8)
+
+    CURL_OUT=$(curl -i -s --show-error \
+        -XPOST "$BRIG_HOST/i/users" \
+        -H'Content-type: application/json' \
+        -d'{"email":"'"$EMAIL"'","password":"'"$PASSWORD"'","name":"demo","team":{"name":"Wire team","icon":"default"}}')
+
+    UUID=$(echo "$CURL_OUT" | tail -1 | sed 's/.*\"id\":\"\([a-z0-9-]*\)\".*/\1/')
+    TEAM=$(echo "$CURL_OUT" | tail -1 | sed 's/.*\"team\":\"\([a-z0-9-]*\)\".*/\1/')
+
+    if [ "$CSV" == "false" ]
+        then echo -e "Succesfully created a team admin user: $UUID on team: $TEAM with email: $EMAIL and password: $PASSWORD"
+        else echo -e "$UUID,$EMAIL,$PASSWORD"
+    fi
+done
