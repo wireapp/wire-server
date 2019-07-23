e6ff7b13091c512f6ff21b1ee362895c2534d8e9
Matthias Fischmann <mf@zerobuzz.net>
2019-07-23 23:02:40 +0200

    Fix tasty-to-hspec bridge.

--- services/spar/test/Test/Spar/APISpec.hs
+++ services/spar/test/Test/Spar/APISpec.hs
@@ -5,10 +5,12 @@ module Test.Spar.APISpec where
 import Imports
 
 import Arbitrary ()
+import Control.Exception
 import Data.Metrics.Test (servantApiConsistency)
 import Data.Proxy
 import Servant.Swagger (validateEveryToJSON)
 import Spar.API as API
+import System.Exit
 import Test.Hspec
 
 import qualified Test.Tasty
@@ -18,4 +20,9 @@ spec :: Spec
 spec = do
   -- Note: SCIM types are not validated because their content-type is 'SCIM'.
   validateEveryToJSON (Proxy @API.OutsideWorldAPI)
-  it "..." $ Test.Tasty.defaultMain (servantApiConsistency (Proxy @API.API))
+  tastyToHspec (servantApiConsistency (Proxy @API.API))
+
+
+tastyToHspec :: Test.Tasty.TestTree -> Spec
+tastyToHspec tasty = it "tasty" $ do
+  Test.Tasty.defaultMain tasty `catch` (`shouldBe` ExitSuccess)
