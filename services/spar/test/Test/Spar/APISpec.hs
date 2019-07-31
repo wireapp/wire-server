3dec44645d232bd7f379dd6eeffd6856716743b7
Matthias Fischmann <mf@zerobuzz.net>
2019-07-31 16:58:29 +0200

    Revert hack to remove some types out of the roundtrip tests.
    (somebody must have updated servant-swagger in between, in any case it
    stopped working, and also we shouldn't remove types from the roundtrip
    tests.)

--- services/spar/test/Test/Spar/APISpec.hs
+++ services/spar/test/Test/Spar/APISpec.hs
@@ -5,19 +5,12 @@ module Test.Spar.APISpec where
 import Imports
 
 import Arbitrary ()
-import Data.Aeson (ToJSON, encode, eitherDecode)
+import Data.Aeson (encode, eitherDecode)
 import Data.Metrics.Servant (routesToPaths)
 import Data.Metrics.Test (pathsConsistencyCheck)
 import Data.Proxy (Proxy(Proxy))
-import Data.Swagger.Schema (ToSchema)
-import Data.Swagger.Schema.Validation (validateToJSON)
-import Servant (JSON)
-import Servant.Swagger.Internal.Test (props)
-import Servant.Swagger.Internal.TypeLevel.API (Remove)
-import Servant.Swagger.TypeLevel (BodyTypes)
 import Servant.Swagger (validateEveryToJSON)
 import Spar.API as API
-import Spar.API (OutsideWorldAPI)
 import Spar.Types (IdPMetadataInfo)
 import Test.Hspec (Spec, it, shouldBe)
 import Test.QuickCheck (property)
@@ -25,12 +18,8 @@ import Test.QuickCheck (property)
 
 spec :: Spec
 spec = do
-  -- Note: SCIM types are not validated because their content-type is 'SCIM', or because they
-  -- are removed here explicitly.
-  props
-    (Proxy @[ToJSON, ToSchema])
-    (null . validateToJSON)
-    (Proxy @(Remove IdPMetadataInfo (BodyTypes JSON OutsideWorldAPI)))
+  -- Note: SCIM types are not validated because their content-type is 'SCIM'.
+  validateEveryToJSON (Proxy @API.OutsideWorldAPI)
 
   it "api consistency" $ do
     pathsConsistencyCheck (routesToPaths @API.API) `shouldBe` mempty
