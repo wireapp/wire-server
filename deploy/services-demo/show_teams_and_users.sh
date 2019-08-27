fa365560ce30ca5d795f430a83cb1a75c188e890
jschaul <jschaul@users.noreply.github.com>
2019-08-27 11:56:19 +0200

    bash, comment

--- deploy/services-demo/show_teams_and_users.sh
+++ deploy/services-demo/show_teams_and_users.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # this is a very hacky bash script designed to show teams and team members for small installations and small amounts of data.
-# Such a tool should instead be implemented in haskell.
+# Such a tool should instead be implemented in haskell, a docker image could be provided to make it easier to run this.
 # the only reason for this bash script is its relative portability: haskell or docker need not be installed on the target machine.
 
 
@@ -9,6 +9,8 @@
 
 # cqlsh -e "use galley; paging off; select team,name,creator from team;" > teams.txt
 # cqlsh -e "use brig; paging off; select id,email,handle,name,team from user;" > users.txt
+
+## The following operates on the two files users.txt and teams.txt and attempts to do a join operation.
 
 function print_team_user() {
     local tid=$1
