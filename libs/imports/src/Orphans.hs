a97af0646e76571e20e3121ed6f640ca5afdd4be
Arian van Putten <arian.vanputten@gmail.com>
2019-08-27 15:04:08 +0200

    Orphan orphans

--- libs/imports/src/Orphans.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE DeriveGeneric      #-}
-
-{-# OPTIONS_GHC -Wno-orphans #-}
-
--- | Orphan instances for non-Wire-specific types and classes.
-module Orphans () where
-
