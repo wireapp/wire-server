ed287866ccb6550ea190b15a74eb32e3bddf2e9a
Matthias Fischmann <mf@zerobuzz.net>
2019-09-11 16:05:06 +0200

    Fixup

--- tools/stern/src/Stern/Intra.hs
+++ tools/stern/src/Stern/Intra.hs
@@ -358,7 +358,6 @@ getLegalholdStatus tid = do
     (>>= fromResponseBody) . catchRpcErrors $ rpc' "galley" gly
          ( method GET
          . paths ["/i/teams", toByteString' tid, "features", "legalhold"]
-         . expect2xx
          )
   where
     fromResponseBody :: Response (Maybe LByteString) -> Handler SetLegalHoldStatus
@@ -376,7 +375,6 @@ setLegalholdStatus tid status = do
          . paths ["/i/teams", toByteString' tid, "features", "legalhold"]
          . lbytes (encode $ toRequestBody status)
          . contentJson
-         . expect2xx
          )
   where
     toRequestBody SetLegalHoldDisabled = LegalHoldTeamConfig LegalHoldDisabled
@@ -389,7 +387,6 @@ getSSOStatus tid = do
     (>>= fromResponseBody) . catchRpcErrors $ rpc' "galley" gly
          ( method GET
          . paths ["/i/teams", toByteString' tid, "features", "sso"]
-         . expect2xx
          )
   where
     fromResponseBody :: Response (Maybe LByteString) -> Handler SetSSOStatus
@@ -407,7 +404,6 @@ setSSOStatus tid status = do
          . paths ["/i/teams", toByteString' tid, "features", "sso"]
          . lbytes (encode $ toRequestBody status)
          . contentJson
-         . expect2xx
          )
   where
     toRequestBody SetSSODisabled = SSOTeamConfig SSODisabled
