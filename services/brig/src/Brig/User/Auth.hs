21ddcf3e49566b3cec03b304213071ba74ef7a29
Tiago Manuel Ventura Loureiro <tiago.loureiro@gmail.com>
2019-09-16 22:00:22 +0200

    Log warnings only when users are suspended (#854)

--- services/brig/src/Brig/User/Auth.hs
+++ services/brig/src/Brig/User/Auth.hs
@@ -159,10 +159,10 @@ revokeAccess u pw cc ll = do
 catchSuspendInactiveUser :: UserId -> e -> ExceptT e AppIO ()
 catchSuspendInactiveUser uid errval = do
   mustsuspend <- lift $ mustSuspendInactiveUser uid
-  Log.warn $ msg (val "catchSuspendInactiveUser")
-    ~~ field "user" (toByteString uid)
-    ~~ field "mustsuspend" mustsuspend
   when mustsuspend $ do
+    Log.warn $ msg (val "Suspending user due to inactivity")
+      ~~ field "user" (toByteString uid)
+      ~~ field "action" ("user.suspend" :: String)
     lift $ suspendAccount (singleton uid)
     throwE errval
 
