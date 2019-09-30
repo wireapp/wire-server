dd7201dd522a9c0ac14c616df4478ed1fe8eecf0
Arian van Putten <arian.vanputten@gmail.com>
2019-09-30 14:29:51 +0200

    Add wire theme (#16)
    Based on alabaster with some tweaks

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,8 @@ if tags.has('administrate'):
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme_path = ['..'] 
+html_theme = 'wire-theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
