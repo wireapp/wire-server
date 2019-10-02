6f46bf625df2f6200304721b90704f1af9f8c002
Matthias Fischmann <mf@zerobuzz.net>
2019-10-02 16:26:11 +0200

    Test.

---
+++ services/gundeck/test/unit/ThreadBudget.hs
@@ -0,0 +1,95 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns -Wno-orphans #-}
+
+module ThreadBudget where
+
+import Imports
+
+import Control.Concurrent.Async
+import Data.Metrics.Middleware (metrics)
+import Data.String.Conversions (cs)
+import Gundeck.ThreadBudget
+import System.IO.Unsafe (unsafePerformIO)
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import qualified System.Logger.Class as LC
+
+
+tests :: TestTree
+tests = testGroup "thread budgets" $
+  [ testCase "works with mock actions" testThreadBudgets
+  ]
+
+
+data LogEntry = NoBudget | Unknown String
+  deriving (Eq, Show)
+
+logHistory :: MVar [LogEntry]
+logHistory = unsafePerformIO $ newMVar []
+
+expectLogHistory :: HasCallStack => ([LogEntry] -> Bool) -> IO ()
+expectLogHistory expected = do
+  found <- modifyMVar logHistory (\found -> pure ([], found))
+  expected found @? ("unexpected log data: " <> show found)
+
+enterLogHistory :: LogEntry -> IO ()
+enterLogHistory entry = modifyMVar_ logHistory (\found -> pure (entry : found))
+
+instance LC.MonadLogger IO where
+  log _level msg = do
+    let raw :: String = cs $ LC.render LC.renderNetstr msg
+        parsed
+          | "runWithBudget: out of budget." `isInfixOf` raw = NoBudget
+          | otherwise                                       = Unknown raw
+    enterLogHistory parsed
+
+
+testThreadBudgets :: Assertion
+testThreadBudgets = do
+  mtr <- metrics
+  tbs <- mkThreadBudgetState 5
+
+  let delayms :: Int -> IO ()
+      delayms = threadDelay . (* 1000)
+
+      burstActions :: Int -> Int -> IO ()
+      burstActions howlong howmany
+        = void . forkIO . void $
+          mapConcurrently (\jitter -> do
+                              threadDelay (jitter * 20)  -- @* 2@ is enough on normal hardware.
+                              runWithBudget tbs $ delayms howlong)
+                          [1..howmany]
+
+  watcher <- async $ watchThreadBudgetState mtr tbs 1
+
+  burstActions 1000 5
+  delayms 100
+  expectLogHistory null
+
+  burstActions 1000 3
+  delayms 100
+  expectLogHistory (== [NoBudget, NoBudget, NoBudget])
+
+  burstActions 1000 3
+  delayms 100
+  expectLogHistory (== [NoBudget, NoBudget, NoBudget])
+
+  delayms 800
+
+  burstActions 1000 3
+  delayms 100
+  expectLogHistory null
+
+  burstActions 1000 3
+  delayms 100
+  expectLogHistory (== [NoBudget])
+
+  cancel watcher
+
+
+-- TODO: test watcher: we can kill runWithBudget with async exceptions, that should keep the
+-- finalizer from running, no?  interesting to find out if that's true in its own right.  if
+-- it's true, we can run a watcher every 1s (that's the highest frequency there is), run
+-- long-running actions with runWithBudget, and kill them in a way that keeps the finalizer
+-- from running.  then we can check we get the watcher to clean things up.  can we also check
+-- that that's the *only* time the watcher gets something to do?
