wireapp/wire-server

commit 923d555d1e8b7c23a0644814d18fa34ad258b423

Better events for deletion of team conversations (#849)
* Also send `conversation.delete` to team members.

* Refactor.

services/galley/src/Galley/API/Teams.hs
@@ -193,7 +193,7 @@ uncheckedDeleteTeam zusr zcon tid = do
         membs    <- Data.teamMembers tid
         now      <- liftIO getCurrentTime
         convs    <- filter (not . view managedConversation) <$> Data.teamConversations tid
-        (ue, be) <- foldrM (pushEvents now membs) ([],[]) convs
+        (ue, be) <- foldrM (pushConvDeleteEvents now membs) ([],[]) convs
         let e = newEvent TeamDelete tid now
         let r = list1 (userRecipient zusr) (membersToRecipients (Just zusr) membs)
         pushSome ((newPush1 zusr (TeamEvent e) r & pushConn .~ zcon) : ue)
@@ -206,12 +206,17 @@ uncheckedDeleteTeam zusr zcon tid = do
             Journal.teamDelete tid
         Data.deleteTeam tid
   where
-    pushEvents :: UTCTime -> [TeamMember] -> TeamConversation -> ([Push],[(BotMember, Conv.Event)]) -> Galley ([Push],[(BotMember, Conv.Event)])
-    pushEvents now membs c (pp, ee) = do
-        (bots, users) <- botsAndUsers <$> Data.members (c^.conversationId)
-        let mm = nonTeamMembers users membs
-        let e = Conv.Event Conv.ConvDelete (c^.conversationId) zusr now Nothing
-        let p = newPush zusr (ConvEvent e) (map recipient mm)
+    pushConvDeleteEvents
+        :: UTCTime
+        -> [TeamMember]
+        -> TeamConversation
+        -> ([Push],[(BotMember, Conv.Event)])
+        -> Galley ([Push],[(BotMember, Conv.Event)])
+    pushConvDeleteEvents now teamMembs c (pp, ee) = do
+        (bots, convMembs) <- botsAndUsers <$> Data.members (c ^. conversationId)
+        let mm = convMembsAndTeamMembs convMembs teamMembs
+        let e = Conv.Event Conv.ConvDelete (c ^. conversationId) zusr now Nothing
+        let p = newPush zusr (ConvEvent e) mm
         let ee' = bots `zip` repeat e
         let pp' = maybe pp (\x -> (x & pushConn .~ zcon) : pp) p
         pure (pp', ee' ++ ee)
@@ -392,10 +397,11 @@ deleteTeamConversation (zusr::: zcon ::: tid ::: cid ::: _) = do
     let te = newEvent Teams.ConvDelete tid now & eventData .~ Just (Teams.EdConvDelete cid)
     let ce = Conv.Event Conv.ConvDelete cid zusr now Nothing
     let tr = list1 (userRecipient zusr) (membersToRecipients (Just zusr) tmems)
-    let p  = newPush1 zusr (TeamEvent te) tr & pushConn .~ Just zcon
-    case map recipient (nonTeamMembers cmems tmems) of
-        []     -> push1 p
-        (m:mm) -> pushSome [p, newPush1 zusr (ConvEvent ce) (list1 m mm) & pushConn .~ Just zcon]
+    let teamPush = [newPush1 zusr (TeamEvent te) tr & pushConn .~ Just zcon]
+        convPush = case convMembsAndTeamMembs cmems tmems of
+            []     -> []
+            (m:mm) -> [newPush1 zusr (ConvEvent ce) (list1 m mm) & pushConn .~ Just zcon]
+    pushSome $ teamPush <> convPush
     void . forkIO $ void $ External.deliver (bots `zip` repeat ce)
     -- TODO: we don't delete bots here, but we should do that, since every
     -- bot user can only be in a single conversation

services/galley/test/integration/API/Teams.hs
@@ -600,6 +600,8 @@ testDeleteTeam = do
             const 202 === statusCode
         checkTeamDeleteEvent tid wsOwner
         checkTeamDeleteEvent tid wsMember
+        checkConvDeleteEvent cid1 wsOwner
+        checkConvDeleteEvent cid1 wsMember
         checkConvDeleteEvent cid1 wsExtern
         WS.assertNoEvent timeout [wsOwner, wsExtern, wsMember]
 
@@ -724,6 +726,9 @@ testDeleteTeamConv = do
 
         checkTeamConvDeleteEvent tid cid2 wsOwner
         checkTeamConvDeleteEvent tid cid2 wsMember
+        checkConvDeleteEvent cid2 wsOwner
+        checkConvDeleteEvent cid2 wsMember
+        WS.assertNoEvent timeout [wsOwner, wsMember]
 
         delete ( g
                . paths ["teams", toByteString' tid, "conversations", toByteString' cid1]
@@ -733,9 +738,10 @@ testDeleteTeamConv = do
 
         checkTeamConvDeleteEvent tid cid1 wsOwner
         checkTeamConvDeleteEvent tid cid1 wsMember
-        checkConvDeletevent cid1 wsExtern
-
-        WS.assertNoEvent timeout [wsOwner, wsExtern, wsMember]
+        checkConvDeleteEvent cid1 wsOwner
+        checkConvDeleteEvent cid1 wsMember
+        checkConvDeleteEvent cid1 wsExtern
+        WS.assertNoEvent timeout [wsOwner, wsMember, wsExtern]
 
     for_ [cid1, cid2] $ \x ->
         for_ [owner, member^.userId, extern] $ \u -> do
@@ -743,20 +749,6 @@ testDeleteTeamConv = do
             Util.assertNotConvMember u x
 
     postConvCodeCheck code !!! const 404 === statusCode
-  where
-    checkTeamConvDeleteEvent tid cid w = WS.assertMatch_ timeout w $ \notif -> do
-        ntfTransient notif @?= False
-        let e = List1.head (WS.unpackPayload notif)
-        e^.eventType @?= ConvDelete
-        e^.eventTeam @?= tid
-        e^.eventData @?= Just (EdConvDelete cid)
-
-    checkConvDeletevent cid w = WS.assertMatch_ timeout w $ \notif -> do
-        ntfTransient notif @?= False
-        let e = List1.head (WS.unpackPayload notif)
-        evtType e @?= Conv.ConvDelete
-        evtConv e @?= cid
-        evtData e @?= Nothing
 
 testUpdateTeam :: TestM ()
 testUpdateTeam = do
@@ -925,6 +917,14 @@ checkTeamDeleteEvent tid w = WS.assertMatch_ timeout w $ \notif -> do
     e^.eventType @?= TeamDelete
     e^.eventTeam @?= tid
     e^.eventData @?= Nothing
+
+checkTeamConvDeleteEvent :: HasCallStack => TeamId -> ConvId -> WS.WebSocket -> TestM ()
+checkTeamConvDeleteEvent tid cid w = WS.assertMatch_ timeout w $ \notif -> do
+    ntfTransient notif @?= False
+    let e = List1.head (WS.unpackPayload notif)
+    e^.eventType @?= ConvDelete
+    e^.eventTeam @?= tid
+    e^.eventData @?= Just (EdConvDelete cid)
 
 checkConvDeleteEvent :: HasCallStack => ConvId -> WS.WebSocket -> TestM ()
 checkConvDeleteEvent cid w = WS.assertMatch_ timeout w $ \notif -> do

libs/tasty-cannon/src/Test/Tasty/Cannon.hs
@@ -53,6 +53,7 @@ import Data.Aeson (decodeStrict', FromJSON, fromJSON, Value (..))
 import Data.ByteString.Conversion
 import Data.Id
 import Data.List1
+import Data.Misc ((<$$>))
 import Data.Timeout (Timeout, TimeoutUnit (..), (#))
 import Gundeck.Types
 import Network.HTTP.Client
@@ -250,7 +251,14 @@ awaitMatchN :: (HasCallStack, MonadIO m)
             -> [WebSocket]
             -> (Notification -> Assertion)
             -> m [Either MatchTimeout Notification]
-awaitMatchN t wss f = liftIO $ mapConcurrently (\ws -> awaitMatch t ws f) wss
+awaitMatchN t wss f = snd <$$> awaitMatchN' t (((),) <$> wss) f
+
+awaitMatchN' :: (HasCallStack, MonadIO m)
+            => Timeout
+            -> [(extra, WebSocket)]
+            -> (Notification -> Assertion)
+            -> m [(extra, Either MatchTimeout Notification)]
+awaitMatchN' t wss f = liftIO $ mapConcurrently (\(extra, ws) -> (extra,) <$> awaitMatch t ws f) wss
 
 assertMatchN :: (HasCallStack, MonadIO m, MonadThrow m)
              => Timeout
@@ -264,11 +272,11 @@ assertSuccess = either throwM return
 
 assertNoEvent :: (HasCallStack, MonadIO m, MonadCatch m) => Timeout -> [WebSocket] -> m ()
 assertNoEvent t ww = do
-    results <- awaitMatchN t ww (const $ pure ())
-    for_ results $
-        either (const $ pure ()) (liftIO . f)
+    results <- awaitMatchN' t (zip [(0 :: Int)..] ww) (const $ pure ())
+    for_ results $ \(ix, result) ->
+        either (const $ pure ()) (liftIO . f ix) result
   where
-    f n = assertFailure $ "unexpected notification received: " ++ show n
+    f ix n = assertFailure $ "unexpected notification received: " ++ show (ix, n)
 
 -----------------------------------------------------------------------------
 -- Unpacking Notifications

services/galley/src/Galley/API/Util.hs
@@ -22,6 +22,7 @@ import Network.Wai.Predicate
 import Network.Wai.Utilities
 import UnliftIO (concurrently)
 
+import qualified Data.Set       as Set
 import qualified Data.Text.Lazy as LT
 import qualified Galley.Data    as Data
 
@@ -151,6 +152,12 @@ location = addHeader hLocation . toByteString'
 
 nonTeamMembers :: [Member] -> [TeamMember] -> [Member]
 nonTeamMembers cm tm = filter (not . flip isTeamMember tm . memId) cm
+
+convMembsAndTeamMembs :: [Member] -> [TeamMember] -> [Recipient]
+convMembsAndTeamMembs convMembs teamMembs
+    = fmap userRecipient . setnub $ map memId convMembs <> map (view userId) teamMembs
+  where
+    setnub = Set.toList . Set.fromList
 
 membersToRecipients :: Maybe UserId -> [TeamMember] -> [Recipient]
 membersToRecipients Nothing  = map (userRecipient . view userId)

commit 1596d7881dc69df419f0fef5f7f9df8b01d61a2d

Do not send events to team members to be deleted
Events about deleted conversations do not need to be sent to everyone
when a team is deleted. Indeed, it is harmful in that it leads to performance problems:
For large teams with many conversations, notifying
every team member about that fact that every conversation has been
deleted creates a lot of events.
None of these events are useful, since on a team deletion, the accounts
of every user are deleted, i.e. in practice these events cannot be read.

This PR fixes the bug that was introduced in #849

services/galley/src/Galley/API/Teams.hs
@@ -214,9 +214,12 @@ uncheckedDeleteTeam zusr zcon tid = do
         -> Galley ([Push],[(BotMember, Conv.Event)])
     pushConvDeleteEvents now teamMembs c (pp, ee) = do
         (bots, convMembs) <- botsAndUsers <$> Data.members (c ^. conversationId)
-        let mm = convMembsAndTeamMembs convMembs teamMembs
+        -- Only nonTeamMembers need to get any events, since on team deletion,
+        -- all team users are deleted immediately after these events are sent
+        -- and will thus never be able to see these events in practice.
+        let mm = nonTeamMembers convMembs teamMembs
         let e = Conv.Event Conv.ConvDelete (c ^. conversationId) zusr now Nothing
-        let p = newPush zusr (ConvEvent e) mm
+        let p = newPush zusr (ConvEvent e) (map recipient mm)
         let ee' = bots `zip` repeat e
         let pp' = maybe pp (\x -> (x & pushConn .~ zcon) : pp) p
         pure (pp', ee' ++ ee)

services/galley/test/integration/API/Teams.hs
@@ -598,8 +598,7 @@ testDeleteTeam = do
             const 202 === statusCode
         checkTeamDeleteEvent tid wsOwner
         checkTeamDeleteEvent tid wsMember
-        checkConvDeleteEvent cid1 wsOwner
-        checkConvDeleteEvent cid1 wsMember
+        -- team members should not receive conversation delete events
         checkConvDeleteEvent cid1 wsExtern
         WS.assertNoEvent timeout [wsOwner, wsExtern, wsMember]
 