5c450f199c8ccf1b322be0cda787cd7ea295da66
Sergiusz Bazanski <serge@wire.com>
2019-11-18 23:45:43 +0100

    services/brig: reindent Whitelist.hs

--- services/brig/src/Brig/Whitelist.hs
+++ services/brig/src/Brig/Whitelist.hs
@@ -44,34 +44,34 @@ deriveJSON toOptionFieldName 'internalWhitelistDomains
 -- consults a statically configured whitelist of allowed email domains..
 verifyInternal :: InternalWhitelist -> Either Email Phone -> Bool
 verifyInternal (InternalWhitelist domains) key =
-  let allowFromDomains (Left e) = (emailDomain e) `elem` domains
-      allowFromDomains _        = False
-  in allowFromDomains key
+    let allowFromDomains (Left e) = (emailDomain e) `elem` domains
+        allowFromDomains _        = False
+    in allowFromDomains key
 
 -- | Do a request to the whitelist service and verify that the provided email/phone address is
 -- whitelisted.
 verifyService :: (MonadIO m, MonadMask m, MonadHttp m) => Whitelist -> Either Email Phone -> m Bool
 verifyService (Whitelist url user pass) key = if isKnownDomain key
-  then return True
-  else recovering x3 httpHandlers . const $ do
-     rq <- parseRequest $ T.unpack url
-     rsp <- get' rq $ req (encodeUtf8 user) (encodeUtf8 pass)
-     case statusCode rsp of
-         200 -> return True
-         404 -> return False
-         _   -> throwM $
-             HttpExceptionRequest rq (StatusCodeException (rsp { responseBody = () }) mempty)
-  where
-    -- Duplicate behaviour from verifyLocal, keeping old behaviour for
-    -- compatibility.
-    isKnownDomain (Left e) = emailDomain e == "wire.com"
-    isKnownDomain _        = False
+    then return True
+    else recovering x3 httpHandlers . const $ do
+        rq <- parseRequest $ T.unpack url
+        rsp <- get' rq $ req (encodeUtf8 user) (encodeUtf8 pass)
+        case statusCode rsp of
+            200 -> return True
+            404 -> return False
+            _   -> throwM $
+                HttpExceptionRequest rq (StatusCodeException (rsp { responseBody = () }) mempty)
+    where
+      -- Duplicate behaviour from verifyLocal, keeping old behaviour for
+      -- compatibility.
+      isKnownDomain (Left e) = emailDomain e == "wire.com"
+      isKnownDomain _        = False
 
-    urlEmail = queryItem "email" . encodeUtf8 . fromEmail
-    urlPhone = queryItem "mobile" . encodeUtf8 . fromPhone
+      urlEmail = queryItem "email" . encodeUtf8 . fromEmail
+      urlPhone = queryItem "mobile" . encodeUtf8 . fromPhone
 
-    req u p = port 443 . secure
-            . either urlEmail urlPhone key
-            . applyBasicAuth u p
+      req u p = port 443 . secure
+              . either urlEmail urlPhone key
+              . applyBasicAuth u p
 
-    x3 = limitRetries 3 <> exponentialBackoff 100000
+      x3 = limitRetries 3 <> exponentialBackoff 100000
