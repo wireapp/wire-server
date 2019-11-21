465e3ce6fc9bd1044b1fc81ffe1d7dd42ca86eb4
Tiago Loureiro <tiago.loureiro@gmail.com>
2019-11-21 23:24:32 +0100

    Better naming

--- services/galley/test/integration/API/Util.hs
+++ services/galley/test/integration/API/Util.hs
@@ -806,7 +806,7 @@ encodeCiphertext :: ByteString -> Text
 encodeCiphertext = decodeUtf8 . B64.encode
 
 memberUpdate :: MemberUpdate
-memberUpdate = MemberUpdate Nothing Nothing Nothing Nothing Nothing Nothing Nothing
+memberUpdate = MemberUpdate Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing
 
 genRandom :: (Q.Arbitrary a, MonadIO m) => m a
 genRandom = liftIO . Q.generate $ Q.arbitrary
