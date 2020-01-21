44ffdf4a817bd9fd6b2e442e26c289971021ddb1
Akshay Mankar <akshay@wire.com>
2020-01-21 16:58:30 +0100

    Update `cargoSha256` for cryptobox-c in stack-deps.nix

--- stack-deps.nix
+++ stack-deps.nix
@@ -28,7 +28,7 @@ let
         rev = "4067ad96b125942545dbdec8c1a89f1e1b65d013";
         sha256 = "1i9dlhw0xk1viglyhail9fb36v1awrypps8jmhrkz8k1bhx98ci3";
       };
-      cargoSha256 = "1373rpy0fi3cpacv06x1cv4cv0brwdri2680ymdkq8w44syp20ym";
+      cargoSha256 = "0m85c49hvvxxv7jdipfcaydy4n8iw4h6myzv63v7qc0fxnp1vfm8";
       postInstall = ''
         mkdir -p $out/include
         cp src/cbox.h $out/include
