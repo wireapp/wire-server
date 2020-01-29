b9b5527c71ad025343eb5c54fb48d3ab467cb9e0
Matthias Heinzel <mheinzel@posteo.de>
2020-01-29 10:10:33 +0100

    change table schema

--- services/spar/schema/src/V7.hs
+++ services/spar/schema/src/V7.hs
@@ -5,8 +5,14 @@ import Cassandra.Schema
 import Text.RawString.QQ
 
 migration :: Migration
-migration = Migration 7 "Add extra idp flag for default SSO code" $ do
+migration = Migration 7 "Store default SSO code" $ do
 
+    -- `primary_key_always_default` should always be the string "default"
+    -- to guarantee there is a single value
     void $ schema' [r|
-        ALTER TABLE idp ADD is_default boolean;
+        CREATE TABLE if not exists default_idp
+            ( primary_key_always_default text
+            , idp                        uuid
+            , PRIMARY KEY (primary_key_always_default)
+            ) with compaction = {'class': 'LeveledCompactionStrategy'};
     |]
