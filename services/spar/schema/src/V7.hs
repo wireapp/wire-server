28cc4a2d2f4a01acf4d387b07c3241a3b99d785b
Matthias Heinzel <mheinzel@posteo.de>
2020-01-31 10:23:28 +0100

    change schema

--- services/spar/schema/src/V7.hs
+++ services/spar/schema/src/V7.hs
@@ -7,12 +7,9 @@ import Text.RawString.QQ
 migration :: Migration
 migration = Migration 7 "Store default SSO code" $ do
 
-    -- `primary_key_always_default` should always be the string "default"
-    -- to guarantee there is a single value
     void $ schema' [r|
         CREATE TABLE if not exists default_idp
-            ( primary_key_always_default text
-            , idp                        uuid
-            , PRIMARY KEY (primary_key_always_default)
+            ( idp uuid
+            , PRIMARY KEY (idp)
             ) with compaction = {'class': 'LeveledCompactionStrategy'};
     |]
