cb4ef385a39f5249c78a80602de9bb291b74e999
Matthias Heinzel <mheinzel@posteo.de>
2020-01-31 16:13:30 +0100

    test to protect from accidential deletion

--- services/spar/test/Test/Spar/APISpec.hs
+++ services/spar/test/Test/Spar/APISpec.hs
@@ -11,9 +11,15 @@ import Data.Metrics.Test (pathsConsistencyCheck)
 import Data.Proxy (Proxy(Proxy))
 import Servant.Swagger (validateEveryToJSON)
 import Spar.API as API
-import Spar.Types (IdPMetadataInfo(IdPMetadataValue))
-import Test.Hspec (Spec, it, shouldBe)
+import Spar.Types (IdPMetadataInfo(IdPMetadataValue), SSOSettings)
+import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
 import Test.QuickCheck (property)
+
+import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Lens as Aeson
+import qualified Data.Aeson.Types as Aeson
+import qualified Data.HashMap.Strict as HM
+import qualified Control.Lens as Lens
 
 
 spec :: Spec
@@ -27,3 +33,13 @@ spec = do
   it "roundtrip: IdPMetadataInfo" . property $ \(val :: IdPMetadataInfo) -> do
     let withoutRaw (IdPMetadataValue _ x) = x
     (withoutRaw <$> (eitherDecode . encode) val) `shouldBe` Right (withoutRaw val)
+
+  describe "SSOSettings JSON instance" $ do
+    it "always has and requires the field default_sso_code" $
+      property $ \(ssoSettings :: SSOSettings) -> do
+        let object = Aeson.toJSON ssoSettings
+        let objectWithoutKey = Lens.over Aeson._Object (HM.delete "default_sso_key") $ object
+        (HM.lookup "sso_default_code" =<< Lens.preview Aeson._Object object)
+          `shouldSatisfy` isJust
+        Aeson.parseMaybe (Aeson.parseJSON @SSOSettings) objectWithoutKey
+          `shouldSatisfy` isNothing
