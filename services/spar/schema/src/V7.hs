f6ca0d122f7e8423701284395e93f719a7b21080
Matthias Heinzel <mheinzel@posteo.de>
2020-02-03 10:44:50 +0100

    singleton primary key schema (errors)
    test-integration/Test/Spar/APISpec.hs:978:9:
    1) Spar.API, SSO settings endpoint, removes the default SSO code if the IdP gets removed
      predicate failed on: Response {responseStatus = Status {statusCode = 500, statusMessage = "server-error"}, responseVersion = HTTP/1.1, responseHeaders = [("Transfer-Encoding","chunked"),("Date","Mon, 03 Feb 2020 10:33:38 GMT"),("Server","Warp/3.2.25"),("Content-Type","application/json")], responseBody = Just "{\"code\":500,\"message\":\"{\\\"code\\\":500,\\\"message\\\":\\\"ResponseError {reHost = datacenter1:rack1:127.0.0.1:9042, reTrace = Nothing, reWarn = [], reCause = ServerError \\\\\\\"java.lang.IndexOutOfBoundsException: readerIndex(207) + length(4) exceeds writerIndex(208): SlicedAbstractByteBuf(ridx: 207, widx: 208, cap: 208/208, unwrapped: PooledUnsafeDirectByteBuf(ridx: 217, widx: 217, cap: 1024))\\\\\\\"}\\\",\\\"label\\\":\\\"server-error\\\"}\",\"label\":\"server-error\"}", responseCookieJar = CJ {expose = []}, responseClose' = ResponseClose}

--- services/spar/schema/src/V7.hs
+++ services/spar/schema/src/V7.hs
@@ -7,9 +7,12 @@ import Text.RawString.QQ
 migration :: Migration
 migration = Migration 7 "Store default SSO code" $ do
 
+    -- primary_key_always_default should always be "default".
+    -- It exists to guarantee that there is only single default idp at all times.
     void $ schema' [r|
         CREATE TABLE if not exists default_idp
-            ( idp uuid
-            , PRIMARY KEY (idp)
+            ( primary_key_always_default text
+            , idp uuid
+            , PRIMARY KEY (primary_key_always_default)
             ) with compaction = {'class': 'LeveledCompactionStrategy'};
     |]
--- services/spar/src/Spar/Data.hs
+++ services/spar/src/Spar/Data.hs
@@ -369,16 +369,32 @@ getIdPConfigsByTeam team = do
 deleteIdPConfig
   :: (HasCallStack, MonadClient m)
   => SAML.IdPId -> SAML.Issuer -> TeamId -> m ()
-deleteIdPConfig idp issuer team = retry x5 $ batch $ do
-    setType BatchLogged
-    setConsistency Quorum
-    addPrepQuery delDefaultIdp (Identity idp)
-    addPrepQuery delIdp (Identity idp)
-    addPrepQuery delIssuerIdp (Identity issuer)
-    addPrepQuery delTeamIdp (team, idp)
+deleteIdPConfig idp issuer team = do
+    currentDefaultIdP <- runIdentity <$$> retry x1 (query1 selDefaultIdp $ params Quorum ())
+
+    if currentDefaultIdP == Just idp
+    then
+      retry x5 $ batch $ do
+        setType BatchLogged
+        setConsistency Quorum
+        addPrepQuery delDefaultIdp (Identity idp)
+        addPrepQuery delIdp (Identity idp)
+        addPrepQuery delIssuerIdp (Identity issuer)
+        addPrepQuery delTeamIdp (team, idp)
+    else
+      retry x5 $ batch $ do
+        setType BatchLogged
+        setConsistency Quorum
+        addPrepQuery delIdp (Identity idp)
+        addPrepQuery delIssuerIdp (Identity issuer)
+        addPrepQuery delTeamIdp (team, idp)
+
   where
+    selDefaultIdp :: PrepQuery R () (Identity SAML.IdPId)
+    selDefaultIdp = "SELECT FROM default_idp WHERE primary_key_always_default = 'default'"
+
     delDefaultIdp :: PrepQuery W (Identity SAML.IdPId) ()
-    delDefaultIdp = "DELETE FROM default_idp WHERE idp = ?"
+    delDefaultIdp = "DELETE FROM default_idp WHERE primary_key_always_default = 'default' IF idp = ?"
 
     delIdp :: PrepQuery W (Identity SAML.IdPId) ()
     delIdp = "DELETE FROM idp WHERE idp = ?"
@@ -443,24 +459,17 @@ getDefaultSSOCode = fmap runIdentity . minimumMay <$>
   (retry x1 . query sel $ params Quorum ())
   where
     sel :: PrepQuery R () (Identity SAML.IdPId)
-    sel = "SELECT idp FROM default_idp"
+    sel = "SELECT idp FROM default_idp WHERE primary_key_always_default = 'default'"
 
 storeDefaultSSOCode
   :: (HasCallStack, MonadClient m)
   => SAML.IdPId -> m ()
 storeDefaultSSOCode idpId = do
-  -- there is a race condition here which means there could potentially be more
-  -- than one entry (violating invariant 2).
-  -- However, the SELECT query will deterministally pick one of them and the
-  -- others will get removed by TRUNCATE the next time this function is called.
-  retry x5 . write trunc $ params Quorum ()
   retry x5 . write ins $ params Quorum (Identity idpId)
 
   where
-    trunc :: PrepQuery W () ()
-    trunc = "TRUNCATE default_idp"
     ins :: PrepQuery W (Identity SAML.IdPId) ()
-    ins = "INSERT INTO default_idp (idp) VALUES (?)"
+    ins = "INSERT INTO default_idp (primary_key_always_default, idp) VALUES ('default', ?)"
 
 deleteDefaultSSOCode
   :: (HasCallStack, MonadClient m)
@@ -468,7 +477,7 @@ deleteDefaultSSOCode
 deleteDefaultSSOCode = retry x5 . write del $ params Quorum ()
   where
     del :: PrepQuery W () ()
-    del = "TRUNCATE default_idp"
+    del = "DELETE FROM default_idp WHERE primary_key_always_default = 'default'"
 
 ----------------------------------------------------------------------
 -- SCIM auth
