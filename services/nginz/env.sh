7ea1d1789f5ff89e08cd97680c2b4575570c353f
Matthias Fischmann <mf@zerobuzz.net>
2020-02-11 13:40:48 +0000

    ...

--- services/nginz/env.sh
+++ services/nginz/env.sh
@@ -1,4 +1,5 @@
 export SODIUM_LIB_DIR=/nix/store/y95hjmcslzmlziv3zllqq3j654gqpqnx-libsodium-1.0.18-dev/lib/pkgconfig/
 export PREFIX_INSTALL=`pwd`/libzauth
-export EXTRA_CC_LIB="-l$PREFIX_INSTALL/lib/libzauth.so"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX_INSTALL/lib/
+export EXTRA_CC_LIB="-L$PREFIX_INSTALL/lib/ -lzauth"
 export EXTRA_CC_INC="-I$PREFIX_INSTALL/include"
