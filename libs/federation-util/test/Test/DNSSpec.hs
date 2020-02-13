4c2e35b5cbd2000fec52583086faf432f68a309c
jschaul <jschaul@users.noreply.github.com>
2020-02-13 14:03:52 +0100

    Add weight-randomness test

--- libs/federation-util/test/Test/DNSSpec.hs
+++ libs/federation-util/test/Test/DNSSpec.hs
@@ -20,15 +20,24 @@ spec = do
                         ]
             (fst4 <$> actual) `shouldBe` [0,0,0,2,2,3,15]
 
-        -- "orders records with the same priority according to weight with certain probability"
-            -- actual <- orderSrvResult [
-            --              (2, 10, 443, "server1.com")
-            --             ,(2, 20, 443, "server2.com")
-            --             ,(2, 0, 443, "dontuseoften.com")
-            --             ]
-            -- (snd4 <$> actual) has a higher chance of being [20,10,0] than
-            -- other combinations, but can be in any combination
-            -- does it make sense to test the randomness here?
+        it "orders records with the same priority according to weight with certain probability" $ do
+            let raw = [  (2, 10, 443, "server1.com")
+                        ,(2, 20, 443, "server2.com")
+                        ,(2, 0, 443, "dontuseoften.com")]
+            -- order the list 50 times
+            actuals <- replicateM 50 (orderSrvResult raw)
+            let weightLists = fmap (fmap snd4) actuals
+            let x = filter ((==20) . head) weightLists
+            let y = filter ((==10) . head) weightLists
+            -- we may have e.g.
+            -- the server with weight 20 first in the list 30 times,
+            -- the server with weight 10 15 times
+            -- and the server with weight 0 5 times.
+            -- We only check that there is *some* distribution that is not 50 times one order
+            length x `shouldSatisfy` (> 0)
+            length x `shouldSatisfy` (< 49)
+            length y `shouldSatisfy` (> 0)
+            length y `shouldSatisfy` (< 49)
 
 fst4 :: (a,b,c,d) -> a
 fst4 (a,_,_,_) = a
