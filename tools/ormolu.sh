fca7d06c833d266cd7ef9f72080eb7b7edd2d9df
Matthias Fischmann <mf@zerobuzz.net>
2020-02-15 21:42:48 +0100

    Work on shell script.

--- tools/ormolu.sh
+++ tools/ormolu.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 set -e
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-# TODO: change to stack project root directory.
-
+stack exec -- which ormolu >/dev/null || ( echo "please run stack install ormolu."; exit 1 )
 
 if [ "`git status -s src/ test/ | grep -v \?\?`" != "" ]; then
     echo "working copy not clean."
     if [ "$1" == "-f" ]; then
-        echo "running with -f.  THIS WILL DESTROY YOUR UNCOMMITTED CHANGES."
-        git reset --hard HEAD
+        echo "running with -f.  this will mix ormolu and other changes."
     else
-        echo "run with -f if you want to force changing the uncommitted files."
-        echo "WARNING: THIS WILL DESTROY YOUR UNCOMMITTED CHANGES."
+        echo "run with -f if you want to force mixing ormolu and other changes."
         exit 1
     fi
 fi
+
+export LANGUAGE_EXTS=$(perl -ne '$x=1 if /default-extensions:/?1:(/^[^-]/?0:$x); print "--ghc-opt -X$1 " if ($x && /^- (.+)/);' package-defaults.yaml)
 
 for i in `git ls-files | grep '\.hs$'`; do
     echo -n $i
@@ -23,9 +23,15 @@ for i in `git ls-files | grep '\.hs$'`; do
         echo "  *** ignored: -XCPP"
     else
         echo
-        stack exec -- ormolu $i || echo -e "*** ormolu failed on $i\n"
+        stack exec -- ormolu --mode inplace --check-idempotency ${LANGUAGE_EXTS} $i || echo -e "*** ormolu failed on $i\n"
     fi
 done
 
 
-# TODO: we may need to set the language extensions from package-defaults on the ormolu command line.
+
+
+# TODO: use getopts and avoid dirty-working-copy test if not needed.
+
+# TODO: have test param that makes the script pass if idempotency is reached, and fail if ormolu made changes.  (--mode check?)
+
+# TODO: isolate or remove CPP flag; consider going for the module name matching /CPP$/ and enforcing this as a convention.
