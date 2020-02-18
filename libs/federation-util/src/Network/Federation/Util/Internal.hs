ec2ffa7f07ae2010eb46a368208fbdb448870788
jschaul <jschaul@users.noreply.github.com>
2020-02-18 13:15:53 +0100

    three example files with a regular header; one manually-edited more complicated licensed file

--- libs/federation-util/src/Network/Federation/Util/Internal.hs
+++ libs/federation-util/src/Network/Federation/Util/Internal.hs
@@ -1,3 +1,53 @@
+-- Copyright (C) 2020 Wire Swiss GmbH. This program is free software: you can
+-- redistribute it and/or modify it under the terms of the GNU Affero General
+-- Public License as published by the Free Software Foundation, either version 3
+-- of  the License, or (at your option) any later version. This program is
+-- distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+-- without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+-- PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details. You should have received a copy of the GNU Affero General Public
+-- License along with this program. If not, see <https://www.gnu.org/licenses/>
+--
+-- Parts of this code, namely functions srvLookup'' and orderSrvResult,
+-- which were taken from http://hackage.haskell.org/package/pontarius-xmpp
+-- are also licensed under the three-clause BSD license:
+--
+-- Copyright © 2005-2011 Dmitry Astapov
+-- Copyright © 2005-2011 Pierre Kovalev
+-- Copyright © 2010-2011 Mahdi Abdinejadi
+-- Copyright © 2010-2013 Jon Kristensen
+-- Copyright © 2011      IETF Trust
+-- Copyright © 2012-2013 Philipp Balzarek
+
+-- All rights reserved.
+
+-- Pontarius XMPP is licensed under the three-clause BSD license.
+
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+
+-- * Redistributions of source code must retain the above copyright notice, this
+--   list of conditions and the following disclaimer.
+
+-- * Redistributions in binary form must reproduce the above copyright notice,
+--   this list of conditions and the following disclaimer in the documentation
+--   and/or other materials provided with the distribution.
+
+-- * Neither the name of the Pontarius project nor the names of its contributors
+--   may be used to endorse or promote products derived from this software without
+--   specific prior written permission.
+
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR THE PONTARIUS PROJECT BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+-- GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+-- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+-- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 module Network.Federation.Util.Internal where
 
 import Data.Text.Encoding (encodeUtf8)
