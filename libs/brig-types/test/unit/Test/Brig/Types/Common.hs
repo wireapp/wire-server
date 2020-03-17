931754f5e6ce2d9bff2eae1290a13a731d90faaa
Akshay Mankar <akshay@wire.com>
2020-03-17 16:06:08 +0100

    Extract helper for roundtrip tests for brig-types

--- libs/brig-types/test/unit/Test/Brig/Types/Common.hs
+++ libs/brig-types/test/unit/Test/Brig/Types/Common.hs
@@ -12,12 +12,11 @@ import Brig.Types.Team.LegalHold
 import Brig.Types.Test.Arbitrary ()
 import Control.Lens
 import Data.Aeson
-import Data.Aeson.Types
-import Data.Typeable (typeOf)
 import Galley.Types (CustomBackend)
 import Galley.Types.Teams
 import Galley.Types.Teams.SSO
 import Imports
+import Test.Brig.Roundtrip (testRoundTrip)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
@@ -28,49 +27,38 @@ tests :: TestTree
 tests =
   testGroup
     "Common (types vs. aeson)"
-    [ run @Name,
-      run @ColourId,
-      run @Email,
-      run @Phone,
-      run @UserIdentity,
-      run @UserSSOId,
-      run @AssetSize,
-      run @Asset,
-      run @ExcludedPrefix,
-      run @ManagedBy,
-      run @TeamMemberDeleteData,
-      run @LegalHoldStatus,
-      run @LegalHoldTeamConfig,
-      run @NewLegalHoldService,
-      run @LegalHoldService,
-      run @ViewLegalHoldService,
-      run @NewLegalHoldClient,
-      run @RequestNewLegalHoldClient,
-      run @UserLegalHoldStatusResponse,
-      run @LegalHoldServiceConfirm,
-      run @LegalHoldClientRequest,
-      run @RemoveLegalHoldSettingsRequest,
-      run @DisableLegalHoldForUserRequest,
-      run @ApproveLegalHoldForUserRequest,
-      run @SSOStatus,
-      run @SSOTeamConfig,
-      run @CustomBackend,
-      run @FeatureFlags,
-      run @TruncatedTeamSize,
+    [ testRoundTrip @Name,
+      testRoundTrip @ColourId,
+      testRoundTrip @Email,
+      testRoundTrip @Phone,
+      testRoundTrip @UserIdentity,
+      testRoundTrip @UserSSOId,
+      testRoundTrip @AssetSize,
+      testRoundTrip @Asset,
+      testRoundTrip @ExcludedPrefix,
+      testRoundTrip @ManagedBy,
+      testRoundTrip @TeamMemberDeleteData,
+      testRoundTrip @LegalHoldStatus,
+      testRoundTrip @LegalHoldTeamConfig,
+      testRoundTrip @NewLegalHoldService,
+      testRoundTrip @LegalHoldService,
+      testRoundTrip @ViewLegalHoldService,
+      testRoundTrip @NewLegalHoldClient,
+      testRoundTrip @RequestNewLegalHoldClient,
+      testRoundTrip @UserLegalHoldStatusResponse,
+      testRoundTrip @LegalHoldServiceConfirm,
+      testRoundTrip @LegalHoldClientRequest,
+      testRoundTrip @RemoveLegalHoldSettingsRequest,
+      testRoundTrip @DisableLegalHoldForUserRequest,
+      testRoundTrip @ApproveLegalHoldForUserRequest,
+      testRoundTrip @SSOStatus,
+      testRoundTrip @SSOTeamConfig,
+      testRoundTrip @CustomBackend,
+      testRoundTrip @FeatureFlags,
+      testRoundTrip @TruncatedTeamSize,
       testCase "{} is a valid TeamMemberDeleteData" $ do
         assertEqual "{}" (Right $ newTeamMemberDeleteData Nothing) (eitherDecode "{}")
     ]
-  where
-    run ::
-      forall a.
-      (Arbitrary a, Typeable a, ToJSON a, FromJSON a, Eq a, Show a) =>
-      TestTree
-    run = testProperty msg trip
-      where
-        msg = show $ typeOf (undefined :: a)
-        trip (v :: a) =
-          counterexample (show $ toJSON v) $
-            Right v === (parseEither parseJSON . toJSON) v
 
 instance Arbitrary TeamMemberDeleteData where
   arbitrary = newTeamMemberDeleteData <$> arbitrary
