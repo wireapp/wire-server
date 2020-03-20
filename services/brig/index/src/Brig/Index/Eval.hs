8874af0fa8d1ec91e2ec6416b6b45b7321407f55
Akshay Mankar <akshay@wire.com>
2020-03-20 11:37:25 +0100

    Make brig-index modules more namespaced

--- services/brig/index/src/Brig/Index/Eval.hs
+++ services/brig/index/src/Brig/Index/Eval.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Eval
+module Brig.Index.Eval
   ( runCommand,
   )
 where
 
+import Brig.Index.Options
 import Brig.User.Search.Index
 import qualified Cassandra as C
 import qualified Cassandra.Settings as C
@@ -15,7 +16,6 @@ import Data.Text.Strict.Lens
 import qualified Database.V5.Bloodhound as ES
 import Imports
 import Network.HTTP.Client
-import Options
 import qualified System.Logger as Log
 import System.Logger.Class (Logger, MonadLogger (..))
 import URI.ByteString
