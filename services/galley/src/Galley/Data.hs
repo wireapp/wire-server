a572ac1bcf99701c651b958971d6ae88efdb524c
Akshay Mankar <akshay@wire.com>
2020-04-28 16:40:51 +0200

    Add table to store billing team members

--- services/galley/src/Galley/Data.hs
+++ services/galley/src/Galley/Data.hs
@@ -170,7 +170,7 @@ resultSetResult :: ResultSet a -> [a]
 resultSetResult = result . page
 
 schemaVersion :: Int32
-schemaVersion = 37
+schemaVersion = 38
 
 -- | Insert a conversation code
 insertCode :: MonadClient m => Code -> m ()
