9991591cd2518724dc9168a65df29f2aaea588ad
Arian van Putten <arian.vanputten@gmail.com>
2020-05-04 16:52:08 +0200

    WIP: Run things with kind
    kind.sh is kind of assembled from ./cailliach/integration-setup.sh but
    is a bit simpler and works with Helm 3 instead.
    
    It should have a better place; and not do everything at the same time
    for better debugability

--- kind.sh
+++ kind.sh
@@ -1,13 +1,48 @@
 #!/usr/bin/env bash
 set -e
+# make docker-intermediate
+# make docker-services
 
 # higher not supported yet alas
-kind create cluster --image kindest/node:v1.15.0 || true
+kind create cluster --config ./kind.yaml --image kindest/node:v1.15.0 || true
 
-export KUBECONFIG="$(kind get kubeconfig-path)"
 
-services=(brig gundeck galley cannon proxy spar zauth nginz)
+# TODO would be easier to just use one image IMO
+services=(brig gundeck cargohold galley cannon proxy spar zauth nginz)
 for i in ${services[@]}; do
   kind load docker-image quay.io/wire/$i:local
+done
+
+# TODO brig option domain
+
+for i in $(seq 1 2); do
+  kubectl create namespace "fed${i}" || true
+done
+
+for i in $(seq 1 2); do
+  helm upgrade --install --namespace "fed${i}" fake-aws wire-develop/fake-aws --devel --wait --values ./values.yaml
+done
+for i in $(seq 1 2); do
+  helm upgrade --install --namespace "fed${i}" databases-ephemeral wire-develop/databases-ephemeral --devel --wait --values ./values.yaml
+done
+for i in $(seq 1 2); do
+  helm upgrade \
+    --install \
+    --atomic \
+    --namespace "fed${i}" \
+    wire-server wire-develop/wire-server \
+    --devel \
+    --wait \
+    --values ./values.yaml \
+    --set brig.config.optSettings.setCookieDomain="fed${i}.svc.cluster.local"
+
+  # wait for fakeSNS to create resources. TODO, cleaner: make
+  # initiate-fake-aws-sns a post hook. See cassandra-migrations chart for an
+  # example.
+  resourcesReady() {
+      SNS_POD=$(kubectl -n "fed${i}" get pods | grep fake-aws-sns | grep Running | awk '{print $1}')
+      kubectl -n "fed${i}" logs "$SNS_POD" -c initiate-fake-aws-sns | grep created
+  }
+  until resourcesReady; do echo 'waiting for SNS resources'; sleep 1; done
 done
 
