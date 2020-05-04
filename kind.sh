08e8aae511a4e34beb09b3cd5b148309af87814f
Arian van Putten <arian.vanputten@gmail.com>
2020-05-04 17:54:00 +0200

    Add links to issues

--- kind.sh
+++ kind.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -e
+
+# NOTE: Currently I just copy paste the relevant things to test this interactively. This script needs rewriting
+
+# Uncomment if you want to rebuild. Commented for now as building our docker containers is slow
 # make docker-intermediate
 # make docker-services
 
 # higher not supported yet alas
+# See  https://github.com/zinfra/backend-issues/issues/1231  https://github.com/zinfra/backend-issues/issues/1210
 kind create cluster --config ./kind.yaml --image kindest/node:v1.15.0 || true
 
 
 # TODO would be easier to just use one image IMO
+
 services=(brig gundeck cargohold galley cannon proxy spar zauth nginz)
 for i in ${services[@]}; do
   kind load docker-image quay.io/wire/$i:local
@@ -20,10 +26,10 @@ for i in $(seq 1 2); do
 done
 
 for i in $(seq 1 2); do
-  helm upgrade --install --namespace "fed${i}" fake-aws wire-develop/fake-aws --devel --wait --values ./values.yaml
+  helm upgrade --install --namespace "fed${i}" fake-aws wire-develop/fake-aws --devel --wait --values ./values.yaml --wait
 done
 for i in $(seq 1 2); do
-  helm upgrade --install --namespace "fed${i}" databases-ephemeral wire-develop/databases-ephemeral --devel --wait --values ./values.yaml
+  helm upgrade --install --namespace "fed${i}" databases-ephemeral wire-develop/databases-ephemeral --devel --wait --values ./values.yaml --wait
 done
 for i in $(seq 1 2); do
   helm upgrade \
