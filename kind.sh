80e28b53628e78566a8508fd9777070ecb9f56aa
Arian van Putten <arian.vanputten@gmail.com>
2020-05-11 13:06:59 +0200

    Run integration tests too, and clean up the script a bit
    Also make startup times of cassandra a bit less slow an parallelise some
    of the scripts (E.g. no unneeded --waits)
    
    Integration tests are failing due to lack of SES. not sure why?

--- kind.sh
+++ kind.sh
@@ -13,24 +13,29 @@ kind create cluster --config ./kind.yaml --image kindest/node:v1.15.0 || true
 
 
 # TODO would be easier to just use one image IMO
-
 services=(brig gundeck cargohold galley cannon proxy spar zauth nginz)
 for i in ${services[@]}; do
   kind load docker-image quay.io/wire/$i:local
+  kind load docker-image quay.io/wire/$i-integration:local || true # some components dont have integration tests? ignore: HACK
 done
-
-# TODO brig option domain
 
 for i in $(seq 1 2); do
   kubectl create namespace "fed${i}" || true
 done
 
 for i in $(seq 1 2); do
-  helm upgrade --install --namespace "fed${i}" fake-aws wire-develop/fake-aws --devel --wait --values ./values.yaml --wait
+  helm upgrade --install --namespace "fed${i}" fake-aws wire-develop/fake-aws --devel --values ./values.yaml
 done
 for i in $(seq 1 2); do
-  helm upgrade --install --namespace "fed${i}" databases-ephemeral wire-develop/databases-ephemeral --devel --wait --values ./values.yaml --wait
+  helm upgrade --install --namespace "fed${i}" databases-ephemeral wire-develop/databases-ephemeral --devel --values ./values.yaml
 done
+
+
+# Needed because our services fail to start unless cassandra is up.
+for i in $(seq 1 2); do
+  kubectl -n "fed${i}" wait --for=condition=Ready pod --selector app=cassandra-ephemeral
+done
+
 for i in $(seq 1 2); do
   helm upgrade \
     --install \
@@ -42,13 +47,15 @@ for i in $(seq 1 2); do
     --values ./values.yaml \
     --set brig.config.optSettings.setCookieDomain="fed${i}.svc.cluster.local"
 
-  # wait for fakeSNS to create resources. TODO, cleaner: make
-  # initiate-fake-aws-sns a post hook. See cassandra-migrations chart for an
-  # example.
-  resourcesReady() {
-      SNS_POD=$(kubectl -n "fed${i}" get pods | grep fake-aws-sns | grep Running | awk '{print $1}')
-      kubectl -n "fed${i}" logs "$SNS_POD" -c initiate-fake-aws-sns | grep created
-  }
-  until resourcesReady; do echo 'waiting for SNS resources'; sleep 1; done
+
 done
+
+# More elegant way for the "resourcesReady" function. Waits tils fake-aws-sns is initialised
+for i in $(seq 1 2); do
+  kubectl wait -n "fed${i}" --for=condition=Ready pod --selector app=fake-aws-sns
+done
+
+
+helm test -n fed1 wire-server --logs
+
 
