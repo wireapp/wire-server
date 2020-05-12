5c7b5f0046e376e94ece31d58fd0d91a1864d1eb
Arian van Putten <arian.vanputten@gmail.com>
2020-05-12 19:09:04 +0200

    Only make setup-kind.sh do kind-specific things
    All the other code could be used for e.g. CI as well. We'll factor it
    out later

--- setup-kind.sh
+++ setup-kind.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Sets up a local kubernetes cluster using kind and sets the kubectl context to
+# talk to it.
 set -e
 
 kind create cluster --config ./kind.yaml --image kindest/node:v1.15.0 || (echo "Try 'kind delete cluster' first." && exit 1)
@@ -28,41 +30,6 @@ for i in ${services[@]}; do
   kind load docker-image quay.io/wire/$i-integration:local || true # some components dont have integration tests? ignore: HACK
 done
 
-for i in $(seq 1 2); do
-  kubectl create namespace "fed${i}" || true
-done
 
-for i in $(seq 1 2); do
-  helm upgrade --install --namespace "fed${i}" fake-aws wire-develop/fake-aws --devel --values ./values.yaml
-done
-for i in $(seq 1 2); do
-  helm upgrade --install --namespace "fed${i}" databases-ephemeral wire-develop/databases-ephemeral --devel --values ./values.yaml
-done
-
-
-# Needed because our services fail to start unless cassandra is up.
-# Can't use condition=available on statefulsets due to upstream bug
-# https://github.com/kubernetes/kubernetes/issues/79606
-for i in $(seq 1 2); do
-  kubectl --namespace "fed${i}" wait --for=condition=Ready pod --selector app=cassandra-ephemeral --timeout=90s
-done
-
-for i in $(seq 1 2); do
-  helm upgrade \
-    --install \
-    --atomic \
-    --namespace "fed${i}" \
-    wire-server wire-develop/wire-server \
-    --devel \
-    --values ./values.yaml \
-    --set brig.config.optSettings.setCookieDomain="fed${i}.svc.cluster.local"
-    # TODO: Override federation-specific config here?
-done
-
-# More elegant way for the "resourcesReady" function. Waits tils fake-aws-sns is initialised
-for i in $(seq 1 2); do
-  kubectl wait --namespace "fed${i}" --for=condition=available deployment --selector release=wire-server
-  kubectl wait --namespace "fed${i}" --for=condition=complete job --selector release=wire-server
-  kubectl wait --namespace "fed${i}" --for=condition=Ready pod --selector app=fake-aws-sns
-done
-
+# Kind _should_ already set this but still to it to be sure
+kubectl config use-context kind-kind
