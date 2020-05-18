383a20ce9229cb29382960bfca1ff3555648b16f
Matthias Heinzel <mheinzel@posteo.de>
2020-05-18 12:18:07 +0200

    remove copied tests

--- libs/brig-types/test/unit/Test/Brig/Types/Common.hs
+++ libs/brig-types/test/unit/Test/Brig/Types/Common.hs
@@ -27,18 +27,12 @@ module Test.Brig.Types.Common where
 import Brig.Types.Common
 import Brig.Types.Team.LegalHold
 import Brig.Types.Test.Arbitrary ()
-import Data.Aeson
-import Galley.Types
 import Galley.Types.Teams
-import Galley.Types.Teams.SSO
-import Galley.Types.Teams.SearchVisibility
 import Imports
 import Test.Brig.Roundtrip (testRoundTrip)
 import qualified Test.QuickCheck as QC
 import Test.Tasty
-import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
-import Wire.API.Arbitrary ()
 
 -- NB: validateEveryToJSON from servant-swagger doesn't render these tests unnecessary!
 
@@ -46,40 +40,11 @@ tests :: TestTree
 tests =
   testGroup
     "Common (types vs. aeson)"
-    [ testRoundTrip @Name,
-      testRoundTrip @ColourId,
-      testRoundTrip @Email,
-      testRoundTrip @Phone,
-      testRoundTrip @UserIdentity,
-      testRoundTrip @UserSSOId,
-      testRoundTrip @AssetSize,
-      testRoundTrip @Asset,
+    [ --
       testRoundTrip @ExcludedPrefix,
-      testRoundTrip @ManagedBy,
-      testRoundTrip @TeamMemberDeleteData,
-      testRoundTrip @LegalHoldStatus,
-      testRoundTrip @LegalHoldTeamConfig,
-      testRoundTrip @NewLegalHoldService,
       testRoundTrip @LegalHoldService,
-      testRoundTrip @ViewLegalHoldService,
-      testRoundTrip @NewLegalHoldClient,
-      testRoundTrip @RequestNewLegalHoldClient,
-      testRoundTrip @UserLegalHoldStatusResponse,
-      testRoundTrip @LegalHoldServiceConfirm,
       testRoundTrip @LegalHoldClientRequest,
-      testRoundTrip @RemoveLegalHoldSettingsRequest,
-      testRoundTrip @DisableLegalHoldForUserRequest,
-      testRoundTrip @ApproveLegalHoldForUserRequest,
-      testRoundTrip @SSOStatus,
-      testRoundTrip @SSOTeamConfig,
-      testRoundTrip @TeamSearchVisibility,
-      testRoundTrip @TeamSearchVisibilityView,
-      testRoundTrip @TeamSearchVisibilityAvailable,
-      testRoundTrip @TeamSearchVisibilityAvailableView,
-      testRoundTrip @CustomBackend,
-      testRoundTrip @FeatureFlags,
-      testCase "{} is a valid TeamMemberDeleteData" $ do
-        assertEqual "{}" (Right $ newTeamMemberDeleteData Nothing) (eitherDecode "{}")
+      testRoundTrip @FeatureFlags
     ]
 
 instance Arbitrary FeatureFlags where
