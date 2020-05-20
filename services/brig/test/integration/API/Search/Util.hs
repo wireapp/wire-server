3f95562ccecfced7032e22d4bd3dd542463793f7
Akshay Mankar <akshay@wire.com>
2020-05-20 19:13:54 +0200

    Ormolu

--- services/brig/test/integration/API/Search/Util.hs
+++ services/brig/test/integration/API/Search/Util.hs
@@ -49,7 +49,7 @@ reindex :: (Monad m, MonadCatch m, MonadIO m, MonadHttp m, HasCallStack) => Brig
 reindex brig =
   post (brig . path "/i/index/reindex") !!! const 200 === statusCode
 
-assertCanFindByName :: (MonadCatch m, MonadIO m, MonadHttp m, HasCallStack) => Brig -> User -> User  -> m ()
+assertCanFindByName :: (MonadCatch m, MonadIO m, MonadHttp m, HasCallStack) => Brig -> User -> User -> m ()
 assertCanFindByName brig self expected =
   assertCanFind brig (userId self) (userId expected) (fromName $ userDisplayName expected)
 
