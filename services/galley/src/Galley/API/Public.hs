65bbe62d1b35c3df7818205f4bc3ec2dbfd6df9a
Matthias Fischmann <mf@zerobuzz.net>
2020-05-27 10:58:14 +0200

    Fixup.

--- services/galley/src/Galley/API/Public.hs
+++ services/galley/src/Galley/API/Public.hs
@@ -211,19 +211,22 @@ sitemap = do
       \\n\
       \Note that `/teams/notifications` behaves different from `/notifications`:\
       \\n\
-      \- Team queues do not respond with status 404 and valid data in the body if there \
-      \is a gap between the notification id requested with `since` and the available data.\
+      \- If there is a gap between the notification id requested with `since` and the \
+      \available data, team queues respond with 200 and the data that could be found. \
+      \The do NOT respond with status 404, but valid data in the body.\
       \\n\
-      \- Instead, the notification with the id given via `since` is included in the \
+      \- The notification with the id given via `since` is included in the \
       \response if it exists.  You should remove this and only use it to decide whether \
       \there was a gap between your last request and this one.\
       \\n\
-      \- If the UUIDv1 does *not* exist, you get the more recent events from the queue \
-      \(instead of all of them).  This way, if you have never requested the queue before and \
-      \have no prior notification id, just pull with timestamp 'now'.\
+      \- If the notification id does *not* exist, you get the more recent events from the queue \
+      \(instead of all of them).  This can be done because a notification id is a UUIDv1, which \
+      \is essentially a time stamp.\
       \\n\
-      \- There is no corresponding `/last`-end-point to get only the most recent event. \
-      \That end-point was only useful to avoid having to pull the entire queue (see above)."
+      \- There is no corresponding `/last` end-point to get only the most recent event. \
+      \That end-point was only useful to avoid having to pull the entire queue.  In team \
+      \queues, if you have never requested the queue before and \
+      \have no prior notification id, just pull with timestamp 'now'."
     parameter Query "since" bytes' $ do
       optional
       description "Notification id to start with in the response (UUIDv1)"
