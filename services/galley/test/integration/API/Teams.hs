4e27281c200d5611e8402842eef6bfe01efbe00a
Matthias Fischmann <mf@zerobuzz.net>
2020-06-05 21:00:49 +0200

    FUTUREWORK (refactoring).

--- services/galley/test/integration/API/Teams.hs
+++ services/galley/test/integration/API/Teams.hs
@@ -1878,6 +1878,11 @@ postCryptoBroadcastMessage100OrMaxConns = do
 newTeamMember' :: Permissions -> UserId -> TeamMember
 newTeamMember' perms uid = newTeamMember uid perms Nothing
 
+-- FUTUREWORK: all client functions calling @{/i,}/teams/*/features/*@ can be replaced by
+-- hypothetical functions 'getTeamFeatureFlag', 'getTeamFeatureFlagInternal',
+-- 'putTeamFeatureFlagInternal'.  Since these functions all work in slightly different mondas
+-- and with different kinds of internal checks, it's quite tedious to do so.
+
 getSSOEnabled :: HasCallStack => UserId -> TeamId -> TestM ResponseLBS
 getSSOEnabled uid tid = do
   g <- view tsGalley
