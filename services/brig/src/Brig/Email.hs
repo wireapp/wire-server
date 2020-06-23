b359a1d64d14614fd4dd146e068a7db6a3eba45a
Matthias Heinzel <mheinzel@posteo.de>
2020-06-23 17:41:50 +0200

    move validateDomain

--- services/brig/src/Brig/Email.hs
+++ services/brig/src/Brig/Email.hs
@@ -43,49 +43,16 @@ import qualified Brig.AWS as AWS
 import Brig.App (AppIO, awsEnv, smtpEnv)
 import qualified Brig.SMTP as SMTP
 import Brig.Types
-import Control.Applicative (optional)
 import Control.Lens (view)
-import Data.Attoparsec.ByteString.Char8
 import qualified Data.Text as Text
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Imports
 import Network.Mail.Mime
-import qualified Text.Email.Validate as Email
 
 -------------------------------------------------------------------------------
 sendMail :: Mail -> AppIO ()
 sendMail m = view smtpEnv >>= \case
   Just smtp -> SMTP.sendMail smtp m
   Nothing -> view awsEnv >>= \e -> AWS.execute e $ AWS.sendMail m
-
--- Validation
-
--- | (Check out 'mkEmailKey' if you wonder about equality of emails with @+@ in their local part.)
-validateEmail :: Email -> Either String Email
-validateEmail (fromEmail -> e) =
-  validateLength
-    >>= Email.validate
-    >>= validateDomain
-    >>= pure . mkEmail
-  where
-    len = Text.length e
-    validateLength
-      | len <= 100 = Right $ encodeUtf8 e
-      | otherwise = Left $ "length " <> show len <> " exceeds 100"
-    -- cf. https://en.wikipedia.org/wiki/Email_address#Domain
-    -- n.b. We do not allow IP address literals, comments or non-ASCII
-    --      characters, mostly because SES (and probably many other mail
-    --      systems) don't support that (yet?) either.
-    validateDomain e' = parseOnly parser (Email.domainPart e')
-      where
-        parser = label *> many1 (char '.' *> label) *> endOfInput *> pure e'
-        label =
-          satisfy (inClass "a-zA-Z0-9")
-            *> count 61 (optional (satisfy (inClass "-a-zA-Z0-9")))
-            *> optional (satisfy (inClass "a-zA-Z0-9"))
-    mkEmail v = Email (mkLocal v) (mkDomain v)
-    mkLocal = decodeUtf8 . Email.localPart
-    mkDomain = decodeUtf8 . Email.domainPart
 
 -------------------------------------------------------------------------------
 -- Unique Keys
