7101bd9c9e559a839a8a8d50ef0897d0ffa4eeaa
jschaul <jschaul@users.noreply.github.com>
2020-07-28 22:47:23 +0200

    merge TURN into Calling

--- services/brig/src/Brig/Calling.hs
+++ services/brig/src/Brig/Calling.hs
@@ -18,10 +18,15 @@
 module Brig.Calling where
 
 import Brig.Options (SFTOptions (..))
+import Brig.Types (TurnURI)
+import Control.Lens
 import Data.List.NonEmpty
+import Data.List1
 import Imports
 import qualified Network.DNS as DNS
+import OpenSSL.EVP.Digest (Digest)
 import Polysemy
+import System.Random.MWC (GenIO, createSystemRandom)
 import Wire.Network.DNS.Effect
 import Wire.Network.DNS.SRV
 
@@ -61,3 +66,19 @@ mkSFTEnv opts =
 startSFTServiceDiscovery :: SFTEnv -> IO ()
 startSFTServiceDiscovery =
   runM . runDNSLookupDefault . sftDiscoveryLoop
+
+-- TURN specific
+
+data Env = Env
+  { _turnServers :: List1 TurnURI,
+    _turnTokenTTL :: Word32,
+    _turnConfigTTL :: Word32,
+    _turnSecret :: ByteString,
+    _turnSHA512 :: Digest,
+    _turnPrng :: GenIO
+  }
+
+makeLenses ''Env
+
+newEnv :: Digest -> List1 TurnURI -> Word32 -> Word32 -> ByteString -> IO Env
+newEnv sha512 srvs tTTL cTTL secret = Env srvs tTTL cTTL secret sha512 <$> createSystemRandom
