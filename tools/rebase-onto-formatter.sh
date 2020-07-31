2a7a19b10e44ec3369245ba6b3608a7d0f320546
Matthias Heinzel <mheinzel@posteo.de>
2020-07-31 14:59:18 +0200

    reverse order of arguments

--- tools/rebase-onto-formatter.sh
+++ tools/rebase-onto-formatter.sh
@@ -8,15 +8,17 @@ BASE_COMMIT=${1:-}
 TARGET_COMMIT=${2:-}
 FORMATTING_COMMAND='make formatf'
 USAGE="
-USAGE: $0 BASE_COMMIT TARGET_COMMIT
-    BASE_COMMIT:
+USAGE: $0 TARGET_COMMIT BASE_COMMIT
+
     TARGET_COMMIT:
+        The commit introducing the formatting that you want to rebase onto.
+    BASE_COMMIT:
+        A commit very similar to TARGET_COMMIT, just that the automated formatting changes are not applied yet.
+        It has to include changes to formatting version and config already.
 
 Rebase a branch onto changes created by an automated formatter. The script
 will keep the (linear) history of the branch intact and make the commits appear
 as if the changes had been applied onto the newly-formatted version all along.
-
-TODO: explain base commit etc.
 
 INSTRUCTIONS:
 1. Make a copy of your branch (or be prepared to salvage it from reflog).
