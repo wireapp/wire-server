7aefaa3d97fdce1b260ac765d44dbb85b4329a1e
Matthias Fischmann <mf@zerobuzz.net>
2020-08-03 11:38:33 +0200

    Recover "Leave domain empty in cookies. (#1076)"
    This reverts commit 14fb65f6035c5569259bb1b19ca7f82e524bd574.

--- services/brig/src/Brig/Provider/API.hs
+++ services/brig/src/Brig/Provider/API.hs
@@ -1045,7 +1045,7 @@ setProviderCookie t r = do
       Cookie.def
         { Cookie.setCookieName = "zprovider",
           Cookie.setCookieValue = toByteString' t,
-          Cookie.setCookieDomain = Just $ Text.encodeUtf8 . setCookieDomain $ s,
+          Cookie.setCookieDomain = Nothing,
           Cookie.setCookiePath = Just "/provider",
           Cookie.setCookieExpires = Just (ZAuth.tokenExpiresUTC t),
           Cookie.setCookieSecure = not (setCookieInsecure s),
