1af02e73a33cc7cde9001c33d03833399c9eb47d
Matthias Fischmann <mf@zerobuzz.net>
2020-08-04 20:59:49 +0200

    Stronger event types.

--- services/brig/src/Brig/User/Event.hs
+++ services/brig/src/Brig/User/Event.hs
@@ -25,29 +25,43 @@ import Data.Id
 import Imports
 
 data Event
-  = UserEvent !UserEvent
-  | ConnectionEvent !ConnectionEvent
+  = ConnectionEvent !ConnectionEvent
   | PropertyEvent !PropertyEvent
   | ClientEvent !ClientEvent
 
-data UserEvent
-  = UserCreated !User
-  | -- | A user is activated when the first user identity (email address or phone number)
-    -- is verified. {#RefActivationEvent}
-    UserActivated !User
-  | -- | Account & API access of a user has been suspended.
-    UserSuspended !UserId
-  | -- | Account & API access of a previously suspended user
-    -- has been restored.
-    UserResumed !UserId
-  | -- | The user account has been deleted.
-    UserDeleted !UserId
-  | UserUpdated !UserUpdatedData
-  | UserIdentityUpdated !UserIdentityUpdatedData
-  | UserIdentityRemoved !UserIdentityRemovedData
-  | UserLegalHoldDisabled !UserId
-  | UserLegalHoldEnabled !UserId
-  | LegalHoldClientRequested LegalHoldClientRequestedData
+data UserCreated = UserCreated !User
+
+-- | A user is activated when the first user identity (email address or phone number)
+-- is verified. {#RefActivationEvent}
+data UserActivated = UserActivated !User
+
+-- | Account & API access of a user has been suspended.
+data UserSuspended = UserSuspended !UserId
+
+-- | Account & API access of a previously suspended user
+-- has been restored.
+data UserResumed = UserResumed !UserId
+
+-- | The user account has been deleted.
+data UserDeleted = UserDeleted !UserId
+
+data UserNameUpdated = UserNameUpdated UserId Name
+
+data UserHandleUpdated = UserHandleUpdated UserId Handle
+
+data UserLocaleUpdated = UserLocaleUpdated UserId Locale
+
+-- ...  (there were a few more updates, but they can be added when we actually do this.)
+
+data UserIdentityUpdated = UserIdentityUpdated !UserIdentityUpdatedData
+
+data UserIdentityRemoved = UserIdentityRemoved !UserIdentityRemovedData
+
+data UserLegalHoldDisabled = UserLegalHoldDisabled !UserId
+
+data UserLegalHoldEnabled = UserLegalHoldEnabled !UserId
+
+data LegalHoldClientRequested = LegalHoldClientRequested LegalHoldClientRequestedData
 
 data ConnectionEvent = ConnectionUpdated
   { ucConn :: !UserConnection,
@@ -63,19 +77,6 @@ data PropertyEvent
 data ClientEvent
   = ClientAdded !UserId !Client
   | ClientRemoved !UserId !Client
-
-data UserUpdatedData = UserUpdatedData
-  { eupId :: !UserId,
-    eupName :: !(Maybe Name),
-    -- | DEPRECATED
-    eupPict :: !(Maybe Pict),
-    eupAccentId :: !(Maybe ColourId),
-    eupAssets :: !(Maybe [Asset]),
-    eupHandle :: !(Maybe Handle),
-    eupLocale :: !(Maybe Locale),
-    eupManagedBy :: !(Maybe ManagedBy)
-  }
-  deriving stock (Show)
 
 data UserIdentityUpdatedData = UserIdentityUpdatedData
   { eiuId :: !UserId,
@@ -98,75 +99,16 @@ data LegalHoldClientRequestedData = LegalHoldClientRequestedData
   }
   deriving stock (Show)
 
-emailRemoved :: UserId -> Email -> UserEvent
-emailRemoved u e =
-  UserIdentityRemoved $ UserIdentityRemovedData u (Just e) Nothing
-
-phoneRemoved :: UserId -> Phone -> UserEvent
-phoneRemoved u p =
-  UserIdentityRemoved $ UserIdentityRemovedData u Nothing (Just p)
-
-emailUpdated :: UserId -> Email -> UserEvent
-emailUpdated u e =
-  UserIdentityUpdated $ UserIdentityUpdatedData u (Just e) Nothing
-
-phoneUpdated :: UserId -> Phone -> UserEvent
-phoneUpdated u p =
-  UserIdentityUpdated $ UserIdentityUpdatedData u Nothing (Just p)
-
-handleUpdated :: UserId -> Handle -> UserEvent
-handleUpdated u h =
-  UserUpdated $ (emptyUserUpdatedData u) {eupHandle = Just h}
-
-localeUpdate :: UserId -> Locale -> UserEvent
-localeUpdate u loc =
-  UserUpdated $ (emptyUserUpdatedData u) {eupLocale = Just loc}
-
-managedByUpdate :: UserId -> ManagedBy -> UserEvent
-managedByUpdate u mb =
-  UserUpdated $ (emptyUserUpdatedData u) {eupManagedBy = Just mb}
-
-profileUpdated :: UserId -> UserUpdate -> UserEvent
-profileUpdated u UserUpdate {..} =
-  UserUpdated $
-    (emptyUserUpdatedData u)
-      { eupName = uupName,
-        eupPict = uupPict,
-        eupAccentId = uupAccentId,
-        eupAssets = uupAssets
-      }
-
-emptyUpdate :: UserId -> UserEvent
-emptyUpdate = UserUpdated . emptyUserUpdatedData
-
-emptyUserUpdatedData :: UserId -> UserUpdatedData
-emptyUserUpdatedData u =
-  UserUpdatedData
-    { eupId = u,
-      eupName = Nothing,
-      eupPict = Nothing,
-      eupAccentId = Nothing,
-      eupAssets = Nothing,
-      eupHandle = Nothing,
-      eupLocale = Nothing,
-      eupManagedBy = Nothing
-    }
-
 connEventUserId :: ConnectionEvent -> UserId
 connEventUserId ConnectionUpdated {..} = ucFrom ucConn
 
-userEventUserId :: UserEvent -> UserId
-userEventUserId (UserCreated u) = userId u
-userEventUserId (UserActivated u) = userId u
-userEventUserId (UserSuspended u) = u
-userEventUserId (UserResumed u) = u
-userEventUserId (UserDeleted u) = u
-userEventUserId (UserUpdated u) = eupId u
-userEventUserId (UserIdentityUpdated u) = eiuId u
-userEventUserId (UserIdentityRemoved u) = eirId u
-userEventUserId (UserLegalHoldDisabled uid) = uid
-userEventUserId (UserLegalHoldEnabled uid) = uid
-userEventUserId (LegalHoldClientRequested dat) = lhcTargetUser dat
+class Typeable a => IsUserEvent a where
+  userEventUserId :: a -> UserId
+
+instance IsUserEvent UserCreated where
+  userEventUserId = undefined
+
+-- ...
 
 propEventUserId :: PropertyEvent -> UserId
 propEventUserId (PropertySet u _ _) = u
--- services/brig/src/Brig/User/Event/Log.hs
+++ services/brig/src/Brig/User/Event/Log.hs
@@ -22,7 +22,6 @@ module Brig.User.Event.Log where
 import Brig.User.Event
 import Data.ByteString.Conversion
 import Data.Id
-import Imports
 import System.Logger.Class
 
 connection :: UserId -> UserId -> Msg -> Msg
@@ -31,23 +30,14 @@ connection from to =
     ~~ "connection.to" .= toByteString to
 
 instance ToBytes Event where
-  bytes (UserEvent e) = bytes e
   bytes (ConnectionEvent e) = bytes e
   bytes (PropertyEvent e) = bytes e
   bytes (ClientEvent e) = bytes e
 
-instance ToBytes UserEvent where
+instance ToBytes UserCreated where
   bytes e@UserCreated {} = val "user.new: " +++ toByteString (userEventUserId e)
-  bytes e@UserActivated {} = val "user.activate: " +++ toByteString (userEventUserId e)
-  bytes e@UserUpdated {} = val "user.update: " +++ toByteString (userEventUserId e)
-  bytes e@UserIdentityUpdated {} = val "user.update: " +++ toByteString (userEventUserId e)
-  bytes e@UserIdentityRemoved {} = val "user.identity-remove: " +++ toByteString (userEventUserId e)
-  bytes e@UserSuspended {} = val "user.suspend: " +++ toByteString (userEventUserId e)
-  bytes e@UserResumed {} = val "user.resume: " +++ toByteString (userEventUserId e)
-  bytes e@UserDeleted {} = val "user.delete: " +++ toByteString (userEventUserId e)
-  bytes e@UserLegalHoldDisabled {} = val "user.legalhold-disable: " +++ toByteString (userEventUserId e)
-  bytes e@UserLegalHoldEnabled {} = val "user.legalhold-enable: " +++ toByteString (userEventUserId e)
-  bytes (LegalHoldClientRequested payload) = val "user.legalhold-request: " +++ show payload
+
+-- ...
 
 instance ToBytes ConnectionEvent where
   bytes e@ConnectionUpdated {} = val "user.connection: " +++ toByteString (connEventUserId e)
