ff107d39642aaef61eebbb3bbb417264da2ad9a2
Matthias Fischmann <mf@zerobuzz.net>
2020-10-12 10:28:37 +0200

    Fix log message field labels; include payload on debug lvl.

--- services/gundeck/src/Gundeck/Push/Native.hs
+++ services/gundeck/src/Gundeck/Push/Native.hs
@@ -68,10 +68,7 @@ push1 m a = do
     Success _ -> do
       Log.debug $
         field "user" (toByteString (a ^. addrUser))
-          ~~ field "arn" (toText (a ^. addrEndpoint))
-          ~~ field "notification_id" (toText (npNotificationid m))
-          ~~ field "prio" (show (npPriority m))
-          ~~ field "payload" (encode (npApsData m))
+          ~~ field "notificationId" (toText (npNotificationid m))
           ~~ Log.msg (val "Native push success")
       view monitor >>= counterIncr (path "push.native.success")
     Failure EndpointDisabled _ -> onDisabled
@@ -123,6 +120,14 @@ publish m a = flip catches pushException $ do
       uid = a ^. addrUser
       transp = a ^. addrTransport
   txt <- liftIO $ serialise m (uid, transp)
+  Log.debug $
+    field "user" (toByteString (a ^. addrUser))
+      ~~ field "arn" (toText (a ^. addrEndpoint))
+      ~~ field "notificationId" (toText (npNotificationid m))
+      ~~ field "prio" (show (npPriority m))
+      ~~ field "apsData" (encode (npApsData m))
+      ~~ field "payload" (show txt)
+      ~~ Log.msg (val "Native push")
   case txt of
     Left f -> return $! Failure f a
     Right v -> toResult <$> Aws.publish ept v mempty
