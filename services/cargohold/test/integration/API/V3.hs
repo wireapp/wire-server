wireapp/wire-server

commit 66f69a0a88181a7daa6bfd2157ecd222fabc540e

remove Content-MD5 header for asset upload (#1247)

libs/wire-api/src/Wire/API/Asset/V3.hs
@@ -59,11 +59,8 @@ where
 
 import qualified Codec.MIME.Type as MIME
 import Control.Lens (makeLenses)
-import Crypto.Hash (Digest, MD5, hashlazy)
 import Data.Aeson
 import Data.Attoparsec.ByteString.Char8
-import qualified Data.ByteArray as B
-import qualified Data.ByteString.Base64 as B64
 import Data.ByteString.Builder
 import Data.ByteString.Conversion
 import qualified Data.ByteString.Lazy as LBS
@@ -185,7 +182,7 @@ buildMultipartBody sets typ bs =
 -- | Begin building a @multipart/mixed@ request body for a non-resumable upload.
 -- The returned 'Builder' can be immediately followed by the actual asset bytes.
 beginMultipartBody :: AssetSettings -> AssetHeaders -> Builder
-beginMultipartBody sets (AssetHeaders t l d) =
+beginMultipartBody sets (AssetHeaders t l) =
   byteString
     "--frontier\r\n\
     \Content-Type: application/json\r\n\
@@ -205,11 +202,7 @@ beginMultipartBody sets (AssetHeaders t l d) =
       \Content-Length: "
     <> wordDec l
     <> "\r\n\
-       \Content-MD5: "
-    <> byteString (B64.encode (B.convert d))
-    <> byteString
-      "\r\n\
-      \\r\n"
+       \\r\n"
   where
     settingsJson = encode sets
 
@@ -224,12 +217,11 @@ endMultipartBody = byteString "\r\n--frontier--\r\n"
 -- | Headers provided during upload.
 data AssetHeaders = AssetHeaders
   { hdrType :: MIME.Type,
-    hdrLength :: Word,
-    hdrMD5 :: Digest MD5
+    hdrLength :: Word
   }
 
 mkHeaders :: MIME.Type -> LByteString -> AssetHeaders
-mkHeaders t b = AssetHeaders t (fromIntegral (LBS.length b)) (hashlazy b)
+mkHeaders t b = AssetHeaders t (fromIntegral (LBS.length b))
 
 --------------------------------------------------------------------------------
 -- AssetSettings

services/cargohold/src/CargoHold/API/V3.hs
@@ -40,11 +40,9 @@ import Control.Applicative (optional)
 import Control.Error
 import Control.Lens (set, view, (^.))
 import Control.Monad.Trans.Resource
-import Crypto.Hash
 import Crypto.Random (getRandomBytes)
 import Data.Aeson (eitherDecodeStrict')
 import Data.Attoparsec.ByteString.Char8
-import qualified Data.ByteString.Base64 as B64
 import qualified Data.CaseInsensitive as CI
 import Data.Conduit
 import qualified Data.Conduit.Attoparsec as Conduit
@@ -171,13 +169,13 @@ assetHeaders :: Parser AssetHeaders
 assetHeaders =
   eol
     *> boundary
-    *> (headers [hContentType, hContentLength, hContentMD5] >>= go)
+    *> (headers [hContentType, hContentLength] >>= go)
     <* eol
   where
     go hdrs =
-      AssetHeaders <$> contentType hdrs
+      AssetHeaders
+        <$> contentType hdrs
         <*> contentLength hdrs
-        <*> contentMD5 hdrs
 
 contentType :: [(HeaderName, ByteString)] -> Parser MIME.Type
 contentType hdrs =
@@ -192,13 +190,6 @@ contentLength hdrs =
     (fail "Missing Content-Type")
     (either fail return . parseOnly decimal)
     (lookup (CI.mk "Content-Length") hdrs)
-
-contentMD5 :: [(HeaderName, ByteString)] -> Parser (Digest MD5)
-contentMD5 hdrs =
-  maybe
-    (fail "Missing Content-MD5")
-    (maybe (fail "Invalid Content-MD5") return . digestFromByteString . B64.decodeLenient)
-    (lookup (CI.mk "Content-MD5") hdrs)
 
 boundary :: Parser ()
 boundary =

services/cargohold/src/CargoHold/S3.hs
@@ -80,7 +80,6 @@ import qualified Data.UUID as UUID
 import Imports
 import Network.AWS hiding (Error)
 import Network.AWS.Data.Body
-import Network.AWS.Data.Crypto
 import Network.AWS.S3
 import Network.Wai.Utilities.Error (Error (..))
 import Safe (readMay)
@@ -106,7 +105,7 @@ uploadV3 ::
   Maybe V3.AssetToken ->
   Conduit.ConduitM () ByteString (ResourceT IO) () ->
   ExceptT Error App ()
-uploadV3 prc (s3Key . mkKey -> key) (V3.AssetHeaders ct cl md5) tok src = do
+uploadV3 prc (s3Key . mkKey -> key) (V3.AssetHeaders ct cl) tok src = do
   Log.info $
     "remote" .= val "S3"
       ~~ "asset.owner" .= toByteString prc
@@ -124,11 +123,9 @@ uploadV3 prc (s3Key . mkKey -> key) (V3.AssetHeaders ct cl md5) tok src = do
         -- Ignore any 'junk' after the content; take only 'cl' bytes.
         .| Conduit.isolate (fromIntegral cl)
     reqBdy = ChunkedBody defaultChunkSize (fromIntegral cl) stream
-    md5Res = Text.decodeLatin1 $ digestToBase Base64 md5
     req b =
       putObject (BucketName b) (ObjectKey key) (toBody reqBdy)
         & poContentType ?~ MIME.showType ct
-        & poContentMD5 ?~ md5Res
         & poMetadata .~ metaHeaders tok prc
 
 getMetadataV3 :: V3.AssetKey -> ExceptT Error App (Maybe S3AssetMeta)

commit 55f5399c82a31311b3757dd03edd995922ad02cd

Make Content-MD5 header optional for asset upload (#1252)
To avoid undetected API breakage as in #1247, this contains a testcase that uses a hardcoded
multipart request as it is currently sent by clients.The test fails on #1247, but passes now.

Ideally we would not have our own multipart parser in the first place, but the change in `headers`
was relatively straight-forward.

* add cargohold integration test for client compatibility

* make Content-MD5 header optional for asset upload

libs/wire-api/src/Wire/API/Asset/V3.hs
@@ -59,11 +59,8 @@ where
 
 import qualified Codec.MIME.Type as MIME
 import Control.Lens (makeLenses)
-import Crypto.Hash (Digest, MD5, hashlazy)
 import Data.Aeson
 import Data.Attoparsec.ByteString.Char8
-import qualified Data.ByteArray as B
-import qualified Data.ByteString.Base64 as B64
 import Data.ByteString.Builder
 import Data.ByteString.Conversion
 import qualified Data.ByteString.Lazy as LBS
@@ -185,7 +182,7 @@ buildMultipartBody sets typ bs =
 -- | Begin building a @multipart/mixed@ request body for a non-resumable upload.
 -- The returned 'Builder' can be immediately followed by the actual asset bytes.
 beginMultipartBody :: AssetSettings -> AssetHeaders -> Builder
-beginMultipartBody sets (AssetHeaders t l d) =
+beginMultipartBody sets (AssetHeaders t l) =
   byteString
     "--frontier\r\n\
     \Content-Type: application/json\r\n\
@@ -205,11 +202,7 @@ beginMultipartBody sets (AssetHeaders t l d) =
       \Content-Length: "
     <> wordDec l
     <> "\r\n\
-       \Content-MD5: "
-    <> byteString (B64.encode (B.convert d))
-    <> byteString
-      "\r\n\
-      \\r\n"
+       \\r\n"
   where
     settingsJson = encode sets
 
@@ -224,12 +217,11 @@ endMultipartBody = byteString "\r\n--frontier--\r\n"
 -- | Headers provided during upload.
 data AssetHeaders = AssetHeaders
   { hdrType :: MIME.Type,
-    hdrLength :: Word,
-    hdrMD5 :: Digest MD5
+    hdrLength :: Word
   }
 
 mkHeaders :: MIME.Type -> LByteString -> AssetHeaders
-mkHeaders t b = AssetHeaders t (fromIntegral (LBS.length b)) (hashlazy b)
+mkHeaders t b = AssetHeaders t (fromIntegral (LBS.length b))
 
 --------------------------------------------------------------------------------
 -- AssetSettings

services/cargohold/src/CargoHold/API/V3.hs
@@ -40,15 +40,14 @@ import Control.Applicative (optional)
 import Control.Error
 import Control.Lens (set, view, (^.))
 import Control.Monad.Trans.Resource
-import Crypto.Hash
 import Crypto.Random (getRandomBytes)
 import Data.Aeson (eitherDecodeStrict')
 import Data.Attoparsec.ByteString.Char8
-import qualified Data.ByteString.Base64 as B64
 import qualified Data.CaseInsensitive as CI
 import Data.Conduit
 import qualified Data.Conduit.Attoparsec as Conduit
 import Data.Id
+import qualified Data.List as List
 import qualified Data.Text.Ascii as Ascii
 import Data.Text.Encoding (decodeLatin1)
 import qualified Data.Text.Lazy as LT
@@ -175,9 +174,9 @@ assetHeaders =
     <* eol
   where
     go hdrs =
-      AssetHeaders <$> contentType hdrs
+      AssetHeaders
+        <$> contentType hdrs
         <*> contentLength hdrs
-        <*> contentMD5 hdrs
 
 contentType :: [(HeaderName, ByteString)] -> Parser MIME.Type
 contentType hdrs =
@@ -193,13 +192,6 @@ contentLength hdrs =
     (either fail return . parseOnly decimal)
     (lookup (CI.mk "Content-Length") hdrs)
 
-contentMD5 :: [(HeaderName, ByteString)] -> Parser (Digest MD5)
-contentMD5 hdrs =
-  maybe
-    (fail "Missing Content-MD5")
-    (maybe (fail "Invalid Content-MD5") return . digestFromByteString . B64.decodeLenient)
-    (lookup (CI.mk "Content-MD5") hdrs)
-
 boundary :: Parser ()
 boundary =
   char '-'
@@ -208,20 +200,27 @@ boundary =
     *> eol
     <?> "MIME boundary"
 
+-- | Always parses until the end of headers is reached
+-- (a line not starting with a char that's valid in header names, usually an empty line),
+-- or fails.
+-- Not all listed headers must be found, but other headers (or duplicates) raise an error.
 headers :: [HeaderName] -> Parser [(HeaderName, ByteString)]
-headers names = count (length names) (header names)
-
-header :: [HeaderName] -> Parser (HeaderName, ByteString)
-header names = do
-  name <- CI.mk <$> takeTill (== ':') <?> "header name"
-  unless (name `elem` names) $
-    fail $
-      "Unexpected header: " ++ show (CI.original name)
-  _ <- char ':'
-  skipSpace
-  value <- takeTill isEOL <?> "header value"
-  eol
-  return (name, value)
+headers allowed = do
+  -- optional in case there is no header left to parse
+  optional (CI.mk <$> takeWhile1 (\c -> isAlphaNum c || c == '-') <?> "header name") >>= \case
+    Nothing ->
+      pure []
+    Just name
+      | name `notElem` allowed ->
+        -- might also be a duplicate
+        fail $ "Unexpected header: " ++ show (CI.original name)
+      | otherwise -> do
+        _ <- char ':'
+        skipSpace
+        value <- takeTill isEOL <?> "header value"
+        eol
+        -- we don't want to parse it again (this also ensures quick termination)
+        ((name, value) :) <$> headers (List.delete name allowed)
 
 eol :: Parser ()
 eol = endOfLine <?> "\r\n"

services/cargohold/src/CargoHold/S3.hs
@@ -80,7 +80,6 @@ import qualified Data.UUID as UUID
 import Imports
 import Network.AWS hiding (Error)
 import Network.AWS.Data.Body
-import Network.AWS.Data.Crypto
 import Network.AWS.S3
 import Network.Wai.Utilities.Error (Error (..))
 import Safe (readMay)
@@ -106,7 +105,7 @@ uploadV3 ::
   Maybe V3.AssetToken ->
   Conduit.ConduitM () ByteString (ResourceT IO) () ->
   ExceptT Error App ()
-uploadV3 prc (s3Key . mkKey -> key) (V3.AssetHeaders ct cl md5) tok src = do
+uploadV3 prc (s3Key . mkKey -> key) (V3.AssetHeaders ct cl) tok src = do
   Log.info $
     "remote" .= val "S3"
       ~~ "asset.owner" .= toByteString prc
@@ -124,11 +123,9 @@ uploadV3 prc (s3Key . mkKey -> key) (V3.AssetHeaders ct cl md5) tok src = do
         -- Ignore any 'junk' after the content; take only 'cl' bytes.
         .| Conduit.isolate (fromIntegral cl)
     reqBdy = ChunkedBody defaultChunkSize (fromIntegral cl) stream
-    md5Res = Text.decodeLatin1 $ digestToBase Base64 md5
     req b =
       putObject (BucketName b) (ObjectKey key) (toBody reqBdy)
         & poContentType ?~ MIME.showType ct
-        & poContentMD5 ?~ md5Res
         & poMetadata .~ metaHeaders tok prc
 
 getMetadataV3 :: V3.AssetKey -> ExceptT Error App (Maybe S3AssetMeta)

services/cargohold/test/integration/API/V3.hs
@@ -55,7 +55,8 @@ tests s =
         "simple"
         [ test s "roundtrip" testSimpleRoundtrip,
           test s "tokens" testSimpleTokens,
-          test s "s3-upstream-closed" testSimpleS3ClosedConnectionReuse
+          test s "s3-upstream-closed" testSimpleS3ClosedConnectionReuse,
+          test s "client-compatibility" testUploadCompatibility
         ],
       testGroup
         "RealAWS"
@@ -196,6 +197,51 @@ testSimpleS3ClosedConnectionReuse c = go >> wait >> go
         !!! const 201 === statusCode
 
 --------------------------------------------------------------------------------
+-- Client compatibility tests
+
+-- Since the other tests use functions from the server code, it can happen that
+-- an API change also changes the requests made here in the tests.
+-- This test tries to prevent us from breaking the API without noticing.
+--
+-- The body is taken directly from a request made by the web app
+-- (just replaced the content with a shorter one and updated the MD5 header).
+testUploadCompatibility :: TestSignature ()
+testUploadCompatibility c = do
+  uid <- liftIO $ Id <$> nextRandom
+  -- Initial upload
+  r1 <-
+    uploadRaw (c . path "/assets/v3") uid exampleMultipart
+      <!! const 201 === statusCode
+  let loc = decodeHeader "Location" r1 :: ByteString
+  -- Lookup and download via redirect.
+  r2 <-
+    get (c . path loc . zUser uid . noRedirect) <!! do
+      const 302 === statusCode
+      const Nothing === responseBody
+  r3 <- flip get' id =<< parseUrlThrow (C8.unpack (getHeader' "Location" r2))
+  liftIO $ do
+    assertEqual "status" status200 (responseStatus r3)
+    assertEqual "content-type mismatch" (Just applicationOctetStream) (getContentType r3)
+    assertEqual "user mismatch" uid (decodeHeader "x-amz-meta-user" r3)
+    assertEqual "data mismatch" (Just "test") (responseBody r3)
+  where
+    exampleMultipart :: LByteString
+    exampleMultipart =
+      "--FrontierIyj6RcVrqMcxNtMEWPsNpuPm325QsvWQ\r\n\
+      \Content-Type: application/json;charset=utf-8\r\n\
+      \Content-length: 37\r\n\
+      \\r\n\
+      \{\"public\":true,\"retention\":\"eternal\"}\r\n\
+      \--FrontierIyj6RcVrqMcxNtMEWPsNpuPm325QsvWQ\r\n\
+      \Content-Type: application/octet-stream\r\n\
+      \Content-length: 4\r\n\
+      \Content-MD5: CY9rzUYh03PK3k6DJie09g==\r\n\
+      \\r\n\
+      \test\r\n\
+      \--FrontierIyj6RcVrqMcxNtMEWPsNpuPm325QsvWQ--\r\n\
+      \\r\n"
+
+--------------------------------------------------------------------------------
 -- Resumable (multi-step) uploads
 
 testResumableSmall :: TestSignature ()
@@ -267,13 +313,21 @@ uploadSimple ::
   Http (Response (Maybe Lazy.ByteString))
 uploadSimple c usr sets (ct, bs) =
   let mp = V3.buildMultipartBody sets ct (Lazy.fromStrict bs)
-   in post $
-        c
-          . method POST
-          . zUser usr
-          . zConn "conn"
-          . content "multipart/mixed"
-          . lbytes (toLazyByteString mp)
+   in uploadRaw c usr (toLazyByteString mp)
+
+uploadRaw ::
+  CargoHold ->
+  UserId ->
+  Lazy.ByteString ->
+  Http (Response (Maybe Lazy.ByteString))
+uploadRaw c usr bs =
+  post $
+    c
+      . method POST
+      . zUser usr
+      . zConn "conn"
+      . content "multipart/mixed"
+      . lbytes bs
 
 createResumable ::
   HasCallStack =>
@@ -377,6 +431,9 @@ getContentType = MIME.parseContentType . decodeLatin1 . getHeader' "Content-Type
 
 applicationText :: MIME.Type
 applicationText = MIME.Type (MIME.Application "text") []
+
+applicationOctetStream :: MIME.Type
+applicationOctetStream = MIME.Type (MIME.Application "octet-stream") []
 
 textPlain :: MIME.Type
 textPlain = MIME.Type (MIME.Text "plain") []