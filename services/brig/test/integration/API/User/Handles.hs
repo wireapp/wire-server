b54e56c6cbf248c0b33ea1add23bc4a34893f7a6
Akshay Mankar <akshay@wire.com>
2020-11-26 09:08:53 +0100

    Remove test to get users by qualified handles

--- services/brig/test/integration/API/User/Handles.hs
+++ services/brig/test/integration/API/User/Handles.hs
@@ -157,9 +157,6 @@ testHandleQuery opts brig = do
   get (brig . path "/users" . queryItem "handles" (toByteString' hdl) . zUser uid) !!! do
     const 200 === statusCode
     const (Just (Handle hdl)) === (userHandle <=< listToMaybe <=< responseJsonMaybe)
-  -- Qualified handles get accepted, but don't have any matches (federation)
-  get (brig . path "/users" . queryItem "handles" (toByteString' hdl <> "@wire.com") . zUser uid) !!! do
-    const 404 === statusCode
   -- Bulk availability check
   hdl2 <- randomHandle
   hdl3 <- randomHandle
