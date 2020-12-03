00c4b6579dcba0b2630dbbd7fa175f2232de8695
Matthias Heinzel <mheinzel@posteo.de>
2020-12-03 08:34:48 +0100

    forgot to add file with tests

---
+++ services/federator/test/unit/Test/Federator/Options.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2020 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Test.Federator.Options where
+
+import Data.Aeson (FromJSON)
+import Data.Domain (mkDomain)
+import qualified Data.Yaml as Yaml
+import Federator.Options
+import Imports
+import Test.Tasty
+import Test.Tasty.HUnit
+
+tests :: TestTree
+tests =
+  testGroup
+    "Options"
+    [ parseFederationStrategy
+    ]
+
+parseFederationStrategy :: TestTree
+parseFederationStrategy =
+  testCase "parse FederationStrategy examples" $ do
+    assertParsesAs AllowAll $
+      "allowAll: null"
+    assertParsesAs (withAllowList []) $
+      "allowedDomains: []"
+    assertParsesAs (withAllowList ["test.org"]) $
+      "allowedDomains:\n\
+      \  - test.org"
+    assertParsesAs (withAllowList ["example.com", "wire.com"]) $
+      "allowedDomains:\n\
+      \  - example.com\n\
+      \  - wire.com"
+  where
+    withAllowList =
+      AllowList . AllowedDomains . map (either error id . mkDomain)
+
+assertParsesAs :: (HasCallStack, Eq a, FromJSON a, Show a) => a -> ByteString -> Assertion
+assertParsesAs v bs =
+  assertEqual "YAML parsing" (Right v) $
+    either (Left . show) Right (Yaml.decodeEither' bs)
