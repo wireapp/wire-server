0b993b9d9e65b68a6eb3d3072a76b8cb4adc9408
Stefan Matting <stefan@wire.com>
2020-12-09 17:28:02 +0100

    Add ParseSchema

---
+++ tools/db/move-team/src/ParseSchema.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module ParseSchema where
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Imports
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void Text
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme (L.space space1 (L.skipLineComment ("--" :: Text)) empty)
+
+data Column = Column
+  { colName :: Text,
+    colType :: Text
+  }
+  deriving (Eq, Show, Ord)
+
+data CreateTable = CreateTable
+  { ctKeyspace :: Text,
+    ctTablename :: Text,
+    ctColumns :: [Column]
+  }
+  deriving (Eq, Show, Ord)
+
+name' :: Parser Text
+name' = T.pack <$> some (satisfy (\c -> isLower c || c == '_'))
+
+keyspace :: Parser Text
+keyspace = name'
+
+tablename :: Parser Text
+tablename = keyspace
+
+columnName :: Parser Text
+columnName = name'
+
+columnType :: Parser Text
+columnType = T.pack <$> some (satisfy (\c -> isLower c || c == '_' || c == '<' || c == '>'))
+
+column :: Parser Column
+column = do
+  col <- Column <$> lexeme columnName <*> lexeme columnType
+  void $ optional (lexeme "PRIMARY KEY")
+  pure col
+
+betweenBrackets :: ParsecT Void Text Identity a -> ParsecT Void Text Identity a
+betweenBrackets = between (lexeme "(") (lexeme ")")
+
+primaryKeyAtEndOfColumns :: Parser ()
+primaryKeyAtEndOfColumns = do
+  void $ lexeme "PRIMARY KEY"
+  void $
+    lexeme $
+      betweenBrackets $
+        (void name' <|> nested) `sepBy1` lexeme ","
+  where
+    nested = void $ betweenBrackets $ name' `sepBy1` lexeme ","
+
+createTable :: Parser CreateTable
+createTable = do
+  void $ lexeme "CREATE TABLE"
+  ks <- keyspace
+  void "."
+  tn <- lexeme tablename
+  cols <-
+    betweenBrackets $
+      catMaybes
+        <$> ((Just <$> column) <|> (Nothing <$ primaryKeyAtEndOfColumns))
+        `sepBy` lexeme ","
+  noise
+  pure (CreateTable ks tn cols)
+
+noise :: Parser ()
+noise = void $ some (satisfy (/= ';'))
+
+stmt :: Parser (Maybe CreateTable)
+stmt =
+  do
+    Just <$> createTable
+    <|> (Nothing <$ (notFollowedBy (string "CREATE TABLE") >> noise))
+
+parser :: Parser [CreateTable]
+parser = catMaybes <$> (stmt `sepEndBy` lexeme (string ";"))
+
+-------------------------------------------------------------------------------
+
+main :: IO ()
+main =
+  T.readFile "/home/stefan/repos/wire-server/docs/reference/cassandra-schema.cql"
+    >>= parseTest parser
