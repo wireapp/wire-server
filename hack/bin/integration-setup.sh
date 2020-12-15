51fda33525c47e5c5c7d24dd3ae662844ef2a310
jschaul <jschaul@users.noreply.github.com>
2020-12-15 21:14:39 +0100

    simplify integration-setup script even more

--- hack/bin/integration-setup.sh
+++ hack/bin/integration-setup.sh
@@ -1,44 +1,24 @@
 #!/usr/bin/env bash
 
-USAGE="Usage: $0 [-i image-version]"
+USAGE="Usage: $0"
 
-# https://sookocheff.com/post/bash/parsing-bash-script-arguments-with-shopts/
-while getopts ":i:" opt; do
-  case ${opt} in
-    i ) IMAGE_VERSION_OVERRIDE="$OPTARG"
-      ;;
-    : ) echo "-$OPTARG" requires an argument 1>&2
-        exit 1
-      ;;
-    \? ) echo "$USAGE" 1>&2
-         exit 1
-      ;;
-  esac
-done
-shift $((OPTIND -1))
-
-if [ "$#" -ne 0 ]; then
-  echo "$USAGE" 1>&2
-  exit 1
-fi;
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOP_LEVEL="$DIR/../.."
+CHARTS_DIR="${TOP_LEVEL}/.local/charts"
 
 NAMESPACE=${NAMESPACE:-test-integration}
 
 kubectl create namespace "${NAMESPACE}" > /dev/null 2>&1 || true
 
-set -e
-
 ${DIR}/integration-cleanup.sh
 
-
-REPOSITORY="${DIR}/../../deploy/charts"
 charts=( fake-aws databases-ephemeral wire-server )
 
 echo "updating recursive dependencies ..."
 for chart in "${charts[@]}"; do
-  "$DIR/update.sh" "$REPOSITORY/$chart"
+  "$DIR/update.sh" "$CHARTS_DIR/$chart"
 done
 
 echo "Installing charts..."
@@ -48,25 +28,6 @@ function printLogs() {
     kubectl -n ${NAMESPACE} get pods | grep Pending | awk '{print $1}' | xargs -n 1 -I{} bash -c "printf '\n\n----DESCRIBE 'pending' {}:\n'; kubectl -n ${NAMESPACE} describe pod {}" || true
 }
 
-# construct override options if we want to run using a specific docker image version for our services
-imageVersionOverrideOptions=""
-if [ -n "$IMAGE_VERSION_OVERRIDE" ]; then
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set brig.image.tag=$IMAGE_VERSION_OVERRIDE"
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set cannon.image.tag=$IMAGE_VERSION_OVERRIDE"
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set gundeck.image.tag=$IMAGE_VERSION_OVERRIDE"
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set galley.image.tag=$IMAGE_VERSION_OVERRIDE"
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set spar.image.tag=$IMAGE_VERSION_OVERRIDE"
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set cargohold.image.tag=$IMAGE_VERSION_OVERRIDE"
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set proxy.image.tag=$IMAGE_VERSION_OVERRIDE"
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set elasticsearch-index.image.tag=$IMAGE_VERSION_OVERRIDE"
-
-    # Some services have the tag at a slightly different location:
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set nginz.images.nginz.tag=$IMAGE_VERSION_OVERRIDE"
-    imageVersionOverrideOptions="$imageVersionOverrideOptions --set cassandra-migrations.images.tag=$IMAGE_VERSION_OVERRIDE"
-fi;
-
-set -x
-
 for chart in "${charts[@]}"; do
     kubectl -n ${NAMESPACE} get pods
     valuesfile="${DIR}/../helm_vars/${chart}/values.yaml"
@@ -75,10 +36,11 @@ for chart in "${charts[@]}"; do
     else
         option=""
     fi
-    helm upgrade --atomic --install --namespace "${NAMESPACE}" "${NAMESPACE}-${chart}" "${REPOSITORY}/${chart}" \
+    set -x
+    helm upgrade --atomic --install --namespace "${NAMESPACE}" "${NAMESPACE}-${chart}" "${CHARTS_DIR}/${chart}" \
         $option \
-        $imageVersionOverrideOptions \
         --wait
+    set +x
 done
 
 # wait for fakeSNS to create resources. TODO, cleaner: make initiate-fake-aws-sns a post hook. See cassandra-migrations chart for an example.
