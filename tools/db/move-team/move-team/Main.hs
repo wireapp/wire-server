270c5f20733b5828a1b29ac1c6ab47fad8164ce3
Stefan Matting <smatting@users.noreply.github.com>
2020-12-17 15:49:00 +0100

    move-team: CLI to export/import team data (#1288)
    This adss an executable `move-team` that can..
    - ... export all data that belongs to a team to a file dump
    - ... import a file dump to a new backend instance
    - ... export all data from some tables. This was only intended for development
    
    Co-authored-by: Matthias Fischmann <mf@zerobuzz.net>

---
+++ tools/db/move-team/move-team/Main.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2020 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Main
+  ( main,
+    debugMainDebugExportFull,
+    debugMainImport,
+    debugMainExport,
+  )
+where
+
+import Cassandra as C
+import Cassandra.Settings as C
+import Control.Lens hiding ((.=))
+import Data.Id (Id (Id))
+import Data.UUID
+import Imports
+import Options as O
+import Options.Applicative
+import System.Environment (withArgs)
+import qualified System.Logger as Log
+import System.Process (system)
+import Types
+import Work
+
+main :: IO ()
+main = do
+  s <- execParser (info (helper <*> settingsParser) desc)
+  lgr <- initLogger
+  case s of
+    Export teamid targetPath connSettings -> do
+      bc <- initCas (connSettings ^. setCasBrig) lgr
+      gc <- initCas (connSettings ^. setCasGalley) lgr
+      sc <- initCas (connSettings ^. setCasSpar) lgr
+      gunC <- initCas (connSettings ^. setCasGundeck) lgr
+      runExport $ Env lgr bc gc sc gunC targetPath (Id teamid) 100
+    Import sourcePath connSettings -> do
+      bc <- initCas (connSettings ^. setCasBrig) lgr
+      gc <- initCas (connSettings ^. setCasGalley) lgr
+      sc <- initCas (connSettings ^. setCasSpar) lgr
+      gunC <- initCas (connSettings ^. setCasGundeck) lgr
+      -- FUTUREWORK: "make illegal states irrepresentable"
+      let dummyTeamId :: UUID = fromJust $ Data.UUID.fromString "c2cc10e1-57d6-4b6f-9899-38d972112d8c"
+      runImport $ Env lgr bc gc sc gunC sourcePath (Id dummyTeamId) 100
+    DebugExportFull targetPath connSettings -> do
+      bc <- initCas (connSettings ^. setCasBrig) lgr
+      gc <- initCas (connSettings ^. setCasGalley) lgr
+      sc <- initCas (connSettings ^. setCasSpar) lgr
+      gunC <- initCas (connSettings ^. setCasGundeck) lgr
+      -- FUTUREWORK: "make illegal states irrepresentable"
+      let dummyTeamId :: UUID = fromJust $ Data.UUID.fromString "c2cc10e1-57d6-4b6f-9899-38d972112d8c"
+      runDebugExportFull $ Env lgr bc gc sc gunC targetPath (Id dummyTeamId) 100
+  where
+    desc =
+      header "service-backfill"
+        <> progDesc "Backfill service tables"
+        <> fullDesc
+    initLogger =
+      Log.new
+        . Log.setOutput Log.StdOut
+        . Log.setFormat Nothing
+        . Log.setBufSize 0
+        $ Log.defSettings
+    initCas cas l =
+      C.init
+        . C.setLogger (C.mkLogger l)
+        . C.setContacts (cas ^. cHosts) []
+        . C.setPortNumber (fromIntegral $ cas ^. cPort)
+        . C.setKeyspace (cas ^. cKeyspace)
+        . C.setProtocolVersion C.V4
+        $ C.defSettings
+
+debugMainDebugExportFull :: IO ()
+debugMainDebugExportFull = do
+  let dir = "/tmp/full-backup"
+  void $ system $ "rm -rf " <> dir
+  void $ system $ "mkdir -p " <> dir
+  withArgs
+    [ "debug-export-full",
+      "--target-path",
+      dir
+    ]
+    main
+
+debugMainImport :: IO ()
+debugMainImport = do
+  let dir = "/tmp/full-backup"
+  withArgs
+    [ "import",
+      "--source-path",
+      dir
+    ]
+    main
+
+debugMainExport :: IO ()
+debugMainExport = do
+  let dir = "/tmp/move-team-export"
+  void $ system $ "rm -rf " <> dir
+  void $ system $ "mkdir -p " <> dir
+  withArgs
+    [ "export",
+      "--teamid",
+      "e09f7a63-b5d4-4db4-a3c1-18bddf3df7fc",
+      "--target-path",
+      dir
+    ]
+    main
---
+++ tools/db/move-team/src/Types.hs
@@ -0,0 +1,167 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2020 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Types where
+
+import Cassandra
+import Data.Aeson (FromJSON (..), ToJSON (..), Value (String), withArray, withText)
+import Data.Aeson.Types (Value (Array))
+import Data.ByteString.Lazy (fromStrict, toStrict)
+import Data.Handle
+import Data.IP (IP (..))
+import Data.Id
+import qualified Data.Text as T
+import Data.Text.Ascii (AsciiText, Base64, decodeBase64, encodeBase64)
+import qualified Data.Vector as V
+import Database.CQL.Protocol (ColumnType (VarCharColumn))
+import Galley.Data.Instances ()
+import Imports
+import System.Logger (Logger)
+import Wire.API.User.Password (PasswordResetKey (..))
+
+data Env = Env
+  { envLogger :: Logger,
+    envBrig :: ClientState,
+    envGalley :: ClientState,
+    envSpar :: ClientState,
+    envGundeck :: ClientState,
+    envTargetPath :: FilePath,
+    envTeamId :: TeamId,
+    envPageSize :: Int32
+  }
+
+-- | (ideally, we shouldn't need this, since we shouldn't even touch tables that have this in them.)
+data AssetIgnoreData = AssetIgnoreData
+
+instance {-# OVERLAPPING #-} FromJSON [AssetIgnoreData] where
+  parseJSON _ = pure []
+
+instance {-# OVERLAPPING #-} ToJSON [AssetIgnoreData] where
+  toJSON _ = Array $ V.fromList []
+
+instance Cql AssetIgnoreData where
+  ctype =
+    Tagged
+      ( UdtColumn
+          "asset"
+          [ ("typ", IntColumn),
+            ("key", VarCharColumn),
+            ("size", IntColumn) -- FUTUREWORK check if this works
+          ]
+      )
+  toCql _ = error "AssetIgnoreData: you should not have any data of this"
+  fromCql _ = pure AssetIgnoreData
+
+instance ToJSON a => ToJSON (Cassandra.Set a) where
+  toJSON = toJSON . Cassandra.fromSet
+
+instance FromJSON a => FromJSON (Cassandra.Set a) where
+  parseJSON = fmap Cassandra.Set . parseJSON
+
+instance ToJSON Blob where
+  toJSON (Blob bstr) = toJSON (encodeBase64 (toStrict bstr))
+
+instance FromJSON Blob where
+  parseJSON x = do
+    y <- parseJSON @(AsciiText Base64) x
+    case decodeBase64 y of
+      Nothing -> fail "Blob': not a valid base64 string"
+      Just bs -> pure . Blob . fromStrict $ bs
+
+instance ToJSON IP where
+  toJSON ip = String (T.pack . show $ ip)
+
+instance FromJSON IP where
+  parseJSON = withText "IP" $ \str ->
+    case (read . T.unpack) str of
+      Nothing -> fail "not a formatted IP address"
+      Just ip -> pure ip
+
+deriving instance Cql Handle
+
+deriving instance Cql PasswordResetKey
+
+deriving instance ToJSON Ascii
+
+deriving instance FromJSON Ascii
+
+deriving instance ToJSON TimeUuid
+
+deriving instance FromJSON TimeUuid
+
+instance (ToJSON a, ToJSON b, ToJSON c, ToJSON d, ToJSON e, ToJSON f, ToJSON g, ToJSON h, ToJSON i, ToJSON j, ToJSON k, ToJSON l, ToJSON m, ToJSON n, ToJSON o, ToJSON p, ToJSON q, ToJSON r, ToJSON s, ToJSON t, ToJSON u) => ToJSON ((,,,,,,,,,,,,,,,,,,,,) a b c d e f g h i j k l m n o p q r s t u) where
+  toJSON (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u) =
+    Array $
+      V.fromList
+        [ toJSON a,
+          toJSON b,
+          toJSON c,
+          toJSON d,
+          toJSON e,
+          toJSON f,
+          toJSON g,
+          toJSON h,
+          toJSON i,
+          toJSON j,
+          toJSON k,
+          toJSON l,
+          toJSON m,
+          toJSON n,
+          toJSON o,
+          toJSON p,
+          toJSON q,
+          toJSON r,
+          toJSON s,
+          toJSON t,
+          toJSON u
+        ]
+
+instance (FromJSON a, FromJSON b, FromJSON c, FromJSON d, FromJSON e, FromJSON f, FromJSON g, FromJSON h, FromJSON i, FromJSON j, FromJSON k, FromJSON l, FromJSON m, FromJSON n, FromJSON o, FromJSON p, FromJSON q, FromJSON r, FromJSON s, FromJSON t, FromJSON u) => FromJSON ((,,,,,,,,,,,,,,,,,,,,) a b c d e f g h i j k l m n o p q r s t u) where
+  parseJSON = withArray "Tuple" $ \case
+    (toList -> [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u]) ->
+      (,,,,,,,,,,,,,,,,,,,,)
+        <$> parseJSON a
+        <*> parseJSON b
+        <*> parseJSON c
+        <*> parseJSON d
+        <*> parseJSON e
+        <*> parseJSON f
+        <*> parseJSON g
+        <*> parseJSON h
+        <*> parseJSON i
+        <*> parseJSON j
+        <*> parseJSON k
+        <*> parseJSON l
+        <*> parseJSON m
+        <*> parseJSON n
+        <*> parseJSON o
+        <*> parseJSON p
+        <*> parseJSON q
+        <*> parseJSON r
+        <*> parseJSON s
+        <*> parseJSON t
+        <*> parseJSON u
+    _ -> fail "Expected array of length 21"
