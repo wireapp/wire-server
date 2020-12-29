29ad7c1ab1b36ed1d5ca546f958069eda4e30095
jschaul <jschaul@users.noreply.github.com>
2020-12-29 20:51:53 +0100

    Makefile: allow overriding NAMESPACE for kube-integration target

--- hack/bin/integration-teardown.sh
+++ hack/bin/integration-teardown.sh
@@ -8,7 +8,7 @@ set -ex
 
 echo "NAMESPACE = $NAMESPACE"
 
-helm ls --all --namespace ${NAMESPACE} | grep "test-" | awk '{print $1}' | xargs -n 1 helm -n "$NAMESPACE" delete
+helm ls --all --namespace ${NAMESPACE} | grep -v NAME | awk '{print $1}' | xargs -n 1 helm -n "$NAMESPACE" delete
 
 sleep 10
 
