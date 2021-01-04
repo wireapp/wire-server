925d027ff600cdaf88753fa278ac9ed92d623ebc
jschaul <jschaul@users.noreply.github.com>
2021-01-05 00:12:21 +0100

    fixup for #1308

--- hack/bin/upload-helm-charts-s3.sh
+++ hack/bin/upload-helm-charts-s3.sh
@@ -47,7 +47,7 @@ if [ -n "$chart_dir" ] && [ -d "$chart_dir" ]; then
     echo "only syncing $chart_name"
     charts=( "$chart_name" )
 else
-    charts=$(make -s -C "$TOP_LEVEL_DIR" echo-release-charts)
+    charts=( $(make -s -C "$TOP_LEVEL_DIR" echo-release-charts) )
     # See Makefile/ CHARTS_RELEASE FUTUREWORK
     #charts=( $(find $CHART_DIR/ -maxdepth 1 -type d | sed -n "s=$CHART_DIR/\(.\+\)=\1 =p") )
 fi
