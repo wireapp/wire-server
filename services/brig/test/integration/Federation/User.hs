3cb75e873f6500d862799697280c3b3a475874ff
jschaul <jschaul@users.noreply.github.com>
2021-01-14 23:19:23 +0100

    sketch the grand plan of end-to-end integration testing

--- services/brig/test/integration/Federation/User.hs
+++ services/brig/test/integration/Federation/User.hs
@@ -19,16 +19,9 @@ module Federation.User where
 
 import Bilge (Manager)
 import qualified Brig.Options as BrigOpts
--- import Control.Lens ((.~), (^.))
--- import qualified Data.Text as Text
--- import qualified Data.Text.Encoding as Text
 import Imports
--- import qualified Network.HTTP.Client as HTTP
--- import qualified System.Logger.Class as Log
--- import System.Random as Random
 import Test.Tasty
 import Test.Tasty.HUnit
--- import URI.ByteString
 import Util (Brig)
 import Util.Options (Endpoint)
 
@@ -43,3 +36,18 @@ spec brigOpts mg brig federator =
 testHandleLookup :: BrigOpts.Opts -> Manager -> Brig -> Endpoint -> Assertion
 testHandleLookup _brigOpts _mg _brig _federator = do
   undefined
+
+-- TODO: we can make use of the 'NAMESPACE' environment variable to get our own namespace
+-- next, we can either do a SRV lookup, or create a host+port combination based on a convention. Let's say if this particular kubernetes test namespace in which this executable is deployed is test-random123, then we expect a second installation for federation to be at test-random123-fed2, thus we can talk to the "second" brig on "http://brig." <> NAMESPACE <> "-fed2.svc.cluster.local:8080"
+-- This allows us to do meaningful end2end tests:
+--
+-- 1. create a user on the "other side" using an internal brig endpoint in the other namespace
+-- 2. query the local-namespace brig for a user sitting on the other backend
+-- which should involve the following network traffic:
+--
+-- brig-integration -> brig -> federator -> fed2-federator -> fed2-brig
+-- (and back)
+--
+-- While individual functions can and should be tested in a more unit-testy way, and end-to-end integration test will also be important.
+--
+-- TODO requires adding an option for "federation-integration-setup" (i.e. deploying two sets of all services - with their own config value overrides)
