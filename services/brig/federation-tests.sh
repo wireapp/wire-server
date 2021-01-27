545e9614dcdacd08b42c5167ea037a4641d26ccc
jschaul <jschaul@users.noreply.github.com>
2021-01-27 11:19:04 +0100

    Federation end2end test scripts and Makefile targets (#1341)
    For easier review, this PR is a small piece from the now-long-running
    "federation" branch https://github.com/wireapp/wire-server/pull/1319 with the
    following purpose:
    
    * introduces telepresence as a new development dependency (right now only for
      running end2end federation tests spanning multiple backends)
    * prepares using brig as a place to keep some federation end2end tests spanning
      more than one backend, by using pattern matching on `brig-federation`: those
    tests will not run by default locally or on CI, until they are more stable and
    CI can actually run them.
    * Makefile targets and bash scripts to spawn up two backends (one backend will
      have a namespace-suffix-by-convention) and run a telepresence-based
    integration test.
    * Each ephemeral backend will have a unique federationDomain (based on the
      kubernetes namespace), so that tests spanning multiple backends become
    possible.

---
+++ services/brig/federation-tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+USAGE="$0 <NAMESPACE>"
+NAMESPACE=${1:?$USAGE}
+
+# This script assumes:
+# * two wire-server backends under NAMEPACE and NAMESPACE-fed2 have been deployed with helm.
+# * you have a locally compiled brig-integration executable
+#
+# It then downloads the configmaps, performs a hacky override for two configuration flags,
+# and then uses telepresence to run a locally-compiled brig-integration executable against
+# the brigs and federators inside kubernetes in the two NAMESPACES.
+
+kubectl -n "$NAMESPACE" get configmap brig-integration -o jsonpath='{.data.integration\.yaml}' > i.yaml
+kubectl -n "$NAMESPACE" get configmap brig -o jsonpath='{.data.brig\.yaml}' > b.yaml
+
+# override some settings so the local brig-integration executable doesn't fail
+sed -i "s=privateKeys: /etc/wire/brig/secrets/secretkey.txt=privateKeys: test/resources/zauth/privkeys.txt=g" b.yaml
+sed -i "s=publicKeys: /etc/wire/brig/secrets/publickey.txt=publicKeys: test/resources/zauth/pubkeys.txt=g" b.yaml
+
+telepresence --namespace "$NAMESPACE" --also-proxy cassandra-ephemeral --run bash -c "./dist/brig-integration -p brig-federation -i i.yaml -s b.yaml"
