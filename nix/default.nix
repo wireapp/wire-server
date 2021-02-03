ef1f51c4d7b8399ad694b9e453bbca5108c8b447
Arian van Putten <arian.vanputten@gmail.com>
2021-02-03 19:19:26 +0100

    niv: use upstream niv
    Nixpkgs ships with a copy of niv already. And latest niv doesn't work
    with latest nixpkgs. Lets use the version that nixpkgs vets for us
    
    https://github.com/nmattia/niv/pull/298

--- nix/default.nix
+++ nix/default.nix
@@ -3,8 +3,6 @@ let
   pkgs = import sources.nixpkgs {
     config.allowUnfree = true;
     overlays = [
-      # the tool we use for versioning (The thing that generates sources.json)
-      (_: _: { niv = (import sources.niv {}).niv; })
       # All wire-server specific packages
       (import ./overlays/wire-server.nix)
 
