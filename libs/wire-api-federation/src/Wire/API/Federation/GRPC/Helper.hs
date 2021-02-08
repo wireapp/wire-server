1e6efc92d97a226792e5301aa88791cb250cebf2
jschaul <jschaul@users.noreply.github.com>
2021-02-08 21:28:57 +0100

    delete some files WIP

--- libs/wire-api-federation/src/Wire/API/Federation/GRPC/Helper.hs
+++ libs/wire-api-federation/src/Wire/API/Federation/GRPC/Helper.hs
@@ -5,16 +5,8 @@ module Wire.API.Federation.GRPC.Helper where
 import Imports
 import Language.Haskell.TH.Syntax (Dec, Q, addDependentFile)
 
-protoFile :: FilePath
-protoFile = "proto/federator.proto"
-
 routerProtoFile :: FilePath
 routerProtoFile = "proto/router.proto"
-
-recompileUponProtoChanges :: Q [Dec]
-recompileUponProtoChanges = do
-  addDependentFile protoFile
-  pure []
 
 recompileRouterUponProtoChanges :: Q [Dec]
 recompileRouterUponProtoChanges = do
