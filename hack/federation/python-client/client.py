3254905d735098ca614bf556f132ffc03beb77ab
jschaul <jschaul@users.noreply.github.com>
2021-02-11 19:31:32 +0100

    Write an example federation api client in python

---
+++ hack/federation/python-client/client.py
@@ -0,0 +1,26 @@
+import grpc
+
+from router_pb2 import *
+import router_pb2_grpc
+
+# make sure to run ./services/start-services-only.sh first to have federator and brig up!
+print("starting client...")
+
+channel = grpc.insecure_channel('localhost:8098') # public-facing federator port
+stub = router_pb2_grpc.InwardStub(channel)
+
+def handle_search(handle):
+    param = QueryParam(key="handle".encode("utf-8"), value=handle.encode("utf-8"))
+    return Request(
+            path="users/by-handle".encode("utf-8"),
+            query=[param],
+            method=GET,
+            component=Brig)
+
+req = handle_search("alice")
+
+print("request: ", req)
+
+response = stub.call(req)
+
+print("response: ", response)
