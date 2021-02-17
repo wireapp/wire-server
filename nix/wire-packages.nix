42c3f410f65bbfebb00903288a43e70ff2032879
Arian van Putten <arian.vanputten@gmail.com>
2021-02-17 15:58:37 +0100

    Introduce haskell.nix
    Also remove dist folders.
    
    They have special meaning in Cabal; and haskell.nix was confused of them
    being in the source code

--- nix/wire-packages.nix
+++ nix/wire-packages.nix
@@ -1,8 +1,10 @@
 # Nix package definitions for all things wire-server
 final: prev: {
+
   # TODO: Do not use buildRustPackage. Ces't horrible
-  cryptobox = final.callPackage (
-    { fetchFromGitHub, rustPlatform, pkg-config, libsodium }:
+  cryptobox = final.callPackage
+    (
+      { fetchFromGitHub, rustPlatform, pkg-config, libsodium }:
       rustPlatform.buildRustPackage rec {
         name = "cryptobox-c-${version}";
         version = "2019-06-17";
@@ -17,8 +19,8 @@ final: prev: {
         cargoSha256 = "0zs8ibv7rinrrzp9naxd7yak7kn1gp3pjb3g8i4wf7xw2hkkq81z";
 
         patchLibs = prev.lib.optionalString prev.stdenv.isDarwin ''
-            install_name_tool -id $out/lib/libcryptobox.dylib $out/lib/libcryptobox.dylib
-          '';
+          install_name_tool -id $out/lib/libcryptobox.dylib $out/lib/libcryptobox.dylib
+        '';
 
         postInstall = ''
           ${patchLibs}
@@ -26,10 +28,12 @@ final: prev: {
           cp src/cbox.h $out/include
         '';
       }
-  ) {};
+    )
+    { };
 
-  zauth = final.callPackage (
-    { fetchFromGitHub, rustPlatform, pkg-config, libsodium }:
+  zauth = final.callPackage
+    (
+      { fetchFromGitHub, rustPlatform, pkg-config, libsodium }:
       rustPlatform.buildRustPackage rec {
         name = "libzauth-${version}";
         version = "3.0.0";
@@ -41,8 +45,8 @@ final: prev: {
         cargoSha256 = "10ijvi3rnnqpy589hhhp8s4p7xfpsbb1c3mzqnf65ra96q4nd6bf"; # final.lib.fakeSha256;
 
         patchLibs = prev.lib.optionalString prev.stdenv.isDarwin ''
-            install_name_tool -id $out/lib/libzauth.dylib $out/lib/libzauth.dylib
-          '';
+          install_name_tool -id $out/lib/libzauth.dylib $out/lib/libzauth.dylib
+        '';
 
         postInstall = ''
           mkdir -p $out/lib/pkg-config
@@ -55,7 +59,8 @@ final: prev: {
           ${patchLibs}
         '';
       }
-  ) {};
+    )
+    { };
 
   nginxModules = prev.nginxModules // {
     zauth = {
@@ -72,4 +77,39 @@ final: prev: {
     ];
   };
 
+  wireHaskellPkgs = final.haskell-nix.stackProject {
+    src = final.haskell-nix.haskellLib.cleanGit {
+      name = "wire-server";
+      src = ../.;
+    };
+
+    # Lets avoid hpack as we have cabal files comitted
+    ignorePackageYaml = true;
+
+    # in stack.yaml only the commit to fetch is given; but this is ambigious as
+    # apparently nix also needs the branch on which this commit is reachable.
+    # (By default it uses HEAD; and if your commit isn't reachable from HEAD it
+    # isn't fetchable!)t
+    branchMap = {
+      "https://github.com/wireapp/hspec-wai"."0a5142cd3ba48116ff059c041348b817fb7bdb25" = "body-contains";
+      "https://github.com/wireapp/bloodhound"."92de9aa632d590f288a353d03591c38ba72b3cb3" = "wire-fork-ghc-8.8";
+      "https://github.com/wireapp/servant-swagger.git"."23e9afafadaade29d21181b935286087457171e3" = "akshaymankar/fix-ghc-version-check";
+    };
+
+    modules = [
+      # Needed because `build-tools` in cabal can refer to either a hackage
+      # package or a system package and if there is a name-clash it is
+      # ambigious. haskell.nix in that case chooses the hackage package instead
+      # which is exactly what we do not want. We want the protobuf c library
+      { packages.types-common-journal.components.library.build-tools = [ final.protobuf ]; }
+    ];
+  };
+
+  # Given a haskell package name; builds a container with all the executables
+  # that that haskell package exposes
+  buildHaskellContainer = name: prev.dockerTools.buildLayeredImage {
+    inherit name;
+    contents = prev.lib.attrValues final.wireHaskellPkgs.${name}.components.exes;
+  };
+  wireContainers = prev.lib.genAttrs [ "brig" "galley" "cannon" "gundeck" "cargohold" ] final.buildHaskellContainer;
 }
