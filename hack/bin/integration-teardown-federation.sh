19777b4836dfbe54c30ce65d44f8e4d500adef4b
Akshay Mankar <akshay@wire.com>
2021-02-25 09:21:42 +0100

    Makefile: Default to setting up and testing federation in K8s (#1381)
    Keep old targets with suffix '-sans-federation' in case anyone needs them.
    The federation tests will still be run by default when using `hack/helm_var/`
    
    Co-authored-by: jschaul <jschaul@users.noreply.github.com>

--- hack/bin/integration-teardown-federation.sh
+++ hack/bin/integration-teardown-federation.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-USAGE="Usage: $0"
-
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TOP_LEVEL="$DIR/../.."
 
 export NAMESPACE=${NAMESPACE:-test-integration}
 
-$DIR/integration-setup.sh
+$DIR/integration-teardown.sh
 
 # The suffix '-fed2' must be kept in sync with configuration inside
 # charts/brig/templates/tests/configmap.yaml and
-# hack/bin/integration-teardown-federation.sh
+# hack/bin/integration-setup-federation.sh
 export NAMESPACE=${NAMESPACE}-fed2
 
-$DIR/integration-setup.sh
+$DIR/integration-teardown.sh
