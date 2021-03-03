13a8053a9cd3ab7f181cf26bd3620ab6a003d7de
fisx <mf@zerobuzz.net>
2021-03-03 14:57:42 +0100

    Add-license. (#1394)

--- libs/polysemy-wire-zoo/src/Polysemy/TinyLog.hs
+++ libs/polysemy-wire-zoo/src/Polysemy/TinyLog.hs
@@ -14,6 +14,7 @@
 --
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
+
 module Polysemy.TinyLog where
 
 import Imports
