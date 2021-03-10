1715738b698044388a17c56901d4a4ab5efd5462
Akshay Mankar <itsakshaymankar@gmail.com>
2021-03-10 16:47:21 +0100

    Set federator DNS name to federation domain

--- hack/bin/integration-setup.sh
+++ hack/bin/integration-setup.sh
@@ -41,16 +41,25 @@ FEDERATION_DOMAIN="federation-test-helper.$NAMESPACE.svc.cluster.local"
 for chart in "${charts[@]}"; do
     kubectl -n ${NAMESPACE} get pods
     valuesfile="${DIR}/../helm_vars/${chart}/values.yaml"
+
+    declare -a options=()
+
     if [ -f "$valuesfile" ]; then
-        option="-f $valuesfile"
-    else
-        option=""
+        options+=(-f "$valuesfile")
     fi
+
+    if [[ "$chart" == "nginx-ingress-services" ]]; then
+        # Federation domain is also the SRV record created by the
+        # federation-test-helper service. Maybe we can find a way to make these
+        # differ, so we don't make any silly assumptions in the code.
+        options+=("--set" "config.dns.federator=$FEDERATION_DOMAIN")
+    fi
+
     # default is 5m but may not be enough on a fresh install including cassandra migrations
     TIMEOUT=10m
     set -x
     helm upgrade --install --namespace "${NAMESPACE}" "${NAMESPACE}-${chart}" "${CHARTS_DIR}/${chart}" \
-        $option \
+        ${options[*]} \
         --set brig.config.optSettings.setFederationDomain="$FEDERATION_DOMAIN" \
         --set galley.config.settings.federationDomain="$FEDERATION_DOMAIN" \
         --wait \
