4cb364b831044336fd4893c06091de133c6e29a3
jschaul <jschaul@users.noreply.github.com>
2021-04-12 15:38:41 +0200

    end2end test on search

--- services/brig/test/integration/Federation/User.hs
+++ services/brig/test/integration/Federation/User.hs
@@ -17,6 +17,7 @@
 
 module Federation.User where
 
+import API.Search.Util
 import Bilge (Http, Manager)
 import qualified Brig.Options as BrigOpts
 import Brig.Types
@@ -35,7 +36,7 @@ import Util.Options (Endpoint)
 -- these more end-to-end integration test serve as a way to test the overall
 -- network flow
 --
--- FUTUREWORK(federation): Add tests for these scenarios:
+-- FUTUREWORK(federation): Add tests for these scenarios (but not here in end2end, but in unit/1-backend-integration tests):
 -- - Remote discovery fails
 -- - Remote discovery succeeds but server doesn't exist
 -- - Remote federator fails to respond in many ways (protocol error, timeout, etc.)
@@ -45,7 +46,8 @@ spec _brigOpts mg brig _federator brigTwo =
   pure $
     testGroup
       "brig-federation-user"
-      [ test mg "lookup user by qualified handle on remote backend" $ testHandleLookup brig brigTwo
+      [ test mg "lookup user by qualified handle on remote backend" $ testHandleLookup brig brigTwo,
+        test mg "search users on remote backend" $ testSearchUsers brig brigTwo
       ]
 
 -- | Path covered by this test:
@@ -58,24 +60,42 @@ testHandleLookup :: Brig -> Brig -> Http ()
 testHandleLookup brig brigTwo = do
   -- Create a user on the "other side" using an internal brig endpoint from a
   -- second brig instance in backendTwo (in another namespace in kubernetes)
-  u <- randomUser brigTwo
-  h <- randomHandle
-  void $ putHandle brigTwo (userId u) h
-
-  -- Verify if creating user and setting handle succeeded
-  self <- selfUser <$> getSelfProfile brigTwo (userId u)
-  let handle = fromJust (userHandle self)
-  liftIO $ assertEqual "creating user with handle should return handle" h (fromHandle handle)
-
+  (handle, userBrigTwo) <- createUserWithHandle brigTwo
   -- Get result from brig two for comparison
-  let domain = qDomain $ userQualifiedId self
+  let domain = qDomain $ userQualifiedId userBrigTwo
   resultViaBrigTwo <- getUserInfoFromHandle brigTwo domain handle
 
   -- query the local-namespace brig for a user sitting on the other backend
-  -- which should involve the following network traffic:
-  --
-  -- brig-integration -> brig -> federator -> fed2-federator -> fed2-brig
-  -- (and back)
+  -- (which will exercise the network traffic via two federators to the remote brig)
   resultViaBrigOne <- getUserInfoFromHandle brig domain handle
-  liftIO $ assertEqual "remote handle lookup via federator should work in the happy case" (profileQualifiedId resultViaBrigOne) (userQualifiedId u)
+
+  liftIO $ assertEqual "remote handle lookup via federator should work in the happy case" (profileQualifiedId resultViaBrigOne) (userQualifiedId userBrigTwo)
   liftIO $ assertEqual "querying brig1 or brig2 about the same user should give same result" resultViaBrigTwo resultViaBrigOne
+
+testSearchUsers :: Brig -> Brig -> Http ()
+testSearchUsers brig brigTwo = do
+  -- Create a user on the "other side" using an internal brig endpoint from a
+  -- second brig instance in backendTwo (in another namespace in kubernetes)
+  (handle, userBrigTwo) <- createUserWithHandle brigTwo
+
+  searcher <- userId <$> randomUser brig
+  let expectedUserId = userQualifiedId userBrigTwo
+      searchTerm = (fromHandle handle)
+      domain = (qDomain expectedUserId)
+  liftIO $ putStrLn "search for user on brigTwo (directly)..."
+  assertCanFindWithDomain brigTwo searcher expectedUserId searchTerm domain
+
+  -- exercises multi-backend network traffic
+  liftIO $ putStrLn "search for user on brigOne via federators to remote brig..."
+  assertCanFindWithDomain brig searcher expectedUserId searchTerm domain
+
+createUserWithHandle :: Brig -> Http (Handle, User)
+createUserWithHandle brig = do
+  u <- randomUser brig
+  h <- randomHandle
+  void $ putHandle brig (userId u) h
+  userWithHandle <- selfUser <$> getSelfProfile brig (userId u)
+  -- Verify if creating user and setting handle succeeded
+  let handle = fromJust (userHandle userWithHandle)
+  liftIO $ assertEqual "creating user with handle should return handle" h (fromHandle handle)
+  pure (handle, userWithHandle)
