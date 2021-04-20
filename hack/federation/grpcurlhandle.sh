212431f96ec539ed3b24ac4fb7ddfc62404c4a03
Akshay Mankar <akshay@wire.com>
2021-04-20 11:10:27 +0200

    grpcurlhandle.sh: Add handle back to the grpc call

--- hack/federation/grpcurlhandle.sh
+++ hack/federation/grpcurlhandle.sh
@@ -3,8 +3,8 @@
 command -v grpcurl >/dev/null 2>&1 || { echo >&2 "grpcurl is not installed, aborting. Maybe try ' nix-env -iA nixpkgs.grpcurl '?"; exit 1; }
 
 path=$(echo -n users/by-handle | base64)
-queryK=$(echo -n handle | base64)
-queryV=$(echo -n alice | base64)
+body=$(echo -n "alice" | base64)
+
 TOP_LEVEL="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
 function getHandle() {
@@ -17,7 +17,7 @@ function getHandle() {
     fi
     set -x
     grpcurl -d @ -format json $AUTHORITY $MODE -import-path "${TOP_LEVEL}/libs/wire-api-federation/proto/" -proto router.proto "$HOST:$PORT" wire.federator.Inward/call <<EOM
-{"component": "Brig", "path": "$path"}
+{"component": "Brig", "path": "$path", "body": "$body"}
 EOM
     { set +x; } 2>/dev/null # stop outputting commands and don't print the set +x line
     echo "===|"
