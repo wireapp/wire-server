53c5e37345e1725b64116c29cc13caf73c1d9def
Akshay Mankar <akshay@wire.com>
2021-04-26 11:37:22 +0200

    Federator: Log error when DNS lookup errors

--- services/federator/src/Federator/Discovery.hs
+++ services/federator/src/Federator/Discovery.hs
@@ -26,6 +26,9 @@ import Polysemy
 import Wire.Network.DNS.Effect (DNSLookup)
 import qualified Wire.Network.DNS.Effect as Lookup
 import Wire.Network.DNS.SRV (SrvEntry (srvTarget), SrvResponse (..), SrvTarget)
+import Polysemy.TinyLog (TinyLog)
+import qualified Polysemy.TinyLog as TinyLog
+import qualified System.Logger.Class as Log
 
 data LookupError
   = LookupErrorSrvNotAvailable ByteString
@@ -37,14 +40,14 @@ data DiscoverFederator m a where
 
 makeSem ''DiscoverFederator
 
-runFederatorDiscovery :: Members '[DNSLookup] r => Sem (DiscoverFederator ': r) a -> Sem r a
+runFederatorDiscovery :: Members '[DNSLookup, TinyLog] r => Sem (DiscoverFederator ': r) a -> Sem r a
 runFederatorDiscovery = interpret $ \(DiscoverFederator d) ->
   -- FUTUREWORK(federation): This string conversation is probably wrong, we should encode this
   -- using IDNA encoding or expect domain to be bytestring everywhere
   let domainSrv = cs $ "_wire-server-federator._tcp." <> domainText d
    in lookupDomainByDNS domainSrv
 
-lookupDomainByDNS :: Member DNSLookup r => ByteString -> Sem r (Either LookupError SrvTarget)
+lookupDomainByDNS :: Members '[DNSLookup, TinyLog] r => ByteString -> Sem r (Either LookupError SrvTarget)
 lookupDomainByDNS domainSrv = do
   res <- Lookup.lookupSRV domainSrv
   case res of
@@ -56,4 +59,6 @@ lookupDomainByDNS domainSrv = do
     SrvNotAvailable -> pure $ Left $ LookupErrorSrvNotAvailable domainSrv
     -- Name error also means that the record is not available
     SrvResponseError DNS.NameError -> pure $ Left $ LookupErrorSrvNotAvailable domainSrv
-    SrvResponseError _ -> pure $ Left $ LookupErrorDNSError domainSrv
+    SrvResponseError err -> do
+      TinyLog.err $ Log.msg ("DNS Lookup failed" :: ByteString) . Log.field "error" (show err)
+      pure $ Left $ LookupErrorDNSError domainSrv
