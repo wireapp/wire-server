475753cd5dbfdab87f101c8c9a1c176eb9aef9c0
jschaul <jschaul@users.noreply.github.com>
2021-04-27 00:09:02 +0200

    Add originDomain to federation API (#1447)
    To eventually support server-to-server authentication, the first step implemented in this PR is to add an originating domain to federated requests. At the moment this domain could be arbitrarily set (i.e. server2server authentication is not yet implemented here). But in this PR, some validation logic compares this domain to the allowList for incoming requests at federator level, if configured.
    
    That domain can then in the future be used for:
    
    * authenticating the domain with DNS/SRV or other means to validate that the request indeed comes from the claimed sender
    * independent of authentication/authorization concerns, the domain may be useful for RPC calls that need to write data (e.g. to create a conversation).
    
    In this PR, a field `originDomain` is added to the Request object in the protobuf definition, which is then turned into a `Wire-Origin-Domain` header when sending the call to a local component via plain http, in case that other component wants to make use of that header.

--- services/federator/src/Federator/Validation.hs
+++ services/federator/src/Federator/Validation.hs
@@ -15,21 +15,43 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-module Federator.Util
+module Federator.Validation
   ( federateWith,
+    federateWith',
   )
 where
 
 import Control.Lens (view)
-import Data.Domain (Domain)
+import Data.Domain (Domain, domainText, mkDomain)
+import Data.Either.Combinators (mapLeft)
+import Data.String.Conversions (cs)
 import Federator.Options
 import Imports
 import Polysemy (Members, Sem)
+import qualified Polysemy.Error as Polysemy
 import qualified Polysemy.Reader as Polysemy
 
+-- | Validates an already-parsed domain against the allowList using the federator
+-- startup configuration. See also federateWith' for an alternative
 federateWith :: Members '[Polysemy.Reader RunSettings] r => Domain -> Sem r Bool
 federateWith targetDomain = do
   strategy <- view federationStrategy <$> Polysemy.ask
   pure $ case strategy of
     AllowAll -> True
     AllowList (AllowedDomains domains) -> targetDomain `elem` domains
+
+-- | Validates an unknown domain string against the allowList using the federator startup configuration
+federateWith' :: Members '[Polysemy.Reader RunSettings] r => Text -> Sem r (Either Text Domain)
+federateWith' targetDomain = Polysemy.runError $ do
+  parsedDomain <- Polysemy.fromEither $ mapLeft (errDomainParsing targetDomain) $ mkDomain targetDomain
+  allowCheck <- federateWith parsedDomain
+  unless allowCheck $
+    Polysemy.throw (errAllowList parsedDomain)
+  pure parsedDomain
+
+-- FUTUREWORK: create error data types and adjust InwardResponseErr and OutwardResponseError
+errDomainParsing :: Text -> String -> Text
+errDomainParsing domain err = "Domain parse failure for [" <> domain <> "]: " <> cs err
+
+errAllowList :: Domain -> Text
+errAllowList domain = "Origin domain [" <> domainText domain <> "] not in the federation allow list"
