b854685d6291efd58f20f2d554ac2ae6ee510ca7
Paolo Capriotti <paolo@capriotti.io>
2021-04-26 13:53:41 +0200

    Add coerce and handle, and some haddock.

--- libs/wire-api/src/Data/Swagger/Typed.hs
+++ libs/wire-api/src/Data/Swagger/Typed.hs
@@ -13,6 +13,8 @@ module Data.Swagger.Typed
     unnamed,
     array,
     enum,
+    handle,
+    coerce,
     schema,
     untypedSchema,
   )
@@ -30,6 +32,41 @@ import Imports
 type Declare = S.Declare (S.Definitions S.Schema)
 
 -- | A typed Swagger schema
+--
+-- /Note/: the 'Applicative' and 'Alternative' instances of 'Schema'
+-- have the same implementation, but different types. For example, the
+-- binary operation of 'Applicative' is:
+-- @
+-- (<*>) :: f (a -> b) -> f a -> f b
+-- @
+-- while that of 'Alternative' is:
+-- @
+-- (<|>) :: f a -> f a -> f a.
+-- @
+-- The simple reason for this distinction is that the types will line
+-- up with the 'Applicative' instance in some cases, and with the
+-- 'Alternative' instance in others, so one needs both.
+--
+-- The deeper reason is that they are conceptually different. A schema like:
+-- @
+-- (,) <$> schemaA <*> schemaB
+-- @
+-- means that you will can have fields from @A@ and @B@ in the
+-- resulting object, while
+-- @
+-- schemaA <|> schemaB
+-- @
+-- is only allowed to have fields from @A@ /or/ from @B@.
+--
+-- Unfortunately, there does not seem to be a way to express the
+-- latter constraint in the resulting schema, and that's why the
+-- implementations are the same. It seems this should be possible in
+-- openapi3, thought.
+--
+-- The difference between 'Applicative' and 'Alternative' also becomes
+-- important when we think about using a typed schema specification to
+-- derive FromJSON instances. Then they would respectively implement
+-- sequencing and alternation of the corresponding parsers.
 newtype Schema (a :: *) = Schema (Declare S.Schema)
   deriving (Functor)
 
@@ -89,9 +126,25 @@ instance HasSchemaRef NamedSchema where
       S.NamedSchema Nothing s ->
         pure (S.Inline s)
 
+-- | Just like 'field', but with a trivial "modifier".
 field' :: HasSchemaRef f => Text -> f a -> Schema a
 field' name = field name id
 
+-- | A schema for an object with a single field.
+--
+-- Multiple fields can be combined into a single object using the
+-- 'Applicative' instance of 'Schema'. Note that 'field' accepts both
+-- a 'NamedSchema' and 'Schema' for the inner field schema.
+--
+-- The second argument is an arbitrary "modifier" function from
+-- 'S.Schema' to itself, to be applied to the field schema. The most
+-- common use for this is to set some values, such as its
+-- description. For example, using lenses:
+-- @
+--     field "name" (description ?~ "The name of this object") schema
+-- @
+-- creates a field called "name" with the given description, with a
+-- type that is instance of 'TypedSchema'.
 field :: HasSchemaRef f => Text -> (S.Schema -> S.Schema) -> f a -> Schema a
 field name f s = Schema $ do
   ref <- schemaRef s
@@ -101,6 +154,10 @@ field name f s = Schema $ do
       & S.type_ ?~ S.SwaggerObject
       & S.properties . at name ?~ ref'
 
+-- | A schema for an array of items.
+--
+-- Just like 'field', it can take both 'NamedSchema' and 'Schema'
+-- definitions.
 array :: HasSchemaRef f => f a -> Schema [a]
 array s = Schema $ do
   ref <- schemaRef s
@@ -109,6 +166,13 @@ array s = Schema $ do
       & S.type_ ?~ S.SwaggerArray
       & S.items ?~ S.SwaggerItemsObject ref
 
+-- | A schema for an enumeration type.
+--
+-- This takes as argument a list of pairs @(value, schema)@ where
+-- @value@ is one of the possible JSON values of the enumeration, and
+-- @schema@ is the corresponding Swagger schema. At the moment, the
+-- schemas are ignored, but they could be used to derive a FromJSON
+-- instance.
 enum :: [(Value, Schema a)] -> Schema a
 enum ss =
   Schema $
@@ -117,22 +181,62 @@ enum ss =
         & S.type_ ?~ S.SwaggerString
         & S.enum_ ?~ map fst ss
 
+-- | Used for schemas that can potentially fail.
+handle :: Schema (Either Text a) -> Schema a
+handle = coerce
+
+-- | Coerce schema to a different type.
+--
+-- This is safe as far as the Swagger schema generation is concerned,
+-- but it makes it impossible to derive a safe 'FromJSON' instance
+-- from a typed schema definition.
+coerce :: Schema a -> Schema b
+coerce (Schema s) = Schema s
+
+-- | Turn a 'Schema' into a 'NamedSchema' with the given name.
 named :: Text -> Schema a -> NamedSchema a
 named name (Schema s) = NamedSchema (S.NamedSchema (Just name) <$> s) Nothing
 
+-- | Turn a 'NamedSchema' into a 'Schema'.
+--
+-- This is mostly useful to attach descriptions to field, since fields
+-- containing referenced schemas cannot have a description. For example:
+-- @
+--     field "elements" (description ?~ "the elements of this object") (unnamed schema)
+-- @
 unnamed :: NamedSchema a -> Schema a
 unnamed (NamedSchema s _) = Schema (view S.schema <$> s)
 
+-- | Typed schema for a type that is instance of 'ToTypedSchema'
+--
+-- Reuse the typed schema definition which is part of the
+-- 'ToTypedSchema' instance of a type. Using 'schema', one can split a
+-- complicated shema definition into manageable parts by defining
+-- instances of 'ToTypedSchema' for the various types involved.
 schema :: ToTypedSchema a => NamedSchema a
 schema = toTypedSchema Proxy
 
+-- | Derive a typed schema from an untyped schema definition
+--
+-- This can be used for types that have a 'ToSchema' instance but lack
+-- a 'ToTypedSchema' instance.
 untypedSchema :: forall a. S.ToSchema a => NamedSchema a
 untypedSchema = NamedSchema (pure (S.toNamedSchema (Proxy :: Proxy a))) Nothing
 
+-- | A type with a canonical typed schema definition.
 class ToTypedSchema a where
   toTypedSchema :: Proxy a -> NamedSchema a
 
--- to use with deriving via
+-- | Wrapper to turn 'ToTypedSchema' into 'ToSchema'.
+--
+-- This is mostly for use in a 'deriving via' clause. For example:
+-- @
+--     data MyType = ...
+--       deriving ToSchema via TypedSchema MyType
+--
+--     instance ToTypedSchema MyType where
+--       toTypedSchema _ = ...
+-- @
 newtype TypedSchema a = TypedSchema a
 
 instance ToTypedSchema a => S.ToSchema (TypedSchema a) where
