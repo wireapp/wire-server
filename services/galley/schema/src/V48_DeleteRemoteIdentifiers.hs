80d031bfc7b1f60c73c4043a744d4f085b5e7ac6
jschaul <jschaul@users.noreply.github.com>
2021-05-04 17:50:29 +0200

    fixup

--- services/galley/schema/src/V48_DeleteRemoteIdentifiers.hs
+++ services/galley/schema/src/V48_DeleteRemoteIdentifiers.hs
@@ -15,7 +15,7 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-module V48_ChangeRemoteIdentifiers
+module V48_DeleteRemoteIdentifiers
   ( migration,
   )
 where
