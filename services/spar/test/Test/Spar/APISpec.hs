3d1c091d65212502003e059376ea057db30db3a1
Matthias Fischmann <mf@zerobuzz.net>
2021-05-05 14:52:04 +0200

    Let's see if that fails.

--- services/spar/test/Test/Spar/APISpec.hs
+++ services/spar/test/Test/Spar/APISpec.hs
@@ -32,15 +32,16 @@ import Imports
 import Servant.Swagger (validateEveryToJSON)
 import Spar.API as API
 import Spar.Types (IdPMetadataInfo (IdPMetadataValue), SsoSettings)
-import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
+import Test.Hspec (Spec, describe, focus, it, shouldBe, shouldSatisfy)
 import Test.QuickCheck (property)
 
 spec :: Spec
 spec = do
   -- Note: SCIM types are not validated because their content-type is 'SCIM'.
   validateEveryToJSON (Proxy @API.OutsideWorldAPI)
-  it "api consistency" $ do
-    pathsConsistencyCheck (routesToPaths @API.API) `shouldBe` mempty
+  focus $
+    it "api consistency" $ do
+      pathsConsistencyCheck (routesToPaths @API.API) `shouldBe` mempty
   it "roundtrip: IdPMetadataInfo" . property $ \(val :: IdPMetadataInfo) -> do
     let withoutRaw (IdPMetadataValue _ x) = x
     (withoutRaw <$> (Aeson.eitherDecode . Aeson.encode) val) `shouldBe` Right (withoutRaw val)
