079ca3449032d5612f4255b33f637477367e3623
Stefan Matting <stefan@wire.com>
2021-05-10 16:37:44 +0200

    remove unused pragma

--- services/galley/src/Galley/API/Mapping.hs
+++ services/galley/src/Galley/API/Mapping.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+
 -- This file is part of the Wire Server implementation.
 --
 -- Copyright (C) 2020 Wire Swiss GmbH <opensource@wire.com>
@@ -15,7 +16,6 @@
 --
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
-{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 module Galley.API.Mapping where
 
