35abb6c4b6d58e2ac535d6df088b607b8f98bd76
Stefan Matting <stefan@wire.com>
2021-05-28 14:54:01 +0200

    remove superfluous pragma

--- services/galley/src/Galley/Intra/Client.hs
+++ services/galley/src/Galley/Intra/Client.hs
@@ -14,7 +14,6 @@
 --
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
-{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
 
 module Galley.Intra.Client
   ( lookupClients,
