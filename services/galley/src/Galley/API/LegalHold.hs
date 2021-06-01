d080823c734af0e382cf36c672700a12af206640
Stefan Matting <stefan@wire.com>
2021-06-01 13:21:06 +0200

    Revert "Recover lost comment."
    This reverts commit c57f37831569eb42f7ef11a266560ada69128d76.

--- services/galley/src/Galley/API/LegalHold.hs
+++ services/galley/src/Galley/API/LegalHold.hs
@@ -82,10 +82,7 @@ isLegalHoldEnabledForTeam tid = do
         Just Public.TeamFeatureDisabled -> False
         Nothing -> False
     FeatureLegalHoldWhitelistTeamsAndImplicitConsent -> do
-      view legalholdWhitelist
-        <&> maybe
-          False {- reasonable default, even though this is impossible due to "Galley.Options.validateOpts" -}
-          (tid `elem`)
+      view legalholdWhitelist <&> maybe False (tid `elem`)
 
 createSettingsH :: UserId ::: TeamId ::: JsonRequest Public.NewLegalHoldService ::: JSON -> Galley Response
 createSettingsH (zusr ::: tid ::: req ::: _) = do
