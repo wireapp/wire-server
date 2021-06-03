8bc59041aefb396bf975cd8e5cd94701b1365cd5
Akshay Mankar <akshay@wire.com>
2021-06-03 15:12:45 +0200

    ErrorDescription: Rename status to statusCode
    It is referred by status or code depending on context

--- libs/wire-api/src/Wire/API/ErrorDescription.hs
+++ libs/wire-api/src/Wire/API/ErrorDescription.hs
@@ -17,25 +17,25 @@ import Servant.Swagger.Internal
 
 -- FUTUREWORK: Ponder about elevating label and messge to the type level. If all
 -- errors are static, there is probably no point in having them at value level.
-data ErrorDescription (status :: Nat) (desc :: Symbol) = ErrorDescription
+data ErrorDescription (statusCode :: Nat) (desc :: Symbol) = ErrorDescription
   { label :: !Text,
     message :: !Text
   }
   deriving stock (Show, Typeable)
-  deriving (A.ToJSON, A.FromJSON, Swagger.ToSchema) via Schema (ErrorDescription status desc)
+  deriving (A.ToJSON, A.FromJSON, Swagger.ToSchema) via Schema (ErrorDescription statusCode desc)
 
-instance (KnownNat status, KnownSymbol desc) => ToSchema (ErrorDescription status desc) where
+instance (KnownNat statusCode, KnownSymbol desc) => ToSchema (ErrorDescription statusCode desc) where
   schema =
     object "ErrorDescription" $
       ErrorDescription
         <$> label .= field "label" schema
         <*> message .= field "message" schema
-        <* const (natVal (Proxy @status)) .= field "code" schema
+        <* const (natVal (Proxy @statusCode)) .= field "code" schema
 
 -- | This insance works with 'UVerb' only becaue of the following overlapping
 -- instance for 'UVerb method cs (ErrorDescription status desc ': rest))'
-instance (KnownNat status, KnownSymbol desc, AllAccept cs, SwaggerMethod method) => HasSwagger (Verb method status cs (ErrorDescription status desc)) where
-  toSwagger _ = overrrideResponseDesc $ mkEndpoint "/" (Proxy @(Verb method status cs (Headers '[] (ErrorDescription status desc))))
+instance (KnownNat statusCode, KnownSymbol desc, AllAccept cs, SwaggerMethod method) => HasSwagger (Verb method statusCode cs (ErrorDescription statusCode desc)) where
+  toSwagger _ = overrrideResponseDesc $ mkEndpoint "/" (Proxy @(Verb method statusCode cs (Headers '[] (ErrorDescription statusCode desc))))
     where
       overrrideResponseDesc :: Swagger -> Swagger
       overrrideResponseDesc =
@@ -51,11 +51,11 @@ instance (KnownNat status, KnownSymbol desc, AllAccept cs, SwaggerMethod method)
           . over (Swagger.options . _Just) overrideOp
       overrideOp :: Swagger.Operation -> Swagger.Operation
       overrideOp =
-        Swagger.responses . Swagger.responses . at (fromInteger $ natVal (Proxy @status))
+        Swagger.responses . Swagger.responses . at (fromInteger $ natVal (Proxy @statusCode))
           ?~ Swagger.Inline
             ( mempty
                 & Swagger.description .~ Text.pack (symbolVal (Proxy @desc))
-                & Swagger.schema ?~ Swagger.toSchemaRef (Proxy @(ErrorDescription status desc))
+                & Swagger.schema ?~ Swagger.toSchemaRef (Proxy @(ErrorDescription statusCode desc))
             )
 
 -- | This is a copy of instance for 'UVerb method cs (a:as)', but without this
