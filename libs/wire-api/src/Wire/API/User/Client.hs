d9d5f36b594f4c11d130c4d46540a2ccd7ed0fca
Akshay Mankar <akshay@wire.com>
2021-06-24 09:28:30 +0200

    Fix typo

--- libs/wire-api/src/Wire/API/User/Client.hs
+++ libs/wire-api/src/Wire/API/User/Client.hs
@@ -19,7 +19,7 @@
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
 module Wire.API.User.Client
-  ( -- * ClientCapabiliton
+  ( -- * ClientCapability
     ClientCapability (..),
     ClientCapabilityList (..),
 
