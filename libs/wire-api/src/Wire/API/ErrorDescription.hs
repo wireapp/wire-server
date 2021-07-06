32c79cb72e3b1fb50b3b9c7156290061917b3b3d
Paolo Capriotti <paolo@capriotti.io>
2021-07-06 16:17:12 +0200

    Add CanThrow annotation for servant endpoints

--- libs/wire-api/src/Wire/API/ErrorDescription.hs
+++ libs/wire-api/src/Wire/API/ErrorDescription.hs
@@ -7,7 +7,7 @@ import Data.Schema
 import Data.Swagger (Swagger)
 import qualified Data.Swagger as Swagger
 import qualified Data.Text as Text
-import GHC.TypeLits (KnownNat, KnownSymbol, Symbol, natVal, symbolVal)
+import GHC.TypeLits (KnownSymbol, Symbol, natVal, symbolVal)
 import GHC.TypeNats (Nat)
 import Imports hiding (head)
 import Servant hiding (Handler, JSON, addHeader, contentType, respond)
@@ -15,11 +15,56 @@ import Servant.API.Status (KnownStatus)
 import Servant.Swagger.Internal
 import Wire.API.ServantSwagger
 
+-- This can be added to an endpoint to document a possible failure
+-- case outside its return type (usually through an exception).
+data CanThrow err
+
+instance
+  (HasSwagger api, KnownStatus code, KnownSymbol desc) =>
+  HasSwagger (CanThrow (ErrorDescription code desc) :> api)
+  where
+  toSwagger _ = errorDescriptionAddToSwagger @code @desc (toSwagger (Proxy @api))
+
+-- CanThrow annotations are ignored by servant
+instance
+  ( HasContextEntry (ctx .++ DefaultErrorFormatters) ErrorFormatters,
+    HasServer api ctx
+  ) =>
+  HasServer (CanThrow t :> api) ctx
+  where
+  type ServerT (CanThrow t :> api) m = ServerT api m
+
+  route _ = route (Proxy @api)
+  hoistServerWithContext _ = hoistServerWithContext (Proxy @api)
+
+errorDescriptionAddToSwagger ::
+  forall (code :: Nat) (desc :: Symbol).
+  (KnownStatus code, KnownSymbol desc) =>
+  Swagger ->
+  Swagger
+errorDescriptionAddToSwagger =
+  over (Swagger.paths . traverse) overridePathItem
+  where
+    overridePathItem :: Swagger.PathItem -> Swagger.PathItem
+    overridePathItem =
+      over (Swagger.get . _Just) overrideOp
+        . over (Swagger.post . _Just) overrideOp
+        . over (Swagger.put . _Just) overrideOp
+        . over (Swagger.head_ . _Just) overrideOp
+        . over (Swagger.patch . _Just) overrideOp
+        . over (Swagger.delete . _Just) overrideOp
+        . over (Swagger.options . _Just) overrideOp
+    overrideOp :: Swagger.Operation -> Swagger.Operation
+    overrideOp =
+      Swagger.responses . Swagger.responses . at (fromInteger $ natVal (Proxy @code))
+        ?~ Swagger.Inline
+          ( mempty
+              & Swagger.description .~ Text.pack (symbolVal (Proxy @desc))
+              & Swagger.schema ?~ Swagger.toSchemaRef (Proxy @(ErrorDescription code desc))
+          )
+
 -- FUTUREWORK: Ponder about elevating label and messge to the type level. If all
 -- errors are static, there is probably no point in having them at value level.
---
--- This type can also be added to an endpoint to document a possible failure
--- case outside its return type (usually through an exception).
 data ErrorDescription (statusCode :: Nat) (desc :: Symbol) = ErrorDescription
   { label :: !Text,
     message :: !Text
@@ -27,7 +72,7 @@ data ErrorDescription (statusCode :: Nat) (desc :: Symbol) = ErrorDescription
   deriving stock (Show, Typeable)
   deriving (A.ToJSON, A.FromJSON, Swagger.ToSchema) via Schema (ErrorDescription statusCode desc)
 
-instance (KnownNat statusCode, KnownSymbol desc) => ToSchema (ErrorDescription statusCode desc) where
+instance (KnownStatus statusCode, KnownSymbol desc) => ToSchema (ErrorDescription statusCode desc) where
   schema =
     object "ErrorDescription" $
       ErrorDescription
@@ -37,43 +82,25 @@ instance (KnownNat statusCode, KnownSymbol desc) => ToSchema (ErrorDescription s
 
 -- | This instance works with 'UVerb' only becaue of the following overlapping
 -- instance for 'UVerb method cs (ErrorDescription status desc ': rest))'
-instance (KnownNat statusCode, KnownSymbol desc, AllAccept cs, SwaggerMethod method) => HasSwagger (Verb method statusCode cs (ErrorDescription statusCode desc)) where
-  toSwagger _ = overrrideResponseDesc $ mkEndpoint "/" (Proxy @(Verb method statusCode cs (Headers '[] (ErrorDescription statusCode desc))))
+instance (KnownStatus statusCode, KnownSymbol desc, AllAccept cs, SwaggerMethod method) => HasSwagger (Verb method statusCode cs (ErrorDescription statusCode desc)) where
+  toSwagger _ = overrideResponseDesc $ mkEndpoint "/" (Proxy @(Verb method statusCode cs (Headers '[] (ErrorDescription statusCode desc))))
     where
-      overrrideResponseDesc :: Swagger -> Swagger
-      overrrideResponseDesc =
-        over (Swagger.paths . at "/" . _Just) overridePathItem
-      overridePathItem :: Swagger.PathItem -> Swagger.PathItem
-      overridePathItem =
-        over (Swagger.get . _Just) overrideOp
-          . over (Swagger.post . _Just) overrideOp
-          . over (Swagger.put . _Just) overrideOp
-          . over (Swagger.head_ . _Just) overrideOp
-          . over (Swagger.patch . _Just) overrideOp
-          . over (Swagger.delete . _Just) overrideOp
-          . over (Swagger.options . _Just) overrideOp
-      overrideOp :: Swagger.Operation -> Swagger.Operation
-      overrideOp =
-        Swagger.responses . Swagger.responses . at (fromInteger $ natVal (Proxy @statusCode))
-          ?~ Swagger.Inline
-            ( mempty
-                & Swagger.description .~ Text.pack (symbolVal (Proxy @desc))
-                & Swagger.schema ?~ Swagger.toSchemaRef (Proxy @(ErrorDescription statusCode desc))
-            )
+      overrideResponseDesc :: Swagger -> Swagger
+      overrideResponseDesc = errorDescriptionAddToSwagger @statusCode @desc
 
 -- | This is a copy of instance for 'UVerb method cs (a:as)', but without this
 -- things don't work because the instance defined in the library is already
 -- compiled with the now overlapped version of `Verb method cs a` and won't
 -- pickup the above instance.
 instance
-  (KnownNat status, KnownSymbol desc, AllAccept cs, SwaggerMethod method, HasSwagger (UVerb method cs rest)) =>
+  (KnownStatus status, KnownSymbol desc, AllAccept cs, SwaggerMethod method, HasSwagger (UVerb method cs rest)) =>
   HasSwagger (UVerb method cs (ErrorDescription status desc ': rest))
   where
   toSwagger _ =
     toSwagger (Proxy @(Verb method (StatusOf (ErrorDescription status desc)) cs (ErrorDescription status desc)))
       `combineSwagger` toSwagger (Proxy @(UVerb method cs rest))
 
-instance (KnownNat status, KnownStatus status) => HasStatus (ErrorDescription status desc) where
+instance (KnownStatus status, KnownStatus status) => HasStatus (ErrorDescription status desc) where
   type StatusOf (ErrorDescription status desc) = status
 
 -- * Errors
