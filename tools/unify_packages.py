da53376d72170d50b670533b46da85ebbcff13b8
Stefan Matting <stefan@wire.com>
2021-07-09 17:18:52 +0200

    get dag from stack.yaml

--- tools/unify_packages.py
+++ tools/unify_packages.py
@@ -98,6 +98,19 @@ def get_dependencies(obj, path, remove=[]):
     deps = set(get_path(obj, path + ['dependencies']) or [])
     return list(deps - set(remove))
 
+def get_all_dependencies(package):
+    all_deps = []
+
+    all_deps.extend([parse_simple_dep(d) for d in (get_path(package, ['dependencies']) or [])])
+
+    executables = list(package.get('executables', {}).items()) + \
+        list(package.get('tests', {}).items())
+
+    for name, exe in executables:
+        all_deps.extend([parse_simple_dep(d) for d in (get_path(exe, ['dependencies']) or [])])
+
+    return list(set(all_deps))
+
 def merge_projects(dir_source, dir_target):
 
     package_source = read_yaml(os.path.join(dir_source, 'package.yaml'))
@@ -153,83 +166,37 @@ def merge_projects(dir_source, dir_target):
     write_yaml(package_target, os.path.join(dir_target, 'package.yaml'))
 
 
+def read_dep_dag():
+    '''
+    Returns all internal packages in topological order w.r.t to dependency
+    '''
+    dag = {}
+    package_to_dir = {}
+    for package_dir in read_yaml('stack.yaml')['packages']:
+        if package_dir == 'wire-server':
+            continue
+        fn = os.path.join(package_dir, 'package.yaml')
+        if not os.path.exists(fn):
+            print(f'{fn} does not exist')
+            continue
+        package = read_yaml(fn)
+        deps = get_all_dependencies(package)
+        dag[package['name']] = set(deps)
+        package_to_dir[package['name']] = package_dir
+
+    ordered = list(reversed(toposort_flatten(dag)))
+    return [package_to_dir[p] for p in ordered if p in package_to_dir]
+
 def main():
     os.makedirs('wire-server', exist_ok=True)
+
     shutil.copyfile('package_start.yaml', 'wire-server/package.yaml')
 
-    packages_topo_order = ['services/brig', 'services/galley']
+    # packages_topo_order = ['services/brig', 'services/galley']
+    packages_topo_order = read_dep_dag()
 
     for package in packages_topo_order:
         merge_projects(package, 'wire-server')
-
-
-def between_lines(line_start, line_stop, lines):
-    record = False
-    result = []
-    for line in lines:
-        if line == line_stop:
-            record = False
-        if record:
-            yield line
-        if line == line_start:
-            record = True
-
-def test_parse_deps_dag():
-    with open('/tmp/build.txt') as f:
-        dag = parse_deps_dag(f.read())
-        return list(reversed(toposort_flatten(dag)))
-
-def remove_version(package):
-    m = re.match('(.*)-([0-9.])+$')
-
-# parses the output of stack build --dry-run --fast brig 2> /tmp/build.txt 1>&2
-# ['brig-1.35.0',
-# 'tasty-cannon-0.4.0',
-# 'spar-0.1',
-# 'wire-api-federation-0.1.0',
-# 'gundeck-types-1.45.0',
-# 'galley-types-0.81.0',
-# 'cargohold-types-1.5.0',
-# 'brig-types-1.35.0',
-# 'wire-api-0.1.0',
-# 'wai-utilities-0.16.1',
-# 'types-common-journal-0.1.0',
-# 'bilge-0.22.0',
-# 'zauth-0.10.3',
-# 'types-common-0.16.0',
-# 'metrics-wai-0.5.7',
-# 'types-common-aws-0.16.0',
-# 'ssl-util-0.1.0',
-# 'sodium-crypto-sign-0.1.2',
-# 'schema-profunctor-0.1.0',
-# 'ropes-0.4.20',
-# 'polysemy-wire-zoo-0.1.0',
-# 'metrics-core-0.3.2',
-# 'extended-0.1.0',
-# 'dns-util-0.1.0',
-# 'deriving-swagger2-0.1.0',
-# 'cassandra-util-0.16.5',
-# 'wire-message-proto-lens-0.1.0',
-# 'imports-0.1.0',
-# 'hscim-0.3.4']
-def parse_deps_dag(output):
-    lines = output.splitlines()
-    result = {}
-    for line in between_lines('Would build:', '', lines):
-        package, deps = parse_line(line)
-        result[package] = set(deps)
-    return result
-
-def parse_line(line):
-    m = re.match('^([^:]+)', line)
-    package, = m.groups()
-    m2 = re.match('.*after: (.+)', line)
-    if m2 is not None:
-        depss, = m2.groups()
-        deps = depss.split(',')
-    else:
-        deps = []
-    return package, deps
 
 if __name__ == '__main__':
     main()
