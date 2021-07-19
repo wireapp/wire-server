7304697bbd957fd5f89c0bebf70ac01e554a19a3
jschaul <jschaul@users.noreply.github.com>
2021-07-19 16:01:29 +0200

    dynamically generate TLS certificates for use in integration (end2end) tests

--- hack/bin/selfsigned.sh
+++ hack/bin/selfsigned.sh
@@ -19,23 +19,54 @@ echo '{
         "algo": "rsa",
         "size": 2048
     }
-}' > "$CSR"
+}' >"$CSR"
 
 # generate CA key and cert
 cfssl gencert -initca "$CSR" | cfssljson -bare "$OUTPUTNAME_CA"
+# TODO: only generate CA once!
 
 echo '{
     "key": {
         "algo": "rsa",
         "size": 2048
     }
-}' > "$CSR"
+}' >"$CSR"
+
+# keep this in sync with hack/bin/integration-setup.sh
+# TODO take variable as argument?
+#
+# For federation end2end tests, only the
+# 'federation-test-helper.$NAMESPACE.svc.cluster.local' is necessary for
+# ingress->federator traffic. For other potential traffic in the integration
+# tests of the future, we use a wildcard certificate here.
+FEDERATION_DOMAIN="*.$NAMESPACE.svc.cluster.local"
 
 # generate cert and key based on CA given comma-separated hostnames as SANs
-cfssl gencert -ca "$OUTPUTNAME_CA.pem" -ca-key "$OUTPUTNAME_CA-key.pem" -hostname=*.integration.example.com,localhost "$CSR" | cfssljson -bare "$OUTPUTNAME_LEAF_CERT"
+cfssl gencert -ca "$OUTPUTNAME_CA.pem" -ca-key "$OUTPUTNAME_CA-key.pem" -hostname="$FEDERATION_DOMAIN" "$CSR" | cfssljson -bare "$OUTPUTNAME_LEAF_CERT"
+# cfssl gencert -ca "$OUTPUTNAME_CA.pem" -ca-key "$OUTPUTNAME_CA-key.pem" -hostname=""*.integration.example.com,localhost "$CSR" | cfssljson -bare "$OUTPUTNAME_LEAF_CERT"
+
+# the following yaml override file is needed as an override to
+# nginx-ingress-services helm chart
+# for domain A, ingress@A needs cert+key for A
+{
+    echo "secrets:"
+    echo "  tlsWildcardCert: |"
+    sed -e 's/^/    /' $OUTPUTNAME_LEAF_CERT.pem
+    echo "  tlsWildcardKey: |"
+    sed -e 's/^/    /' $OUTPUTNAME_LEAF_CERT-key.pem
+} >$OUTPUTNAME_LEAF_CERT.yaml
+
+# the following yaml override file is needed as an override to
+# the wire-server (federator) helm chart
+# for domain A, federator@A needs the CA for B
+{
+    echo "federator:"
+    echo "  remoteCAContents: |"
+    sed -e 's/^/    /' $OUTPUTNAME_CA.pem
+} >$OUTPUTNAME_CA.yaml
 
 # cleanup unneeded files
 rm "$OUTPUTNAME_LEAF_CERT.csr"
 rm "$OUTPUTNAME_CA.csr"
-
-
+rm "$OUTPUTNAME_LEAF_CERT.pem"
+rm "$OUTPUTNAME_LEAF_CERT-key.pem"
