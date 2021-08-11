efbd7c33a07a4c1fce35ef4b66a8cf2e9f95ef52
Akshay Mankar <akshay@wire.com>
2021-08-11 15:11:45 +0200

    Add a TODO to verify old code

--- services/galley/src/Galley/API/Query.hs
+++ services/galley/src/Galley/API/Query.hs
@@ -95,7 +95,7 @@ getConversation zusr cnv = do
   localDomain <- viewFederationDomain
   if qDomain cnv == localDomain
     then getUnqualifiedConversation zusr (qUnqualified cnv)
-    else getRemoteConversation zusr (toRemote cnv)
+    else getRemoteConversation zusr (toRemote cnv) -- TODO: This looks wrong, we shouldn't get a remote conversation is the requesting user is not part of that conversation.
 
 getRemoteConversation :: UserId -> Remote ConvId -> Galley Public.Conversation
 getRemoteConversation zusr remoteConvId = do
