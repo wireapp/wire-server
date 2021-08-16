476e3209f84ac5b1776116cb402636c9ac0ca7fd
Akshay Mankar <akshay@wire.com>
2021-08-16 10:48:11 +0200

    serve-charts.sh: Make compatible with nixos

--- hack/bin/serve-charts.sh
+++ hack/bin/serve-charts.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 : ${HELM_SERVER_PORT:=4001}
 
 # get rid of all helm repositories
-helm repo list -o json | jq -r '.[] | .name' | xargs -I% helm repo remove %
+# We need to deal with helm repo list failing because of https://github.com/helm/helm/issues/10028
+(helm repo list -o json || echo '[]') | jq -r '.[] | .name' | xargs -I% helm repo remove %
 
 cd "$(dirname "$BASH_SOURCE[0]")/../../.local/charts"
 for chart in $@; do
