fc2fc1b8ae70a3d6b61b7bfacba5b8210a88d476
Marko Dimjašević <marko.dimjasevic@wire.com>
2021-08-31 15:07:31 +0200

    Clean up JSON golden tests for AssetRetention

--- libs/wire-api/test/unit/Test/Wire/API/Golden/Generated/AssetRetention_user.hs
+++ libs/wire-api/test/unit/Test/Wire/API/Golden/Generated/AssetRetention_user.hs
@@ -29,53 +29,8 @@ testObject_AssetRetention_user_2 = AssetEternal
 testObject_AssetRetention_user_3 :: AssetRetention
 testObject_AssetRetention_user_3 = AssetEternalInfrequentAccess
 
-testObject_AssetRetention_user_4 :: AssetRetention
-testObject_AssetRetention_user_4 = AssetPersistent
-
-testObject_AssetRetention_user_5 :: AssetRetention
-testObject_AssetRetention_user_5 = AssetEternal
-
-testObject_AssetRetention_user_6 :: AssetRetention
-testObject_AssetRetention_user_6 = AssetPersistent
-
-testObject_AssetRetention_user_7 :: AssetRetention
-testObject_AssetRetention_user_7 = AssetEternalInfrequentAccess
-
 testObject_AssetRetention_user_8 :: AssetRetention
 testObject_AssetRetention_user_8 = AssetVolatile
 
-testObject_AssetRetention_user_9 :: AssetRetention
-testObject_AssetRetention_user_9 = AssetEternalInfrequentAccess
-
-testObject_AssetRetention_user_10 :: AssetRetention
-testObject_AssetRetention_user_10 = AssetEternal
-
-testObject_AssetRetention_user_11 :: AssetRetention
-testObject_AssetRetention_user_11 = AssetEternal
-
-testObject_AssetRetention_user_12 :: AssetRetention
-testObject_AssetRetention_user_12 = AssetVolatile
-
 testObject_AssetRetention_user_13 :: AssetRetention
 testObject_AssetRetention_user_13 = AssetExpiring
-
-testObject_AssetRetention_user_14 :: AssetRetention
-testObject_AssetRetention_user_14 = AssetEternal
-
-testObject_AssetRetention_user_15 :: AssetRetention
-testObject_AssetRetention_user_15 = AssetEternal
-
-testObject_AssetRetention_user_16 :: AssetRetention
-testObject_AssetRetention_user_16 = AssetEternal
-
-testObject_AssetRetention_user_17 :: AssetRetention
-testObject_AssetRetention_user_17 = AssetPersistent
-
-testObject_AssetRetention_user_18 :: AssetRetention
-testObject_AssetRetention_user_18 = AssetEternal
-
-testObject_AssetRetention_user_19 :: AssetRetention
-testObject_AssetRetention_user_19 = AssetPersistent
-
-testObject_AssetRetention_user_20 :: AssetRetention
-testObject_AssetRetention_user_20 = AssetEternal
