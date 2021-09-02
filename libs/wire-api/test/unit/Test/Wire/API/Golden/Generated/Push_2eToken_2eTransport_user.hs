cb76a18215feab51a92592115a1913559d20f198
Marko Dimjašević <marko.dimjasevic@wire.com>
2021-09-02 17:19:56 +0200

    Clean up JSON golden tests for Wire.API.Push.Token.Transport

--- libs/wire-api/test/unit/Test/Wire/API/Golden/Generated/Push_2eToken_2eTransport_user.hs
+++ libs/wire-api/test/unit/Test/Wire/API/Golden/Generated/Push_2eToken_2eTransport_user.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedLists #-}
-
 -- This file is part of the Wire Server implementation.
 --
 -- Copyright (C) 2021 Wire Swiss GmbH <opensource@wire.com>
@@ -22,61 +20,16 @@ import Wire.API.Push.Token (Transport (APNS, APNSSandbox, APNSVoIP, APNSVoIPSand
 import qualified Wire.API.Push.Token as Push.Token (Transport)
 
 testObject_Push_2eToken_2eTransport_user_1 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_1 = APNSVoIPSandbox
+testObject_Push_2eToken_2eTransport_user_1 = GCM
 
 testObject_Push_2eToken_2eTransport_user_2 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_2 = APNSSandbox
+testObject_Push_2eToken_2eTransport_user_2 = APNS
 
 testObject_Push_2eToken_2eTransport_user_3 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_3 = APNS
+testObject_Push_2eToken_2eTransport_user_3 = APNSSandbox
 
 testObject_Push_2eToken_2eTransport_user_4 :: Push.Token.Transport
 testObject_Push_2eToken_2eTransport_user_4 = APNSVoIP
 
 testObject_Push_2eToken_2eTransport_user_5 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_5 = APNSSandbox
-
-testObject_Push_2eToken_2eTransport_user_6 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_6 = APNSVoIPSandbox
-
-testObject_Push_2eToken_2eTransport_user_7 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_7 = APNSVoIP
-
-testObject_Push_2eToken_2eTransport_user_8 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_8 = APNS
-
-testObject_Push_2eToken_2eTransport_user_9 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_9 = GCM
-
-testObject_Push_2eToken_2eTransport_user_10 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_10 = GCM
-
-testObject_Push_2eToken_2eTransport_user_11 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_11 = APNSVoIPSandbox
-
-testObject_Push_2eToken_2eTransport_user_12 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_12 = APNSSandbox
-
-testObject_Push_2eToken_2eTransport_user_13 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_13 = GCM
-
-testObject_Push_2eToken_2eTransport_user_14 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_14 = GCM
-
-testObject_Push_2eToken_2eTransport_user_15 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_15 = GCM
-
-testObject_Push_2eToken_2eTransport_user_16 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_16 = APNSVoIPSandbox
-
-testObject_Push_2eToken_2eTransport_user_17 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_17 = APNSVoIPSandbox
-
-testObject_Push_2eToken_2eTransport_user_18 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_18 = APNSVoIPSandbox
-
-testObject_Push_2eToken_2eTransport_user_19 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_19 = APNS
-
-testObject_Push_2eToken_2eTransport_user_20 :: Push.Token.Transport
-testObject_Push_2eToken_2eTransport_user_20 = APNSSandbox
+testObject_Push_2eToken_2eTransport_user_5 = APNSVoIPSandbox
