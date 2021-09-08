9304458b0d681b50ad870ac6057b2d5a41cecd34
Paolo Capriotti <paolo@capriotti.io>
2021-09-08 09:49:29 +0200

    Revert hack to display helm test output
    The hack in #1700 does not actually seem to work in CI, and in fact it
    prevents log output from being displayed at all when helm test fails.
    
    The integration-test-logs.sh script is still around, and it can be used
    to display helm test output interactive when running tests manually in a
    terminal.

--- hack/bin/integration-test.sh
+++ hack/bin/integration-test.sh
@@ -5,7 +5,5 @@ NAMESPACE=${NAMESPACE:-test-integration}
 
 echo "Running integration tests on wire-server"
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CHART=wire-server
-helm test -n "${NAMESPACE}" "${NAMESPACE}-${CHART}" --timeout 600s |
-  "$DIR/integration-test-logs.sh"
+helm test --logs -n "${NAMESPACE}" "${NAMESPACE}-${CHART}" --timeout 600s
