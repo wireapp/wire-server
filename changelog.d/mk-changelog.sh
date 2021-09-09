c263fbf79f065734bc3b3e5541e53b06943a9711
Paolo Capriotti <paolo@capriotti.io>
2021-09-09 12:53:04 +0200

    Make getPRNumber fail when PR number is absent

--- changelog.d/mk-changelog.sh
+++ changelog.d/mk-changelog.sh
@@ -6,7 +6,8 @@ shopt -s nullglob
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 getPRNumber() {
-    git log --format=%s -- $1 | sed -r 's|^.*\((#.*)\)$|\1|' || echo '#PR_NUMBER_NOT_FOUND'
+    git log --format=%s -- $1 | sed -rn '/\((#.*)\)$/ s|^.*\((#.*)\)$|\1|p' | grep "" ||
+      echo "#PR_NOT_FOUND"
 }
 
 for d in "$DIR"/*; do
