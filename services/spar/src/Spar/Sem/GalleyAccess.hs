6fdaafed7e14a41a92248008ab7090f3d358008b
Sandy Maguire <sandy@sandymaguire.me>
2021-09-24 12:07:42 -0700

    Stale comment

--- services/spar/src/Spar/Sem/GalleyAccess.hs
+++ services/spar/src/Spar/Sem/GalleyAccess.hs
@@ -5,8 +5,6 @@ import Galley.Types.Teams (IsPerm, TeamMember)
 import Imports
 import Polysemy
 
--- FUTUREWORK: A better effect here would be to exactly model the requests we
--- want to make of Galley. This would allow for non-HTTP interpretations.
 data GalleyAccess m a where
   GetTeamMembers :: TeamId -> GalleyAccess m [TeamMember]
   AssertHasPermission :: (Show perm, IsPerm perm) => TeamId -> perm -> UserId -> GalleyAccess m ()
