fa25b0939e848b88d1e173d68f3ec20614a4885e
Paolo Capriotti <paolo@capriotti.io>
2021-10-07 11:49:33 +0200

    Remove deprecated endpoint for listing convs (#1840)
    * Remove deprecated endpoint for listing convs
    
    Also removed the V2 from the name of the endpoint (in the code, not in
    the endpoint path).
    
    * Remove /list-conversations from nginx conf
    
    * Remove use of /list-conversations from End2end

--- services/galley/src/Galley/API/Public.hs
+++ services/galley/src/Galley/API/Public.hs
@@ -84,7 +84,6 @@ servantSitemap =
         GalleyAPI.getConversations = Query.getConversations,
         GalleyAPI.getConversationByReusableCode = Query.getConversationByReusableCode,
         GalleyAPI.listConversations = Query.listConversations,
-        GalleyAPI.listConversationsV2 = Query.listConversationsV2,
         GalleyAPI.createGroupConversation = Create.createGroupConversation,
         GalleyAPI.createSelfConversation = Create.createSelfConversation,
         GalleyAPI.createOne2OneConversation = Create.createOne2OneConversation,
