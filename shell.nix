04538795b9a0ccdea9998dc241a400e46a4a8bf9
Stefan Matting <smatting@users.noreply.github.com>
2021-10-12 13:05:26 +0200

    Remove shell.nix (#221)

--- shell.nix
+++
@@ -1,7 +0,0 @@
-{pkgs ? import ./nix/nixpkgs.nix }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    gnumake
-    (python3.withPackages (ps: with ps; [ sphinx recommonmark awscli rst2pdf sphinxcontrib-fulltoc ]))
-  ];
-}
