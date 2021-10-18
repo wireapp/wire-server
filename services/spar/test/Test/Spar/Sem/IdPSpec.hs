35c66b678e2cd86fe76afb10cbda32eb7b6bcec5
Sandy Maguire <sandy@sandymaguire.me>
2021-10-18 13:30:53 -0700

    Add a test of laws for IdP

---
+++ services/spar/test/Test/Spar/Sem/IdPSpec.hs
@@ -0,0 +1,142 @@
+{-# OPTIONS_GHC -fplugin=Polysemy.Plugin #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.Spar.Sem.IdPSpec where
+
+import Arbitrary ()
+import Imports
+import qualified Spar.Sem.IdP as E
+import Polysemy.Check
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Polysemy
+import SAML2.WebSSO.Types
+import Spar.Sem.IdP.Mem
+import qualified Wire.API.User.IdentityProvider as IdP
+import qualified SAML2.WebSSO as SAML
+import Control.Lens
+
+
+deriveGenericK ''E.IdP
+deriving stock instance Show (E.IdP r a)
+
+
+prop_storeGet
+    :: Member E.IdP r
+    => (forall a. Sem r a -> IO (IS, a))
+    -> Property
+prop_storeGet x =
+  prepropLaw @'[E.IdP]
+    ( do
+        s <- arbitrary
+        pure
+          ( do
+              E.storeConfig s
+              E.getConfig $ s ^. idpId
+          , do
+              E.storeConfig s
+              pure (Just s)
+          )
+    )
+    x
+
+prop_storeGetRaw
+    :: Member E.IdP r
+    => (forall a. Sem r a -> IO (IS, a))
+    -> Property
+prop_storeGetRaw x =
+  prepropLaw @'[E.IdP]
+    ( do
+      idpid <- arbitrary
+      t <- arbitrary
+      pure
+        ( do
+            E.storeRawMetadata idpid t
+            E.getRawMetadata idpid
+        , do
+            E.storeRawMetadata idpid t
+            pure (Just t)
+        )
+    )
+    x
+
+prop_storeDeleteRaw
+    :: Member E.IdP r
+    => (forall a. Sem r a -> IO (IS, a))
+    -> Property
+prop_storeDeleteRaw x =
+  prepropLaw @'[E.IdP]
+    ( do
+      idpid <- arbitrary
+      t <- arbitrary
+      pure
+        ( do
+            E.storeRawMetadata idpid t
+            E.deleteRawMetadata idpid
+        , do
+            E.deleteRawMetadata idpid
+        )
+    )
+    x
+
+prop_deleteGetRaw
+    :: Member E.IdP r
+    => (forall a. Sem r a -> IO (IS, a))
+    -> Property
+prop_deleteGetRaw x =
+  prepropLaw @'[E.IdP]
+    ( do
+      idpid <- arbitrary
+      pure
+        ( do
+            E.deleteRawMetadata idpid
+            E.getRawMetadata idpid
+        , do
+            E.deleteRawMetadata idpid
+            pure Nothing
+        )
+    )
+    x
+
+prop_storeGetByIssuer
+    :: Member E.IdP r
+    => (forall a. Sem r a -> IO (IS, a))
+    -> Property
+prop_storeGetByIssuer x =
+  prepropLaw @'[E.IdP]
+    ( do
+      s <- arbitrary
+      pure
+        ( do
+            E.storeConfig s
+            E.getIdByIssuerWithoutTeam $ s ^. idpMetadata . edIssuer
+        , do
+            E.storeConfig s
+            pure $ E.GetIdPFound $ s ^. idpId
+        )
+    )
+    x
+
+testInterpreter :: Sem '[E.IdP] a -> IO ((Map SAML.IdPId IdP.IdP, Map SAML.IdPId Text), a)
+testInterpreter = pure . run . idPToMem
+
+propsForInterpreter
+    :: Member E.IdP r
+    => (forall a. Sem r a -> IO (IS, a))
+    -> Spec
+propsForInterpreter lower = do
+  describe "Config Actions" $ do
+    prop "storeConfig/getConfig" $ prop_storeGet lower
+    prop "storeConfig/getIdByIssuerWithoutTeam" $ prop_storeGetByIssuer lower
+
+  describe "Raw Metadata Actions" $ do
+    prop "storeRawMetadata/getRawMetadata" $ prop_storeGetRaw lower
+    prop "storeRawMetadata/deleteRawMetadata" $ prop_storeDeleteRaw lower
+    prop "deleteRawMetadata/getRawMetadata" $ prop_deleteGetRaw lower
+
+
+spec :: Spec
+spec = modifyMaxSuccess (const 10000) $ do
+  propsForInterpreter testInterpreter
+
