c5f250bf1ca1af1b62e6c0f83ee75a53890fcc38
Stefan Matting <stefan@wire.com>
2021-11-08 18:37:13 +0100

    fix generate.sh

--- tools/convert-to-cabal/generate.sh
+++ tools/convert-to-cabal/generate.sh
@@ -5,7 +5,9 @@ TOP_LEVEL="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 
 cd "$TOP_LEVEL"
 
+nix-shell ./tools/convert-to-cabal/shell.nix --command "stack2cabal --no-run-hpack"
+
 {
- echo -e "\n-- Changes by ./tools/convert-to-cabal/generate.sh \n\ntests: True\n\n"
- ./hack/bin/cabal-project-local-template.sh "ghc-options: -Werror" >> ./cabal.project
+ echo -e "\n-- Changes by ./tools/convert-to-cabal/generate.sh \n\ntests: True\n\n";
+ ./hack/bin/cabal-project-local-template.sh "ghc-options: -Werror"
 } >> ./cabal.project
