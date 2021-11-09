c4b51ef9328e7800bbe0dd005d68eacbee981871
Sandy Maguire <sandy@sandymaguire.me>
2021-11-09 13:42:22 -0800

    Add ScimTokenStore.Mem

---
+++ services/spar/src/Spar/Sem/ScimTokenStore/Mem.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS_GHC -fplugin=Polysemy.Plugin #-}
+
+module Spar.Sem.ScimTokenStore.Mem where
+
+import qualified Data.Map as M
+import Imports
+import Polysemy
+import Polysemy.State
+import Spar.Sem.ScimTokenStore
+import Spar.Scim
+
+scimTokenStoreToMem ::
+  Sem (ScimTokenStore ': r) a ->
+  Sem r a
+scimTokenStoreToMem = (evalState @(Map ScimToken ScimTokenInfo) mempty .) $
+  reinterpret $ \case
+  Insert st sti -> modify $ M.insert st sti
+  Lookup st -> gets $ M.lookup st
+  GetByTeam tid -> gets $ filter ((== tid) . stiTeam) . M.elems
+  Delete tid stid -> modify $ M.filter $ \sti -> not $ stiTeam sti == tid && stiId sti == stid
+  DeleteByTeam tid -> modify $ M.filter (not . (== tid) . stiTeam)
+
