3db57a0e11583eb79d6714c86e29e5f55098c8e3
Sandy Maguire <sandy@sandymaguire.me>
2021-11-09 13:32:49 -0800

    Add ScimExternalIdStore.Mem

---
+++ services/spar/src/Spar/Sem/ScimExternalIdStore/Mem.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -fplugin=Polysemy.Plugin #-}
+
+module Spar.Sem.ScimExternalIdStore.Mem where
+
+import qualified Data.Map as M
+import Imports
+import Polysemy
+import Polysemy.State
+import Spar.Sem.ScimExternalIdStore
+import Data.Id (UserId, TeamId)
+import Wire.API.User.Identity (Email)
+
+scimExternalIdStoreToMem ::
+  Sem (ScimExternalIdStore ': r) a ->
+  Sem r a
+scimExternalIdStoreToMem = (evalState @(Map (TeamId, Email) UserId) mempty .) $
+  reinterpret $ \case
+    Insert tid em uid -> modify $ M.insert (tid, em) uid
+    Lookup tid em -> gets $ M.lookup (tid, em)
+    Delete tid em -> modify $ M.delete (tid, em)
+
