4fe7711d996284955b0912f6162016a59c688975
Stefan Matting <stefan@wire.com>
2021-11-10 18:08:46 +0100

    Expose the server to server API version

--- services/brig/src/Brig/Run.hs
+++ services/brig/src/Brig/Run.hs
@@ -27,7 +27,7 @@ import Brig.API (sitemap)
 import Brig.API.Federation (federationSitemap)
 import Brig.API.Handler
 import qualified Brig.API.Internal as IAPI
-import Brig.API.Public (ServantAPI, SwaggerDocsAPI, servantSitemap, swaggerDocsAPI)
+import Brig.API.Public (ServantAPI, SwaggerDocsAPI, servantSitemap, swaggerDocsAPI, versionAPISitemap)
 import qualified Brig.API.User as API
 import Brig.AWS (sesQueue)
 import qualified Brig.AWS as AWS
@@ -70,6 +70,7 @@ import System.Logger (msg, val, (.=), (~~))
 import System.Logger.Class (MonadLogger, err)
 import Util.Options
 import qualified Wire.API.Federation.API.Brig as FederationBrig
+import qualified Wire.API.Routes.Public.Federation as VersionAPI
 
 -- FUTUREWORK: If any of these async threads die, we will have no clue about it
 -- and brig could start misbehaving. We should ensure that brig dies whenever a
@@ -128,6 +129,7 @@ mkApp o = do
             :<|> Servant.hoistServer (Proxy @ServantAPI) (toServantHandler e) servantSitemap
             :<|> Servant.hoistServer (Proxy @IAPI.API) (toServantHandler e) IAPI.servantSitemap
             :<|> Servant.hoistServer (genericApi (Proxy @FederationBrig.Api)) (toServantHandler e) federationSitemap
+            :<|> Servant.hoistServer (Proxy @VersionAPI.ServantAPI) (toServantHandler e) versionAPISitemap
             :<|> Servant.Tagged (app e)
         )
 
@@ -136,6 +138,7 @@ type ServantCombinedAPI =
       :<|> ServantAPI
       :<|> IAPI.API
       :<|> ToServantApi FederationBrig.Api
+      :<|> VersionAPI.ServantAPI
       :<|> Servant.Raw
   )
 
