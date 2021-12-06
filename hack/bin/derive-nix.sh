047beb64eebd799254092356da144308295c1a65
Akshay Mankar <akshay@wire.com>
2021-12-06 17:13:28 +0100

    Add nix derivations of git extra-deps to overrides

--- hack/bin/derive-nix.sh
+++ hack/bin/derive-nix.sh
@@ -8,22 +8,30 @@ pkgDirs=$(find "$TOP_LEVEL" -name '*.cabal' | grep  -v dist-newstyle | xargs dir
 
 echo "$pkgDirs" | xargs -I{} bash -c 'cabal2nix {} > {}/default.nix'
 
-mkLocalOverlay() {
-    echo 'hself: hsuper: {'
-    echo "$pkgDirs" | xargs -I{} bash -c 'echo \ \ $(basename {}) = hsuper.callPackage $(realpath --relative-to='"$TOP_LEVEL"'/nix/overlays {})/default.nix \{\}\;'
-    echo '}'
-}
-mkLocalOverlay > "$TOP_LEVEL/nix/overlays/haskell-local.nix"
-
 rm -rf "$TOP_LEVEL/nix/extra-deps"
 mkdir -p "$TOP_LEVEL/nix/extra-deps"
 
-yq -r '.["extra-deps"][] | select(type == "object") | select (.subdirs == null) | .git' "$TOP_LEVEL/stack.yaml" |
-   xargs -I {} bash -c 'cabal2nix {} > '"$TOP_LEVEL"'/nix/extra-deps/$(basename {}).nix'
+repos_without_subdirs=$(yq -r '.["extra-deps"][] | select(type == "object") | select (.subdirs == null) | .git' "$TOP_LEVEL/stack.yaml")
+echo "$repos_without_subdirs" | xargs -I {} bash -c 'cabal2nix {} > '"$TOP_LEVEL"'/nix/extra-deps/$(basename {}).nix'
 
 repos_with_subdirs=$(yq -r '.["extra-deps"][] | select(type == "object") | select (.subdirs != null) | .git' "$TOP_LEVEL/stack.yaml")
-
 while read repo; do
     yq -r '.["extra-deps"][] | select(type == "object") | select (.git == $repo) | .subdirs[]' "$TOP_LEVEL/stack.yaml" --arg repo "$repo" |
         xargs -I {} bash -c 'cabal2nix '"$repo"' --subpath {} > '"$TOP_LEVEL"'/nix/extra-deps/'"$(basename "$repo")"'-$(basename {}).nix'
 done <<<"$repos_with_subdirs"
+
+mkOverlay() {
+    echo 'hself: hsuper: {'
+    echo "$pkgDirs" |
+        xargs -I{} bash -c 'echo \ \ $(basename {}) = hsuper.callPackage $(realpath --relative-to='"$TOP_LEVEL"'/nix/overlays {})/default.nix \{\}\;'
+    echo "$repos_without_subdirs" | xargs -n 1 basename |
+        xargs -I {} bash -c 'echo \ \ {} = hsuper.callPackage $(realpath --relative-to='"$TOP_LEVEL"'/nix/overlays '"$TOP_LEVEL"'/nix/extra-deps/{}.nix) \{\}\;'
+    while read repo; do
+        while read subdir; do
+            name="$(basename "$repo")-$(basename "$subdir")"
+            echo "  $name = hsuper.callPackage $(realpath --relative-to="$TOP_LEVEL/nix/overlays" "$TOP_LEVEL/nix/extra-deps/$name.nix") {};"
+        done <<<"$(yq -r '.["extra-deps"][] | select(type == "object") | select (.git == $repo) | .subdirs[]' "$TOP_LEVEL/stack.yaml" --arg repo "$repo")"
+    done <<<"$repos_with_subdirs"
+    echo '}'
+}
+mkOverlay > "$TOP_LEVEL/nix/overlays/haskell.nix"
