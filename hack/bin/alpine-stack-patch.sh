cc65ebad1610570a901b1ed289e2f59776fd56c0
Akshay Mankar <akshay@wire.com>
2021-12-09 10:26:11 +0100

    alpine/docker: Better patch not dependant on yq
    alpine distribution has a different yq

--- hack/bin/alpine-stack-patch.sh
+++ hack/bin/alpine-stack-patch.sh
@@ -4,6 +4,14 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP_LEVEL="$(cd "$DIR/../.." && pwd)"
 
-tmpStackYaml=$(mktemp)
-yq '. + {flags: {cryptonite: {"integer-gmp": false}, hashable: {"interger-gmp": false}, "integer-logarithms": {"integer-gmp": false}, scientific: {"integer-simple": true}}}' "$TOP_LEVEL/stack.yaml" > $tmpStackYaml
-cp $tmpStackYaml "$TOP_LEVEL/stack.yaml"
+cat >>$TOP_LEVEL/stack.yaml <<EOF
+flags:
+  cryptonite:
+    integer-gmp: false
+  hashable:
+    integer-gmp: false
+  integer-logarithms:
+    integer-gmp: false
+  scientific:
+    integer-simple: true
+EOF
