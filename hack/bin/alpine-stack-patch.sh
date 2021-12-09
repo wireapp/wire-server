b52ed589d5c3851766e70fed1d3c5e9ada1cfdff
Akshay Mankar <akshay@wire.com>
2021-12-09 11:15:57 +0100

    alpine/docker: Add ghc boot dependencies
    stack complains that these packages have been pruned

--- hack/bin/alpine-stack-patch.sh
+++ hack/bin/alpine-stack-patch.sh
@@ -5,6 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP_LEVEL="$(cd "$DIR/../.." && pwd)"
 
 cat >>$TOP_LEVEL/stack.yaml <<EOF
+
+- Cabal-3.6.2.0@sha256:e2266e14758c1f799220fad7f0d4b0b4ec567d81b7ba3faea17ff76d4c31de95,12437
+- parsec-3.1.15.0@sha256:a162d4cc8884014ba35192545cad293af0529fe11497aad8834bbaaa3dfffc26,4429
+- text-1.2.5.0@sha256:791f0f6c97ed96113f17ab520cf0efe1a3a4f883a8c85910a5660567c8241c40,7895
+
 flags:
   cryptonite:
     integer-gmp: false
