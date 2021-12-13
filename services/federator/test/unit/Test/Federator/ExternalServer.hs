cdc1cd84436ef8482a98a00f071500d09b80416b
Paolo Capriotti <paolo@capriotti.io>
2021-12-13 16:56:30 +0100

    Remove leftover option

--- services/federator/test/unit/Test/Federator/ExternalServer.hs
+++ services/federator/test/unit/Test/Federator/ExternalServer.hs
@@ -14,7 +14,7 @@
 --
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
-{-# OPTIONS_GHC -Wno-orphans -fprint-potential-instances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Test.Federator.ExternalServer where
 
