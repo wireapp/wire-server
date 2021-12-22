ac8ecb975287248989619fcc749a300d7b433a72
Paolo Capriotti <paolo@capriotti.io>
2021-12-22 16:40:24 +0100

    Test streaming invalid assets

--- services/cargohold/test/integration/API/Federation.hs
+++ services/cargohold/test/integration/API/Federation.hs
@@ -13,6 +13,7 @@ import Data.Qualified
 import Data.UUID.V4
 import Imports
 import qualified Network.HTTP.Types as HTTP
+import qualified Network.Wai.Utilities.Error as Wai
 import Test.Tasty
 import Test.Tasty.HUnit
 import TestSetup
@@ -33,7 +34,9 @@ tests s =
       testGroup
         "stream-asset"
         [ test s "streaming large asset" testLargeAsset,
-          test s "stream an asset" testStreamAsset
+          test s "stream an asset" testStreamAsset,
+          test s "stream asset not available" testStreamAssetNotAvailable,
+          test s "stream asset wrong token" testStreamAssetWrongToken
         ]
     ]
 
@@ -185,3 +188,48 @@ testStreamAsset c = do
         <!! const 200 === statusCode
 
   liftIO $ respBody @?= Just "Hello World"
+
+testStreamAssetNotAvailable :: TestSignature ()
+testStreamAssetNotAvailable c = do
+  uid <- liftIO $ Id <$> nextRandom
+  token <- randToken
+
+  assetId <- liftIO $ Id <$> nextRandom
+  let key = AssetKeyV3 assetId AssetPersistent
+  let ga =
+        GetAsset
+          { gaUser = uid,
+            gaToken = Just token,
+            gaKey = key
+          }
+  err <-
+    responseJsonError
+      =<< post (c . path "/federation/stream-asset" . json ga)
+      <!! const 404 === statusCode
+  liftIO $ Wai.label err @?= "not-found"
+
+testStreamAssetWrongToken :: TestSignature ()
+testStreamAssetWrongToken c = do
+  -- Initial upload
+  let bdy = (applicationOctetStream, "Hello World")
+      settings = defAssetSettings & set setAssetRetention (Just AssetVolatile)
+  uid <- liftIO $ Id <$> nextRandom
+  ast :: Asset <-
+    responseJsonError
+      =<< uploadSimple (c . path "/assets/v3") uid settings bdy
+      <!! const 201 === statusCode
+
+  -- Call get-asset federation API with wrong (random) token
+  tok <- randToken
+  let key = view assetKey ast
+  let ga =
+        GetAsset
+          { gaUser = uid,
+            gaToken = Just tok,
+            gaKey = qUnqualified key
+          }
+  err <-
+    responseJsonError
+      =<< post (c . path "/federation/stream-asset" . json ga)
+      <!! const 404 === statusCode
+  liftIO $ Wai.label err @?= "not-found"
