5927f5d743be71dc16de898fd803fa554c2dee96
jschaul <jschaul@users.noreply.github.com>
2022-01-11 17:51:56 +0100

    WIP

--- tools/sftd-disco/Main.hs
+++ tools/sftd-disco/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 -- This file is part of the Wire Server implementation.
 --
 -- Copyright (C) 2020 Wire Swiss GmbH <opensource@wire.com>
@@ -17,36 +20,88 @@
 
 module Main (main) where
 
+import Control.Monad (forM)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as ByteString
+import Data.String.Conversions (cs)
 import Data.Text (Text)
-import qualified Data.Text as Text
-import Network.DNS hiding (header)
+import Data.Word (Word16)
+import Network.DNS hiding (Domain, header)
+import Network.HTTP.Client hiding (port)
+import Network.HTTP.Types.Status
 import Network.Socket
-import Options.Applicative
+import qualified Options.Applicative as P
 import qualified System.Logger as Log
+import System.Logger.Class
+import URI.ByteString
 
 newtype Opts = Opts
-  { optSrvRecord :: Text
+  { optSrvRecord :: ByteString
   }
   deriving (Show)
 
-parseOpts :: ParserInfo Opts
-parseOpts = info (helper <*> parser) desc
+parseOpts :: P.ParserInfo Opts
+parseOpts = P.info (P.helper <*> parser) desc
   where
-    desc = header "SFT discovery service" <> fullDesc
+    desc = P.header "SFT discovery service" <> P.fullDesc
 
     parser =
       Opts
-        <$> option
-          txt
-          ( long "srvRecord"
-              <> help "SRV record containing SFT services. Example: _sft._tcp.wire-server-sftd.wire.svc.cluster.local"
+        <$> P.option
+          bs
+          ( P.long "srvRecord"
+              <> P.help "SRV record containing SFT services. Example: _sft._tcp.wire-server-sftd.wire.svc.cluster.local"
           )
 
-    txt = Text.pack <$> str
+    bs :: P.ReadM ByteString
+    bs = ByteString.pack <$> P.str
+
+initHttpManager :: IO Manager
+initHttpManager =
+  newManager
+    defaultManagerSettings
+      { managerResponseTimeout = responseTimeoutMicro 10000000
+      }
 
 main :: IO ()
 main = withSocketsDo $ do
-  _opts <- execParser parseOpts
-  _lgr <- Log.new Log.defSettings
-  _rlv <- makeResolvSeed defaultResolvConf
+  opts <- P.execParser parseOpts
+  lgr <- Log.new Log.defSettings
+  manager <- initHttpManager
+  rlv <- makeResolvSeed defaultResolvConf
+  withResolver rlv $ \resolver ->
+    lookupSRV resolver (optSrvRecord opts) >>= \case
+      Left e -> undefined
+      Right results -> do
+        urls <- resolveUrls lgr manager results
+        -- writeToFile
+        undefined
+
+  undefined
+
+resolveUrls :: Logger -> Manager -> [(Word16, Word16, Word16, ByteString)] -> IO [Text]
+resolveUrls l m input = forM input (resolveUrl l m)
+
+resolveUrl :: Logger -> Manager -> (Word16, Word16, Word16, ByteString) -> IO Text
+resolveUrl lgr mgr (_, _, port, dom) = do
+  let url = "http://" <> cs dom <> ":" <> show port
+  request <- parseRequest url
+  response <- httpLbs request mgr
+
+  Log.info lgr $
+    msg (val "The status code was: ")
+      ~~ field "code" (statusCode $ responseStatus response)
+  let body = cs $ responseBody response
+
+  case parseURI strictURIParserOptions body of
+    Left e ->
+      Log.err lgr $
+        msg (val "Error parsing response")
+          ~~ field "request" url
+          ~~ field "error" (show e)
+    Right uri -> do
+      print "uri = "
+      print uri
+      undefined
+
   undefined
