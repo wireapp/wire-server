2fca4e782ba2cad45e27c4e182050ef2025706c3
Marko Dimjašević <marko.dimjasevic@wire.com>
2022-02-14 14:04:43 +0100

    Drop the `managed` column from `team_conv` table

--- services/galley/schema/src/V71_DropManagedConversations.hs
+++ services/galley/schema/src/V71_DropManagedConversations.hs
@@ -15,9 +15,16 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-module Galley.Cassandra (schemaVersion) where
+module V71_DropManagedConversations where
 
+import Cassandra.Schema
 import Imports
+import Text.RawString.QQ
 
-schemaVersion :: Int32
-schemaVersion = 71
+migration :: Migration
+migration = Migration 71 "Drop the managed column from team_conv" $ do
+  schema'
+    [r| ALTER TABLE team_conv DROP (
+          managed
+      );
+    |]
