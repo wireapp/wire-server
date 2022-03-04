36ad8c5de95960bbc370d5a5226039862315800a
Paolo Capriotti <paolo@capriotti.io>
2022-03-04 11:22:49 +0100

    Read actual federation domain in api-version test

--- services/brig/test/integration/API/Version.hs
+++ services/brig/test/integration/API/Version.hs
@@ -19,7 +19,7 @@ module API.Version (tests) where
 
 import Bilge
 import Bilge.Assert
-import Data.Domain
+import Brig.Options
 import Imports
 import qualified Network.Wai.Utilities.Error as Wai
 import Test.Tasty
@@ -27,14 +27,14 @@ import Test.Tasty.HUnit
 import Util
 import Wire.API.Routes.Version
 
-tests :: Manager -> Brig -> TestTree
-tests p brig =
+tests :: Manager -> Opts -> Brig -> TestTree
+tests p opts brig =
   testGroup
     "version"
     [ test p "GET /api-version" $ testVersion brig,
       test p "GET /v1/api-version" $ testVersionV1 brig,
       test p "GET /v500/api-version" $ testUnsupportedVersion brig,
-      test p "GET /api-version (federation info)" $ testFederationDomain brig
+      test p "GET /api-version (federation info)" $ testFederationDomain opts brig
     ]
 
 testVersion :: Brig -> Http ()
@@ -60,11 +60,12 @@ testUnsupportedVersion brig = do
       <!! const 404 === statusCode
   liftIO $ Wai.label e @?= "unsupported-version"
 
-testFederationDomain :: Brig -> Http ()
-testFederationDomain brig = do
+testFederationDomain :: Opts -> Brig -> Http ()
+testFederationDomain opts brig = do
+  let domain = setFederationDomain (optSettings opts)
   vinfo <-
     responseJsonError =<< get (brig . path "/api-version")
       <!! const 200 === statusCode
   liftIO $ do
     vinfoFederation vinfo @?= True
-    vinfoDomain vinfo @?= Domain "example.com"
+    vinfoDomain vinfo @?= domain
