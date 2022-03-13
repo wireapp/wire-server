8db387a8798e7bd39f6f0551dda05e8aa6f720cf
fisx <mf@zerobuzz.net>
2022-03-13 16:29:11 +0100

    More test labels (#2185)
    Co-authored-by: Sven Jost <sven@wire.com>

--- services/galley/test/integration/API/Teams.hs
+++ services/galley/test/integration/API/Teams.hs
@@ -1533,6 +1533,11 @@ testBillingInLargeTeamWithoutIndexedBillingTeamMembers = do
             . json change
         )
 
+-- | @SF.Management @TSFI.RESTfulAPI @S2
+-- This test covers:
+-- Promotion, demotion of team roles.
+-- Demotion by superior roles is allowed.
+-- Demotion by inferior roles is NOT allowed.
 testUpdateTeamMember :: TestM ()
 testUpdateTeamMember = do
   g <- view tsGalley
@@ -1596,6 +1601,8 @@ testUpdateTeamMember = do
       let e = List1.head (WS.unpackPayload notif)
       e ^. eventTeam @?= tid
       e ^. eventData @?= EdMemberUpdate uid mPerm
+
+-- @END
 
 testUpdateTeamStatus :: TestM ()
 testUpdateTeamStatus = do
