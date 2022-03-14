877644c97419527f9b9056539f8dad557230edb7
Marko Dimjašević <marko.dimjasevic@wire.com>
2022-03-14 16:58:31 +0100

    Add the proposals table

--- services/brig/schema/src/Main.hs
+++ services/brig/schema/src/Main.hs
@@ -79,6 +79,7 @@ import qualified V66_PersonalFeatureConfCallInit
 import qualified V67_MLSKeyPackages
 import qualified V68_AddMLSPublicKeys
 import qualified V69_MLSKeyPackageRefMapping
+import qualified V70_AddMLSProposals
 import qualified V9
 
 main :: IO ()
@@ -147,7 +148,8 @@ main = do
       V66_PersonalFeatureConfCallInit.migration,
       V67_MLSKeyPackages.migration,
       V68_AddMLSPublicKeys.migration,
-      V69_MLSKeyPackageRefMapping.migration
+      V69_MLSKeyPackageRefMapping.migration,
+      V70_AddMLSProposals.migration
       -- When adding migrations here, don't forget to update
       -- 'schemaVersion' in Brig.App
 
