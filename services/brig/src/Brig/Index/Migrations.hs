8380f4deb2592ca9e1c6bd3e4ccb8ffa64aab0a5
Stefan Matting <stefan@wire.com>
2022-03-24 11:40:40 +0100

    Bump MigrationVersion

--- services/brig/src/Brig/Index/Migrations.hs
+++ services/brig/src/Brig/Index/Migrations.hs
@@ -50,7 +50,7 @@ migrate l es cas galleyEndpoint = do
 
 -- | Increase this number any time you want to force reindexing.
 expectedMigrationVersion :: MigrationVersion
-expectedMigrationVersion = MigrationVersion 3
+expectedMigrationVersion = MigrationVersion 4
 
 indexName :: ES.IndexName
 indexName = ES.IndexName "wire_brig_migrations"
