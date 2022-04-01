79141c1dee131377a83310eb9310f88203148211
Paolo Capriotti <paolo@capriotti.io>
2022-04-01 09:18:19 +0200

    Update to most recent crypto-cli (#2256)

--- nix/pkgs/crypto_cli/default.nix
+++ nix/pkgs/crypto_cli/default.nix
@@ -15,11 +15,11 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "wireapp";
     repo = "core-crypto";
-    rev = "c63942e7a87b9d5a331109f472cb26f1efa147e9";
-    sha256 = "sha256-RtlSAg9eGfLUmeYxLDHXmQ77E36vGnKyPaijcTj5Y3E=";
+    rev = "6d28cf49945b77932335d85264f61016b0ba49f2";
+    sha256 = "sha256-8li6yaVhGf4O63NAVzYfNIzNODEwdtN/9gilG8EkdqE=";
   };
   doCheck = false;
-  cargoPatches = [ ./crypto_cli.patch ./ciphersuite.patch ];
-  cargoSha256 = "sha256-knBqluuw64NL5I3CQ8qnabu7jRy0+BL2RYR+ks3xhJI=";
+  cargoPatches = [ ./crypto_cli.patch ];
+  cargoSha256 = "sha256-ql6j3e6J0pI6gXiaGhjDkL09i1hg/PFEj6hhBIqzKbo=";
 }
 
