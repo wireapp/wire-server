f1a01f291597115e689f4101a508131817dcf37a
Leif Battermann <leif.battermann@wire.com>
2022-04-12 11:44:12 +0000

    call internal endpoint for changing email from stern

--- tools/stern/src/Stern/Intra.hs
+++ tools/stern/src/Stern/Intra.hs
@@ -333,7 +333,7 @@ changeEmail u upd = do
       "brig"
       b
       ( method PUT
-          . path "/self/email"
+          . path "i/self/email"
           . header "Z-User" (toByteString' u)
           . header "Z-Connection" (toByteString' "")
           . lbytes (encode upd)
