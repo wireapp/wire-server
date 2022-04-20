26499abdb84ebdf566ada7fe48ed8cfb5f040e6f
Akshay Mankar <akshay@wire.com>
2022-04-20 11:21:16 +0200

    Extract MockServer out of the federator package into wai-utilities
    Also remove the duplicated MockServer from gundeck

---
+++ libs/wai-utilities/src/Network/Wai/Utilities/MockServer.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE NumericUnderscores #-}
+
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2022 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Network.Wai.Utilities.MockServer where
+
+import qualified Control.Concurrent.Async as Async
+import Control.Exception (throw)
+import Control.Monad.Catch
+import Control.Monad.Codensity
+import Data.Streaming.Network (bindRandomPortTCP)
+import Imports
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai.Handler.WarpTLS as Warp
+import qualified System.Timeout as System
+
+-- | Thrown in IO by mock federator if the server could not be started after 10
+-- seconds.
+newtype MockTimeout = MockTimeout Warp.Port
+  deriving (Eq, Show, Typeable)
+
+instance Exception MockTimeout
+
+withMockServer :: Wai.Application -> Codensity IO Word16
+withMockServer app = Codensity $ \k ->
+  bracket
+    (liftIO $ startMockServer Nothing app)
+    (liftIO . fst)
+    (k . fromIntegral . snd)
+
+-- | Start a mock warp server on a random port, serving the given Wai application.
+--
+-- If the 'Warp.TLSSettings` argument is provided, start an HTTPS server,
+-- otherwise start a plain HTTP server.
+--
+-- Returns an action to kill the spawned server, and the port on which the
+-- server is running.
+--
+-- This function should normally be used within 'bracket', e.g.:
+-- @
+--     bracket (startMockServer Nothing app) fst $ \(close, port) ->
+--       makeRequest "localhost" port
+-- @
+startMockServer :: Maybe Warp.TLSSettings -> Wai.Application -> IO (IO (), Warp.Port)
+startMockServer mtlsSettings app = do
+  (port, sock) <- bindRandomPortTCP "*6"
+  serverStarted <- newEmptyMVar
+  let wsettings =
+        Warp.defaultSettings
+          & Warp.setPort port
+          & Warp.setGracefulCloseTimeout2 0 -- Defaults to 2 seconds, causes server stop to take very long
+          & Warp.setBeforeMainLoop (putMVar serverStarted ())
+
+  serverThread <- Async.async $ case mtlsSettings of
+    Just tlsSettings -> Warp.runTLSSocket tlsSettings wsettings sock app
+    Nothing -> Warp.runSettingsSocket wsettings sock app
+  serverStartedSignal <- System.timeout 10_000_000 (readMVar serverStarted)
+  let closeMock = do
+        me <- Async.poll serverThread
+        case me of
+          Nothing -> Async.cancel serverThread
+          Just (Left e) -> throw e
+          Just (Right a) -> pure a
+  case serverStartedSignal of
+    Nothing -> do
+      Async.cancel serverThread
+      throw (MockTimeout port)
+    Just _ -> pure (closeMock, port)
--- services/gundeck/test/integration/Util.hs
+++ services/gundeck/test/integration/Util.hs
@@ -1,17 +1,12 @@
-{-# LANGUAGE NumericUnderscores #-}
-
 module Util where
 
 import qualified Bilge
 import Control.Concurrent.Async (race_)
-import qualified Control.Concurrent.Async as Async
-import Control.Exception (throw)
 import Control.Lens
 import Control.Monad.Catch
 import Control.Monad.Codensity
 import qualified Data.ByteString as S
 import Data.Metrics.Middleware (metrics)
-import Data.Streaming.Network (bindRandomPortTCP)
 import qualified Data.Text as Text
 import Gundeck.Env (createEnv)
 import Gundeck.Options
@@ -20,18 +15,8 @@ import Imports
 import Network.Run.TCP (runTCPServer)
 import Network.Socket hiding (listen)
 import Network.Socket.ByteString (recv, sendAll)
-import qualified Network.Wai as Wai
-import qualified Network.Wai.Handler.Warp as Warp
-import qualified Network.Wai.Handler.WarpTLS as Warp
-import qualified System.Timeout as System
+import Network.Wai.Utilities.MockServer (withMockServer)
 import TestSetup
-
-withMockServer :: Wai.Application -> Codensity IO Word16
-withMockServer app = Codensity $ \k ->
-  bracket
-    (liftIO $ startMockServer Nothing app)
-    (liftIO . fst)
-    (k . fromIntegral . snd)
 
 withSettingsOverrides :: (Opts -> Opts) -> TestM a -> TestM a
 withSettingsOverrides f action = do
@@ -46,47 +31,6 @@ withSettingsOverrides f action = do
   where
     mkRequest p = Bilge.host "127.0.0.1" . Bilge.port p
 
--- TODO: Copy the comment from federator, perhaps create a new package or put it in one of the shared ones?
-startMockServer :: Maybe Warp.TLSSettings -> Wai.Application -> IO (IO (), Warp.Port)
-startMockServer mtlsSettings app = do
-  (port, sock) <- bindRandomPortTCP "*6"
-  serverStarted <- newEmptyMVar
-  let wsettings =
-        Warp.defaultSettings
-          & Warp.setPort port
-          & Warp.setGracefulCloseTimeout2 0 -- Defaults to 2 seconds, causes server stop to take very long
-          & Warp.setBeforeMainLoop (putMVar serverStarted ())
-
-  serverThread <- Async.async $ case mtlsSettings of
-    Just tlsSettings -> Warp.runTLSSocket tlsSettings wsettings sock app
-    Nothing -> Warp.runSettingsSocket wsettings sock app
-  serverStartedSignal <- System.timeout 10_000_000 (readMVar serverStarted)
-  let closeMock = do
-        me <- Async.poll serverThread
-        case me of
-          Nothing -> Async.cancel serverThread
-          Just (Left e) -> throw e
-          Just (Right a) -> pure a
-  case serverStartedSignal of
-    Nothing -> do
-      Async.cancel serverThread
-      throw (MockTimeout port)
-    Just _ -> pure (closeMock, port)
-
--- | Thrown in IO by mock federator if the server could not be started after 10
--- seconds.
-newtype MockTimeout = MockTimeout Warp.Port
-  deriving (Eq, Show, Typeable)
-
-instance Exception MockTimeout
-
---- TCP Proxy
-
--- data ProxySetting = ProxySetting {locPort :: PortNumber, remHost :: String, remPort :: String}
-
--- proxySetting :: ProxySetting
--- proxySetting = ProxySetting 9900 "ftp.free.fr" "80"
-
 runRedisProxy :: Text -> Word16 -> Word16 -> IO ()
 runRedisProxy redisHost redisPort proxyPort = do
   (servAddr : _) <- getAddrInfo Nothing (Just $ Text.unpack redisHost) (Just $ show redisPort)
@@ -98,17 +42,6 @@ runRedisProxy redisHost redisPort proxyPort = do
             server <- getServerSocket servAddr
             client <~~> server
   where
-    -- installHandler sigPIPE Ignore Nothing >> do
-    --   (servAddr : _) <- getAddrInfo Nothing (Just $ remHost proxySetting) (Just $ remPort proxySetting)
-    --   withSocketsDo $ do
-    --     listener <- open serverAddr
-    --     forever $
-    --       accept listener >>= \(client, _) ->
-    --         void $
-    --           forkIO $ do
-    --             server <- getServerSocket servAddr
-    --             client <~~> server
-
     getServerSocket servAddr = do
       server <- socket (addrFamily servAddr) Stream defaultProtocol
       connect server (addrAddress servAddr) >> return server
@@ -116,11 +49,3 @@ runRedisProxy redisHost redisPort proxyPort = do
     mapData f t = do
       content <- recv f 4096
       unless (S.null content) $ sendAll t content >> mapData f t
-
---   open addr = do
---     sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
---     setSocketOption sock ReuseAddr 1
---     withFdSocket sock $ setCloseOnExecIfNeeded
---     bind sock $ addrAddress addr
---     listen sock 1024
---     return sock
