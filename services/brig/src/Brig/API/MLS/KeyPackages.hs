8f1f8414bc46b0df2c29d6b88a75b1c34a1772e3
Paolo Capriotti <paolo@capriotti.io>
2022-05-09 13:42:25 +0200

    Implement remote key package claims (#2353)
    * Implement remote key package claims
    
    * Add e2e key package claim test
    
    * Add key package mapping for remote claims
    
    * Test key package claim fed endpoint
    
    * Test remote key package claim and mapping
    
    * Add CHANGELOG entry

--- services/brig/src/Brig/API/MLS/KeyPackages.hs
+++ services/brig/src/Brig/API/MLS/KeyPackages.hs
@@ -18,6 +18,7 @@
 module Brig.API.MLS.KeyPackages
   ( uploadKeyPackages,
     claimKeyPackages,
+    claimLocalKeyPackages,
     countKeyPackages,
   )
 where
@@ -25,9 +26,11 @@ where
 import Brig.API.Error
 import Brig.API.Handler
 import Brig.API.MLS.KeyPackages.Validation
+import Brig.API.Types
 import Brig.App
 import qualified Brig.Data.Client as Data
 import qualified Brig.Data.MLS.KeyPackage as Data
+import Brig.Federation.Client
 import Brig.IO.Intra
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.Maybe
@@ -35,7 +38,8 @@ import Data.Id
 import Data.Qualified
 import qualified Data.Set as Set
 import Imports
-import Wire.API.Federation.Error
+import Wire.API.Federation.API
+import Wire.API.Federation.API.Brig
 import Wire.API.MLS.Credential
 import Wire.API.MLS.KeyPackage
 import Wire.API.Team.LegalHold
@@ -47,21 +51,38 @@ uploadKeyPackages lusr cid (kpuKeyPackages -> kps) = do
   kps' <- traverse (validateKeyPackage identity) kps
   lift . wrapClient $ Data.insertKeyPackages (tUnqualified lusr) cid kps'
 
-claimKeyPackages :: Local UserId -> Qualified UserId -> Maybe ClientId -> Handler r KeyPackageBundle
+claimKeyPackages ::
+  Local UserId ->
+  Qualified UserId ->
+  Maybe ClientId ->
+  Handler r KeyPackageBundle
 claimKeyPackages lusr target skipOwn =
-  foldQualified
-    lusr
-    (claimLocalKeyPackages lusr skipOwn)
-    (\_ -> throwStd federationNotImplemented)
-    target
-
-claimLocalKeyPackages :: Local UserId -> Maybe ClientId -> Local UserId -> Handler r KeyPackageBundle
-claimLocalKeyPackages lusr skipOwn target = do
-  -- skip own client when the target is the requesting user itself
-  let own = guard (lusr == target) *> skipOwn
-  clients <- map clientId <$> wrapClientE (Data.lookupClients (tUnqualified target))
   withExceptT clientError $
-    wrapHttpClientE $ guardLegalhold (ProtectedUser (tUnqualified lusr)) (mkUserClients [(tUnqualified target, clients)])
+    foldQualified
+      lusr
+      (claimLocalKeyPackages (qUntagged lusr) skipOwn)
+      (claimRemoteKeyPackages lusr)
+      target
+
+claimLocalKeyPackages ::
+  Qualified UserId ->
+  Maybe ClientId ->
+  Local UserId ->
+  ExceptT ClientError (AppT r) KeyPackageBundle
+claimLocalKeyPackages qusr skipOwn target = do
+  -- skip own client when the target is the requesting user itself
+  let own = guard (qusr == qUntagged target) *> skipOwn
+  clients <- map clientId <$> wrapClientE (Data.lookupClients (tUnqualified target))
+  foldQualified
+    target
+    ( \lusr ->
+        wrapHttpClientE $
+          guardLegalhold
+            (ProtectedUser (tUnqualified lusr))
+            (mkUserClients [(tUnqualified target, clients)])
+    )
+    (\_ -> pure ())
+    qusr
   lift $
     KeyPackageBundle . Set.fromList . catMaybes <$> traverse (mkEntry own) clients
   where
@@ -71,6 +92,31 @@ claimLocalKeyPackages lusr skipOwn target = do
         guard $ Just c /= own
         uncurry (KeyPackageBundleEntry (qUntagged target) c)
           <$> wrapClientM (Data.claimKeyPackage target c)
+
+claimRemoteKeyPackages ::
+  Local UserId ->
+  Remote UserId ->
+  ExceptT ClientError (AppT r) KeyPackageBundle
+claimRemoteKeyPackages lusr target = do
+  bundle <-
+    (handleFailure =<<) $
+      withExceptT ClientFederationError $
+        runBrigFederatorClient (tDomain target) $
+          fedClient @'Brig @"claim-key-packages" $
+            ClaimKeyPackageRequest
+              { ckprClaimant = tUnqualified lusr,
+                ckprTarget = tUnqualified target
+              }
+
+  -- set up mappings for all claimed key packages
+  wrapClientE $
+    for_ (kpbEntries bundle) $ \e ->
+      Data.mapKeyPackageRef (kpbeRef e) (kpbeUser e) (kpbeClient e)
+
+  pure bundle
+  where
+    handleFailure :: Monad m => Maybe x -> ExceptT ClientError m x
+    handleFailure = maybe (throwE (ClientUserNotFound (tUnqualified target))) pure
 
 countKeyPackages :: Local UserId -> ClientId -> Handler r KeyPackageCount
 countKeyPackages lusr c =
---
+++ services/brig/test/integration/API/MLS/Util.hs
@@ -0,0 +1,88 @@
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2022 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module API.MLS.Util where
+
+import Bilge
+import Bilge.Assert
+import Data.Aeson (object, toJSON, (.=))
+import Data.ByteString.Conversion
+import Data.Domain
+import Data.Id
+import Data.Json.Util
+import qualified Data.Map as Map
+import Data.Qualified
+import qualified Data.Text as T
+import Imports
+import System.Process
+import Util
+import Wire.API.MLS.Credential
+import Wire.API.MLS.KeyPackage
+import Wire.API.User.Client
+
+data SetKey = SetKey | DontSetKey
+  deriving (Eq)
+
+uploadKeyPackages ::
+  HasCallStack =>
+  Brig ->
+  FilePath ->
+  SetKey ->
+  Qualified UserId ->
+  ClientId ->
+  Int ->
+  Http ()
+uploadKeyPackages brig store sk u c n = do
+  let cmd0 = ["crypto-cli", "--store", store, "--enc-key", "test"]
+      clientId =
+        show (qUnqualified u)
+          <> ":"
+          <> T.unpack (client c)
+          <> "@"
+          <> T.unpack (domainText (qDomain u))
+  kps <-
+    replicateM n . liftIO . spawn . shell . unwords $
+      cmd0 <> ["key-package", clientId]
+  when (sk == SetKey) $
+    do
+      pk <-
+        liftIO . spawn . shell . unwords $
+          cmd0 <> ["public-key", clientId]
+      put
+        ( brig
+            . paths ["clients", toByteString' c]
+            . zUser (qUnqualified u)
+            . json defUpdateClient {updateClientMLSPublicKeys = Map.fromList [(Ed25519, pk)]}
+        )
+      !!! const 200 === statusCode
+  let upload = object ["key_packages" .= toJSON (map Base64ByteString kps)]
+  post
+    ( brig
+        . paths ["mls", "key-packages", "self", toByteString' c]
+        . zUser (qUnqualified u)
+        . json upload
+    )
+    !!! const (case sk of SetKey -> 201; DontSetKey -> 400) === statusCode
+
+getKeyPackageCount :: HasCallStack => Brig -> Qualified UserId -> ClientId -> Http KeyPackageCount
+getKeyPackageCount brig u c =
+  responseJsonError
+    =<< get
+      ( brig . paths ["mls", "key-packages", "self", toByteString' c, "count"]
+          . zUser (qUnqualified u)
+      )
+    <!! const 200 === statusCode
