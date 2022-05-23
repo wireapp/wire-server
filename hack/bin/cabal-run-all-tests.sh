5e3333eb5fed5c05494f492d660215c2be3dc1f4
jschaul <jschaul@users.noreply.github.com>
2022-05-24 01:21:26 +0200

    guard against imaginary case and make the bash code a little more unreadable upon request

--- hack/bin/cabal-run-all-tests.sh
+++ hack/bin/cabal-run-all-tests.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TOP_LEVEL="$(cd "$DIR/../.." && pwd)"
 
-packages=$(find "$TOP_LEVEL" -name '*.cabal' |
+mapfile -t packages < <(find "$TOP_LEVEL" -name '*.cabal' |
     grep -v dist-newstyle |
     xargs -n 1 dirname |
     xargs -n 1 basename)
 
-for p in $packages; do
+for p in "${packages[@]}"; do
     echo "==== Testing $p..."
     "$DIR/cabal-run-tests.sh" "$p"
 done
