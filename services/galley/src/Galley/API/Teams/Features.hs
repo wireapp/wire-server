50b84984cbbc5b3ae551fb11dcc315da9753b055
Stefan Matting <stefan@wire.com>
2022-06-09 16:55:32 +0200

    remove todo

--- services/galley/src/Galley/API/Teams/Features.hs
+++ services/galley/src/Galley/API/Teams/Features.hs
@@ -15,10 +15,6 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-{-
-TODO: ttl was in legalhold, filesharing, guest links, 2ndfactor and searchvisibility inbound, but NOT conference calling?
--}
-
 module Galley.API.Teams.Features
   ( getFeatureStatus,
     getFeatureStatusNoPermissionCheck,
