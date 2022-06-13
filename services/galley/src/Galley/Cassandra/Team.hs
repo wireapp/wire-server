82727426b4f426fa11bb1b12dd8d3e1e30df45d5
Leif Battermann <leif.battermann@wire.com>
2022-06-13 13:42:57 +0000

    formatting

--- services/galley/src/Galley/Cassandra/Team.hs
+++ services/galley/src/Galley/Cassandra/Team.hs
@@ -382,7 +382,7 @@ updateTeam tid u = retry x5 . batch $ do
   for_ (u ^. iconKeyUpdate) $ \k ->
     addPrepQuery Cql.updateTeamIconKey (fromRange k, tid)
   for_ (u ^. splashScreenUpdate) $ \ss ->
-    addPrepQuery Cql.updateTeamSplashScreen (ss, tid)    
+    addPrepQuery Cql.updateTeamSplashScreen (ss, tid)
 
 -- | Construct 'TeamMember' from database tuple.
 -- If FeatureLegalHoldWhitelistTeamsAndImplicitConsent is enabled set UserLegalHoldDisabled
