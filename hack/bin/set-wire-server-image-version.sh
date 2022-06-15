eb62b0a66ae4f9ab05b1179a47d4721543c8968b
Akshay Mankar <akshay@wire.com>
2022-06-15 14:13:37 +0200

    charts/wire-server: Add backoffice
    Ensure that scripts can handle updating stern version in the backoffice chart

--- hack/bin/set-wire-server-image-version.sh
+++ hack/bin/set-wire-server-image-version.sh
@@ -12,5 +12,9 @@ for chart in "${charts[@]}"; do
     sed -i "s/^  tag: .*/  tag: $target_version/g" "$CHARTS_DIR/$chart/values.yaml"
 done
 
-#special case nginz
+# special case nginz
 sed -i "s/^    tag: .*/    tag: $target_version/g" "$CHARTS_DIR/nginz/values.yaml"
+
+# special case backoffice as there are two images at the same level and we want
+# update only one.
+sed -i "s/tag: do-not-use/tag: $target_version/g" "$CHARTS_DIR/backoffice/values.yaml"
