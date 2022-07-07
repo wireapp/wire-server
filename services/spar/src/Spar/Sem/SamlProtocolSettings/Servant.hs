a4610d92130e24f4ae501a76e47e950f62b2e7bf
Matthias Fischmann <mf@zerobuzz.net>
2022-07-07 13:45:08 +0200

    Bump saml2-web-sso.

--- services/spar/src/Spar/Sem/SamlProtocolSettings/Servant.hs
+++ services/spar/src/Spar/Sem/SamlProtocolSettings/Servant.hs
@@ -29,10 +29,6 @@ import qualified SAML2.WebSSO as SAML
 import Spar.Sem.SamlProtocolSettings
 import Wire.API.Routes.Public.Spar
 
--- TODO(sandy): Why is this instance not provided by SAML? Very rude!
-instance SAML.HasConfig ((->) SAML.Config) where
-  getConfig = id
-
 sparRouteToServant :: SAML.Config -> Sem (SamlProtocolSettings ': r) a -> Sem r a
 sparRouteToServant cfg = interpret $ \case
   SpIssuer mitlt -> pure $ sparSPIssuer mitlt cfg
