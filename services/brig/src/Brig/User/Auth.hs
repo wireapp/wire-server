011b2c3f505b0df5153011715a7c96b4e7cf37b3
Marko Dimjašević <marko.dimjasevic@wire.com>
2022-08-12 15:43:03 +0200

    Add back code for Birg.User.Auth.legalHoldLogin

--- services/brig/src/Brig/User/Auth.hs
+++ services/brig/src/Brig/User/Auth.hs
@@ -589,11 +589,11 @@ legalHoldLogin ::
   LegalHoldLogin ->
   CookieType ->
   ExceptT LegalHoldLoginError m (Access ZAuth.LegalHoldUser)
-legalHoldLogin (LegalHoldLogin uid _plainTextPassword label) typ = do
-  -- locale <- Opt.setDefaultUserLocale <$> view settings
-  -- locDomain <- qualifyLocal ()
-  -- o <- runUserQueryAction $ runInputConst locDomain $ Data.reauthenticate locale uid plainTextPassword
-  -- except o !>> LegalHoldReAuthError
+legalHoldLogin (LegalHoldLogin uid plainTextPassword label) typ = do
+  locale <- Opt.setDefaultUserLocale <$> view settings
+  locDomain <- qualifyLocal ()
+  o <- runUserQueryAction $ runInputConst locDomain $ Data.reauthenticate locale uid plainTextPassword
+  except o !>> LegalHoldReAuthError
   -- legalhold login is only possible if
   -- the user is a team user
   -- and the team has legalhold enabled
