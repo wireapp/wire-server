e23e0320a9d041c39918c3c991e511cacbcc8764
Leif Battermann <leif.battermann@wire.com>
2022-08-15 16:30:05 +0200

    Update services/spar/test-integration/Test/Spar/Scim/UserSpec.hs
    Co-authored-by: fisx <mf@zerobuzz.net>

--- services/spar/test-integration/Test/Spar/Scim/UserSpec.hs
+++ services/spar/test-integration/Test/Spar/Scim/UserSpec.hs
@@ -477,6 +477,7 @@ specCreateUser = describe "POST /Users" $ do
     it "set locale to default and update to de" $ testCreateUserWithSamlIdPWithPreferredLanguage Nothing (Just (Locale (Language DE) Nothing))
     it "set locale to fr and update to de" $ testCreateUserWithSamlIdPWithPreferredLanguage (Just (Locale (Language FR) Nothing)) (Just (Locale (Language DE) Nothing))
     it "set locale to hr and update to de" $ testCreateUserWithSamlIdPWithPreferredLanguage (Just (Locale (Language HR) Nothing)) (Just (Locale (Language DE) Nothing))
+    it "set locale to hr and update to default" $ testCreateUserWithSamlIdPWithPreferredLanguage (Just (Locale (Language HR) Nothing)) Nothing
     it "set locale to default and update to default" $ testCreateUserWithSamlIdPWithPreferredLanguage (Just (Locale (Language HR) Nothing)) Nothing
   it "requires externalId to be present" $ testExternalIdIsRequired
   it "rejects invalid handle" $ testCreateRejectsInvalidHandle
