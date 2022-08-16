05a155c260c3635a21da7f315a6922b87505ed7e
Stefan Matting <stefan@wire.com>
2022-08-16 14:41:30 +0200

    Add test

--- libs/wire-api/test/unit/Test/Wire/API/MLS.hs
+++ libs/wire-api/test/unit/Test/Wire/API/MLS.hs
@@ -17,22 +17,34 @@
 
 module Test.Wire.API.MLS where
 
+import Control.Concurrent.Async
+import qualified Crypto.PubKey.Ed25519 as Ed25519
+import Data.ByteArray
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import Data.Domain
 import Data.Either.Combinators
 import Data.Hex
 import Data.Id
+import Data.Json.Util (toBase64Text)
+import Data.Qualified
 import qualified Data.Text as T
+import qualified Data.Text as Text
 import qualified Data.UUID as UUID
+import qualified Data.UUID.V4 as UUID
 import Imports
+import System.Exit
+import System.FilePath ((</>))
+import System.Process
 import Test.Tasty
 import Test.Tasty.HUnit
+import UnliftIO (withSystemTempDirectory)
 import Wire.API.MLS.CipherSuite
 import Wire.API.MLS.Commit
 import Wire.API.MLS.Credential
 import Wire.API.MLS.Epoch
 import Wire.API.MLS.Extension
+import Wire.API.MLS.Group
 import Wire.API.MLS.KeyPackage
 import Wire.API.MLS.Message
 import Wire.API.MLS.Proposal
@@ -47,7 +59,8 @@ tests =
       testCase "parse application message" testParseApplication,
       testCase "parse welcome message" testParseWelcome,
       testCase "key package ref" testKeyPackageRef,
-      testCase "validate message signature" testVerifyMLSPlainTextWithKey
+      testCase "validate message signature" testVerifyMLSPlainTextWithKey,
+      testCase "create signed remove proposal" testRemoveProposalMessageSignature
     ]
 
 testParseKeyPackage :: IO ()
@@ -154,3 +167,84 @@ testVerifyMLSPlainTextWithKey = do
     assertBool
       "message signature verification failed"
       $ verifyMessageSignature MLS_128_DHKEMX25519_AES128GCM_SHA256_Ed25519 msg pubkey
+
+testRemoveProposalMessageSignature :: IO ()
+testRemoveProposalMessageSignature = withSystemTempDirectory "mls" $ \tmp -> do
+  qcid <- do
+    let c = newClientId 0x3ae58155
+    usr <- flip Qualified (Domain "example.com") <$> (Id <$> UUID.nextRandom)
+    pure (userClientQid usr c)
+  void . liftIO $ spawn (cli qcid tmp ["init", qcid]) Nothing
+
+  qcid2 <- do
+    let c = newClientId 0x4ae58157
+    usr <- flip Qualified (Domain "example.com") <$> (Id <$> UUID.nextRandom)
+    pure (userClientQid usr c)
+  void . liftIO $ spawn (cli qcid2 tmp ["init", qcid2]) Nothing
+  kp <- liftIO $ decodeMLSError <$> spawn (cli qcid2 tmp ["key-package", "create"]) Nothing
+  liftIO $ BS.writeFile (tmp </> qcid2) (rmRaw kp)
+
+  let groupFilename = "group"
+  let gid = GroupId "abcd"
+  createGroup tmp qcid groupFilename gid
+
+  void $ liftIO $ spawn (cli qcid tmp ["member", "add", "--group", tmp </> groupFilename, "--in-place", tmp </> qcid2]) Nothing
+
+  secretKey <- Ed25519.generateSecretKey
+  let publicKey = Ed25519.toPublic secretKey
+  let message = fromJust (mkRemoveProposalMessage secretKey publicKey gid (Epoch 1) (fromJust (kpRef' kp)))
+  let messageFilename = "signed-message.mls"
+  BS.writeFile (tmp </> messageFilename) (rmRaw (mkRawMLS message))
+  let signerKeyFilename = "signer-key.bin"
+  BS.writeFile (tmp </> signerKeyFilename) (convert publicKey)
+
+  void . liftIO $ spawn (cli qcid tmp ["check-signature", "--group", tmp </> groupFilename, "--message", tmp </> messageFilename, "--signer-key", tmp </> signerKeyFilename]) Nothing
+
+createGroup :: FilePath -> String -> String -> GroupId -> IO ()
+createGroup tmp store groupName gid = do
+  groupJSON <-
+    liftIO $
+      spawn
+        ( cli
+            store
+            tmp
+            ["group", "create", T.unpack (toBase64Text (unGroupId gid))]
+        )
+        Nothing
+  liftIO $ BS.writeFile (tmp </> groupName) groupJSON
+
+decodeMLSError :: ParseMLS a => ByteString -> a
+decodeMLSError s = case decodeMLS' s of
+  Left e -> error ("Could not parse MLS object: " <> Text.unpack e)
+  Right x -> x
+
+userClientQid :: Qualified UserId -> ClientId -> String
+userClientQid usr c =
+  show (qUnqualified usr)
+    <> ":"
+    <> T.unpack (client c)
+    <> "@"
+    <> T.unpack (domainText (qDomain usr))
+
+spawn :: HasCallStack => CreateProcess -> Maybe ByteString -> IO ByteString
+spawn cp minput = do
+  (mout, ex) <- withCreateProcess
+    cp
+      { std_out = CreatePipe,
+        std_in = if isJust minput then CreatePipe else Inherit
+      }
+    $ \minh mouth _ ph ->
+      let writeInput = for_ ((,) <$> minput <*> minh) $ \(input, inh) ->
+            BS.hPutStr inh input >> hClose inh
+          readOutput = (,) <$> traverse BS.hGetContents mouth <*> waitForProcess ph
+       in snd <$> concurrently writeInput readOutput
+  case (mout, ex) of
+    (Just out, ExitSuccess) -> pure out
+    _ -> assertFailure "Failed spawning process"
+
+-- void . liftIO $ spawn (cli qcid tmp ["init", qcid]) Nothing
+
+cli :: String -> FilePath -> [String] -> CreateProcess
+cli store tmp args =
+  proc "mls-test-cli" $
+    ["--store", tmp </> (store <> ".db")] <> args
