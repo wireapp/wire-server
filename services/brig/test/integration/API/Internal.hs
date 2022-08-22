a3e97390cdbec1a40cc398bfd54259f94e41d712
Paolo Capriotti <paolo@capriotti.io>
2022-08-22 10:26:34 +0200

    Test getMLSClients

--- services/brig/test/integration/API/Internal.hs
+++ services/brig/test/integration/API/Internal.hs
@@ -68,13 +68,15 @@ tests opts mgr db brig brigep gundeck galley = do
         test mgr "suspend and unsuspend user" $ testSuspendUser db brig,
         test mgr "suspend non existing user and verify no db entry" $
           testSuspendNonExistingUser db brig,
-        testGroup "mls/key-packages" $
-          [ test mgr "fresh get" $ testKpcFreshGet brig,
-            test mgr "put,get" $ testKpcPutGet brig,
-            test mgr "get,get" $ testKpcGetGet brig,
-            test mgr "put,put" $ testKpcPutPut brig,
-            test mgr "add key package ref" $ testAddKeyPackageRef brig
-          ]
+        test mgr "mls/clients" $ testGetMlsClients brig,
+        testGroup
+          "mls/key-packages"
+          $ [ test mgr "fresh get" $ testKpcFreshGet brig,
+              test mgr "put,get" $ testKpcPutGet brig,
+              test mgr "get,get" $ testKpcGetGet brig,
+              test mgr "put,put" $ testKpcPutPut brig,
+              test mgr "add key package ref" $ testAddKeyPackageRef brig
+            ]
       ]
 
 testSuspendUser :: forall m. TestConstraints m => Cass.ClientState -> Brig -> m ()
@@ -221,6 +223,31 @@ testFeatureConferenceCallingByAccount (Opt.optSettings -> settings) mgr db brig 
   check $ ApiFt.WithStatusNoLock ApiFt.FeatureStatusEnabled ApiFt.ConferenceCallingConfig ApiFt.FeatureTTLUnlimited
   check $ ApiFt.WithStatusNoLock ApiFt.FeatureStatusDisabled ApiFt.ConferenceCallingConfig ApiFt.FeatureTTLUnlimited
   check'
+
+testGetMlsClients :: Brig -> Http ()
+testGetMlsClients brig = do
+  qusr <- userQualifiedId <$> randomUser brig
+  c <- createClient brig qusr 0
+  (cs0 :: Set ClientId) <-
+    responseJsonError
+      =<< get
+        ( brig
+            . paths ["i", "mls", "clients", toByteString' (qUnqualified qusr)]
+            . queryItem "sig_scheme" "ed25519"
+        )
+  liftIO $ cs0 @?= mempty
+
+  withSystemTempDirectory "mls" $ \tmp ->
+    uploadKeyPackages brig tmp def qusr c 2
+
+  (cs1 :: Set ClientId) <-
+    responseJsonError
+      =<< get
+        ( brig
+            . paths ["i", "mls", "clients", toByteString' (qUnqualified qusr)]
+            . queryItem "sig_scheme" "ed25519"
+        )
+  liftIO $ toList cs1 @?= [c]
 
 keyPackageCreate :: HasCallStack => Brig -> Http KeyPackageRef
 keyPackageCreate brig = do
