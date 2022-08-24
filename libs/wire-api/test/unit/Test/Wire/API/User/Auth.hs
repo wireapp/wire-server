97f7319c13fc6ca563c8b7e81c00c06db27b33d9
Leif Battermann <leif.battermann@wire.com>
2022-08-24 08:10:21 +0200

    Sqservices 1645 additional test for LoginId (#2645)

--- libs/wire-api/test/unit/Test/Wire/API/User/Auth.hs
+++ libs/wire-api/test/unit/Test/Wire/API/User/Auth.hs
@@ -24,7 +24,7 @@ import Wire.API.User
 import Wire.API.User.Auth
 
 tests :: T.TestTree
-tests = T.testGroup "Auth" [loginIdHappyCase, loginIdFailFast]
+tests = T.testGroup "Auth" [loginIdHappyCase, loginIdFailFastOnEmail, loginIdFailFastOnPhone]
 
 loginIdHappyCase :: T.TestTree
 loginIdHappyCase = testCase "LoginId parser: valid email" $ do
@@ -32,8 +32,14 @@ loginIdHappyCase = testCase "LoginId parser: valid email" $ do
   let expected = Just $ LoginByEmail (Email {emailLocal = "foo", emailDomain = "bar.com"})
   assertEqual "should succeed" expected actual
 
-loginIdFailFast :: T.TestTree
-loginIdFailFast = testCase "LoginId parser: invalid email, valid phone" $ do
+loginIdFailFastOnEmail :: T.TestTree
+loginIdFailFastOnEmail = testCase "LoginId parser: invalid email, valid phone" $ do
   let actual :: Maybe LoginId = Aeson.decode "{\"email\":\"invalid-email\",\"phone\":\"+123456789\"}"
+  let expected = Nothing
+  assertEqual "should fail if any provided login id is invalid" expected actual
+
+loginIdFailFastOnPhone :: T.TestTree
+loginIdFailFastOnPhone = testCase "LoginId parser: invalid phone, valid email" $ do
+  let actual :: Maybe LoginId = Aeson.decode "{\"email\":\"foo@bar.com\",\"phone\":\"invalid-phone\"}"
   let expected = Nothing
   assertEqual "should fail if any provided login id is invalid" expected actual
