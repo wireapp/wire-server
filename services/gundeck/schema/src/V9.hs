931e570c1d333a7cdf5d94e4b32589a1389bd52a
jschaul <jschaul@users.noreply.github.com>
2022-09-01 13:24:05 +0200

    gundeck/cassandra: TWCS for 'notifications' table (#2615)
    * gundeck/cassandra: TWCS for 'notifications' table
    
    In Gundeck's 'notifications' cassandra table, switch to [TWCS](https://cassandra.apache.org/doc/latest/cassandra/operating/compaction/twcs.html) compaction strategy, which should be more efficient for this workload, and possibly bring performance benefits to latencies.
    
    It may be beneficial to run a manual compaction before rolling out this
    change (but things should also work without this manual operation).
    
    In case you have time, run the following before deploying this update:
    
    ```
    nodetool compact gundeck notifications
    ```
    
    Co-authored-by: Akshay Mankar <akshay@wire.com>

--- services/gundeck/schema/src/V9.hs
+++ services/gundeck/schema/src/V9.hs
@@ -15,41 +15,19 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-module Main where
+module V9
+  ( migration,
+  )
+where
 
 import Cassandra.Schema
-import Control.Exception (finally)
 import Imports
-import qualified System.Logger.Extended as Log
-import Util.Options
-import qualified V1
-import qualified V2
-import qualified V3
-import qualified V4
-import qualified V5
-import qualified V6
-import qualified V7
-import qualified V8
-import qualified V9
+import Text.RawString.QQ
 
-main :: IO ()
-main = do
-  o <- getOptions desc (Just migrationOptsParser) defaultPath
-  l <- Log.mkLogger'
-  migrateSchema
-    l
-    o
-    [ V1.migration,
-      V2.migration,
-      V3.migration,
-      V4.migration,
-      V5.migration,
-      V6.migration,
-      V7.migration,
-      V8.migration,
-      V9.migration
-    ]
-    `finally` Log.close l
-  where
-    desc = "Gundeck Cassandra Schema Migrations"
-    defaultPath = "/etc/wire/gundeck/conf/gundeck-schema.yaml"
+migration :: Migration
+migration = Migration 9 "Remove deprecated tables" $ do
+  -- all data in notifications is written with a TTL, therefore should be a good fit for TWCS.
+  -- TTL is 28 days (see https://github.com/wireapp/wire-server/blob/434f7a874ce5e3f7e3e57aa98afb6441d4a53169/charts/gundeck/templates/configmap.yaml#L51)
+  -- so 28 windows of 1 day each fits well with the suggestion of 20-30 windows.
+  -- https://cassandra.apache.org/doc/latest/cassandra/operating/compaction/twcs.html
+  schema' [r| ALTER TABLE notifications WITH compaction = {'class': 'TimeWindowCompactionStrategy', 'compaction_window_unit': 'DAYS', 'compaction_window_size': 1}; |]
