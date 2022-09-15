8b7c5f4b8a90ac996d59842587c7b4d1a98bbd26
Leif Battermann <leif.battermann@wire.com>
2022-09-15 09:00:36 +0000

    feature flag for cargo

--- nix/pkgs/rusty_jwt_tools/default.nix
+++ nix/pkgs/rusty_jwt_tools/default.nix
@@ -12,6 +12,7 @@ rustPlatform.buildRustPackage rec {
     rev = "6370cd556f03f6834d0b8043615ffaf0044ef1fa";
     sha256 = "sha256-vnTvKITie4pu+ISIl/RdYPfb/yWCdCI9eHl1KcZb050=";
   };
+  cargoBuildFeatures = "haskell";
   cargoSha256 = "sha256-gR9XKcJslCcXo3EnD3MweWm+pQr4/EAEuFyOq3/l97g=";
   cargoPatches = [
     # a patch file to add/update Cargo.lock in the source code
