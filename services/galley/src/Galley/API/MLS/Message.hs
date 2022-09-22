7ae35ded70d8997f0df608c78e49eedff58fff0f
Leif Battermann <leif.battermann@wire.com>
2022-09-22 08:03:39 +0000

    format

--- services/galley/src/Galley/API/MLS/Message.hs
+++ services/galley/src/Galley/API/MLS/Message.hs
@@ -15,7 +15,6 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-
 module Galley.API.MLS.Message
   ( postMLSCommitBundle,
     postMLSCommitBundleFromLocalUser,
