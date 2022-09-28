c2f604c52726021a4efd5cb4f7c5c78b1af3e61f
Leif Battermann <leif.battermann@wire.com>
2022-09-28 11:06:51 +0000

    test for toResult

--- libs/jwt-tools/test/Spec.hs
+++ libs/jwt-tools/test/Spec.hs
@@ -17,6 +17,7 @@
 
 import Control.Monad.Trans.Except
 import Data.Jwt.Tools
+import Data.String.Conversions (cs)
 import Imports
 import Test.Hspec
 
@@ -25,8 +26,57 @@ main = hspec $ do
   describe "generateDpopToken FFI" $ do
     it "should return a value" $ do
       actual <- callFFIWithConstValues
-      let expected = Right $ "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
+      let expected = Right $ cs token
       actual `shouldBe` expected
+  describe "toResult" $ do
+    it "should convert to correct error" $ do
+      -- the only valid case is when the error=0 (meaning no error) and the token is not null
+      toResult Nothing (Just token) `shouldBe` Right (cs token)
+
+      -- error=1 corresponds to an unknown error on FFI side
+      toResult (Just 1) Nothing `shouldBe` Left FfiError
+      toResult (Just 1) (Just token) `shouldBe` Left FfiError
+      -- error=2 corresponds to 'FfiError' on FFI side
+      toResult (Just 2) Nothing `shouldBe` Left FfiError
+      toResult (Just 2) (Just token) `shouldBe` Left FfiError
+      -- error=3 corresponds to 'ImplementationError' on FFI side
+      toResult (Just 3) Nothing `shouldBe` Left FfiError
+      toResult (Just 3) (Just token) `shouldBe` Left FfiError
+      toResult (Just 4) Nothing `shouldBe` Left DpopSyntaxError
+      toResult (Just 4) (Just token) `shouldBe` Left DpopSyntaxError
+      toResult (Just 5) Nothing `shouldBe` Left DpopTypError
+      toResult (Just 5) (Just token) `shouldBe` Left DpopTypError
+      toResult (Just 6) Nothing `shouldBe` Left DpopUnsupportedAlgorithmError
+      toResult (Just 6) (Just token) `shouldBe` Left DpopUnsupportedAlgorithmError
+      toResult (Just 7) Nothing `shouldBe` Left DpopInvalidSignatureError
+      toResult (Just 7) (Just token) `shouldBe` Left DpopInvalidSignatureError
+      toResult (Just 8) Nothing `shouldBe` Left ClientIdMismatchError
+      toResult (Just 8) (Just token) `shouldBe` Left ClientIdMismatchError
+      toResult (Just 9) Nothing `shouldBe` Left BackendNonceMismatchError
+      toResult (Just 9) (Just token) `shouldBe` Left BackendNonceMismatchError
+      toResult (Just 10) Nothing `shouldBe` Left HtuMismatchError
+      toResult (Just 10) (Just token) `shouldBe` Left HtuMismatchError
+      toResult (Just 11) Nothing `shouldBe` Left HtmMismatchError
+      toResult (Just 11) (Just token) `shouldBe` Left HtmMismatchError
+      toResult (Just 12) Nothing `shouldBe` Left MissingJtiError
+      toResult (Just 12) (Just token) `shouldBe` Left MissingJtiError
+      toResult (Just 13) Nothing `shouldBe` Left MissingChallengeError
+      toResult (Just 13) (Just token) `shouldBe` Left MissingChallengeError
+      toResult (Just 14) Nothing `shouldBe` Left MissingIatError
+      toResult (Just 14) (Just token) `shouldBe` Left MissingIatError
+      toResult (Just 15) Nothing `shouldBe` Left IatError
+      toResult (Just 15) (Just token) `shouldBe` Left IatError
+      toResult (Just 16) Nothing `shouldBe` Left MissingExpError
+      toResult (Just 16) (Just token) `shouldBe` Left MissingExpError
+      toResult (Just 17) Nothing `shouldBe` Left ExpMismatchError
+      toResult (Just 17) (Just token) `shouldBe` Left ExpMismatchError
+      toResult (Just 18) Nothing `shouldBe` Left ExpError
+      toResult (Just 18) (Just token) `shouldBe` Left ExpError
+      -- this should also not happen, but apparently something went wrong
+      toResult Nothing Nothing `shouldBe` Left FfiError
+
+token :: String
+token = "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
 
 callFFIWithConstValues :: IO (Either DPoPTokenGenerationError ByteString)
 callFFIWithConstValues = do
