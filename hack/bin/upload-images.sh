6687edb1037fd828f010f0b83f126b1b4a3553c9
Akshay Mankar <akshay@wire.com>
2022-09-29 12:09:18 +0200

    Run nix-build with --show-trace

--- hack/bin/upload-images.sh
+++ hack/bin/upload-images.sh
@@ -28,15 +28,15 @@ if [[ "${DOCKER_USER+x}" != "" ]]; then
     credsArgs="--dest-creds=$DOCKER_USER:$DOCKER_PASSWORD"
 fi
 
-images_list="$(nix-build "$ROOT_DIR/nix" -A wireServer.imagesList)"
+images_list="$(nix-build --show-trace "$ROOT_DIR/nix" -A wireServer.imagesList)"
 
 # Build everything first so we can benefit the most from having many cores.
-nix-build "$ROOT_DIR/nix" -A "wireServer.$IMAGES_ATTR"
+nix-build --show-trace "$ROOT_DIR/nix" -A "wireServer.$IMAGES_ATTR"
 
 while IFS="" read -r image_name || [ -n "$image_name" ]
 do
     printf '*** Uploading image %s\n' "$image_name"
-    image=$(nix-build "$ROOT_DIR/nix" -A "wireServer.$IMAGES_ATTR.$image_name")
+    image=$(nix-build --show-trace "$ROOT_DIR/nix" -A "wireServer.$IMAGES_ATTR.$image_name")
     repo=$(skopeo list-tags "docker-archive://$image" | jq -r '.Tags[0] | split(":") | .[0]')
     echo "Uploading $image to $repo:$DOCKER_TAG"
     # shellcheck disable=SC2086
