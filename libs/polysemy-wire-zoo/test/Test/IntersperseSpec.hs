9e40e6336dc86e295af28a0117f1df36a93a430b
Sandy Maguire <sandy@sandymaguire.me>
2022-10-15 15:16:05 -0700

    chore: format

--- libs/polysemy-wire-zoo/test/Test/IntersperseSpec.hs
+++ libs/polysemy-wire-zoo/test/Test/IntersperseSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NumDecimals                 #-}
+{-# LANGUAGE NumDecimals #-}
 {-# OPTIONS_GHC -fplugin=Polysemy.Plugin #-}
 
 module Test.IntersperseSpec where
@@ -8,8 +8,8 @@ import Imports hiding (intersperse)
 import Polysemy
 import Polysemy.Testing
 import Polysemy.Trace
-import UnliftIO (async)
 import Test.Hspec
+import UnliftIO (async)
 
 -- | This test spins up an async thread that communicates with the main
 -- polysemy monad via an 'MVar'. We then use 'intersperse' to inject polling
@@ -18,13 +18,14 @@ spec :: Spec
 spec = do
   it "should poll from async-written channel" $ do
     result <- liftIO test
-    let desired = S.fromList $ mconcat
-          [ fmap ("loaded: " <>) ["hello", "world", "last"]
-          , fmap (show @Int) [1..4]
-          , ["finished"]
-          ]
+    let desired =
+          S.fromList $
+            mconcat
+              [ fmap ("loaded: " <>) ["hello", "world", "last"],
+                fmap (show @Int) [1 .. 4],
+                ["finished"]
+              ]
     result `shouldBe` desired
-
 
 pull :: (Member (Embed IO) r, Member Trace r) => MVar String -> Sem r ()
 pull chan = do
@@ -34,24 +35,21 @@ pull chan = do
       trace $ "loaded: " <> s
       pull chan
 
-
 push :: MVar String -> IO ()
 push chan = do
   putMVar chan "hello"
   putMVar chan "world"
   putMVar chan "last"
 
-
-
 test :: IO (Set String)
 test = fmap S.fromList $ do
   chan <- newEmptyMVar @_ @String
   _ <- async $ push chan
-  fmap fst $ runM $
-    runTraceList $
-      intersperse (pull chan) $ do
-        for_ [1 .. 4] $ \i -> do
-          trace $ show @Int i
-          liftIO $ threadDelay 1e5
-        trace "finished"
-
+  fmap fst $
+    runM $
+      runTraceList $
+        intersperse (pull chan) $ do
+          for_ [1 .. 4] $ \i -> do
+            trace $ show @Int i
+            liftIO $ threadDelay 1e5
+          trace "finished"
