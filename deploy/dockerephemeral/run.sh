7df4a4accb07197d15d8b984979f6ce6e7c8ac97
Akshay Mankar <akshay@wire.com>
2022-10-24 14:53:31 +0200

    Use locally built schema binaries for db migrations  (#2791)
    * Makefile: Allow db-migrate and db-reset to migrate/reset all keyspaces
    
    * Use locally built schema binaries for db migrations
    
    Also make sure DB is up to date every time tests are run and not just when DBs
    are spun up.

--- deploy/dockerephemeral/run.sh
+++ deploy/dockerephemeral/run.sh
@@ -4,17 +4,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOCKER_FILE="$SCRIPT_DIR/docker-compose.yaml"
 
-# If there are schema changes and you don't force pull the docker
-# migrations, you may run out of sync and you would get this error
-# message (or a similar one):
-#
-#     brig: Schema Version too old! Expecting at least: 49, but got: 48
-#
-# So we always pull these migration images first.
-docker pull quay.io/wire/brig-schema
-docker pull quay.io/wire/galley-schema
-docker pull quay.io/wire/gundeck-schema
-docker pull quay.io/wire/spar-schema
-docker pull quay.io/wire/brig-index
-
 docker-compose --file "$DOCKER_FILE" up
