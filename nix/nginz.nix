3129dfd9974d85e130c182e7e2073f21fa242502
jschaul <jschaul@users.noreply.github.com>
2022-10-26 16:46:21 +0200

    Support from flokli to fix issues

--- nix/nginz.nix
+++ nix/nginz.nix
@@ -1,14 +1,22 @@
 { nginxModules
 , pkg-config
 , callPackage
-, stdenvNoCC
+, stdenv
+, symlinkJoin
 , dockerTools
+, writeTextDir
+, runCommand
+, gnugrep
+, coreutils
 , nginx
 , inotify-tools
 , dumb-init
 , cacert
 , bashInteractive
 , openssl
+, lib
+, makeWrapper
+, writers
 }:
 let zauth = callPackage ./pkgs/zauth { };
 
@@ -20,6 +28,11 @@ let zauth = callPackage ./pkgs/zauth { };
     };
 
     nginz = nginx.override {
+      # openssl = (openssl.overrideAttrs (oldAttrs: {
+      #   patches = oldAttrs.patches or [] ++ [
+      #     ./patch.patch
+      #   ];
+      # }));
       modules = [
         nginzModules.vts
         nginzModules.moreheaders
@@ -27,53 +40,60 @@ let zauth = callPackage ./pkgs/zauth { };
       ];
     };
 
-    reload-script = stdenvNoCC.mkDerivation {
+    reload-script = stdenv.mkDerivation {
       name = "reload-script";
-      src = ../services/nginz/nginz_reload.sh;
+      src = (writers.writeBash "nginz_reload.sh" ../services/nginz/nginz_reload.sh);
       phases = "installPhase";
+      nativeBuildInputs = [ makeWrapper ];
       installPhase = ''
-         mkdir -p $out/usr/bin
-         cp $src $out/usr/bin/nginz_reload.sh
+         mkdir -p $out/bin
+         cp $src $out/bin/nginz_reload.sh
+         wrapProgram $out/bin/nginz_reload.sh \
+           --prefix PATH : "${lib.makeBinPath [ inotify-tools nginz ]}"
       '';
     };
 
-    # Use the following to get the digest and sha:
-    # nix-shell -p pkgs.nix-prefetch-docker --run 'nix-prefetch-docker --image-name alpine --image-tag 3.15'
-    # And the sha this gives back seems incorrect, so build it, wait for the error, then adjust the sha again.
-    alpine-image = dockerTools.pullImage {
-      imageName = "alpine";
-      imageDigest = "sha256:69463fdff1f025c908939e86d4714b4d5518776954ca627cbeff4c74bcea5b22";
-      sha256 = "sha256-LTayEaBtNOho1mhGyivKLZdu5+pSL3SZWbIAMpUJM4E=";
+    # copied from nixpkgs fakeNss, but using nginx as username
+    nginxFakeNss =  symlinkJoin {
+      name = "fake-nss";
+      paths = [
+        (writeTextDir "etc/passwd" ''
+          root:x:0:0:root user:/var/empty:/bin/sh
+          nginx:x:101:101:nginx:/var/empty:/bin/sh
+          nobody:x:65534:65534:nobody:/var/empty:/bin/sh
+        '')
+        (writeTextDir "etc/group" ''
+          root:x:0:
+          nginx:x:101:
+          nobody:x:65534:
+        '')
+        (writeTextDir "etc/nsswitch.conf" ''
+          hosts: files dns
+        '')
+        (runCommand "var-empty" { } ''
+          mkdir -p $out/var/empty
+        '')
+        # it seems nginx still tries to log, and doesn't create
+        # these directories automatically
+        (runCommand "nginx-misc" { } ''
+          mkdir -p $out/var/log/nginx
+          mkdir -p $out/var/cache/nginx
+        '')
+      ];
     };
 
     nginzImage = dockerTools.buildLayeredImage {
       name = "quay.io/wire/nginz";
       maxLayers = 5;
-      fromImage = alpine-image;
       contents = [
         cacert
         bashInteractive
-        openssl
-        dumb-init
-        inotify-tools
-        reload-script
-        # nginz # adding this removes the 'apk' command from alpine for some reason...
+        gnugrep
+        coreutils
+        nginxFakeNss
       ];
-      # TODO add user and group for nginx, doesn't work! :(
-      enableFakechroot = true;
-      fakeRootCommands = ''
-          /sbin/apk update
-          /sbin/apk add curl
-          # nginx still tries to read this directory even if error_log
-          # directive is specifying another file :/
-          mkdir -p /var/log/nginx
-          mkdir -p /var/cache/nginx
-          # nginz config hardcodes nginx user/group
-          addgroup -g 101 -S nginx
-          adduser -S -D -H -u 101 -h /var/cache/nginx -s /sbin/nologin -G nginx -g nginx nginx
-      '';
       config = {
-        Entrypoint = ["${dumb-init}/bin/dumb-init" "--" "/usr/bin/nginz_reload.sh" "-g" "daemon off;" "-c" "/etc/wire/nginz/conf/nginx.conf"];
+        Entrypoint = ["${dumb-init}/bin/dumb-init" "--" "${reload-script}/bin/nginz_reload.sh" "-g" "daemon off;" "-c" "/etc/wire/nginz/conf/nginx.conf"];
         Env = ["SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"];
         ExposedPorts = {
           "80/tcp" = {};
