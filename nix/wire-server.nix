f9acd7991db62001846881ee2e84bc99ea269269
Akshay Mankar <akshay@wire.com>
2022-10-31 13:39:07 +0100

    nix: selectively pin openssl to 1.1 for Haskell packages.
    * nix: Pin openssl to 1.1
    
    * Revert "nix: Pin openssl to 1.1"
    
    This causes a world rebuild of nixpkgs (which takes a lot of time), and the
    build in CI doesn't complete successfully anyhow.
    
    This reverts commit dc1f54eceb0d98ab192f2004bf335a17b05217ba.
    
    * nix: selectively pin openssl to 1.1 for Haskell packages.
    
    This injects openssl 1.1 into the build of specifically the HsOpenSSL package,
    which other Haskell derivations depend on for TLS functionality.
    
    Co-authored-by: Molly Miller <molly.miller@wire.com>

--- nix/wire-server.nix
+++ nix/wire-server.nix
@@ -123,6 +123,7 @@ let lib = pkgs.lib;
       ];
     manualOverrides = import ./manual-overrides.nix (with pkgs; {
       inherit hlib libsodium protobuf mls-test-cli;
+      openssl = openssl_1_1;
     });
 
     executables = hself: hsuper:
