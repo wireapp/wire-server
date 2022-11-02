1a22e54fb8184ee8d5da852f8a0a8adb61ec3272
Sven Tennie <sven.tennie@wire.com>
2022-11-02 15:55:32 +0100

    Good case: Receive mail via SMTP

--- services/brig/test/integration/SMTP.hs
+++ services/brig/test/integration/SMTP.hs
@@ -1,27 +1,33 @@
 module SMTP where
 
-import Bilge
+import qualified Bilge
 import Brig.SMTP
 import Control.Exception
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import Data.Text (unpack)
+import Data.Text.Lazy (toStrict)
 import Imports
 import Network.Mail.Mime
 import qualified Network.Mail.Postie as Postie
+import qualified Pipes.Prelude
 import qualified System.Logger as Logger
 import Test.Tasty
 import Test.Tasty.HUnit
 import Util
 
 -- TODO: Is IO needed here?
-tests :: Manager -> Logger.Logger -> IO TestTree
+tests :: Bilge.Manager -> Logger.Logger -> IO TestTree
 tests m lg =
   pure $
     testGroup
       "SMTP"
-      [ test m "should send no mail without receiver" $ testSendMailNoReceiver lg
+      [ test m "should send mail" $ testSendMail lg,
+        test m "should send no mail without receiver" $ testSendMailNoReceiver lg
       ]
 
 -- TODO: Is Http the best Monad for this?
-testSendMailNoReceiver :: Logger.Logger -> Http ()
+testSendMailNoReceiver :: Logger.Logger -> Bilge.Http ()
 testSendMailNoReceiver lg = do
   receivedMailRef <- liftIO $ newIORef Nothing
   liftIO
@@ -34,11 +40,47 @@ testSendMailNoReceiver lg = do
           (sendMail lg conPool (emptyMail (Address Nothing "foo@example.com")) >> pure False)
       caughtException @? "Expected exception due to missing mail receiver."
 
---      traceM "Sent mail"
---      mbMail <-
---        retryWhileN 3 isJust $ do
---          readIORef receivedMailRef
---      isJust mbMail @? "Expected to receive mail"
+testSendMail :: Logger.Logger -> Bilge.Http ()
+testSendMail lg = do
+  receivedMailRef <- liftIO $ newIORef Nothing
+  liftIO
+    . withMailServer (mailStoringApp receivedMailRef)
+    $ do
+      conPool <- initSMTP lg "localhost" (Just 4242) Nothing Plain
+      sendMail lg conPool mail
+      mbMail <-
+        retryWhileN 3 isJust $ do
+          readIORef receivedMailRef
+      isJust mbMail @? "Expected to receive mail"
+      postieAddressAsString . rmSender <$> mbMail
+        @=? (Just . unpack . addressEmail) sender
+      postieAddressAsString <$> (concat . maybeToList) (rmReceipients <$> mbMail)
+        @=? [(unpack . addressEmail) receiver]
+      let mailContent = (rmContent . fromJust) mbMail
+      elem ((unpack . toStrict) body) mailContent @? "Expected the SMTP server to receive the mail body."
+  where
+    receiver = Address Nothing "foo@example.com"
+    sender = Address Nothing "bar@example.com"
+    subject = "Some Subject"
+    body = "Some body"
+    mail =
+      simpleMail'
+        receiver
+        sender
+        subject
+        body
+    postieAddressAsString :: Postie.Address -> String
+    postieAddressAsString addr =
+      toString
+        ( B.concat
+            [ Postie.addressLocalPart addr,
+              C.singleton '@',
+              Postie.addressDomain addr
+            ]
+        )
+
+toString :: B.ByteString -> String
+toString bs = C.foldr (:) [] bs
 
 withMailServer :: Postie.Application -> IO () -> IO ()
 withMailServer app action =
@@ -47,7 +89,20 @@ withMailServer app action =
     killThread
     (const action)
 
-mailStoringApp :: IORef (Maybe Postie.Mail) -> Postie.Application
-mailStoringApp receivedMailRef mail =
-  writeIORef receivedMailRef (Just mail)
-    >> pure Postie.Accepted
+data ReceivedMail = ReceivedMail
+  { rmSender :: Postie.Address,
+    rmReceipients :: [Postie.Address],
+    rmContent :: [String]
+  }
+
+mailStoringApp :: IORef (Maybe ReceivedMail) -> Postie.Application
+mailStoringApp receivedMailRef mail = do
+  c <- Pipes.Prelude.toListM (Postie.mailBody mail)
+  let receivedMail =
+        ReceivedMail
+          { rmSender = Postie.mailSender mail,
+            rmReceipients = Postie.mailRecipients mail,
+            rmContent = C.unpack <$> c
+          }
+  writeIORef receivedMailRef (Just receivedMail)
+  pure Postie.Accepted
