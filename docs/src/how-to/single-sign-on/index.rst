c360c148489cec219c4a7146450ebfc0e6e520c2
Sebastian Willenborg <sebastian.willenborg@wire.com>
2022-11-03 12:02:03 +0100

    fixup: add redirects for pages renamed in #2808 (#2811)
    add sphinx extension sphinx-reredirects and configuration to generate simple JavaScript based redirects to new locations of previously inconsistently named files/URLs. Cleanup of old redirect pages.

--- docs/src/how-to/single-sign-on/index.rst
+++
@@ -1,4 +0,0 @@
-Single-sign-on how-tos
-======================
-
-This page moved to :ref:`Setting up SSO externally`.
--- nix/overlay-docs.nix
+++ nix/overlay-docs.nix
@@ -3,6 +3,7 @@ self: super: rec {
     packageOverrides = pself: psuper: {
       rst2pdf = pself.callPackage ./pkgs/python-docs/rst2pdf.nix { };
       sphinx-multiversion = pself.callPackage ./pkgs/python-docs/sphinx-multiversion.nix { };
+      sphinx_reredirects = pself.callPackage ./pkgs/python-docs/sphinx_reredirects.nix { };
       sphinxcontrib-kroki = pself.callPackage ./pkgs/python-docs/sphinxcontrib-kroki.nix { };
       svg2rlg = pself.callPackage ./pkgs/python-docs/svg2rlg.nix { };
     };
