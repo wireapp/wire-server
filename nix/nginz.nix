de6ebb642607371cba68d5a362baff2a1a2830ee
jschaul <jschaul@users.noreply.github.com>
2022-11-08 16:32:27 +0100

    Build nginz and nginz_disco docker images using nix, too (#2796)
    * WIP
    
    * make inputs explicit
    
    * fixup for paths
    
    * use alpine base image
    
    * Support from flokli to fix issues
    
    * formatting
    
    * add nixified nginz-disco image
    
    * attempt to add to default.nix
    
    * nginx: [warn] could not build optimal variables_hash, you should increase either variables_hash_max_size: 1024 or variables_hash_bucket_size: 64; ignoring variables_hash_bucket_size
    
    * PR feedback: openssl 1.1
    
    * changelog
    
    * also upload nginz and nginz-disco images
    
    * PR feedback
    
    * nix/nginz.nix: add nginz to contents
    
    cannon has a preStop lifecycle hook that currently invokes nginx -c …
    quit on stop.
    
    * nginz container: drop exposed ports
    
    All config is defined in the helm chart, just by building the container
    we don't know which ports are exposed.
    
    * services/nginz: drop Dockerfile, README and Makefile
    
    We use Nix to build this now.
    
    * Hi CI
    
    * remove no-longer-necessary openssl1.1 override
    
    Co-authored-by: Florian Klink <flokli@flokli.de>

---
+++ nix/nginz.nix
@@ -0,0 +1,80 @@
+{ stdenv
+, symlinkJoin
+, dockerTools
+, writeTextDir
+, runCommand
+, gnugrep
+, coreutils
+, which
+, inotify-tools
+, dumb-init
+, cacert
+, bashInteractive
+, lib
+, makeWrapper
+, writers
+, nginz
+}:
+let
+
+  nginzWithReloader = stdenv.mkDerivation {
+    name = "reload-script";
+    src = (writers.writeBash "nginz_reload.sh" ../services/nginz/nginz_reload.sh);
+    phases = "installPhase";
+    nativeBuildInputs = [ makeWrapper ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/nginz_reload.sh
+      wrapProgram $out/bin/nginz_reload.sh \
+        --prefix PATH : "${lib.makeBinPath [ inotify-tools nginz ]}"
+    '';
+  };
+
+  # copied from nixpkgs fakeNss, but using nginx as username
+  nginxFakeNss = symlinkJoin {
+    name = "fake-nss";
+    paths = [
+      (writeTextDir "etc/passwd" ''
+        root:x:0:0:root user:/var/empty:/bin/sh
+        nginx:x:101:101:nginx:/var/empty:/bin/sh
+        nobody:x:65534:65534:nobody:/var/empty:/bin/sh
+      '')
+      (writeTextDir "etc/group" ''
+        root:x:0:
+        nginx:x:101:
+        nobody:x:65534:
+      '')
+      (writeTextDir "etc/nsswitch.conf" ''
+        hosts: files dns
+      '')
+      (runCommand "var-empty" { } ''
+        mkdir -p $out/var/empty
+      '')
+      # it seems nginx still tries to log, and doesn't create
+      # these directories automatically
+      (runCommand "nginx-misc" { } ''
+        mkdir -p $out/var/log/nginx
+        mkdir -p $out/var/cache/nginx
+      '')
+    ];
+  };
+
+  nginzImage = dockerTools.streamLayeredImage {
+    name = "quay.io/wire/nginz";
+    maxLayers = 10;
+    contents = [
+      cacert
+      bashInteractive
+      gnugrep
+      which
+      coreutils
+      nginxFakeNss
+      nginz # so preStop lifecycle hook in cannon can nginx -c … quit
+    ];
+    config = {
+      Entrypoint = [ "${dumb-init}/bin/dumb-init" "--" "${nginzWithReloader}/bin/nginz_reload.sh" "-g" "daemon off;" "-c" "/etc/wire/nginz/conf/nginx.conf" ];
+      Env = [ "SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt" ];
+    };
+  };
+in
+nginzImage
