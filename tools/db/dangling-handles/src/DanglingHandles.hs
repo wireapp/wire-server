1abc7227505bfa80bac6d9427c6df2d80ae9ea58
Akshay Mankar <akshay@wire.com>
2022-11-09 15:42:10 +0100

    Add script to find users without claimed handles

--- tools/db/dangling-handles/src/DanglingHandles.hs
+++ tools/db/dangling-handles/src/DanglingHandles.hs
@@ -19,7 +19,7 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-module Work where
+module DanglingHandles where
 
 import Brig.Data.Instances ()
 import Brig.Types.Intra
