dbd014c31bcf024b2696c29dd362ce651e293ea1
Matthias Fischmann <mf@zerobuzz.net>
2022-11-18 11:33:42 +0100

    shellcheck.

--- services/proxy/test/scripts/proxy-test.sh
+++ services/proxy/test/scripts/proxy-test.sh
@@ -10,7 +10,7 @@ fail() {
 
 check_login() {
   echo "checking login..."
-  status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null -I -X GET --header "Authorization: Bearer $BEARER" $WIRE_BACKEND/self)
+  status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null -I -X GET --header "Authorization: Bearer $BEARER" "$WIRE_BACKEND"/self)
 
   if [[ "$status_code" == 200 ]]; then
     echo "login: OK"
@@ -21,7 +21,7 @@ check_login() {
 }
 
 check_url() {
-  status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null -I -X $1 --header "Authorization: Bearer $BEARER" "$2")
+  status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null -I -X "$1" --header "Authorization: Bearer $BEARER" "$2")
   if [[ "$status_code" == "$3" ]]; then
     echo "proxy $2: OK"
   else
@@ -31,15 +31,15 @@ check_url() {
 }
 
 get_access_token() {
-  BEARER=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"email":"'"$WIRE_ADMIN"'","password":"'"$WIRE_PASSWD"'"}' $WIRE_BACKEND/login'?persist=false' | jq -r .access_token)
+  BEARER=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"email":"'"$WIRE_ADMIN"'","password":"'"$WIRE_PASSWD"'"}' "$WIRE_BACKEND"/login'?persist=false' | jq -r .access_token)
 }
 
 get_access_token
 check_login
-check_url "GET" $WIRE_BACKEND'/v2/proxy/youtube/v3/search' 200
-check_url "GET" $WIRE_BACKEND'/v2/proxy/googlemaps/api/staticmap?center=Berlin&zoom=14&size=400x400' 200
-check_url "GET" $WIRE_BACKEND'/v2/proxy/googlemaps/maps/api/geocode/json?place_id=ChIJeRpOeF67j4AR9ydy_PIzPuM' 200
-check_url "GET" $WIRE_BACKEND'/v2/proxy/giphy/v1/gifs/search?limit=100&offset=0&q=kitty' 403
-check_url "POST" $WIRE_BACKEND'/v2/proxy/spotify/api/token' 400
-check_url "GET" $WIRE_BACKEND'/v2/proxy/soundcloud/resolve' 400
-check_url "GET" $WIRE_BACKEND'/v2/proxy/soundcloud/stream' 400
+check_url "GET" "$WIRE_BACKEND"'/v2/proxy/youtube/v3/search' 200
+check_url "GET" "$WIRE_BACKEND"'/v2/proxy/googlemaps/api/staticmap?center=Berlin&zoom=14&size=400x400' 200
+check_url "GET" "$WIRE_BACKEND"'/v2/proxy/googlemaps/maps/api/geocode/json?place_id=ChIJeRpOeF67j4AR9ydy_PIzPuM' 200
+check_url "GET" "$WIRE_BACKEND"'/v2/proxy/giphy/v1/gifs/search?limit=100&offset=0&q=kitty' 403
+check_url "POST" "$WIRE_BACKEND"'/v2/proxy/spotify/api/token' 400
+check_url "GET" "$WIRE_BACKEND"'/v2/proxy/soundcloud/resolve' 400
+check_url "GET" "$WIRE_BACKEND"'/v2/proxy/soundcloud/stream' 400
