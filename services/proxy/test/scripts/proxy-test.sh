3c718ceb53fd12bd54d77988070875bd5341b224
Matthias Fischmann <mf@zerobuzz.net>
2022-11-18 17:35:50 +0100

    test script.

--- services/proxy/test/scripts/proxy-test.sh
+++ services/proxy/test/scripts/proxy-test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -o pipefail
+set -o errexit
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 echo "WIRE_BACKEND: $WIRE_BACKEND"
 echo "WIRE_ADMIN: $WIRE_ADMIN"
 echo "WIRE_PASSWD: <not shown>"
@@ -22,30 +27,43 @@ check_login() {
 }
 
 check_url() {
-  status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null -I -X "$1" --header "Authorization: Bearer $BEARER" --header "Content-Type: application/json" "$2")
-  if [[ "$status_code" == "$3" ]]; then
-    echo "proxy $2: OK"
+  export testnum=$1
+  export verb=$2
+  export uri=$3
+  export status=$4
+
+  status_code=$(curl --write-out '%{http_code}' --silent --output "./proxy-test/$testnum.txt" -I -X "$verb" \
+                     --header "Authorization: Bearer $BEARER" \
+                     --header "Content-Type: application/json" \
+                     "$uri")
+  if [[ "$status_code" == "$status" ]]; then
+    echo "proxy $uri: OK"
   else
-    echo "expected status code: $3, but got $status_code"
-    fail "proxy $2: FAIL"
+    echo "expected status code: $status, but got $status_code"
+    fail "proxy $uri: FAIL"
   fi
+
+  git diff "./proxy-test/$testnum.txt"  # TODO: fail if diff is non-empty
 }
 
 get_access_token() {
-  BEARER=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"email":"'"$WIRE_ADMIN"'","password":"'"$WIRE_PASSWD"'"}' "$WIRE_BACKEND"/login'?persist=false' | jq -r .access_token)
+    BEARER=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
+                  -d '{"email":"'"$WIRE_ADMIN"'","password":"'"$WIRE_PASSWD"'"}' \
+                  "$WIRE_BACKEND"/login'?persist=false' \
+                 | jq -r .access_token)
 }
 
 get_access_token
 check_login
 
-check_url "GET" "$WIRE_BACKEND"/api/swagger.json 200
-check_url "GET" "$WIRE_BACKEND"'/v2/proxy/youtube/v3/search' 200
-check_url "GET" "$WIRE_BACKEND"'/v2/proxy/googlemaps/api/staticmap?center=Berlin&zoom=14&size=400x400' 200
-check_url "GET" "$WIRE_BACKEND"'/v2/proxy/googlemaps/maps/api/geocode/json?place_id=ChIJeRpOeF67j4AR9ydy_PIzPuM' 200
-check_url "GET" "$WIRE_BACKEND"'/v2/proxy/giphy/v1/gifs/search?limit=100&offset=0&q=kitty' 200
-check_url "POST" "$WIRE_BACKEND"'/v2/proxy/spotify/api/token' 415
-check_url "GET" "$WIRE_BACKEND"'/v2/proxy/soundcloud/resolve' 400
-check_url "GET" "$WIRE_BACKEND"'/v2/proxy/soundcloud/stream' 400
+check_url "1" "GET" "$WIRE_BACKEND"/api/swagger.json 200
+check_url "2" "GET" "$WIRE_BACKEND"'/v2/proxy/youtube/v3/search' 200
+check_url "3" "GET" "$WIRE_BACKEND"'/v2/proxy/googlemaps/api/staticmap?center=Berlin&zoom=14&size=400x400' 200
+check_url "4" "GET" "$WIRE_BACKEND"'/v2/proxy/googlemaps/maps/api/geocode/json?place_id=ChIJeRpOeF67j4AR9ydy_PIzPuM' 200
+check_url "5" "GET" "$WIRE_BACKEND"'/v2/proxy/giphy/v1/gifs/search?limit=100&offset=0&q=kitty' 200
+check_url "6" "POST" "$WIRE_BACKEND"'/v2/proxy/spotify/api/token' 415
+check_url "7" "GET" "$WIRE_BACKEND"'/v2/proxy/soundcloud/resolve' 400
+check_url "8" "GET" "$WIRE_BACKEND"'/v2/proxy/soundcloud/stream' 400
 
 # manually:
 # curl -XGET http://localhost:8080/i/status  # from proxy pod
