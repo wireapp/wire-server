fe3af7b4285d2159c8a9c37bd0340a193b9a199f
Matthias Fischmann <mf@zerobuzz.net>
2022-11-18 17:21:52 +0100

    test script.

--- services/proxy/test/scripts/proxy-test.sh
+++ services/proxy/test/scripts/proxy-test.sh
@@ -2,6 +2,7 @@
 
 echo "WIRE_BACKEND: $WIRE_BACKEND"
 echo "WIRE_ADMIN: $WIRE_ADMIN"
+echo "WIRE_PASSWD: <not shown>"
 
 fail() {
   printf "\e[31;1m%s\e[0m\n" "$*" >&2
@@ -21,7 +22,7 @@ check_login() {
 }
 
 check_url() {
-  status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null -I -X "$1" --header "Authorization: Bearer $BEARER" "$2")
+  status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null -I -X "$1" --header "Authorization: Bearer $BEARER" --header "Content-Type: application/json" "$2")
   if [[ "$status_code" == "$3" ]]; then
     echo "proxy $2: OK"
   else
@@ -36,10 +37,16 @@ get_access_token() {
 
 get_access_token
 check_login
+
+check_url "GET" "$WIRE_BACKEND"/api/swagger.json 200
 check_url "GET" "$WIRE_BACKEND"'/v2/proxy/youtube/v3/search' 200
 check_url "GET" "$WIRE_BACKEND"'/v2/proxy/googlemaps/api/staticmap?center=Berlin&zoom=14&size=400x400' 200
 check_url "GET" "$WIRE_BACKEND"'/v2/proxy/googlemaps/maps/api/geocode/json?place_id=ChIJeRpOeF67j4AR9ydy_PIzPuM' 200
-check_url "GET" "$WIRE_BACKEND"'/v2/proxy/giphy/v1/gifs/search?limit=100&offset=0&q=kitty' 403
-check_url "POST" "$WIRE_BACKEND"'/v2/proxy/spotify/api/token' 400
+check_url "GET" "$WIRE_BACKEND"'/v2/proxy/giphy/v1/gifs/search?limit=100&offset=0&q=kitty' 200
+check_url "POST" "$WIRE_BACKEND"'/v2/proxy/spotify/api/token' 415
 check_url "GET" "$WIRE_BACKEND"'/v2/proxy/soundcloud/resolve' 400
 check_url "GET" "$WIRE_BACKEND"'/v2/proxy/soundcloud/stream' 400
+
+# manually:
+# curl -XGET http://localhost:8080/i/status  # from proxy pod
+# curl -XHEAD http://localhost:8080/i/status  # from proxy pod
