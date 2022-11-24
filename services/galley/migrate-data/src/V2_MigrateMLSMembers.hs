d08423e0110d8abce987407df56e7c737acfe1ca
Stefan Matting <stefan@wire.com>
2022-11-24 11:40:44 +0100

    ormolu

--- services/galley/migrate-data/src/V2_MigrateMLSMembers.hs
+++ services/galley/migrate-data/src/V2_MigrateMLSMembers.hs
@@ -15,7 +15,6 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-
 module V2_MigrateMLSMembers where
 
 import Cassandra
