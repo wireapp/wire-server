c812baf632aad3233207e8b54e543f130857389b
Leif Battermann <leif.battermann@wire.com>
2022-12-02 15:19:56 +0000

    endpoint for creating an authorization code

--- services/brig/src/Brig/API/OAuth.hs
+++ services/brig/src/Brig/API/OAuth.hs
@@ -17,21 +17,29 @@
 
 module Brig.API.OAuth where
 
+import Brig.API.Error (throwStd)
 import Brig.API.Handler (Handler)
 import Brig.App (Env, wrapClient)
 import Brig.Password (Password, mkSafePassword)
-import Cassandra
+import Cassandra hiding (Set)
+import qualified Cassandra as C
+import Control.Lens ((.~))
 import Control.Monad.Except
 import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as A
 import Data.ByteString.Conversion
 import Data.ByteString.Lazy (toStrict)
 import Data.Id (OAuthClientId, UserId, randomId)
 import Data.Misc (PlainTextPassword (PlainTextPassword))
 import Data.Range
 import Data.Schema
+import qualified Data.Set as Set
+import Data.String.Conversions (cs)
 import qualified Data.Swagger as S
+import qualified Data.Text as T
 import Data.Text.Ascii
 import qualified Data.Text.Encoding as TE
+import Data.Text.Encoding.Error as TErr
 import Imports
 import OpenSSL.Random (randBytes)
 import Servant hiding (Handler, Tagged)
@@ -58,6 +66,14 @@ instance ToSchema RedirectUrl where
   schema =
     (TE.decodeUtf8 . serializeURIRef' . unRedirectUrl)
       .= (RedirectUrl <$> parsedText "RedirectUrl" (runParser (uriParser strictURIParserOptions) . TE.encodeUtf8))
+
+instance ToHttpApiData RedirectUrl where
+  toUrlPiece = TE.decodeUtf8With TErr.lenientDecode . toHeader
+  toHeader = serializeURIRef' . unRedirectUrl
+
+instance FromHttpApiData RedirectUrl where
+  parseUrlPiece = parseHeader . TE.encodeUtf8
+  parseHeader = bimap (T.pack . show) RedirectUrl . parseURI strictURIParserOptions
 
 newtype OAuthApplicationName = OAuthApplicationName {unOAuthApplicationName :: Range 1 256 Text}
   deriving stock (Eq, Show, Generic)
@@ -120,6 +136,73 @@ instance ToSchema OAuthClient where
         <*> ocName .= field "applicationName" schema
         <*> ocRedirectUrl .= field "redirectUrl" schema
 
+data OAuthResponseType = OAuthResponseTypeCode
+  deriving stock (Eq, Show, Generic)
+  deriving (A.ToJSON, A.FromJSON, S.ToSchema) via (Schema OAuthResponseType)
+
+instance ToSchema OAuthResponseType where
+  schema :: ValueSchema NamedSwaggerDoc OAuthResponseType
+  schema =
+    enum @Text "OAuthResponseType" $
+      mconcat
+        [ element "code" OAuthResponseTypeCode
+        ]
+
+data OAuthScope
+  = ConversationCreate
+  | ConversationCodeCreate
+  deriving stock (Eq, Show, Generic, Ord)
+
+instance ToByteString OAuthScope where
+  builder = \case
+    ConversationCreate -> "conversation:create"
+    ConversationCodeCreate -> "conversation-code:create"
+
+instance FromByteString OAuthScope where
+  parser = do
+    s <- parser @Text
+    case s & T.toLower of
+      "conversation:create" -> pure ConversationCreate
+      "conversation-code:create" -> pure ConversationCodeCreate
+      _ -> fail "invalid scope"
+
+oauthScopesToText :: Set OAuthScope -> Text
+oauthScopesToText = T.intercalate " " . fmap (cs . toByteString') . Set.toList
+
+oauthScopeParser :: Text -> A.Parser (Set OAuthScope)
+oauthScopeParser "" = pure Set.empty
+oauthScopeParser scope =
+  pure $ (not . T.null) `filter` T.splitOn " " scope & maybe Set.empty Set.fromList . mapM (fromByteString' . cs)
+
+data NewOAuthAuthCode = NewOAuthAuthCode
+  { noacClientId :: OAuthClientId,
+    noacScope :: Set OAuthScope,
+    noacResponseType :: OAuthResponseType,
+    noacRedirectUri :: RedirectUrl,
+    noacState :: Text
+  }
+  deriving stock (Eq, Show, Generic)
+  deriving (A.ToJSON, A.FromJSON, S.ToSchema) via (Schema NewOAuthAuthCode)
+
+instance ToSchema NewOAuthAuthCode where
+  schema =
+    object "NewOAuthAuthCode" $
+      NewOAuthAuthCode
+        <$> noacClientId .= field "clientId" schema
+        <*> (oauthScopesToText . noacScope) .= field "scope" (withParser schema oauthScopeParser)
+        <*> noacResponseType .= field "responseType" schema
+        <*> noacRedirectUri .= field "redirectUri" schema
+        <*> noacState .= field "state" schema
+
+newtype OAuthAuthCode = OAuthAuthCode {unOAuthAuthCode :: AsciiBase64Url}
+  deriving stock (Show, Eq, Generic)
+
+instance ToByteString OAuthAuthCode where
+  builder = builder . unOAuthAuthCode
+
+instance FromByteString OAuthAuthCode where
+  parser = OAuthAuthCode <$> parser
+
 --------------------------------------------------------------------------------
 -- API Internal
 
@@ -141,9 +224,16 @@ internalOauthAPI =
 --------------------------------------------------------------------------------
 -- API Public
 
-data OAuthError = OAuthClientNotFound
+data OAuthError
+  = OAuthClientNotFound
+  | RedirectUrlMissMatch
+  | UnsupportedResponseType
 
 type instance MapError 'OAuthClientNotFound = 'StaticError 404 "not-found" "OAuth client not found"
+
+type instance MapError 'RedirectUrlMissMatch = 'StaticError 400 "redirect-url-miss-match" "Redirect URL miss match"
+
+type instance MapError 'UnsupportedResponseType = 'StaticError 400 "unsupported-response-type" "Unsupported response type"
 
 type OAuthAPI =
   Named
@@ -161,10 +251,25 @@ type OAuthAPI =
               ]
              (Maybe OAuthClient)
     )
+    :<|> Named
+           "create-oauth-auth-code"
+           ( Summary ""
+               :> ZUser
+               :> "oauth"
+               :> "authorization"
+               :> "codes"
+               :> ReqBody '[JSON] NewOAuthAuthCode
+               :> MultiVerb
+                    'POST
+                    '[JSON]
+                    '[WithHeaders '[Header "Location" RedirectUrl] RedirectUrl (RespondEmpty 302 "Found")]
+                    RedirectUrl
+           )
 
 oauthAPI :: ServerT OAuthAPI (Handler r)
 oauthAPI =
   Named @"get-oauth-client" getOAuthClient
+    :<|> Named @"create-oauth-auth-code" createNewOAuthAuthCode
 
 --------------------------------------------------------------------------------
 -- Handlers
@@ -177,13 +282,27 @@ createNewOAuthClient (NewOAuthClient name uri) = do
   pure credentials
   where
     createSecret :: MonadIO m => m OAuthClientPlainTextSecret
-    createSecret = liftIO . fmap (OAuthClientPlainTextSecret . encodeBase16) $ randBytes 32
+    createSecret = OAuthClientPlainTextSecret <$> (liftIO . fmap encodeBase16 $ randBytes 32)
 
     hashClientSecret :: MonadIO m => OAuthClientPlainTextSecret -> m Password
     hashClientSecret = mkSafePassword . PlainTextPassword . toText . unOAuthClientPlainTextSecret
 
 getOAuthClient :: UserId -> OAuthClientId -> (Handler r) (Maybe OAuthClient)
 getOAuthClient _ cid = lift $ wrapClient $ lookupOauthClient cid
+
+createNewOAuthAuthCode :: UserId -> NewOAuthAuthCode -> (Handler r) RedirectUrl
+createNewOAuthAuthCode uid (NewOAuthAuthCode cid scope responseType redirectUrl state) = do
+  unless (responseType == OAuthResponseTypeCode) $ throwStd (errorToWai @'UnsupportedResponseType)
+  OAuthClient _ _ uri <- getOAuthClient uid cid >>= maybe (throwStd $ errorToWai @'OAuthClientNotFound) pure
+  unless (uri == redirectUrl) $ throwStd $ errorToWai @'RedirectUrlMissMatch
+  oauthCode <- OAuthAuthCode <$> rand32Bytes
+  lift $ wrapClient $ insertOAuthAuthCode oauthCode cid uid scope redirectUrl
+  let queryParams = [("code", toByteString' oauthCode), ("state", cs state)]
+      returnedRedirectUrl = redirectUrl & unRedirectUrl & (queryL . queryPairsL) .~ queryParams & RedirectUrl
+  pure returnedRedirectUrl
+
+rand32Bytes :: MonadIO m => m AsciiBase64Url
+rand32Bytes = liftIO . fmap encodeBase64Url $ randBytes 32
 
 --------------------------------------------------------------------------------
 -- DB
@@ -202,6 +321,14 @@ lookupOauthClient cid = do
     q :: PrepQuery R (Identity OAuthClientId) (OAuthApplicationName, RedirectUrl)
     q = "SELECT name, redirect_uri FROM oauth_client WHERE id = ?"
 
+insertOAuthAuthCode :: (MonadClient m, MonadReader Env m) => OAuthAuthCode -> OAuthClientId -> UserId -> Set OAuthScope -> RedirectUrl -> m ()
+insertOAuthAuthCode code cid uid scope uri = do
+  let cqlScope = C.Set (Set.toList scope)
+  retry x5 . write q $ params LocalQuorum (code, cid, uid, cqlScope, uri)
+  where
+    q :: PrepQuery W (OAuthAuthCode, OAuthClientId, UserId, C.Set OAuthScope, RedirectUrl) ()
+    q = "INSERT INTO oauth_auth_code (code, client, user, scope, redirect_uri) VALUES (?, ?, ?, ?, ?) USING TTL 300"
+
 --------------------------------------------------------------------------------
 -- CQL instances
 
@@ -216,3 +343,15 @@ instance Cql RedirectUrl where
   toCql = CqlBlob . toByteString
   fromCql (CqlBlob t) = runParser parser (toStrict t)
   fromCql _ = Left "RedirectUrl: Blob expected"
+
+instance Cql OAuthAuthCode where
+  ctype = Tagged AsciiColumn
+  toCql = CqlAscii . toText . unOAuthAuthCode
+  fromCql (CqlAscii t) = OAuthAuthCode <$> validateBase64Url t
+  fromCql _ = Left "OAuthAuthCode: Ascii expected"
+
+instance Cql OAuthScope where
+  ctype = Tagged TextColumn
+  toCql = CqlText . cs . toByteString'
+  fromCql (CqlText t) = maybe (Left "invalid oauth scope") Right $ fromByteString' (cs t)
+  fromCql _ = Left "OAuthScope: Text expected"
