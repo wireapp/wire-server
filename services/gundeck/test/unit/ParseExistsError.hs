81329fcb3e5b9a5900bc51c03412d301efe8a341
Sven Tennie <sven.tennie@wire.com>
2022-12-12 16:18:30 +0100

    Test non-parsable error strings

--- services/gundeck/test/unit/ParseExistsError.hs
+++ services/gundeck/test/unit/ParseExistsError.hs
@@ -12,7 +12,8 @@ tests :: TestTree
 tests =
   testGroup
     "parseExistsError"
-    [ testCase "parse error string from real world example" parseErrorTest
+    [ testCase "parse error string from real world example" parseErrorTest,
+      testCase "parse errors" parseErrors
     ]
 
 parseErrorTest :: Assertion
@@ -21,3 +22,18 @@ parseErrorTest =
       expectedTopic = mkEndpointTopic (ArnEnv "staging") APNS "com.wire.dev.ent" (EndpointId "f90c8f08-a0a1-33bc-aa43-23c94770d936")
       expected = mkSnsArn Ireland (Account "093205192929") expectedTopic
    in parseExistsError e @?= Just expected
+
+parseErrors :: Assertion
+parseErrors = do
+  parseExistsError Nothing @?= Nothing
+  parseExistsError ((Just . ErrorMessage) "Invalid parameter: Token Reason: Endpoint") @?= Nothing
+  parseExistsError
+    ( (Just . ErrorMessage)
+        "Invalid parameter: Token Reason: Endpoint NO_ARN already exists with the same Token, but different attributes."
+    )
+    @?= Nothing
+  parseExistsError
+    ( (Just . ErrorMessage)
+        "Invalid parameter: Token Reason: Endpoint arn:aws:sns:eu-west-1:093205192929:endpoint/APNS/staging-com.wire.dev.ent/f90c8f08-a0a1-33bc-aa43-23c94770d936"
+    )
+    @?= Nothing
