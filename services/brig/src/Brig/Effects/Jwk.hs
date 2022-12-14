2bf93999c62cd0541ecd0e9f88c4f008a91e69ce
Leif Battermann <leif.battermann@wire.com>
2022-12-14 11:38:29 +0000

    formatting

--- services/brig/src/Brig/Effects/Jwk.hs
+++ services/brig/src/Brig/Effects/Jwk.hs
@@ -7,9 +7,9 @@ import Crypto.JOSE.JWK
 import Data.Aeson
 import qualified Data.ByteString as BS
 import Data.String.Conversions (cs)
+import Debug.Trace (traceM)
 import Imports
 import Polysemy
-import Debug.Trace (traceM)
 
 data Jwk m a where
   Get :: FilePath -> Jwk m (Maybe JWK)
@@ -20,7 +20,8 @@ interpretJwk :: Members '[Embed IO] r => Sem (Jwk ': r) a -> Sem r a
 interpretJwk = interpret $ \(Get fp) -> liftIO $ readJwk fp
 
 readJwk :: FilePath -> IO (Maybe JWK)
-readJwk fp = do -- try @IOException (BS.readFile fp) <&> either (const Nothing) (decode . cs)
+readJwk fp = do
+  -- try @IOException (BS.readFile fp) <&> either (const Nothing) (decode . cs)
   bsOrError <- try @IOException $ BS.readFile fp
   case bsOrError of
     Left err -> traceM ("Failed to read file because: " <> show err) $> Nothing
@@ -28,4 +29,4 @@ readJwk fp = do -- try @IOException (BS.readFile fp) <&> either (const Nothing) 
       traceM $ "File contents:\n" <> show bs
       case eitherDecode (cs bs) of
         Left err -> traceM ("Failed to decode file because: " <> show err) $> Nothing
-        Right jwk -> pure $ Just jwk 
+        Right jwk -> pure $ Just jwk
