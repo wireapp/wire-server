4770af7d978e8b0b1519c546ed3ecc300c43652d
Sandy Maguire <sandy@sandymaguire.me>
2022-12-20 15:19:02 -0800

    doc: haddock

--- libs/wire-api/src/Wire/API/MakesFederatedCall.hs
+++ libs/wire-api/src/Wire/API/MakesFederatedCall.hs
@@ -44,18 +44,35 @@ data Component
   deriving (Show, Eq, Generic)
   deriving (Arbitrary) via (GenericUniform Component)
 
+-- | A typeclass corresponding to calls to federated services. This class has
+-- no methods, and exists only to automatically propagate information up to
+-- servant.
+--
+-- The only way to discharge this constraint is via 'callsFed', which should be
+-- invoked for each federated call when connecting handlers to the server
+-- definition.
 class CallsFed (comp :: Component) (name :: Symbol)
 
+-- | A typeclass with the same layout as 'CallsFed', which exists only so we
+-- can discharge 'CallsFeds' constraints by unsafely coercing this one.
 class Nullary
-
 instance Nullary
 
+-- | Construct a dictionary for 'CallsFed'.
 synthesizeCallsFed :: forall (comp :: Component) (name :: Symbol). Dict (CallsFed comp name)
 synthesizeCallsFed = unsafeCoerce $ Dict @Nullary
 
+-- | Servant combinator for tracking calls to federated calls. Annotating API
+-- endpoints with 'MakesFederatedCall' is the only way to eliminate 'CallsFed'
+-- constraints on handlers.
 data MakesFederatedCall (comp :: Component) (name :: Symbol)
 
 instance (HasServer api ctx) => HasServer (MakesFederatedCall comp name :> api :: *) ctx where
+  -- | This should have type @CallsFed comp name => ServerT api m@, but GHC
+  -- complains loudly thinking this is a polytype. We need to introduce the
+  -- 'CallsFed' constraint so that we can eliminate it via
+  -- 'synthesizeCallsFed', which otherwise is too-high rank for GHC to notice
+  -- we've solved our constraint.
   type ServerT (MakesFederatedCall comp name :> api) m = Dict (CallsFed comp name) -> ServerT api m
   route _ ctx f = route (Proxy @api) ctx $ fmap ($ synthesizeCallsFed @comp @name) f
   hoistServerWithContext _ ctx f s = hoistServerWithContext (Proxy @api) ctx f . s
@@ -64,11 +81,14 @@ instance HasLink api => HasLink (MakesFederatedCall comp name :> api :: *) where
   type MkLink (MakesFederatedCall comp name :> api) x = MkLink api x
   toLink f _ l = toLink f (Proxy @api) l
 
+-- | Get a symbol representation of our component.
 type family ShowComponent (x :: Component) :: Symbol where
   ShowComponent 'Brig = "brig"
   ShowComponent 'Galley = "galley"
   ShowComponent 'Cargohold = "cargohold"
 
+-- | 'MakesFederatedCall' annotates the swagger documentation with an extension
+-- tag @x-wire-makes-federated-calls-to@.
 instance (HasSwagger api, KnownSymbol name, KnownSymbol (ShowComponent comp)) => HasSwagger (MakesFederatedCall comp name :> api :: *) where
   toSwagger _ =
     toSwagger (Proxy @api)
@@ -90,17 +110,8 @@ instance HasClient m api => HasClient m (MakesFederatedCall comp name :> api :: 
   clientWithRoute p _ = clientWithRoute p $ Proxy @api
   hoistClientMonad p _ f c = hoistClientMonad p (Proxy @api) f c
 
--- type Test = MakesFederatedCall 'Brig "hello" :> MakesFederatedCall 'Brig "soup" :> Get '[JSON] ()
---       :<|> Get '[JSON] ()
-
+-- | Safely discharge a 'CallsFed' constraint. Intended to be used when
+-- connecting your handler to the server router.
 callsFed :: (c => r) -> Dict c -> r
 callsFed f Dict = f
 
--- test :: Server Test
--- test = callsFed (callsFed yo) :<|> pure ()
-
--- yo :: CallsFed 'Brig "hello" => Handler ()
--- yo = pure ()
-
--- runx :: Application
--- runx = serve (Proxy @Test) (test)
