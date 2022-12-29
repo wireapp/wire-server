0b9988991774ef2c56eaa5ec94d6a65194a6bf3b
Akshay Mankar <akshay@wire.com>
2022-12-29 05:53:55 +0100

    Allow using vhost style addressing for S3 (#2955)
    Path style is not supported for newer buckets, more info:
    https://aws.amazon.com/blogs/aws/amazon-s3-path-deprecation-plan-the-rest-of-the-story/
    
    All object storage providers (like MinIO, ScalityRing, etc) might not work with
    vhost style addressing, so this change introduces a new configuration option in
    cargohold as aws.s3AddressingStyle to choose the addressing style.
    
    Other changes:
    
    * Move wire-server from using forked version of amazonka to upstream HEAD.
    
    The option to choose S3 Addressing Style has been implemented in
    https://github.com/brendanhay/amazonka/pull/832
    
    * Makefile: Skip schema migrations for packages without DB
    
    This allows running something like `make ci package=cargohold` even if cargohold
    doesn't produce a cargohold-schema executable.

--- Makefile
+++ Makefile
@@ -266,8 +266,11 @@ ifeq ($(package), all)
 	./dist/galley-schema --keyspace galley_test --replication-factor 1
 	./dist/gundeck-schema --keyspace gundeck_test --replication-factor 1
 	./dist/spar-schema --keyspace spar_test --replication-factor 1
-else
+# How this check works: https://stackoverflow.com/a/9802777
+else ifeq ($(package), $(filter $(package),brig galley gundeck spar))
 	$(EXE_SCHEMA) --keyspace $(package)_test --replication-factor 1
+else
+	@echo No schema migrations for $(package)
 endif
 
 
--- services/gundeck/src/Gundeck/Aws.hs
+++ services/gundeck/src/Gundeck/Aws.hs
@@ -54,8 +54,9 @@ module Gundeck.Aws
   )
 where
 
-import Amazonka (AWSRequest, AWSResponse, serviceAbbrev, serviceCode, serviceMessage, serviceStatus)
+import Amazonka (AWSRequest, AWSResponse, serviceError_abbrev, serviceError_code, serviceError_message, serviceError_status)
 import qualified Amazonka as AWS
+import qualified Amazonka.Data.Text as AWS
 import qualified Amazonka.SNS as SNS
 import qualified Amazonka.SNS.Lens as SNS
 import qualified Amazonka.SQS as SQS
@@ -160,14 +161,14 @@ mkEnv lgr opts mgr = do
     mkAwsEnv g sqs sns = do
       baseEnv <-
         AWS.newEnv AWS.discover
-          <&> AWS.configure sqs
-          <&> AWS.configure (sns & set AWS.serviceTimeout (Just (AWS.Seconds 5)))
+          <&> AWS.configureService sqs
+          <&> AWS.configureService (sns & set AWS.service_timeout (Just (AWS.Seconds 5)))
       pure $
         baseEnv
-          { AWS.envLogger = awsLogger g,
-            AWS.envRegion = opts ^. optAws . awsRegion,
-            AWS.envRetryCheck = retryCheck,
-            AWS.envManager = mgr
+          { AWS.logger = awsLogger g,
+            AWS.region = opts ^. optAws . awsRegion,
+            AWS.retryCheck = retryCheck,
+            AWS.manager = mgr
           }
 
     awsLogger g l = Logger.log g (mapLevel l) . Logger.msg . toLazyByteString
@@ -240,8 +241,8 @@ updateEndpoint us tk arn = do
     Right _ -> pure ()
     Left x@(AWS.ServiceError e)
       | is "SNS" 400 x
-          && AWS.newErrorCode "InvalidParameter" == e ^. serviceCode
-          && isMetadataLengthError (e ^. serviceMessage) ->
+          && AWS.newErrorCode "InvalidParameter" == e ^. serviceError_code
+          && isMetadataLengthError (e ^. serviceError_message) ->
           throwM $ InvalidCustomData arn
     Left x ->
       throwM $
@@ -303,16 +304,16 @@ createEndpoint u tr arnEnv app token = do
         Nothing -> throwM NoEndpointArn
         Just s -> Right <$> readArn s
     Left x@(AWS.ServiceError e)
-      | is "SNS" 400 x && AWS.newErrorCode "InvalidParameter" == e ^. serviceCode,
-        Just ep <- parseExistsError (e ^. serviceMessage) ->
+      | is "SNS" 400 x && AWS.newErrorCode "InvalidParameter" == e ^. serviceError_code,
+        Just ep <- parseExistsError (e ^. serviceError_message) ->
           pure (Left (EndpointInUse ep))
       | is "SNS" 400 x
-          && AWS.newErrorCode "InvalidParameter" == e ^. serviceCode
-          && isLengthError (e ^. serviceMessage) ->
+          && AWS.newErrorCode "InvalidParameter" == e ^. serviceError_code
+          && isLengthError (e ^. serviceError_message) ->
           pure (Left (TokenTooLong $ tokenLength token))
       | is "SNS" 400 x
-          && AWS.newErrorCode "InvalidParameter" == e ^. serviceCode
-          && isTokenError (e ^. serviceMessage) -> do
+          && AWS.newErrorCode "InvalidParameter" == e ^. serviceError_code
+          && isTokenError (e ^. serviceError_message) -> do
           debug $
             msg @Text "InvalidParameter: InvalidToken"
               . field "response" (show x)
@@ -409,19 +410,19 @@ publish arn txt attrs = do
   case res of
     Right _ -> pure (Right ())
     Left x@(AWS.ServiceError e)
-      | is "SNS" 400 x && AWS.newErrorCode "EndpointDisabled" == e ^. serviceCode ->
+      | is "SNS" 400 x && AWS.newErrorCode "EndpointDisabled" == e ^. serviceError_code ->
           pure (Left (EndpointDisabled arn))
       | is "SNS" 400 x
-          && AWS.newErrorCode "InvalidParameter" == e ^. serviceCode
-          && isProtocolSizeError (e ^. serviceMessage) ->
+          && AWS.newErrorCode "InvalidParameter" == e ^. serviceError_code
+          && isProtocolSizeError (e ^. serviceError_message) ->
           pure (Left (PayloadTooLarge arn))
       | is "SNS" 400 x
-          && AWS.newErrorCode "InvalidParameter" == e ^. serviceCode
-          && isSnsSizeError (e ^. serviceMessage) ->
+          && AWS.newErrorCode "InvalidParameter" == e ^. serviceError_code
+          && isSnsSizeError (e ^. serviceError_message) ->
           pure (Left (PayloadTooLarge arn))
       | is "SNS" 400 x
-          && AWS.newErrorCode "InvalidParameter" == e ^. serviceCode
-          && isArnError (e ^. serviceMessage) ->
+          && AWS.newErrorCode "InvalidParameter" == e ^. serviceError_code
+          && isArnError (e ^. serviceError_message) ->
           pure (Left (InvalidEndpoint arn))
     Left x -> throwM (GeneralError x)
   where
@@ -488,7 +489,7 @@ send :: AWSRequest r => AWS.Env -> r -> Amazon (AWSResponse r)
 send env r = either (throwM . GeneralError) pure =<< sendCatch env r
 
 is :: AWS.Abbrev -> Int -> AWS.Error -> Bool
-is srv s (AWS.ServiceError e) = srv == e ^. serviceAbbrev && s == statusCode (e ^. serviceStatus)
+is srv s (AWS.ServiceError e) = srv == e ^. serviceError_abbrev && s == statusCode (e ^. serviceError_status)
 is _ _ _ = False
 
 isTimeout :: MonadIO m => Either AWS.Error a -> m Bool
