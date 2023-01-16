dd20fee6103053fa74d4c32b7d4c8275d0d6efc9
Marko Dimjašević <marko.dimjasevic@wire.com>
2023-01-16 13:58:11 +0100

    Test: more descriptive test function name

--- services/galley/test/integration/API/MLS.hs
+++ services/galley/test/integration/API/MLS.hs
@@ -215,7 +215,7 @@ tests s =
               test s "get subconversation of Proteus conv - 404" (testCreateSubConv False),
               test s "join subconversation with an external commit bundle" testJoinSubConv,
               test s "join subconversation with a client that is not in the parent conv" testJoinSubNonMemberClient,
-              test s "fail to add another client to a subconversation via internal commit" testAddClientSubConv,
+              test s "fail to add another client to a subconversation via internal commit" testAddClientSubConvFailure,
               test s "remove another client from a subconversation" testRemoveClientSubConv,
               test s "send an application message in a subconversation" testSendMessageSubConv,
               test s "reset a subconversation as a member" (testDeleteSubConv True),
@@ -2401,8 +2401,8 @@ testJoinSubNonMemberClient = do
     getGroupInfo (ciUser bob1) (fmap (flip SubConv subId) qcnv)
       !!! const 404 === statusCode
 
-testAddClientSubConv :: TestM ()
-testAddClientSubConv = do
+testAddClientSubConvFailure :: TestM ()
+testAddClientSubConvFailure = do
   [alice, bob] <- createAndConnectUsers [Nothing, Nothing]
   runMLSTest $ do
     [alice1, bob1] <- traverse createMLSClient [alice, bob]
