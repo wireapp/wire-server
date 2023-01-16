45cd5a11636d75f942782444048931aad6080dc5
Leif Battermann <leif.battermann@wire.com>
2023-01-16 10:28:17 +0000

    improve script

--- hack/bin/oauth_test.sh
+++ hack/bin/oauth_test.sh
@@ -2,10 +2,39 @@
 
 set -e
 
-#
-# It is required to set the `USER` environment variable to the user ID of an existing user.
-# Create a user e.g. with `./create_test_user.sh -n 1 -c`
-#
+USAGE="This script tests the OAuth2 flow by creating a client, requesting an authorization code, and
+then requesting an access token. It then uses the access token to make a request to /self.
+
+Create a user first with './create_test_user.sh -n 1 -c'. Then use the user ID to call this script.
+
+USAGE: $0
+    -u <user_id>: User ID
+"
+
+unset -v USER
+
+while getopts ":u:" opt; do
+  case ${opt} in
+  u)
+    USER="$OPTARG"
+    ;;
+  \?)
+    echo "$USAGE" 1>&2
+    exit 1
+    ;;
+  :)
+    echo "-$OPTARG" requires an argument 1>&2
+    exit 1
+    ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [ -z "$USER" ]; then
+  echo 'missing option -u <user_id>' 1>&2
+  echo "$USAGE" 1>&2
+  exit 1
+fi
 
 SCOPE="self:read"
 
@@ -23,11 +52,11 @@ CLIENT_SECRET=$(echo "$CLIENT" | jq -r '.clientSecret')
 
 AUTH_CODE=$(
   curl -i -s -X POST localhost:8082/oauth/authorization/codes \
-    -H 'Z-User: '$USER \
+    -H 'Z-User: '"$USER" \
     -H "Content-Type: application/json" \
     -d '{
-      "clientId": "'$CLIENT_ID'",
-      "scope": "'$SCOPE'",
+      "clientId": "'"$CLIENT_ID"'",
+      "scope": "'"$SCOPE"'",
       "responseType": "code",
       "redirectUri": "https://example.com",
       "state": "foobar"
@@ -38,7 +67,7 @@ AUTH_CODE=$(
 ACCESS_TOKEN=$(
   curl -s -X POST localhost:8082/oauth/token \
     -H "Content-Type: application/x-www-form-urlencoded" \
-    -d 'code='$AUTH_CODE'&client_id='$CLIENT_ID'&grant_type=authorization_code&redirect_uri=https://example.com&client_secret='$CLIENT_SECRET |
+    -d 'code='"$AUTH_CODE"'&client_id='"$CLIENT_ID"'&grant_type=authorization_code&redirect_uri=https://example.com&client_secret='"$CLIENT_SECRET" |
     jq -r '.accessToken'
 )
 
@@ -50,4 +79,4 @@ echo "access token : $ACCESS_TOKEN"
 
 echo ""
 echo "making a request to /self..."
-curl -s -H 'Z-OAUTH: Bearer '$ACCESS_TOKEN -H "Content-Type: application/json" localhost:8082/self | jq .
+curl -s -H 'Z-OAUTH: Bearer '"$ACCESS_TOKEN" -H "Content-Type: application/json" localhost:8082/self | jq .
