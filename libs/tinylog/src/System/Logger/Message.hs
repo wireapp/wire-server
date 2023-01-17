8c780494970866c0fad17e3cd332d8b6b8441915
Stefan Matting <stefan@wire.com>
2023-01-17 18:35:34 +0100

    tinylog: fix compilation

--- libs/tinylog/src/System/Logger/Message.hs
+++ libs/tinylog/src/System/Logger/Message.hs
@@ -1,11 +1,10 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-{-# LANGUAGE BangPatterns      #-}
-{-# LANGUAGE CPP               #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- | 'Msg' and 'ToBytes' assist in constructing log messages.
 -- For example:
@@ -17,23 +16,24 @@
 -- >
 -- @
 module System.Logger.Message
-    ( ToBytes (..)
-    , Msg
-    , Builder
-    , Element (..)
-    , msg
-    , field
-    , (.=)
-    , (+++)
-    , (~~)
-    , val
-    , eval
-    , builderSize
-    , builderBytes
-    , render
-    , renderDefault
-    , renderNetstr
-    ) where
+  ( ToBytes (..),
+    Msg,
+    Builder,
+    Element (..),
+    msg,
+    field,
+    (.=),
+    (+++),
+    (~~),
+    val,
+    eval,
+    builderSize,
+    builderBytes,
+    render,
+    renderDefault,
+    renderNetstr,
+  )
+where
 
 #if MIN_VERSION_base(4,9,0)
 import Data.Semigroup as Sem
@@ -44,28 +44,27 @@ import Data.Monoid
 #endif
 
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Builder as B
+import qualified Data.ByteString.Builder.Extra as B
+import qualified Data.ByteString.Lazy as L
 import Data.Double.Conversion.Text
 import Data.Int
 import Data.String
 import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
 import Data.Word
 import GHC.Float
-
-import qualified Data.ByteString                     as S
-import qualified Data.ByteString.Lazy                as L
-import qualified Data.ByteString.Lazy.Builder        as B
-import qualified Data.ByteString.Lazy.Builder.Extras as B
-import qualified Data.Text                           as T
-import qualified Data.Text.Lazy                      as TL
-import qualified Data.Text.Lazy.Encoding             as TL
 
 data Builder = Builder !Int B.Builder
 
 instance IsString Builder where
-    fromString = bytes
+  fromString = bytes
 
-appendBuilder:: Builder -> Builder -> Builder
+appendBuilder :: Builder -> Builder -> Builder
 appendBuilder (Builder x a) (Builder y b) = Builder (x + y) (a <> b)
 
 #if MIN_VERSION_base(4,9,0)
@@ -74,7 +73,7 @@ instance Sem.Semigroup Builder where
 #endif
 
 instance Monoid Builder where
-    mempty = Builder 0 mempty
+  mempty = Builder 0 mempty
 #if MIN_VERSION_base(4,11,0)
     -- mappend definitions are redundant now
 #elif MIN_VERSION_base(4,9,0)
@@ -94,46 +93,65 @@ builderBytes (Builder _ b) = b
 
 -- | Convert some value to a 'Builder'.
 class ToBytes a where
-    bytes :: a -> Builder
+  bytes :: a -> Builder
 
-instance ToBytes Builder      where bytes x = x
+instance ToBytes Builder where bytes x = x
+
 instance ToBytes L.ByteString where bytes x = Builder (fromIntegral $ L.length x) (B.lazyByteString x)
-instance ToBytes ByteString   where bytes x = Builder (S.length x) (B.byteString x)
-instance ToBytes Int          where bytes x = Builder (len10 x) (B.intDec x)
-instance ToBytes Int8         where bytes x = Builder (len10 x) (B.int8Dec x)
-instance ToBytes Int16        where bytes x = Builder (len10 x) (B.int16Dec x)
-instance ToBytes Int32        where bytes x = Builder (len10 x) (B.int32Dec x)
-instance ToBytes Int64        where bytes x = Builder (len10 x) (B.int64Dec x)
-instance ToBytes Integer      where bytes x = Builder (len10 x) (B.integerDec x)
-instance ToBytes Word         where bytes x = Builder (len10 x) (B.wordDec x)
-instance ToBytes Word8        where bytes x = Builder (len10 x) (B.word8Dec x)
-instance ToBytes Word16       where bytes x = Builder (len10 x) (B.word16Dec x)
-instance ToBytes Word32       where bytes x = Builder (len10 x) (B.word32Dec x)
-instance ToBytes Word64       where bytes x = Builder (len10 x) (B.word64Dec x)
-instance ToBytes Float        where bytes x = bytes (toShortest $ float2Double x)
-instance ToBytes Double       where bytes x = bytes (toShortest x)
-instance ToBytes Text         where bytes x = bytes (encodeUtf8 x)
-instance ToBytes TL.Text      where bytes x = bytes (TL.encodeUtf8 x)
-instance ToBytes Char         where bytes x = bytes (T.singleton x)
-instance ToBytes [Char]       where bytes x = bytes (TL.pack x)
+
+instance ToBytes ByteString where bytes x = Builder (S.length x) (B.byteString x)
+
+instance ToBytes Int where bytes x = Builder (len10 x) (B.intDec x)
+
+instance ToBytes Int8 where bytes x = Builder (len10 x) (B.int8Dec x)
+
+instance ToBytes Int16 where bytes x = Builder (len10 x) (B.int16Dec x)
+
+instance ToBytes Int32 where bytes x = Builder (len10 x) (B.int32Dec x)
+
+instance ToBytes Int64 where bytes x = Builder (len10 x) (B.int64Dec x)
+
+instance ToBytes Integer where bytes x = Builder (len10 x) (B.integerDec x)
+
+instance ToBytes Word where bytes x = Builder (len10 x) (B.wordDec x)
+
+instance ToBytes Word8 where bytes x = Builder (len10 x) (B.word8Dec x)
+
+instance ToBytes Word16 where bytes x = Builder (len10 x) (B.word16Dec x)
+
+instance ToBytes Word32 where bytes x = Builder (len10 x) (B.word32Dec x)
+
+instance ToBytes Word64 where bytes x = Builder (len10 x) (B.word64Dec x)
+
+instance ToBytes Float where bytes x = bytes (toShortest $ float2Double x)
+
+instance ToBytes Double where bytes x = bytes (toShortest x)
+
+instance ToBytes Text where bytes x = bytes (encodeUtf8 x)
+
+instance ToBytes TL.Text where bytes x = bytes (TL.encodeUtf8 x)
+
+instance ToBytes Char where bytes x = bytes (T.singleton x)
+
+instance ToBytes [Char] where bytes x = bytes (TL.pack x)
 
 instance ToBytes Bool where
-    bytes True  = Builder 4 (B.byteString "True")
-    bytes False = Builder 5 (B.byteString "False")
+  bytes True = Builder 4 (B.byteString "True")
+  bytes False = Builder 5 (B.byteString "False")
 
 {-# INLINE len10 #-}
 len10 :: Integral a => a -> Int
 len10 !n = if n > 0 then go n 0 else 1 + go (-n) 0
   where
-    go  0 !a = a
+    go 0 !a = a
     go !x !a = go (x `div` 10) (a + 1)
 
 -- | Type representing log messages.
-newtype Msg = Msg { elements :: [Element] }
+newtype Msg = Msg {elements :: [Element]}
 
 data Element
-    = Bytes Builder
-    | Field Builder Builder
+  = Bytes Builder
+  | Field Builder Builder
 
 -- | Turn some value into a 'Msg'.
 msg :: ToBytes a => a -> Msg -> Msg
@@ -146,17 +164,20 @@ field k v (Msg m) = Msg $ Field (bytes k) (bytes v) : m
 -- | Alias of 'field'.
 (.=) :: ToBytes a => ByteString -> a -> Msg -> Msg
 (.=) = field
+
 infixr 5 .=
 
 -- | Alias of '.' with lowered precedence to allow combination with '.='
 -- without requiring parentheses.
 (~~) :: (b -> c) -> (a -> b) -> a -> c
 (~~) = (.)
+
 infixr 4 ~~
 
 -- | Concatenate two 'ToBytes' values.
 (+++) :: (ToBytes a, ToBytes b) => a -> b -> Builder
 a +++ b = bytes a <> bytes b
+
 infixr 6 +++
 
 -- | Type restriction. Useful to disambiguate string literals when
@@ -174,14 +195,14 @@ render f m = finish . f . elements . m $ empty
 renderDefault :: ByteString -> [Element] -> B.Builder
 renderDefault s = encAll mempty
   where
-    encAll !acc    []  = acc
-    encAll !acc (b:[]) = acc <> encOne b
-    encAll !acc (b:bb) = encAll (acc <> encOne b <> sep) bb
+    encAll !acc [] = acc
+    encAll !acc (b : []) = acc <> encOne b
+    encAll !acc (b : bb) = encAll (acc <> encOne b <> sep) bb
 
-    encOne (Bytes (Builder _ b))               = b
+    encOne (Bytes (Builder _ b)) = b
     encOne (Field (Builder _ k) (Builder _ v)) = k <> eq <> v
 
-    eq  = B.char8 '='
+    eq = B.char8 '='
     sep = B.byteString s
 
 -- | 'Renderer' that uses <http://cr.yp.to/proto/netstrings.txt netstring>
@@ -189,10 +210,10 @@ renderDefault s = encAll mempty
 renderNetstr :: [Element] -> B.Builder
 renderNetstr = encAll mempty
   where
-    encAll !acc []     = acc
-    encAll !acc (b:bb) = encAll (acc <> encOne b) bb
+    encAll !acc [] = acc
+    encAll !acc (b : bb) = encAll (acc <> encOne b) bb
 
-    encOne (Bytes   e) = netstr e
+    encOne (Bytes e) = netstr e
     encOne (Field k v) = netstr k <> eq <> netstr v
 
     eq = B.byteString "1:=,"
--- libs/tinylog/src/System/Logger/Settings.hs
+++ libs/tinylog/src/System/Logger/Settings.hs
@@ -1,100 +1,106 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 {-# LANGUAGE OverloadedStrings #-}
 
 module System.Logger.Settings
-    ( Settings
-    , Level      (..)
-    , Output     (..)
-    , DateFormat (..)
-    , Renderer
+  ( Settings,
+    Level (..),
+    Output (..),
+    DateFormat (..),
+    Renderer,
+    defSettings,
+    output,
+    setOutput,
+    format,
+    setFormat,
+    bufSize,
+    setBufSize,
+    delimiter,
+    setDelimiter,
+    setNetStrings,
+    setRendererNetstr,
+    setRendererDefault,
+    logLevel,
+    logLevelMap,
+    logLevelOf,
+    setLogLevel,
+    setLogLevelMap,
+    setLogLevelOf,
+    name,
+    setName,
+    nameMsg,
+    renderer,
+    setRenderer,
+    readEnvironment,
+    setReadEnvironment,
+    iso8601UTC,
+  )
+where
 
-    , defSettings
-    , output
-    , setOutput
-    , format
-    , setFormat
-    , bufSize
-    , setBufSize
-    , delimiter
-    , setDelimiter
-    , setNetStrings
-    , setRendererNetstr
-    , setRendererDefault
-    , logLevel
-    , logLevelMap
-    , logLevelOf
-    , setLogLevel
-    , setLogLevelMap
-    , setLogLevelOf
-    , name
-    , setName
-    , nameMsg
-    , renderer
-    , setRenderer
-    , readEnvironment
-    , setReadEnvironment
-    , iso8601UTC
-    ) where
-
-import Data.String
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Builder as B
 import Data.ByteString.Char8 (pack)
 import Data.Map.Strict as Map
+import Data.String
 import Data.Text (Text)
 import Data.UnixTime
 import System.Log.FastLogger (defaultBufSize)
 import System.Logger.Message
 
-import qualified Data.ByteString.Lazy.Builder as B
-
 data Settings = Settings
-    { _logLevel        :: !Level              -- ^ messages below this log level will be suppressed
-    , _levelMap        :: !(Map Text Level)   -- ^ log level per named logger
-    , _output          :: !Output             -- ^ log sink
-    , _format          :: !(Maybe DateFormat) -- ^ the timestamp format (use 'Nothing' to disable timestamps)
-    , _delimiter       :: !ByteString         -- ^ text to intersperse between fields of a log line
-    , _bufSize         :: !Int                -- ^ how many bytes to buffer before commiting to sink
-    , _name            :: !(Maybe Text)       -- ^ logger name
-    , _nameMsg         :: !(Msg -> Msg)
-    , _renderer        :: !Renderer
-    , _readEnvironment :: !Bool               -- ^ should 'new' check @LOG_*@ process environment settings?
-    }
+  { -- | messages below this log level will be suppressed
+    _logLevel :: !Level,
+    -- | log level per named logger
+    _levelMap :: !(Map Text Level),
+    -- | log sink
+    _output :: !Output,
+    -- | the timestamp format (use 'Nothing' to disable timestamps)
+    _format :: !(Maybe DateFormat),
+    -- | text to intersperse between fields of a log line
+    _delimiter :: !ByteString,
+    -- | how many bytes to buffer before commiting to sink
+    _bufSize :: !Int,
+    -- | logger name
+    _name :: !(Maybe Text),
+    _nameMsg :: !(Msg -> Msg),
+    _renderer :: !Renderer,
+    -- | should 'new' check @LOG_*@ process environment settings?
+    _readEnvironment :: !Bool
+  }
 
 output :: Settings -> Output
 output = _output
 
 setOutput :: Output -> Settings -> Settings
-setOutput x s = s { _output = x }
+setOutput x s = s {_output = x}
 
 -- | The time and date format used for the timestamp part of a log line.
 format :: Settings -> Maybe DateFormat
 format = _format
 
 setFormat :: Maybe DateFormat -> Settings -> Settings
-setFormat x s = s { _format = x }
+setFormat x s = s {_format = x}
 
 bufSize :: Settings -> Int
 bufSize = _bufSize
 
 setBufSize :: Int -> Settings -> Settings
-setBufSize x s = s { _bufSize = max 1 x }
+setBufSize x s = s {_bufSize = max 1 x}
 
 -- | Delimiter string which separates log line parts.
 delimiter :: Settings -> ByteString
 delimiter = _delimiter
 
 setDelimiter :: ByteString -> Settings -> Settings
-setDelimiter x s = s { _delimiter = x }
+setDelimiter x s = s {_delimiter = x}
 
 -- | Whether to use <http://cr.yp.to/proto/netstrings.txt netstring>
 -- encoding for log lines.
 --
 -- {#- DEPRECATED setNetStrings "Use setRendererNetstr or setRendererDefault instead" #-}
 setNetStrings :: Bool -> Settings -> Settings
-setNetStrings True  = setRenderer $ \_ _ _ -> renderNetstr
+setNetStrings True = setRenderer $ \_ _ _ -> renderNetstr
 setNetStrings False = setRenderer $ \s _ _ -> renderDefault s
 
 -- | Shortcut for calling 'setRenderer' with 'renderNetstr'.
@@ -112,7 +118,7 @@ logLevel :: Settings -> Level
 logLevel = _logLevel
 
 setLogLevel :: Level -> Settings -> Settings
-setLogLevel x s = s { _logLevel = x }
+setLogLevel x s = s {_logLevel = x}
 
 -- | Log level of some named logger.
 logLevelOf :: Text -> Settings -> Maybe Level
@@ -125,17 +131,17 @@ logLevelMap = _levelMap
 -- 'clone'd and given a name, the 'logLevel' of the cloned logger will be
 -- the provided here.
 setLogLevelOf :: Text -> Level -> Settings -> Settings
-setLogLevelOf n x s = s { _levelMap = Map.insert n x (_levelMap s) }
+setLogLevelOf n x s = s {_levelMap = Map.insert n x (_levelMap s)}
 
 setLogLevelMap :: Map Text Level -> Settings -> Settings
-setLogLevelMap x s = s { _levelMap = x }
+setLogLevelMap x s = s {_levelMap = x}
 
 name :: Settings -> Maybe Text
 name = _name
 
 setName :: Maybe Text -> Settings -> Settings
-setName Nothing   s = s { _name = Nothing, _nameMsg = id }
-setName (Just xs) s = s { _name = Just xs, _nameMsg = "logger" .= xs }
+setName Nothing s = s {_name = Nothing, _nameMsg = id}
+setName (Just xs) s = s {_name = Just xs, _nameMsg = "logger" .= xs}
 
 nameMsg :: Settings -> (Msg -> Msg)
 nameMsg = _nameMsg
@@ -148,35 +154,35 @@ renderer = _renderer
 --
 -- See 'setRendererDefault' and 'setRendererNetstr' for two common special cases.
 setRenderer :: Renderer -> Settings -> Settings
-setRenderer f s = s { _renderer = f }
+setRenderer f s = s {_renderer = f}
 
 readEnvironment :: Settings -> Bool
 readEnvironment = _readEnvironment
 
 setReadEnvironment :: Bool -> Settings -> Settings
-setReadEnvironment f s = s { _readEnvironment = f }
+setReadEnvironment f s = s {_readEnvironment = f}
 
 data Level
-    = Trace
-    | Debug
-    | Info
-    | Warn
-    | Error
-    | Fatal
-    deriving (Eq, Ord, Read, Show)
+  = Trace
+  | Debug
+  | Info
+  | Warn
+  | Error
+  | Fatal
+  deriving (Eq, Ord, Read, Show)
 
 data Output
-    = StdOut
-    | StdErr
-    | Path FilePath
-    deriving (Eq, Ord, Show)
+  = StdOut
+  | StdErr
+  | Path FilePath
+  deriving (Eq, Ord, Show)
 
 newtype DateFormat = DateFormat
-    { display :: UnixTime -> ByteString
-    }
+  { display :: UnixTime -> ByteString
+  }
 
 instance IsString DateFormat where
-    fromString = DateFormat . formatUnixTimeGMT . pack
+  fromString = DateFormat . formatUnixTimeGMT . pack
 
 -- | ISO 8601 date-time format.
 iso8601UTC :: DateFormat
@@ -203,9 +209,9 @@ type Renderer = ByteString -> DateFormat -> Level -> [Element] -> B.Builder
 --   * 'name'            = Nothing
 --
 --   * 'readEnvironment' = True
---
 defSettings :: Settings
-defSettings = Settings
+defSettings =
+  Settings
     Debug
     Map.empty
     StdOut
