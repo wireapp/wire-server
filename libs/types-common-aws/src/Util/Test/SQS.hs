746ff6b0a235dbe015783e93cf07aad7c5351fb1
Akshay Mankar <akshay@wire.com>
2023-01-25 12:34:26 +0100

    Util.Test.SQS: haddocks, re-order code bits

--- libs/types-common-aws/src/Util/Test/SQS.hs
+++ libs/types-common-aws/src/Util/Test/SQS.hs
@@ -39,60 +39,17 @@ import UnliftIO (Async, async)
 import UnliftIO.Resource (MonadResource, ResourceT)
 import UnliftIO.Timeout (timeout)
 
------------------------------------------------------------------------------
--- Queue operations
-
-deleteMessage :: (Monad m, MonadReader AWS.Env m, MonadResource m) => Text -> SQS.Message -> m ()
-deleteMessage url m = do
-  for_
-    (m ^. SQS.message_receiptHandle)
-    (void . sendEnv . SQS.newDeleteMessage url)
-
------------------------------------------------------------------------------
--- Generic AWS execution helpers
-execute ::
-  AWS.Env ->
-  ReaderT AWS.Env (ResourceT IO) a ->
-  IO a
-execute env = AWS.runResourceT . flip runReaderT env
-
------------------------------------------------------------------------------
--- Internal. Most of these functions _can_ be used outside of this function
--- but probably do not need to
-receive :: Int -> Text -> SQS.ReceiveMessage
-receive n url =
-  SQS.newReceiveMessage url
-    & set SQS.receiveMessage_waitTimeSeconds (Just 1)
-      . set SQS.receiveMessage_maxNumberOfMessages (Just n)
-      . set SQS.receiveMessage_visibilityTimeout (Just 1)
-
-fetchMessage :: (MonadIO m, Message a, MonadReader AWS.Env m, MonadResource m) => Text -> String -> (String -> Maybe a -> IO ()) -> m ()
-fetchMessage url label callback = do
-  msgs <- fromMaybe [] . view SQS.receiveMessageResponse_messages <$> sendEnv (receive 1 url)
-  events <- mapM (parseDeleteMessage url) msgs
-  liftIO $ callback label (headDef Nothing events)
-
-parseDeleteMessage :: (Monad m, Message a, MonadIO m, MonadReader AWS.Env m, MonadResource m) => Text -> SQS.Message -> m (Maybe a)
-parseDeleteMessage url m = do
-  let decodedMessage = decodeMessage <=< (B64.decode . Text.encodeUtf8)
-  evt <- case decodedMessage <$> (m ^. SQS.message_body) of
-    Just (Right e) -> pure (Just e)
-    _ -> do
-      liftIO $ print ("Failed to parse SQS message or event" :: String)
-      pure Nothing
-  deleteMessage url m
-  pure evt
-
-sendEnv :: (MonadReader AWS.Env m, MonadResource m, AWS.AWSRequest a) => a -> m (AWS.AWSResponse a)
-sendEnv x = flip AWS.send x =<< ask
-
-----------------
-
 data SQSWatcher a = SQSWatcher
   { watcherProcess :: Async (),
     events :: IORef [a]
   }
 
+-- | Starts an async loop which continuously retrieves messages from a given SQS
+-- queue, parses them and stores them in an `IORef [a]`.
+--
+-- This function drops everything in the queue before starting the async loop.
+-- This helps test run faster and makes sure that initial tests don't timeout if
+-- the queue has too many things in it before the tests start.
 watchSQSQueue :: Message a => AWS.Env -> Text -> IO (SQSWatcher a)
 watchSQSQueue env queueUrl = do
   eventsRef <- newIORef []
@@ -129,6 +86,7 @@ watchSQSQueue env queueUrl = do
           execute env $ mapM_ (deleteMessage queueUrl) ms
           ensureEmpty
 
+-- | Waits for a message matching a predicate for a given number of seconds.
 waitForMessage :: (MonadUnliftIO m, Eq a, Show a) => SQSWatcher a -> Int -> (a -> Bool) -> m (Maybe a)
 waitForMessage watcher seconds predicate = timeout (seconds * 1_000_000) poll
   where
@@ -139,7 +97,54 @@ waitForMessage watcher seconds predicate = timeout (seconds * 1_000_000) poll
           (x : _) -> (delete x events, Just x)
       maybe (threadDelay 1000 >> poll) pure matched
 
+-- | First waits for a message matching a given predicate for 3 seconds (this
+-- number could be chosen more scientifically) and then uses a callback to make
+-- an assertion on such a message.
 assertMessage :: (Show a, MonadUnliftIO m, Eq a, HasCallStack) => SQSWatcher a -> String -> (a -> Bool) -> (String -> Maybe a -> m ()) -> m ()
 assertMessage watcher label predicate callback = do
   matched <- waitForMessage watcher 3 predicate
   callback label matched
+
+-----------------------------------------------------------------------------
+-- Generic AWS execution helpers
+execute ::
+  AWS.Env ->
+  ReaderT AWS.Env (ResourceT IO) a ->
+  IO a
+execute env = AWS.runResourceT . flip runReaderT env
+
+-----------------------------------------------------------------------------
+-- Internal. Most of these functions _can_ be used outside of this function
+-- but probably do not need to
+receive :: Int -> Text -> SQS.ReceiveMessage
+receive n url =
+  SQS.newReceiveMessage url
+    & set SQS.receiveMessage_waitTimeSeconds (Just 1)
+      . set SQS.receiveMessage_maxNumberOfMessages (Just n)
+      . set SQS.receiveMessage_visibilityTimeout (Just 1)
+
+fetchMessage :: (MonadIO m, Message a, MonadReader AWS.Env m, MonadResource m) => Text -> String -> (String -> Maybe a -> IO ()) -> m ()
+fetchMessage url label callback = do
+  msgs <- fromMaybe [] . view SQS.receiveMessageResponse_messages <$> sendEnv (receive 1 url)
+  events <- mapM (parseDeleteMessage url) msgs
+  liftIO $ callback label (headDef Nothing events)
+
+deleteMessage :: (Monad m, MonadReader AWS.Env m, MonadResource m) => Text -> SQS.Message -> m ()
+deleteMessage url m = do
+  for_
+    (m ^. SQS.message_receiptHandle)
+    (void . sendEnv . SQS.newDeleteMessage url)
+
+parseDeleteMessage :: (Monad m, Message a, MonadIO m, MonadReader AWS.Env m, MonadResource m) => Text -> SQS.Message -> m (Maybe a)
+parseDeleteMessage url m = do
+  let decodedMessage = decodeMessage <=< (B64.decode . Text.encodeUtf8)
+  evt <- case decodedMessage <$> (m ^. SQS.message_body) of
+    Just (Right e) -> pure (Just e)
+    _ -> do
+      liftIO $ print ("Failed to parse SQS message or event" :: String)
+      pure Nothing
+  deleteMessage url m
+  pure evt
+
+sendEnv :: (MonadReader AWS.Env m, MonadResource m, AWS.AWSRequest a) => a -> m (AWS.AWSResponse a)
+sendEnv x = flip AWS.send x =<< ask
