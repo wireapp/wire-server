f76f3990fb05e5772d5a1ffd9f305dead07fe15a
jschaul <jschaul@users.noreply.github.com>
2023-01-30 17:48:09 +0100

    run helm tests in parallel

--- hack/bin/integration-test.sh
+++ hack/bin/integration-test.sh
@@ -6,4 +6,11 @@ NAMESPACE=${NAMESPACE:-test-integration}
 echo "Running integration tests on wire-server"
 
 CHART=wire-server
-helm test --logs -n "${NAMESPACE}" "${NAMESPACE}-${CHART}" --timeout 900s
+tests=(cargohold gundeck spar brig galley federator)
+# Run tests in parallel using GNU parallel (see https://www.gnu.org/software/parallel/)
+printf '%s\n' "${tests[@]}" | parallel echo "Running helm tests for {}..."
+printf '%s\n' "${tests[@]}" | parallel \
+    helm test -n "${NAMESPACE}" "${NAMESPACE}-${CHART}" --timeout 900s --filter name="${NAMESPACE}-${CHART}-{}-integration" '| grep -v NOTES;' \
+    echo "==== Done testing {}. Here are logs: ====" ';' \
+    kubectl -n "${NAMESPACE}" logs "${NAMESPACE}-${CHART}-{}-integration" ';' \
+    echo "==== Above logs are for {}.===="
