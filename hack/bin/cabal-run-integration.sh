41646c159a419891a1598af6d4f5fbcc4d29d6bd
Paolo Capriotti <paolo@capriotti.io>
2023-02-03 14:48:08 +0100

    Support multiple instances of the same service

--- hack/bin/cabal-run-integration.sh
+++ hack/bin/cabal-run-integration.sh
@@ -46,6 +46,7 @@ run_integration_tests() {
   service_dir="$TOP_LEVEL/services/$package"
 
   cd "$service_dir"
+  # "$TOP_LEVEL/services/integration.sh" \
   "/usr/bin/python3" "$TOP_LEVEL/services/run-services" \
     "$TOP_LEVEL/dist/$package-integration" \
     -s "$service_dir/$package.integration.yaml" \
