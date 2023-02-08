a572a9aa4bb79858362274fba937e4cdd35ab0fe
Sven Tennie <sven.tennie@wire.com>
2023-02-08 11:39:19 +0100

    Add swagger to servantified endpoint

--- libs/wire-api/src/Wire/API/Notification.hs
+++ libs/wire-api/src/Wire/API/Notification.hs
@@ -42,9 +42,11 @@ module Wire.API.Notification
   )
 where
 
-import Control.Lens (makeLenses)
+import Control.Lens (makeLenses, (.~))
+import Control.Lens.Operators ((?~))
 import Data.Aeson (FromJSON (..), ToJSON (..))
 import qualified Data.Aeson.Types as Aeson
+import qualified Data.HashMap.Strict.InsOrd as InsOrdHashMap
 import Data.Id
 import Data.Json.Util
 import Data.List.NonEmpty (NonEmpty)
@@ -73,6 +75,22 @@ modelEvent = Doc.defineModel "NotificationEvent" $ do
   Doc.property "type" Doc.string' $
     Doc.description "Event type"
 
+-- | A schema for an arbitrary JSON object.
+eventSchema :: ValueSchema NamedSwaggerDoc Event
+eventSchema = mkSchema sdoc Aeson.parseJSON (Just . Aeson.toJSON)
+  where
+    sdoc :: NamedSwaggerDoc
+    sdoc =
+      swaggerDoc @Aeson.Object
+        & S.schema . S.title ?~ "Event"
+        & S.schema . S.description ?~ "A single notification event"
+        & S.schema . S.properties
+          .~ InsOrdHashMap.fromList
+            [ ( "type",
+                S.Inline (S.toSchema (Proxy @Text) & S.description ?~ "Event type")
+              )
+            ]
+
 --------------------------------------------------------------------------------
 -- QueuedNotification
 
@@ -89,12 +107,15 @@ queuedNotification = QueuedNotification
 
 instance ToSchema QueuedNotification where
   schema =
-    object "QueuedNotification" $
+    objectWithDocModifier "QueuedNotification" queuedNotificationDoc $
       QueuedNotification
         <$> _queuedNotificationId
           .= field "id" schema
         <*> _queuedNotificationPayload
-          .= field "payload" (nonEmptyArray jsonObject)
+          .= fieldWithDocModifier "payload" payloadDoc (nonEmptyArray eventSchema)
+    where
+      queuedNotificationDoc = description ?~ "A single notification"
+      payloadDoc d = d & description ?~ "List of events"
 
 makeLenses ''QueuedNotification
 
@@ -128,14 +149,18 @@ modelNotificationList = Doc.defineModel "NotificationList" $ do
 
 instance ToSchema QueuedNotificationList where
   schema =
-    object "QueuedNotificationList" $
+    objectWithDocModifier "QueuedNotificationList" queuedNotificationListDoc $
       QueuedNotificationList
         <$> _queuedNotifications
-          .= field "notifications" (array schema)
+          .= fieldWithDocModifier "notifications" notificationsDoc (array schema)
         <*> _queuedHasMore
-          .= fmap (fromMaybe False) (optField "has_more" schema)
+          .= fmap (fromMaybe False) (optFieldWithDocModifier "has_more" hasMoreDoc schema)
         <*> _queuedTime
           .= maybe_ (optField "time" utcTimeSchema)
+    where
+      queuedNotificationListDoc = description ?~ "Zero or more notifications"
+      notificationsDoc = description ?~ "Notifications"
+      hasMoreDoc = description ?~ "Whether there are still more notifications."
 
 makeLenses ''QueuedNotificationList
 
--- libs/wire-api/src/Wire/API/Routes/Public/Galley/TeamNotification.hs
+++ libs/wire-api/src/Wire/API/Routes/Public/Galley/TeamNotification.hs
@@ -1,25 +1,62 @@
 module Wire.API.Routes.Public.Galley.TeamNotification where
 
-import Servant
-import Wire.API.Routes.Public
-import Wire.API.Routes.Named
-import Imports
-import Wire.API.Notification
 import Data.Range
+import Imports
+import Servant
 import Wire.API.Error
 import Wire.API.Error.Galley
+import Wire.API.Notification
+import Wire.API.Routes.Named
+import Wire.API.Routes.Public
 
 type TeamNotificationAPI =
   Named
     "get-team-notifications"
-    ( "team"
+    ( Summary "Read recently added team members from team queue"
+        :> Description GetTeamNotificationsDescription
+        :> "teams"
         :> "notifications"
         :> ZUser
         :> CanThrow 'TeamNotFound
         :> CanThrow 'InvalidTeamNotificationId
         -- :> CanThrow InvalidInput
         -- TODO: Ensure/check this is a V1 UUID
-        :> QueryParam "since" NotificationId
-        :> QueryParam "size" (Range 1 10000 Int32)
+        :> QueryParam'
+             [ Optional,
+               Strict,
+               Description "Notification id to start with in the response (UUIDv1)"
+             ]
+             "since"
+             NotificationId
+        :> QueryParam'
+             [ Optional,
+               Strict,
+               Description "Maximum number of events to return (1..10000; default: 1000)"
+             ]
+             "size"
+             (Range 1 10000 Int32)
         :> Get '[Servant.JSON] QueuedNotificationList
     )
+
+type GetTeamNotificationsDescription =
+  "This is a work-around for scalability issues with gundeck user event fan-out. \
+  \It does not track all team-wide events, but only `member-join`.\
+  \\n\
+  \Note that `/teams/notifications` behaves different from `/notifications`:\
+  \\n\
+  \- If there is a gap between the notification id requested with `since` and the \
+  \available data, team queues respond with 200 and the data that could be found. \
+  \The do NOT respond with status 404, but valid data in the body.\
+  \\n\
+  \- The notification with the id given via `since` is included in the \
+  \response if it exists.  You should remove this and only use it to decide whether \
+  \there was a gap between your last request and this one.\
+  \\n\
+  \- If the notification id does *not* exist, you get the more recent events from the queue \
+  \(instead of all of them).  This can be done because a notification id is a UUIDv1, which \
+  \is essentially a time stamp.\
+  \\n\
+  \- There is no corresponding `/last` end-point to get only the most recent event. \
+  \That end-point was only useful to avoid having to pull the entire queue.  In team \
+  \queues, if you have never requested the queue before and \
+  \have no prior notification id, just pull with timestamp 'now'."
