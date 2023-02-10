7aad2e94267ff22f34d1bae4def5cdc26330cadc
Sven Tennie <sven.tennie@wire.com>
2023-02-10 14:33:23 +0100

    Stern: Use valid versions in URL path (#3070)
    The pattern is `/v<number>/<path>`, not `/<number>/<path>`.

--- libs/wire-api/test/unit/Main.hs
+++ libs/wire-api/test/unit/Main.hs
@@ -31,6 +31,7 @@ import qualified Test.Wire.API.Roundtrip.CSV as Roundtrip.CSV
 import qualified Test.Wire.API.Roundtrip.HttpApiData as Roundtrip.HttpApiData
 import qualified Test.Wire.API.Roundtrip.MLS as Roundtrip.MLS
 import qualified Test.Wire.API.Routes as Routes
+import qualified Test.Wire.API.Routes.Version as Routes.Version
 import qualified Test.Wire.API.Swagger as Swagger
 import qualified Test.Wire.API.Team.Export as Team.Export
 import qualified Test.Wire.API.Team.Member as Team.Member
@@ -59,5 +60,6 @@ main =
         Roundtrip.CSV.tests,
         Routes.tests,
         Conversation.tests,
-        MLS.tests
+        MLS.tests,
+        Routes.Version.tests
       ]
