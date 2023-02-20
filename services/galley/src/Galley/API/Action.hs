b5ddb893c5b81009c262f7fbd65a4fc3cf6f3cf5
Sandy Maguire <sandy@sandymaguire.me>
2023-02-20 10:36:19 -0800

    chore: hlint

--- services/galley/src/Galley/API/Action.hs
+++ services/galley/src/Galley/API/Action.hs
@@ -14,7 +14,7 @@
 --
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
-{-# LANGUAGE StandaloneKindSignatures #-}
+
 
 module Galley.API.Action
   ( -- * Conversation action types
