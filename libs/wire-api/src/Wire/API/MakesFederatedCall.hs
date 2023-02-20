8c4e6035df230fe36579a746049a8a327ad6da91
Sandy Maguire <sandy@sandymaguire.me>
2023-02-20 08:06:21 -0800

    chore: add plugin to cargohold

--- libs/wire-api/src/Wire/API/MakesFederatedCall.hs
+++ libs/wire-api/src/Wire/API/MakesFederatedCall.hs
@@ -15,6 +15,7 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 {-# LANGUAGE OverloadedLists #-}
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
 
 module Wire.API.MakesFederatedCall
   ( CallsFed,
--- services/cargohold/src/CargoHold/API/Public.hs
+++ services/cargohold/src/CargoHold/API/Public.hs
@@ -60,13 +60,13 @@ servantSitemap =
     providerAPI = uploadAssetV3 @tag :<|> downloadAssetV3 @tag :<|> deleteAssetV3 @tag
     legacyAPI = legacyDownloadPlain :<|> legacyDownloadPlain :<|> legacyDownloadOtr
     qualifiedAPI :: ServerT QualifiedAPI Handler
-    qualifiedAPI = callsFed downloadAssetV4 :<|> deleteAssetV4
+    qualifiedAPI = callsFed (exposeAnnotations downloadAssetV4) :<|> deleteAssetV4
     mainAPI :: ServerT MainAPI Handler
     mainAPI =
       renewTokenV3
         :<|> deleteTokenV3
         :<|> uploadAssetV3 @'UserPrincipalTag
-        :<|> callsFed downloadAssetV4
+        :<|> callsFed (exposeAnnotations downloadAssetV4)
         :<|> deleteAssetV4
 
 internalSitemap :: ServerT InternalAPI Handler
