2e88febcc2eb9d8463da84fac5360df6db2c61d8
Akshay Mankar <akshay@wire.com>
2023-03-06 11:07:38 +0100

    Less convoluted way to send requests

--- libs/wire-api-federation/src/Wire/API/Federation/HTTP2.hs
+++ libs/wire-api-federation/src/Wire/API/Federation/HTTP2.hs
@@ -3,7 +3,6 @@
 module Wire.API.Federation.HTTP2 where
 
 import Control.Concurrent.Async
-import Control.Concurrent.Chan
 import Control.Exception
 import qualified Data.Map as Map
 import Data.Streaming.Network
@@ -13,26 +12,29 @@ import qualified Network.Socket as NS
 
 data HTTP2Conn = HTTP2Conn
   { backgroundThread :: Async (),
+    -- Maybe the 'disconect' action can also take care of cleaning up the thread
+    disconnect :: IO (),
     -- See comment in 'startPersistentHTTP2Connection' about what this 'Chan'
     -- contains
-    requestChan :: Chan (HTTP2.Request, HTTP2.Response -> IO (), MVar (Async ()))
+    sendRequest :: HTTP2.Request -> (HTTP2.Response -> IO ()) -> IO ()
   }
 
 data HTTP2Manager = HTTP2Manager
-  { connections :: MVar (Map (ByteString, Int) HTTP2Conn),
+  { -- TODO: Look into some STM based map to avoid contention
+    connections :: MVar (Map (ByteString, Int) HTTP2Conn),
     cacheLimit :: Int
   }
 
 withHTTP2Request :: HTTP2Manager -> ByteString -> Int -> HTTP2.Request -> (HTTP2.Response -> IO a) -> IO a
 withHTTP2Request mgr host port req f = do
-  reqChan <- modifyMVar (connections mgr) $ \conns -> do
+  sendReq <- modifyMVar (connections mgr) $ \conns -> do
     case Map.lookup (host, port) conns of
       Nothing -> newConn conns
       Just conn ->
         -- If there is a connection for the (host,port), ensure that it is alive
         -- before using it.
         poll (backgroundThread conn) >>= \case
-          Nothing -> pure (conns, requestChan conn)
+          Nothing -> pure (conns, sendRequest conn)
           Just (Left (SomeException _err)) ->
             -- TODO: Log the error, maybe by adding a generic logger function to Http2Manager
             newConn (Map.delete (host, port) conns)
@@ -40,23 +42,17 @@ withHTTP2Request mgr host port req f = do
             -- NOTE: or maybe just create a new connection?
             error "impossible: http2 client ended successfully"
   result <- newEmptyMVar
-  reqThreadMVar <- newEmptyMVar
-  writeChan reqChan (req, putMVar result <=< f, reqThreadMVar)
-  reqThread <- takeMVar reqThreadMVar
-  let waitForReqThread =
-        waitCatch reqThread >>= \case
-          Left (SomeException e) -> throw e
-          -- The reqeust thread can only exit with an exception or after writing
-          -- the 'result' MVar, so this _should_ never happen.
-          Right () -> throw FailedToGetResponse
-  either id id <$> race (takeMVar result) waitForReqThread
+  sendReq req (putMVar result <=< f)
+  -- The reqeust can only exit with an exception or after writing
+  -- the 'result' MVar, so the `Nothing` case _should_ never happen.
+  maybe (throw FailedToGetResponse) pure =<< tryTakeMVar result
   where
     newConn conns = do
-      reqChan <- newChan
-      started <- newEmptyMVar
-      thread <- async $ startPersistentHTTP2Connection host port (cacheLimit mgr) started reqChan
-      takeMVar started
-      pure (Map.insert (host, port) (HTTP2Conn thread reqChan) conns, reqChan)
+      sendReqMVar <- newEmptyMVar
+      stopMVar <- newEmptyMVar
+      thread <- async $ startPersistentHTTP2Connection host port (cacheLimit mgr) sendReqMVar stopMVar
+      sr <- takeMVar sendReqMVar
+      pure (Map.insert (host, port) (HTTP2Conn thread (putMVar stopMVar ()) sr) conns, sr)
 
 data Http2ManagerError = FailedToGetResponse
   deriving (Show)
@@ -70,7 +66,15 @@ startPersistentHTTP2Connection ::
   Int ->
   -- cacheLimit
   Int ->
-  -- empty mvar, written when the client is ready to send requests
+  -- empty mvar, written when connection is established and request can be sent,
+  -- the MVar will contain a continuation to be used for each request
+  MVar (HTTP2.Request -> (HTTP2.Response -> IO ()) -> IO ()) ->
+  -- empty mvar, the connection will wait for this MVar to be written. The
+  -- connection will close immediately, any pending requests might get
+  -- terminated abruptly.
+  --
+  -- Another way to terminate the connection would be to cancel the thread which
+  -- runs this function
   MVar () ->
   -- Channel for recieveing requests to be executed, contains a request, a
   -- response consumer and an empty mvar for the handle to thread actually
@@ -81,9 +85,9 @@ startPersistentHTTP2Connection ::
   -- consumer to the return type of itself. Even if the response consumer
   -- returned something else we would need another empty MVar to write the
   -- result, this is being done in 'withHTTP2Request'.
-  Chan (HTTP2.Request, HTTP2.Response -> IO (), MVar (Async ())) ->
+  -- Chan (HTTP2.Request, HTTP2.Response -> IO (), MVar (Async ())) ->
   IO ()
-startPersistentHTTP2Connection hostname port cacheLimit started reqChan = do
+startPersistentHTTP2Connection hostname port cacheLimit sendReqMVar gracefulStop = do
   let clientConfig =
         HTTP2.ClientConfig
           { HTTP2.scheme = "https",
@@ -93,7 +97,5 @@ startPersistentHTTP2Connection hostname port cacheLimit started reqChan = do
   bracket (fst <$> getSocketTCP hostname port) NS.close $ \sock ->
     bracket (HTTP2.allocSimpleConfig sock 4096) HTTP2.freeSimpleConfig $ \http2Cfg ->
       HTTP2.run clientConfig http2Cfg $ \sendRequest -> do
-        putMVar started ()
-        forever $ do
-          (req, respConsumer, reqThreadMVar) <- readChan reqChan
-          putMVar reqThreadMVar =<< async (sendRequest req respConsumer)
+        putMVar sendReqMVar sendRequest
+        takeMVar gracefulStop
