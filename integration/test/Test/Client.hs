99c589940c8de743cf383f17c2279dc3b3b74640
Paolo Capriotti <paolo@capriotti.io>
2023-03-15 14:19:56 +0100

    AddClient api function

--- integration/test/Test/Client.hs
+++ integration/test/Test/Client.hs
@@ -2,9 +2,12 @@
 
 module Test.Client where
 
+import API
 import App
 import Data.Default
 import Imports
+import qualified Network.HTTP.Types as HTTP
+import Response
 import SetupHelpers
 
 -- | Cannot delete a legalhold client
@@ -13,7 +16,8 @@ import SetupHelpers
 testCantDeleteLHClient :: App ()
 testCantDeleteLHClient = do
   user <- randomUser def
-  print user
+  bindResponse (addClient user def {ctype = "legalhold", internal = True}) $ \resp -> do
+    (@?=) resp.status HTTP.status201
 
 testOtherWithoutComments :: App ()
 testOtherWithoutComments = pure ()
