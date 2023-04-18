f37734b07bb05116e6589036ef3d63e295ca22ef
Stefan Matting <stefan@wire.com>
2023-04-18 11:41:58 +0200

    remove arguments nonesense

--- integration/test/Run.hs
+++ integration/test/Run.hs
@@ -1,13 +1,10 @@
 module Run (main) where
 
-import Config
-import qualified Data.Yaml as Yaml
 import Imports
 import RunAllTests
 import System.Exit (exitFailure, exitSuccess)
 import System.IO (hPutStrLn)
 import Test.Tasty (testGroup)
-import Test.Tasty.Options (lookupOption, setOption)
 import Test.Tasty.Providers (singleTest)
 import Test.Tasty.Runners (consoleTestReporter, listingTests, parseOptions, tryIngredients)
 
@@ -23,18 +20,7 @@ main = do
 
   optsCLI <- parseOptions ingredients tree
 
-  let ConfigFile cfgFile = lookupOption @ConfigFile optsCLI
-
-  eith <- Yaml.decodeFileEither cfgFile
-  serviceMap <- case eith of
-    Left err -> do
-      hPutStrLn stderr $ "Could not parse " <> cfgFile <> ": " <> Yaml.prettyPrintParseException err
-      exitFailure
-    Right serviceMap -> pure serviceMap
-
-  let opts = setOption @ServiceMap serviceMap optsCLI
-
-  case tryIngredients ingredients opts tree of
+  case tryIngredients ingredients optsCLI tree of
     Nothing -> do
       hPutStrLn
         stderr
