c7af4fae8e54d73e02b48694848cdc165723f062
Stefan Matting <stefan@wire.com>
2023-04-19 15:40:18 +0200

    Don't to use AWS / internal queues in modified services

--- services/brig/src/Brig/Queue/Types.hs
+++ services/brig/src/Brig/Queue/Types.hs
@@ -24,7 +24,7 @@ import Data.Aeson
 import Imports
 
 -- | A remote queue that you can publish to and listen from.
-data Queue = StompQueue Text | SqsQueue Text
+data Queue = StompQueue Text | SqsQueue Text | NoQueueOnlyForTesting
   deriving (Eq, Show)
 
 instance FromJSON Queue where
@@ -32,4 +32,5 @@ instance FromJSON Queue where
     o .: "queueType" >>= \case
       "stomp" -> StompQueue <$> o .: "queueName"
       "sqs" -> SqsQueue <$> o .: "queueName"
+      "no-queue-only-for-testing" -> pure NoQueueOnlyForTesting
       other -> fail ("unknown 'queueType': " <> other)
