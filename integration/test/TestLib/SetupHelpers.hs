95ef3fb581718cf8c845af8fe3a66f797eb8fdf2
Stefan Matting <stefan@wire.com>
2023-04-21 14:40:33 +0200

    shouldMatchInt and nested lookup

--- integration/test/TestLib/SetupHelpers.hs
+++ integration/test/TestLib/SetupHelpers.hs
@@ -8,7 +8,7 @@ import TestLib.App
 
 randomUser :: API.CreateUser -> App Value
 randomUser cu = bindResponse (API.createUser cu) $ \resp -> do
-  resp.status `shouldMatchPlain` 201
+  resp.status `shouldMatchInt` 201
   resp.json
 
 -- | returns (user, team id)
