7a31fdf8411cc45dc51927a89afe8f3f2f989ef4
Matthias Fischmann <mf@zerobuzz.net>
2023-05-11 09:28:07 +0200

    Fixup

--- services/start-services-only.sh
+++ services/start-services-only.sh
@@ -1,4 +1,4 @@
-n#!/usr/bin/env bash
+#!/usr/bin/env bash
 
 SERVICES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo -e "\n\n\n*** $0 is deprecated.  please run '$SERVICES_DIR/run-services' instead.\n\n\n"
