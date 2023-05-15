8d9e69b8163c891626f3df8e4fe99d385f396245
Igor Ranieri <igor@elland.me>
2023-05-15 17:02:21 +0200

    Refactor rabbitmq / background worker life-cycle

--- services/background-worker/src/Wire/BackgroundWorker.hs
+++ services/background-worker/src/Wire/BackgroundWorker.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DisambiguateRecordFields #-}
+
 module Wire.BackgroundWorker where
 
 import qualified Data.Text as Text
@@ -15,12 +17,14 @@ run opts = do
   env <- mkEnv opts
   -- FUTUREWORK: Make some way to tracking all the workers, currently there is
   -- only one so we can just block on it.
-  withRabbitMqChannel env.logger opts.rabbitmq $
+  stopped <- newEmptyMVar
+  runWithRabbitMq env.logger opts.rabbitmq $
     RabbitMqHooks
       { onNewChannel = BackendNotificationPusher.startWorker env opts.remoteDomains,
-        onGracefulStop = pure (),
+        onGracefulStop = putMVar stopped (),  
         onException = const $ pure ()
       }
+  takeMVar stopped
 
 data RabbitMqHooks = RabbitMqHooks
   { onGracefulStop :: IO (),
@@ -30,29 +34,38 @@ data RabbitMqHooks = RabbitMqHooks
 
 -- TODO(elland): Find out if there are benefits of having one channel for everything
 -- or should we create more channels?
--- rabbitmqChannel :: IORef Q.Channel,
-withRabbitMqChannel :: Logger -> RabbitMqOpts -> RabbitMqHooks -> IO ()
-withRabbitMqChannel l opts hooks = do
+runWithRabbitMq :: Logger -> RabbitMqOpts -> RabbitMqHooks -> IO ()
+runWithRabbitMq l opts hooks = do
   username <- Text.pack <$> getEnv "RABBITMQ_USERNAME"
   password <- Text.pack <$> getEnv "RABBITMQ_PASSWORD"
   connect username password
-  forever $ threadDelay maxBound
   where
     connect username password = do
       conn <- Q.openConnection' opts.host (fromIntegral opts.port) opts.vHost username password
-      Log.info l $ Log.msg (Log.val "RabbitMQ connection established")
+      Q.addConnectionClosedHandler conn True (connect username password)
+      openChan conn
+
+    openChan conn = do
+      Log.info l $ Log.msg (Log.val "Opening channel with RabbitMQ")
       chan <- Q.openChannel conn
       -- TODO(elland): Q.addConnectionClosedHandler
       -- TODO(elland): Q.addConnectionBlockedHandler (Probably not required: https://www.rabbitmq.com/connection-blocked.html)
-      Q.addChannelExceptionHandler chan (handler username password)
+      Q.addChannelExceptionHandler chan (handler conn)
       Log.info l $ Log.msg (Log.val "RabbitMQ channel opened")
       hooks.onNewChannel chan
-    handler username password e =
-      if Q.isNormalChannelClose e
-        then do
-          Log.info l $ Log.msg (Log.val "RabbitMQ channel gracefully closed")
-          hooks.onGracefulStop
-        else do
-          Log.err l $ Log.msg (Log.val "RabbitMQ channel closed with an exception") . Log.field "error" (displayException e)
-          hooks.onException e
-          connect username password
+    handler conn e =
+      if
+          | Q.isNormalChannelClose e -> do
+              Log.info l $ Log.msg (Log.val "RabbitMQ channel gracefully closed, not retrying to open channel")
+              hooks.onGracefulStop
+          | isConnectionClosed e -> do
+              -- if ConnectionClosedException, do not reconnect channel
+              Log.info l $ Log.msg (Log.val "RabbitMQ connection closed, not retrying to open channel")
+          | otherwise -> do
+              Log.err l $ Log.msg (Log.val "RabbitMQ channel closed with an exception") . Log.field "error" (displayException e)
+              hooks.onException e
+              openChan conn
+
+isConnectionClosed :: SomeException -> Bool
+isConnectionClosed (fromException -> Just (Q.ConnectionClosedException {})) = True
+isConnectionClosed _ = False
