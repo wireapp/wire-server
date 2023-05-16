8ec2d61c900f514f3d21cf4bc88963d4e4d3c9fa
Akshay Mankar <akshay@wire.com>
2023-05-16 11:29:48 +0200

    backgroun-worker: Move rabbitmq code to separate module, log retries

--- services/background-worker/src/Wire/BackgroundWorker.hs
+++ services/background-worker/src/Wire/BackgroundWorker.hs
@@ -1,19 +1,12 @@
 {-# LANGUAGE DisambiguateRecordFields #-}
-{-# LANGUAGE NumericUnderscores #-}
 
 module Wire.BackgroundWorker where
 
-import Control.Monad.Catch
-import Control.Monad.Trans.Control
-import Control.Retry
-import qualified Data.Text as Text
 import Imports
-import qualified Network.AMQP as Q
-import System.Logger.Extended (Logger)
-import qualified System.Logger.Extended as Log
 import qualified Wire.BackendNotificationPusher as BackendNotificationPusher
 import Wire.BackgroundWorker.Env
 import Wire.BackgroundWorker.Options
+import Wire.BackgroundWorker.RabbitMQ
 
 -- TODO(elland): Start an http service with status and metrics endpoints
 run :: Opts -> IO ()
@@ -29,57 +22,3 @@ run opts = do
         onException = const $ pure ()
       }
   takeMVar stopped
-
-data RabbitMqHooks m = RabbitMqHooks
-  { onGracefulStop :: m (),
-    onException :: SomeException -> m (),
-    onNewChannel :: Q.Channel -> m ()
-  }
-
--- TODO(elland): Find out if there are benefits of having one channel for everything
--- or should we create more channels?
-runWithRabbitMq :: forall m. (MonadIO m, MonadMask m, MonadBaseControl IO m) => Logger -> RabbitMqOpts -> RabbitMqHooks m -> m ()
-runWithRabbitMq l opts hooks = do
-  username <- liftIO $ Text.pack <$> getEnv "RABBITMQ_USERNAME"
-  password <- liftIO $ Text.pack <$> getEnv "RABBITMQ_PASSWORD"
-  connectWithRetries username password
-  where
-    connectWithRetries :: Text -> Text -> m ()
-    connectWithRetries username password = do
-      let policy = capDelay 5_000_000 $ fullJitterBackoff 1000
-      recoverAll
-        policy
-        ( const $ do
-            Log.info l $ Log.msg (Log.val "Trying to connect to RabbitMQ")
-            connect username password
-        )
-    connect :: Text -> Text -> m ()
-    connect username password = do
-      conn <- liftIO $ Q.openConnection' opts.host (fromIntegral opts.port) opts.vHost username password
-      liftBaseWith $ \runInIO ->
-        Q.addConnectionClosedHandler conn True (void $ runInIO $ connectWithRetries username password)
-      openChan conn
-
-    openChan :: Q.Connection -> m ()
-    openChan conn = do
-      Log.info l $ Log.msg (Log.val "Opening channel with RabbitMQ")
-      chan <- liftIO $ Q.openChannel conn
-      liftBaseWith $ \runInIO ->
-        Q.addChannelExceptionHandler chan (void . runInIO . handler conn)
-      Log.info l $ Log.msg (Log.val "RabbitMQ channel opened")
-      hooks.onNewChannel chan
-    handler conn e =
-      if
-          | Q.isNormalChannelClose e -> do
-              Log.info l $ Log.msg (Log.val "RabbitMQ channel gracefully closed, not retrying to open channel")
-              hooks.onGracefulStop
-          | isConnectionClosed e -> do
-              Log.info l $ Log.msg (Log.val "RabbitMQ connection closed, not retrying to open channel")
-          | otherwise -> do
-              Log.err l $ Log.msg (Log.val "RabbitMQ channel closed with an exception") . Log.field "error" (displayException e)
-              hooks.onException e
-              openChan conn
-
-isConnectionClosed :: SomeException -> Bool
-isConnectionClosed (fromException -> Just (Q.ConnectionClosedException {})) = True
-isConnectionClosed _ = False
---
+++ services/background-worker/src/Wire/BackgroundWorker/RabbitMQ.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE NumericUnderscores #-}
+
+module Wire.BackgroundWorker.RabbitMQ where
+
+import Control.Monad.Catch
+import Control.Monad.Trans.Control
+import Control.Retry
+import qualified Data.Text as Text
+import Imports
+import qualified Network.AMQP as Q
+import System.Logger.Extended (Logger)
+import qualified System.Logger.Extended as Log
+import Wire.BackgroundWorker.Options
+
+data RabbitMqHooks m = RabbitMqHooks
+  { onGracefulStop :: m (),
+    onException :: SomeException -> m (),
+    onNewChannel :: Q.Channel -> m ()
+  }
+
+-- TODO(elland): Find out if there are benefits of having one channel for everything
+-- or should we create more channels?
+runWithRabbitMq ::
+  forall m.
+  (MonadIO m, MonadMask m, MonadBaseControl IO m) =>
+  Logger ->
+  RabbitMqOpts ->
+  RabbitMqHooks m ->
+  m ()
+runWithRabbitMq l opts hooks = do
+  username <- liftIO $ Text.pack <$> getEnv "RABBITMQ_USERNAME"
+  password <- liftIO $ Text.pack <$> getEnv "RABBITMQ_PASSWORD"
+  connectWithRetries username password
+  where
+    connectWithRetries :: Text -> Text -> m ()
+    connectWithRetries username password = do
+      let policy = capDelay 5_000_000 $ fullJitterBackoff 1000
+          logError willRetry e retryStatus = do
+            Log.err l $
+              Log.msg (Log.val "Failed to connect to RabbitMQ")
+                . Log.field "error" (displayException @SomeException e)
+                . Log.field "willRetry" willRetry
+                . Log.field "retryCount" retryStatus.rsIterNumber
+      recovering
+        policy
+        [logRetries (const $ pure True) logError]
+        ( const $ do
+            Log.info l $ Log.msg (Log.val "Trying to connect to RabbitMQ")
+            connect username password
+        )
+    connect :: Text -> Text -> m ()
+    connect username password = do
+      conn <- liftIO $ Q.openConnection' opts.host (fromIntegral opts.port) opts.vHost username password
+      liftBaseWith $ \runInIO ->
+        Q.addConnectionClosedHandler conn True (void $ runInIO $ connectWithRetries username password)
+      openChan conn
+
+    openChan :: Q.Connection -> m ()
+    openChan conn = do
+      Log.info l $ Log.msg (Log.val "Opening channel with RabbitMQ")
+      chan <- liftIO $ Q.openChannel conn
+      liftBaseWith $ \runInIO ->
+        Q.addChannelExceptionHandler chan (void . runInIO . chanExceptionHandler conn)
+      Log.info l $ Log.msg (Log.val "RabbitMQ channel opened")
+      hooks.onNewChannel chan
+
+    chanExceptionHandler :: Q.Connection -> SomeException -> m ()
+    chanExceptionHandler conn e =
+      if
+          | Q.isNormalChannelClose e -> do
+              Log.info l $ Log.msg (Log.val "RabbitMQ channel gracefully closed, not retrying to open channel")
+              hooks.onGracefulStop
+          | isConnectionClosed e -> do
+              Log.info l $ Log.msg (Log.val "RabbitMQ connection closed, not retrying to open channel")
+          | otherwise -> do
+              Log.err l $ Log.msg (Log.val "RabbitMQ channel closed with an exception") . Log.field "error" (displayException e)
+              hooks.onException e
+              openChan conn
+
+isConnectionClosed :: SomeException -> Bool
+isConnectionClosed (fromException -> Just (Q.ConnectionClosedException {})) = True
+isConnectionClosed _ = False
