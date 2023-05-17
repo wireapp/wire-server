b58139cb74b62cad8236da27aa29403d5ee2543b
Akshay Mankar <akshay@wire.com>
2023-05-17 11:21:26 +0200

    Forgotten rename

--- services/background-worker/exec/Main.hs
+++ services/background-worker/exec/Main.hs
@@ -7,7 +7,7 @@ import Wire.BackgroundWorker
 
 main :: IO ()
 main = withOpenSSL $ do
-  let desc = "Backend Notification Pusher"
+  let desc = "Background Worker"
       defaultPath = "/etc/wire/background-worker/conf/background-worker.yaml"
   options <- getOptions desc Nothing defaultPath
   run options
