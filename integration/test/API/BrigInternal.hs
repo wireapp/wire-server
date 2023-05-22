af21829ba8fb3eca64e1c4e8d4fd5f9727ddfc36
Matthias Fischmann <mf@zerobuzz.net>
2023-05-22 14:22:22 +0200

    ...

--- integration/test/API/BrigInternal.hs
+++ integration/test/API/BrigInternal.hs
@@ -1,10 +1,10 @@
 module API.BrigInternal where
 
 import API.Common
+import qualified Data.Aeson as Aeson
 import Data.Function
 import Data.Maybe
 import Testlib.Prelude
-import qualified Data.Aeson as Aeson
 
 data CreateUser = CreateUser
   { email :: Maybe String,
@@ -54,63 +54,76 @@ data FedConn = FedConn
 instance ToJSON FedConn where
   toJSON (FedConn d s) =
     Aeson.object
-      [ "domain" .= d
-      , "search_policy" .= s
+      [ "domain" .= d,
+        "search_policy" .= s
       ]
+
+instance FromJSON FedConn where
+  parseJSON = withObject "FedConn" $ \obj -> do
+    FedConn
+      <$> obj .: fromString "domain"
+      <*> obj .: fromString "search_policy"
 
 createFedConn :: HasCallStack => FedConn -> App Response
 createFedConn fedConn = do
-  dom <- ownDomain
-  req <- rawBaseRequest dom Brig Versioned "/i/federation/remotes"
-  res <- submit "POST" $ req & addJSON fedConn
+  res <- createFedConn' fedConn
   res.status `shouldMatchRange` (200, 299)
   pure res
 
-{-
-  post (brig . paths ["i", "federation", "remotes"] . contentJson . json remote . expect2xx)
--}
+createFedConn' :: HasCallStack => FedConn -> App Response
+createFedConn' fedConn = do
+  owndom <- ownDomain
+  req <- rawBaseRequest owndom Brig Versioned "/i/federation/remotes"
+  submit "POST" $ req & addJSON fedConn
 
 readFedConns :: HasCallStack => App Response
 readFedConns = do
-  dom <- ownDomain
-  req <- rawBaseRequest dom Brig Versioned "/i/federation/remotes"
-  res <- submit "GET" req
+  res <- readFedConns'
   res.status `shouldMatchRange` (200, 299)
-  pure re
+  pure res
 
-{-
-  remotes . responseJsonUnsafe <$> do
-    get (brig . paths ["i", "federation", "remotes"] . contentJson . expect2xx)
--}
+readFedConns' :: HasCallStack => App Response
+readFedConns' = do
+  owndom <- ownDomain
+  req <- rawBaseRequest owndom Brig Versioned "/i/federation/remotes"
+  submit "GET" req
 
 updateFedConn :: HasCallStack => String -> FedConn -> App Response
-updateFedConn _dom _fedConn = do
-  dom <- ownDomain
-  req <- rawBaseRequest
+updateFedConn dom fedConn = do
+  res <- updateFedConn' dom fedConn
+  res.status `shouldMatchRange` (200, 299)
+  pure res
+
+updateFedConn' :: HasCallStack => String -> FedConn -> App Response
+updateFedConn' dom fedConn = do
+  owndom <- ownDomain
+  req <- rawBaseRequest owndom Brig Versioned ("/i/federation/remotes" <> dom)
+  submit "PUT" (fedConn `addJSON` req)
 
 deleteFedConn :: HasCallStack => String -> App Response
-deleteFedConn _dom = undefined
+deleteFedConn dom = do
+  res <- deleteFedConn' dom
+  res.status `shouldMatchRange` (200, 299)
+  pure res
+
+deleteFedConn' :: HasCallStack => String -> App Response
+deleteFedConn' dom = do
+  owndom <- ownDomain
+  req <- rawBaseRequest owndom Brig Versioned ("/i/federation/remotes" <> dom)
+  submit "DELETE" req
 
 resetFedConns :: HasCallStack => App Response
-resetFedConns = undefined
-
-{-
-updateFederationRemote' :: (Request -> Request) -> Brig -> Domain -> FederationDomainConfig -> Http ResponseLBS
-updateFederationRemote' mods brig rdom remote =
-  put (brig . paths ["i", "federation", "remotes", toByteString' rdom] . contentJson . json remote . mods)
-
-deleteFederationRemote' :: (Request -> Request) -> Brig -> Domain -> Http ResponseLBS
-deleteFederationRemote' mods brig rdom =
-  delete (brig . paths ["i", "federation", "remotes", toByteString' rdom] . contentJson . mods)
-
--- this one needs to go elsewhere
-resetFederationRemotes :: Opts -> Brig -> Http ()
-resetFederationRemotes opts brig = do
-  rs <- getFederationRemotes brig
-  -- Filter out domains that are in the config file.
-  -- These values can't be deleted yet, so don't even try.
-  forM_ (notCfgRemotes rs) $ \(FederationDomainConfig rdom _) -> deleteFederationRemote brig rdom
-  where
-    cfgRemotes = fromMaybe [] . Opt.setFederationDomainConfigs $ Opt.optSettings opts
-    notCfgRemotes = filter (`notElem` cfgRemotes)
--}
+resetFedConns = do
+  {-
+  -- this one needs to go elsewhere
+  resetFederationRemotes :: Opts -> Brig -> Http ()
+  resetFederationRemotes opts brig = do
+    rs <- getFederationRemotes brig
+    -- Filter out domains that are in the config file.
+    -- These values can't be deleted yet, so don't even try.
+    forM_ (notCfgRemotes rs) $ \(FederationDomainConfig rdom _) -> deleteFederationRemote brig rdom
+    where
+      cfgRemotes = fromMaybe [] . Opt.setFederationDomainConfigs $ Opt.optSettings opts
+      notCfgRemotes = filter (`notElem` cfgRemotes)
+  -}
+  pure undefined
