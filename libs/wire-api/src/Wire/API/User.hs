b05b8eaa1d27e8c4976f0a00d41dde295a0dd330
Matthias Fischmann <mf@zerobuzz.net>
2023-06-07 12:06:01 +0200

    Refactor: move `AccountStatus` and friends to wire-api.

--- libs/wire-api/src/Wire/API/User.hs
+++ libs/wire-api/src/Wire/API/User.hs
@@ -101,6 +101,11 @@ module Wire.API.User
     DeleteUserResponse (..),
     DeleteUserResult (..),
 
+    -- * Account Status
+    AccountStatus (..),
+    AccountStatusUpdate (..),
+    AccountStatusResp (..),
+
     -- * List Users
     ListUsersQuery (..),
 
@@ -146,6 +151,7 @@ import Data.Qualified
 import Data.Range
 import Data.SOP
 import Data.Schema
+import qualified Data.Schema as Schema
 import qualified Data.Set as Set
 import qualified Data.Swagger as S
 import qualified Data.Text as T
@@ -1500,6 +1506,53 @@ instance S.ToSchema ListUsersQuery where
           & S.description ?~ "exactly one of qualified_ids or qualified_handles must be provided."
           & S.properties .~ InsOrdHashMap.fromList [("qualified_ids", uids), ("qualified_handles", handles)]
           & S.example ?~ toJSON (ListUsersByIds [Qualified (Id UUID.nil) (Domain "example.com")])
+
+-------------------------------------------------------------------------------
+-- AccountStatus
+
+data AccountStatus
+  = Active
+  | Suspended
+  | Deleted
+  | Ephemeral
+  | -- | for most intents & purposes, this is another form of inactive.  it is used for
+    -- allowing scim to find users that have not accepted their invitation yet after
+    -- creating via scim.
+    PendingInvitation
+  deriving (Eq, Show, Generic)
+  deriving (Arbitrary) via (GenericUniform AccountStatus)
+  deriving (ToJSON, FromJSON, S.ToSchema) via Schema.Schema AccountStatus
+
+instance Schema.ToSchema AccountStatus where
+  schema =
+    Schema.enum @Text "AccountStatus" $
+      mconcat
+        [ Schema.element "active" Active,
+          Schema.element "suspended" Suspended,
+          Schema.element "deleted" Deleted,
+          Schema.element "ephemeral" Ephemeral,
+          Schema.element "pending-invitation" PendingInvitation
+        ]
+
+data AccountStatusResp = AccountStatusResp {fromAccountStatusResp :: AccountStatus}
+  deriving (Eq, Show, Generic)
+  deriving (Arbitrary) via (GenericUniform AccountStatusResp)
+  deriving (ToJSON, FromJSON, S.ToSchema) via Schema.Schema AccountStatusResp
+
+instance Schema.ToSchema AccountStatusResp where
+  schema =
+    object "AccountStatusResp" $
+      AccountStatusResp <$> fromAccountStatusResp .= field "status" schema
+
+newtype AccountStatusUpdate = AccountStatusUpdate {suStatus :: AccountStatus}
+  deriving (Generic)
+  deriving (Arbitrary) via (GenericUniform AccountStatusUpdate)
+  deriving (ToJSON, FromJSON, S.ToSchema) via Schema.Schema AccountStatusUpdate
+
+instance Schema.ToSchema AccountStatusUpdate where
+  schema =
+    object "AccountStatusUpdate" $
+      AccountStatusUpdate <$> suStatus .= field "status" schema
 
 -----------------------------------------------------------------------------
 -- SndFactorPasswordChallenge
