7f24aea43585dc4c96f2f9dc557f743c6a9d77c1
Leif Battermann <leif.battermann@wire.com>
2023-06-08 09:51:36 +0000

    remove exec_until_ready

--- integration/scripts/integration-dynamic-backends-ses.sh
+++ integration/scripts/integration-dynamic-backends-ses.sh
@@ -13,7 +13,7 @@ aws configure set region "$AWS_REGION"
 for i in $(seq "$INTEGRATION_DYNAMIC_BACKENDS_POOLSIZE"); do
     suffix=$((i + 2))
     # Verify sender's email address (ensure the sender address is in sync with the config in brig)
-    exec_until_ready "aws --endpoint-url=$ENDPOINT_URL ses verify-email-identity --email-address backend-integration$suffix@wire.com"
+    aws --endpoint-url=$ENDPOINT_URL ses verify-email-identity --email-address backend-integration$suffix@wire.com
 done
 
 echo 'AWS ses created successfully!'
