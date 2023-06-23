8d8242a85a74e3f0cabaec3ce2dcdc68909b7c02
Leif Battermann <leif.battermann@wire.com>
2023-06-23 10:55:07 +0000

    correct error in case of allowDynamic

--- services/galley/src/Galley/API/Action.hs
+++ services/galley/src/Galley/API/Action.hs
@@ -752,7 +752,12 @@ notifyConversationAction tag quid notifyOrigDomain con lconv targets action = do
     for_ failedNotifies $ \case
       -- rethrow invalid-domain errors and mis-configured federation errors
       (_, ex@(FederationCallFailure (FederatorClientError (Wai.Error (Wai.Status 422 _) _ _ _)))) -> throw ex
+      -- FUTUREWORK: This error occurs when federation strategy is set to `allowDynamic`
+      -- and the remote domain is not in the allow list
+      -- Is it ok to throw all 400 errors?
+      (_, ex@(FederationCallFailure (FederatorClientError (Wai.Error (Wai.Status 400 _) _ _ _)))) -> throw ex
       (_, ex@(FederationCallFailure (FederatorClientHTTP2Error (FederatorClientConnectionError _)))) -> throw ex
+      -- FUTUREWORK: Do we really want to ignore any other errors?
       _ -> pure ()
     updates <-
       E.runFederatedConcurrentlyEither (toList (bmRemotes targets)) $
