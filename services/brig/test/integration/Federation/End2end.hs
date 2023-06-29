c09cb4bd9114aaa248d135d57f30caa16d9ed41c
Igor Ranieri <igor@elland.me>
2023-06-29 12:28:27 +0200

    Bumped timeout

--- services/brig/test/integration/Federation/End2end.hs
+++ services/brig/test/integration/Federation/End2end.hs
@@ -714,7 +714,7 @@ testRemoteTypingIndicator brig1 brig2 galley1 galley2 cannon1 cannon2 = do
           )
           !!! const 200 === statusCode
   let checkEvent ws u s =
-        WS.assertMatch_ (5 # Second) ws $ \n -> do
+        WS.assertMatch_ (8 # Second) ws $ \n -> do
           let e = List1.head (WS.unpackPayload n)
           ntfTransient n @?= True
           evtConv e @?= cnvQualifiedId cnv
