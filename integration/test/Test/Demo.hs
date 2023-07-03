b326f40d2ef4992677954f986bd323e90d8fd489
Leif Battermann <leif.battermann@wire.com>
2023-07-03 10:58:49 +0000

    removed unused import

--- integration/test/Test/Demo.hs
+++ integration/test/Test/Demo.hs
@@ -5,7 +5,6 @@ import qualified API.Brig as Public
 import qualified API.BrigInternal as Internal
 import qualified API.GalleyInternal as Internal
 import qualified API.Nginz as Nginz
-import Control.Concurrent (threadDelay)
 import Control.Monad.Cont
 import qualified Data.Map as Map
 import GHC.Stack
