3f68e9ae54d85131c5a2b46d97f86105a28ab8fd
Owen Harvey <owenlharvey@gmail.com>
2023-07-17 13:53:55 +1000

    WPB-2986: Script formatting for CI

--- services/background-worker/shutdown_test.sh
+++ services/background-worker/shutdown_test.sh
@@ -15,9 +15,9 @@
 
 progress-bar() {
   local duration=${1}
-    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
-    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
-    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
+    already_done() { for ((done=0; done<elapsed; done++)); do printf "▇"; done }
+    remaining() { for ((remain=elapsed; remain<duration; remain++)); do printf " "; done }
+    percentage() { printf "| %s%%" $(( ((elapsed)*100)/(duration)*100/100 )); }
     clean_line() { printf "\r"; }
   for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
       already_done; remaining; percentage
