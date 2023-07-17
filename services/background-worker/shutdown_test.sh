011b6be85c4e58d9273c3970be8f89add60e23bd
Owen Harvey <owenlharvey@gmail.com>
2023-07-17 14:32:36 +1000

    WPB-2986: More formatting

--- services/background-worker/shutdown_test.sh
+++ services/background-worker/shutdown_test.sh
@@ -17,7 +17,7 @@ progress-bar() {
   local duration=${1}
     already_done() { for ((done=0; done<elapsed; done++)); do printf "▇"; done }
     remaining() { for ((remain=elapsed; remain<duration; remain++)); do printf " "; done }
-    percentage() { printf "| %s%%" $(( ((elapsed)*100)/(duration)*100/100 )); }
+    percentage() { printf "| %s%%" $(( ((elapsed)*100)*100/(duration)/100 )); }
     clean_line() { printf "\r"; }
   for (( elapsed=1; elapsed<=duration; elapsed++ )); do
       already_done; remaining; percentage
