9fdeacae8062501c74d3fd35cf93e24bdc91d899
Akshay Mankar <akshay@wire.com>
2023-07-25 11:20:11 +0200

    integration-dynamic-backends-s3.sh: Create bucket only if it doesn't exist

--- integration/scripts/integration-dynamic-backends-s3.sh
+++ integration/scripts/integration-dynamic-backends-s3.sh
@@ -5,9 +5,13 @@ set -eo pipefail
 ENDPOINT_URL=$1
 
 for i in $(seq "$INTEGRATION_DYNAMIC_BACKENDS_POOLSIZE"); do
-    suffix=$((i + 2))
-    aws --endpoint-url="$ENDPOINT_URL" s3api create-bucket --bucket "dummy-bucket$suffix"
-    aws --endpoint-url="$ENDPOINT_URL" s3api wait bucket-exists --bucket "dummy-bucket$suffix"
+    bucket="dummy-bucket$((i + 2))"
+    if aws --endpoint-url="$ENDPOINT_URL" s3api head-bucket --bucket "$bucket"; then
+        echo "AWS s3-bucket '$bucket' already exists"
+    else
+        aws --endpoint-url="$ENDPOINT_URL" s3api create-bucket --bucket "$bucket"
+        aws --endpoint-url="$ENDPOINT_URL" s3api wait bucket-exists --bucket "$bucket"
+    fi
 done
 
-echo 'AWS s3-buckets queues created successfully!'
+echo 'AWS s3 buckets created successfully!'
