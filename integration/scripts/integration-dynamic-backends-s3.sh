ed34c53843d36c2585292252ee9d60734cdf2c0b
Akshay Mankar <akshay@wire.com>
2023-07-25 10:51:37 +0200

    integration/scripts: No need to run `aws configure`
    Running `aws configure` doesn't work because the image is setup to be run as
    `nobody` which cannot write to its `$HOME` at `/var/empty`. However, running
    `aws configure` is unnecessary given we expect the scripts to run when
    `$AWS_ACCESS_KEY_ID`, `$AWS_SECRET_ACCESS_KEY` and `$AWS_REGION` are already
    set.

--- integration/scripts/integration-dynamic-backends-s3.sh
+++ integration/scripts/integration-dynamic-backends-s3.sh
@@ -4,12 +4,6 @@ set -eo pipefail
 
 ENDPOINT_URL=$1
 
-# Assumes this to be run in an environment with `aws` installed
-echo 'Creating AWS resources'
-aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-aws configure set region "$AWS_REGION"
-
 for i in $(seq "$INTEGRATION_DYNAMIC_BACKENDS_POOLSIZE"); do
     suffix=$((i + 2))
     aws --endpoint-url="$ENDPOINT_URL" s3api create-bucket --bucket "dummy-bucket$suffix"
