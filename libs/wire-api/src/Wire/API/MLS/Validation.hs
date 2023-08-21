7932a1b9c49860b768d599a81c07790cb3a302d3
Paolo Capriotti <paolo@capriotti.io>
2023-08-21 13:18:50 +0200

    Linter fixes

--- libs/wire-api/src/Wire/API/MLS/Validation.hs
+++ libs/wire-api/src/Wire/API/MLS/Validation.hs
@@ -23,9 +23,9 @@ module Wire.API.MLS.Validation
 where
 
 import Control.Applicative
-import qualified Data.Text.Lazy as LT
-import qualified Data.Text.Lazy.Builder as LT
-import qualified Data.Text.Lazy.Builder.Int as LT
+import Data.Text.Lazy qualified as LT
+import Data.Text.Lazy.Builder qualified as LT
+import Data.Text.Lazy.Builder.Int qualified as LT
 import Imports hiding (cs)
 import Wire.API.MLS.Capabilities
 import Wire.API.MLS.CipherSuite
