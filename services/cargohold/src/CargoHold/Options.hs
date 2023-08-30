afd404d61f7551376974fc3d7754b86f3ba3fcdf
Stefan Matting <stefan@wire.com>
2023-08-30 12:08:11 +0200

    Fix incorrect qualified import

--- services/cargohold/src/CargoHold/Options.hs
+++ services/cargohold/src/CargoHold/Options.hs
@@ -21,7 +21,7 @@
 module CargoHold.Options where
 
 import Amazonka (S3AddressingStyle (..))
-import CargoHold.CloudFront qualified as CF
+import qualified CargoHold.CloudFront as CF
 import Control.Lens hiding (Level)
 import Data.Aeson (FromJSON (..), withText)
 import Data.Aeson.TH
