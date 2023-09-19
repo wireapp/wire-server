2763521f1a414ba48b59a3def874844ec683c7e1
Akshay Mankar <akshay@wire.com>
2023-09-19 16:19:34 +0200

    background-notification-pusher: Better function name

--- services/background-worker/src/Wire/BackendNotificationPusher.hs
+++ services/background-worker/src/Wire/BackendNotificationPusher.hs
@@ -11,7 +11,6 @@ import Data.Map.Strict qualified as Map
 import Data.Set qualified as Set
 import Data.Text qualified as Text
 import Imports
-import Network.AMQP (cancelConsumer)
 import Network.AMQP qualified as Q
 import Network.AMQP.Extended
 import Network.AMQP.Lifted qualified as QL
@@ -117,7 +116,7 @@ startPusher consumersRef chan = do
   let cleanup :: (Exception e, MonadThrow m, MonadIO m) => e -> m ()
       cleanup e = do
         consumers <- liftIO $ readIORef consumersRef
-        traverse_ (liftIO . cancelConsumer chan . fst) $ Map.elems consumers
+        traverse_ (liftIO . Q.cancelConsumer chan . fst) $ Map.elems consumers
         throwM e
 
   -- If this thread is cancelled, catch the exception, kill the consumers, and carry on.
@@ -145,10 +144,10 @@ ensureConsumers consumers chan domains = do
   traverse_ (ensureConsumer consumers chan) domains
   -- Loop over all of the dropped domains. These need to be cancelled as they are no longer
   -- on the domain list.
-  traverse_ (cancelConsumer' consumers chan) droppedDomains
+  traverse_ (cancelConsumer consumers chan) droppedDomains
 
-cancelConsumer' :: IORef (Map Domain (Q.ConsumerTag, MVar ())) -> Q.Channel -> Domain -> AppT IO ()
-cancelConsumer' consumers chan domain = do
+cancelConsumer :: IORef (Map Domain (Q.ConsumerTag, MVar ())) -> Q.Channel -> Domain -> AppT IO ()
+cancelConsumer consumers chan domain = do
   Log.info $ Log.msg (Log.val "Stopping consumer") . Log.field "domain" (domainText domain)
   -- The ' version of atomicModifyIORef is strict in the function update and is useful
   -- for not leaking memory.
