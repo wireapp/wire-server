e5c8baa1252a77d36c6ea03234b580ced7064d40
Paolo Capriotti <paolo@capriotti.io>
2023-09-26 09:49:02 +0200

    Fix warning

--- integration/test/Test/Conversation.hs
+++ integration/test/Test/Conversation.hs
@@ -23,7 +23,6 @@ import API.Brig
 import API.BrigInternal
 import API.Galley
 import API.GalleyInternal
-import API.Gundeck
 import Control.Applicative
 import Control.Concurrent (threadDelay)
 import Control.Monad.Codensity
