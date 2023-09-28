56d3b380b12863a281d9eab922b68df49d9301c4
Leif Battermann <leif.battermann@wire.com>
2023-09-28 13:44:42 +0000

    use uuid

--- hack/bin/integration-test.sh
+++ hack/bin/integration-test.sh
@@ -25,10 +25,11 @@ cleanup() {
 # Copy to the concourse output (indetified by $OUTPUT_DIR) for propagation to
 # following steps.
 copyToAwsS3(){
+    build_uuid = uuidgen
     if (( UPLOAD_LOGS > 0 )); then
         for t in "${tests[@]}"; do
-            echo "Copy logs-$t to s3://wire-server-test-logs/test-logs-$VERSION/$t-$VERSION/$BUILD_ID.log"
-            aws s3 cp "logs-$t" "s3://wire-server-test-logs/test-logs-$VERSION/$t-$VERSION/$BUILD_ID.log"
+            echo "Copy logs-$t to s3://wire-server-test-logs/test-logs-$VERSION/$t-$VERSION/$build_uuid.log"
+            aws s3 cp "logs-$t" "s3://wire-server-test-logs/test-logs-$VERSION/$t-$VERSION/$build_uuid.log"
         done
     fi
 }
