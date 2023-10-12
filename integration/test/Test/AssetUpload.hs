1803dc5dc2b75496d4134bc338fdc915fcfdc210
Owen Harvey <owenlharvey@gmail.com>
2023-10-12 11:53:49 +1000

    WPB-1906: Adding an additional test for unknown users

--- integration/test/Test/AssetUpload.hs
+++ integration/test/Test/AssetUpload.hs
@@ -16,3 +16,19 @@ testAssetUploadVerifiedUser = do
   user <- randomUser OwnDomain def
   bindResponse (uploadAsset user) $ \resp -> do
     resp.status `shouldMatchInt` 201
+
+testAssetUploadUnknownUser :: HasCallStack => App ()
+testAssetUploadUnknownUser = do
+  uid <- randomId
+  domain <- make OwnDomain
+  let user =
+        object
+          [ "id" .= uid,
+            "qualified_id"
+              .= object
+                [ "domain" .= domain,
+                  "id" .= uid
+                ]
+          ]
+  bindResponse (uploadAsset user) $ \resp -> do
+    resp.status `shouldMatchInt` 403
