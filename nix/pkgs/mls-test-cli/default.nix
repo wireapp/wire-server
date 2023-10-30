2511d87cc21e5452e3479b5092243b911e8552af
Paolo Capriotti <paolo@capriotti.io>
2023-10-30 15:12:15 +0100

    Update group state after application messages (#3678)
    After an application message the ratchet is updated, therefore we need to save the updated group state so that future messages are generated correctly.
    
    This commit includes an mls-test-cli update. The new mls-test-cli version modifies the `message` command to include both `group-in` and `group-out` options, as other similar commands already do.

--- nix/pkgs/mls-test-cli/default.nix
+++ nix/pkgs/mls-test-cli/default.nix
@@ -11,8 +11,8 @@ let
   src = fetchFromGitHub {
     owner = "wireapp";
     repo = "mls-test-cli";
-    rev = "e6e6ce0c29f0e48e84b4ccef058130aca0625492";
-    sha256 = "sha256-J9M8w3GJnULH3spKEuPGCL/t43zb2Wd+YfZ0LY3YITo=";
+    rev = "baaa5c78411a5bf6d697803276b991523c111631";
+    sha256 = "sha256-M6bWB5hWl+WSblcH6L+AyGD+7ef9TvRs8wKYq7lJyS8=";
   };
   cargoLockFile = builtins.toFile "cargo.lock" (builtins.readFile "${src}/Cargo.lock");
 in rustPlatform.buildRustPackage rec {
@@ -23,7 +23,7 @@ in rustPlatform.buildRustPackage rec {
     lockFile = cargoLockFile;
     outputHashes = {
       "hpke-0.10.0" = "sha256-T1+BFwX6allljNZ/8T3mrWhOejnUU27BiWQetqU+0fY=";
-      "openmls-1.0.0" = "sha256-s1ejM/aicFGvsKY7ajEun1Mc645/k8QVrE8YSbyD3Fg=";
+      "openmls-1.0.0" = "sha256-tAIm8+IgubNnU2M2A5cxHY5caiEQmisw73I9/cqfvUc=";
       "safe_pqc_kyber-0.6.0" = "sha256-Ch1LA+by+ezf5RV0LDSQGC1o+IWKXk8IPvkwSrAos68=";
       "tls_codec-0.3.0" = "sha256-IO6tenXKkC14EoUDp/+DtFNOVzDfOlLu8K1EJI7sOzs=";
     };
