abd8c6df8b63c6a68fbd8dc648d82701a634ea39
Igor Ranieri <igor@elland.me>
2023-11-07 10:51:37 +0100

    Use `password` instead of `scrypt`.

--- libs/wire-api/src/Wire/API/Password.hs
+++ libs/wire-api/src/Wire/API/Password.hs
@@ -24,17 +24,18 @@ module Wire.API.Password
 where
 
 import Cassandra
-import Crypto.Scrypt
 import Data.ByteString.Base64 qualified as B64
-import Data.ByteString.Lazy (fromStrict, toStrict)
+import Data.ByteString.Conversion (toByteString)
+import Data.ByteString.Lazy (toStrict)
 import Data.Misc
+import Data.Password.Scrypt qualified as Scrypt
 import Data.Text.Encoding qualified as Text
 import Imports
 import OpenSSL.Random (randBytes)
 
 -- | A derived, stretched password that can be safely stored.
 newtype Password = Password
-  {fromPassword :: EncryptedPass}
+  {fromPassword :: Scrypt.PasswordHash Scrypt.Scrypt}
 
 instance Show Password where
   show _ = "<Password>"
@@ -42,10 +43,10 @@ instance Show Password where
 instance Cql Password where
   ctype = Tagged BlobColumn
 
-  fromCql (CqlBlob lbs) = pure . Password . EncryptedPass $ toStrict lbs
+  fromCql (CqlBlob lbs) = pure . Password . Scrypt.PasswordHash . Text.decodeUtf8 $ toStrict lbs
   fromCql _ = Left "password: expected blob"
 
-  toCql = CqlBlob . fromStrict . getEncryptedPass . fromPassword
+  toCql = CqlBlob . toByteString . Scrypt.unPasswordHash . fromPassword
 
 -- | Generate a strong, random plaintext password of length 16
 -- containing only alphanumeric characters, '+' and '/'.
@@ -56,14 +57,17 @@ genPassword =
 
 -- | Stretch a plaintext password so that it can be safely stored.
 mkSafePassword :: MonadIO m => PlainTextPassword' t -> m Password
-mkSafePassword = liftIO . fmap Password . encryptPassIO' . pass
+mkSafePassword = liftIO . fmap Password . Scrypt.hashPassword . pass
   where
-    pass = Pass . Text.encodeUtf8 . fromPlainTextPassword
+    pass = Scrypt.mkPassword . fromPlainTextPassword
 
 -- | Verify a plaintext password from user input against a stretched
 -- password from persistent storage.
 verifyPassword :: PlainTextPassword' t -> Password -> Bool
 verifyPassword plain opaque =
-  let actual = Pass . Text.encodeUtf8 $ fromPlainTextPassword plain
+  let actual = Scrypt.mkPassword $ fromPlainTextPassword plain
       expected = fromPassword opaque
-   in verifyPass' actual expected
+      checkToBool = \case
+        Scrypt.PasswordCheckFail -> False
+        Scrypt.PasswordCheckSuccess -> True
+   in checkToBool $ Scrypt.checkPassword actual expected
