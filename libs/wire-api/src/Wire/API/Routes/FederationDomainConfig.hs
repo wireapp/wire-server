00923ccb9bfa4365f799eba0126f41da886c1156
Leif Battermann <leif.battermann@wire.com>
2023-11-16 11:27:32 +0000

    fed config store

--- libs/wire-api/src/Wire/API/Routes/FederationDomainConfig.hs
+++ libs/wire-api/src/Wire/API/Routes/FederationDomainConfig.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StrictData #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 -- This file is part of the Wire Server implementation.
 --
--- Copyright (C) 2022 Wire Swiss GmbH <opensource@wire.com>
+-- Copyright (C) 2023 Wire Swiss GmbH <opensource@wire.com>
 --
 -- This program is free software: you can redistribute it and/or modify it under
 -- the terms of the GNU Affero General Public License as published by the Free
@@ -25,7 +29,7 @@ module Wire.API.Routes.FederationDomainConfig
   )
 where
 
-import Control.Lens ((?~))
+import Control.Lens (makePrisms, (?~))
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Domain (Domain)
 import Data.Id
@@ -36,25 +40,32 @@ import Imports
 import Wire.API.User.Search (FederatedUserSearchPolicy)
 import Wire.Arbitrary (Arbitrary, GenericUniform (..))
 
-data FederationRestriction = FederationRestrictionAllowAll | FederationRestrictionByTeam
+data FederationRestriction
+  = FederationRestrictionAllowAll
+  | FederationRestrictionByTeam [TeamId]
   deriving (Eq, Show, Generic, Ord)
-  deriving (ToJSON, FromJSON, S.ToSchema) via Schema FederationRestriction
   deriving (Arbitrary) via (GenericUniform FederationRestriction)
+
+makePrisms ''FederationRestriction
 
 instance ToSchema FederationRestriction where
   schema =
-    enum @Text "FederationRestriction" $
-      mconcat
-        [ element "allow_all" FederationRestrictionAllowAll,
-          element "restrict_by_team" FederationRestrictionByTeam
-        ]
+    named "FederationRestriction" $
+      tag _FederationRestrictionAllowAll null_
+        <> tag _FederationRestrictionByTeam (array (unnamed schema))
+
+deriving via (Schema FederationRestriction) instance FromJSON FederationRestriction
+
+deriving via (Schema FederationRestriction) instance ToJSON FederationRestriction
+
+deriving via (Schema FederationRestriction) instance S.ToSchema FederationRestriction
 
 -- | Everything we need to know about a remote instance in order to federate with it.  Comes
 -- in `AllowedDomains` if `AllowStrategy` is `AllowDynamic`.  If `AllowAll`, we still use this
 -- information for search policy.
 data FederationDomainConfig = FederationDomainConfig
   { domain :: Domain,
-    cfgSearchPolicy :: FederatedUserSearchPolicy,
+    searchPolicy :: FederatedUserSearchPolicy,
     restriction :: FederationRestriction
   }
   deriving (Eq, Ord, Show, Generic)
@@ -66,7 +77,7 @@ instance ToSchema FederationDomainConfig where
     object "FederationDomainConfig" $
       FederationDomainConfig
         <$> domain .= field "domain" schema
-        <*> cfgSearchPolicy .= field "search_policy" schema
+        <*> searchPolicy .= field "search_policy" schema
         <*> restriction .= field "restriction" schema
 
 data FederationDomainConfigs = FederationDomainConfigs
