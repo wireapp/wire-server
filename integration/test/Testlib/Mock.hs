0be8b9f9ef5234dd58065459a01e96db587fa4cb
Paolo Capriotti <paolo@capriotti.io>
2023-11-29 10:06:07 +0100

    Fix root path in integration Mock

--- integration/test/Testlib/Mock.hs
+++ integration/test/Testlib/Mock.hs
@@ -4,6 +4,7 @@ import Control.Concurrent.Async
 import Control.Concurrent.MVar
 import Control.Exception
 import Control.Monad.Codensity
+import Control.Monad.Reader
 import Data.Streaming.Network
 import Network.Socket qualified as Socket
 import Network.Wai qualified as Wai
@@ -24,38 +25,53 @@ instance Default MockServerConfig where
 
 startMockServer :: MockServerConfig -> Wai.Application -> Codensity App Warp.Port
 startMockServer config app = do
-  (port, sock) <- liftIO $ case config.port of
-    Nothing -> bindRandomPortTCP (fromString "*6")
-    Just n -> (n,) <$> bindPortTCP n (fromString "*6")
+  let closeSocket sock = catch (Socket.close sock) (\(_ :: SomeException) -> pure ())
+  (port, sock) <- Codensity $ \k -> do
+    action <- appToIOKleisli k
+    liftIO $
+      bracket
+        ( case config.port of
+            Nothing -> bindRandomPortTCP (fromString "*6")
+            Just n -> (n,) <$> bindPortTCP n (fromString "*6")
+        )
+        (\(_, sock) -> closeSocket sock)
+        action
   serverStarted <- liftIO newEmptyMVar
   let wsettings =
         Warp.defaultSettings
           & Warp.setPort port
           & Warp.setGracefulCloseTimeout2 0
-          & Warp.setBeforeMainLoop (putMVar serverStarted ())
+          & Warp.setBeforeMainLoop (putMVar serverStarted Nothing)
 
   -- Action to start server in a separate thread.
-  let startServer = async $ do
+  base <- fromMaybe "." <$> asks (.servicesCwdBase)
+  let startServer = do
         if config.tls
           then
             Warp.runTLSSocket
               ( Warp.tlsSettings
-                  "services/federator/test/resources/integration-leaf.pem"
-                  "services/federator/test/resources/integration-leaf-key.pem"
+                  (base <> "/federator/test/resources/integration-leaf.pem")
+                  (base <> "/federator/test/resources/integration-leaf-key.pem")
               )
               wsettings
               sock
               app
           else Warp.runSettingsSocket wsettings sock app
+  let startServerAsync = do
+        a <- async $ do
+          catch startServer $ \(e :: SomeException) ->
+            void $ tryPutMVar serverStarted (Just e)
+        mException <- readMVar serverStarted
+        traverse_ throw mException
+        pure a
 
   Codensity $ \k -> do
     action <- appToIO (k ())
     liftIO
       $ bracket
-        (startServer <* readMVar serverStarted)
+        startServerAsync
         ( \serverAsync -> do
-            -- close the socket
-            catch (Socket.close sock) (\(_ :: SomeException) -> pure ())
+            closeSocket sock
             -- kill the thread running the server
             cancel serverAsync
         )
