d0a8bb24c211d83e73a253f44209c507b75f9aff
Leif Battermann <leif.battermann@wire.com>
2023-12-13 17:03:12 +0000

    loglevel

--- integration/test/Testlib/ModService.hs
+++ integration/test/Testlib/ModService.hs
@@ -216,7 +216,7 @@ startDynamicBackend resource beOverrides = do
     setLogLevel =
       def
         { sparCfg = setField "saml.logLevel" ("Warn" :: String),
-          brigCfg = setField "logLevel" ("Info" :: String),
+          brigCfg = setField "logLevel" ("Warn" :: String),
           cannonCfg = setField "logLevel" ("Warn" :: String),
           cargoholdCfg = setField "logLevel" ("Warn" :: String),
           galleyCfg = setField "logLevel" ("Warn" :: String),
