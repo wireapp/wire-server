e6b8000763192959a2d1f5712b2a44b57f12dde5
Akshay Mankar <akshay@wire.com>
2023-12-14 18:03:48 +0100

    WIP more

--- services/brig/src/Brig/Search.hs
+++ services/brig/src/Brig/Search.hs
@@ -3,15 +3,13 @@
 
 module Brig.Search where
 
-import Brig.User.Search.Index
 import Data.Domain
 import Data.Id hiding (UserId)
 import Data.Range
 import Debug.Trace
 import Imports
 import Polysemy
-import Polysemy.Internal
-import Polysemy.Internal.Union
+import Wire.API.User (UserProfile)
 import Wire.API.User.Search
 
 type UserId = String
@@ -24,8 +22,7 @@ type UserId = String
 --
 -- User -> Client
 -- Client -> User
--- User1 -> Client -> User2
-
+--
 -- User, Client, Conversation, Search, Notification, Team, Connection, Federation, Asset
 --
 -- deleteUser ->
@@ -52,8 +49,8 @@ data Search m a where
     Maybe (Range 1 500 Int32) ->
     Maybe PagingState ->
     Search m (SearchResult TeamContact)
+  ReindexUser :: UserId -> Search m ()
   RefreshIndex :: Search m ()
-  ReindexUser :: IndexUser -> Search m ()
 
 makeSem ''Search
 
@@ -61,12 +58,27 @@ data User m a where
   -- GetIndexUser :: UserId -> User m IndexUser
   UserFoo :: String -> User m ()
   UserBar :: String -> User m ()
+  GetUserById :: UserId -> User m UserProfile
 
 makeSem ''User
 
--- runSearch :: Member User r => Sem (Search ': r) a -> Sem r a
--- runSearch = interpret $ \case
---   Search ->
+esReindex :: UserProfile -> IO ()
+esReindex = undefined
+
+runSearch :: (Member User r, Member (Embed IO) r) => Sem (Search ': r) a -> Sem r a
+runSearch = interpret $ \case
+  ReindexUser uid -> embed . esReindex =<< getUserById uid
+  _ -> undefined
+
+runUser :: Member Search r => Sem (User ': r) a -> Sem r a
+runUser = interpret $ \case
+  UserFoo uid -> reindexUser uid
+  _ -> undefined
+
+-- runSearchAndUser ::
+
+-- runBrig :: Sem (User ': Search ': r) a -> Sem r a
+-- runBrig = runUser . runSearch
 
 -- Serach -> User
 -- User -> Search
@@ -90,45 +102,81 @@ makeSem ''Client
 
 -- Client User
 
-interpretClient :: Member User r => Sem (Client ': r) a -> Sem r a
-interpretClient = interpret $ \case
+interpretClient :: InterpreterFor User r -> Sem (Client ': r) a -> Sem r a
+interpretClient iu = interpret $ \case
   ClientFoo _ -> do
     traceM "ClientFoo"
     pure ()
   ClientBar uid -> do
     traceM "ClientBar"
-    userBar uid
+    iu $ userBar uid
 
-interpretUser :: (Member Client r) => Sem (User ': r) a -> Sem r a
-interpretUser = interpret $ \case
+-- data InterpretterStack =
+
+interpretUser :: InterpreterFor Client r -> Sem (User ': r) a -> Sem r a
+interpretUser ic = interpret $ \case
   UserFoo uid -> do
     traceM "UserFoo"
-    clientFoo uid
+    ic $ clientFoo uid
   UserBar uid -> do
     traceM "UserBar"
-    clientFoo uid
+    ic $ clientFoo uid
+
+userBarImpl :: Member (Client) r => UserId -> Sem r ()
+userBarImpl = clientFoo
+
+-- interpretCientUser' :: (forall r'. Member Client r' => Sem (User ': r') a -> Sem r' a) -> Sem (Client : r) a -> Sem r a
+-- interpretCientUser' iu = interpret $ \case
+--   Client
 
 -- >>> run $ interpretClientUser $ userFoo "fasdf"
-interpretClientUser :: Sem (Client ': User ': r) a -> Sem r a
-interpretClientUser action =
-  loopIfRequired $
-    -- Client
-    interpretUser $
-      -- User Client
-      raiseUnder $
-        -- User
-        interpretClient action
+-- interpretClientUser :: Sem (Client ': User ': r) a -> Sem r a
+-- interpretClientUser action =
+--   interpretClientUser $
+--     raiseUnder $
+--       -- Client
+--       interpretUser $
+--         -- User, Client
+--         raiseUnder $
+--           -- User
+--           interpretClient action
 
-loopIfRequired :: Sem (Client ': User ': r) a -> Sem r a
+prodInterpretUser :: Sem (User ': r) a -> Sem r a
+prodInterpretUser = interpretUser prodInterpretClient
+
+prodInterpretClient :: Sem (Client ': r) a -> Sem r a
+prodInterpretClient = interpretClient prodInterpretUser
+
+runStack :: Sem '[Client, User, Embed IO] a -> IO a
+runStack =
+  runM . prodInterpretUser . prodInterpretClient
+
+test :: IO ()
+test = runStack $ do
+  clientBar "fasdf"
+  userFoo "foo"
+
+loopIfRequired :: Sem (Client ': r) a -> Sem r a
 loopIfRequired = undefined
 
-foo ::
-  Sem (Client ': r) a ->
-  Sem r a
-foo action@(Sem m) = Sem $ \k -> m $ \u ->
-  case decomp u of
-    Left x -> k $ hoist (interpretH f) x
-    Right _ -> interpretClientUser $ raiseUnder action
+-- foo ::
+--   -- | A natural transformation from the handled effect to other effects
+--   -- already in 'Sem'.
+--   (forall rInitial x. e (Sem rInitial) x -> Tactical e (Sem rInitial) r x) ->
+--   Sem (e ': r) a ->
+--   Sem r a
+-- foo action@(Sem m) = Sem $ \k -> m $ \u ->
+--   case decomp u of
+--     Left x -> k $ hoist (interpretH f) x
+--     Right _ -> interpretClientUser $ raiseUnder action
+
+-- foo :: Sem (Client ': r) a -> Sem r a
+-- foo action@(Sem m) = Sem $ \k -> m $ \u ->
+--   case decomp u of
+--     -- There is no Client constructors left
+--     Left _ -> undefined
+--     -- There are Client constructors
+--     Right _ -> _
 
 data Env = Env
   { envUserActions :: UserActions,
@@ -171,7 +219,7 @@ userActionBar uid = do
   f <- asks (_userActionBar . envUserActions)
   liftIO $ f uid
 
-defUserActions :: ClientActions -> UserActions
+defUserActions :: Env -> UserActions
 defUserActions e =
   UserActions
     { _userActionFoo = \uid -> runApp e $ clientActionFoo uid,
@@ -195,4 +243,52 @@ defEnv =
 runApp :: b -> ReaderT b m a -> m a
 runApp = flip runReaderT
 
-test = runApp defEnv (clientActionBar "foo")
+data Free f a = Pure a | Free (f (Free f a))
+  deriving (Functor)
+
+instance Functor f => Applicative (Free f) where
+  pure = Pure
+  (<*>) = ap
+
+instance Functor f => Monad (Free f) where
+  return = pure
+  Pure a >>= f = f a
+  Free m >>= f = Free $ fmap (>>= f) m
+
+-- instance Functor (Free f)
+
+data F a = F1 a | F2 a
+  deriving (Functor)
+
+f1 :: Free F ()
+f1 = Free $ F1 (Pure ())
+
+f2 :: Free F ()
+f2 = Free $ F2 $ Pure ()
+
+data G a = G1 a
+  deriving (Functor)
+
+g1 :: Free G ()
+g1 = Free $ G1 (Pure ())
+
+intF :: F a -> Free G a
+-- intF (F1 a) = Free $ fmap (\_ -> Pure a) (G1 (Pure ()))
+intF (F1 a) = g1 >> pure a
+intF (F2 a) = Pure a
+
+intG :: G a -> Free F a
+intG (G1 a) = f2 >> pure a
+
+-- intG (G1 a) = Free $ fmap (\_ -> Pure a) (F2 (Pure ()))
+
+runF :: Free F a -> a
+runF (Pure x) = x
+runF (Free m) = runF $ runG (intF m)
+
+runG :: Free G a -> a
+runG (Pure x) = x
+runG (Free m) = runG $ runF (intG m)
+
+example :: ()
+example = runF f1
