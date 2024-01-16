ccc53b4ee2b34579a8a8fd3cffe911443fab0f08
Sven Tennie <sven.tennie@wire.com>
2024-01-16 15:50:52 +0100

    Upload Bombon files to Deptrack
    This avoids cluttering our release artifact page. And, Security gets the
    files where they need them.

--- hack/bin/bombon.hs
+++ hack/bin/bombon.hs
@@ -1,17 +1,112 @@
-#!/usr/bin/env -S nix -Lv run github:wireapp/ghc-flakr/ecb1f45f1549e06c92d71164e305ce501eb0e36e --extra-experimental-features flakes
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE OverloadedStrings #-}
+#!/usr/bin/env -S nix -Lv run github:wireapp/ghc-flakr/a14de7ecd0c24192bc8d5cb534e8a0e21bbd91fa --extra-experimental-features flakes
+{-# LANGUAGE DataKinds #-}
 
-import Data.Text qualified as T
-import Turtle
+import Data.Aeson
+import Data.Proxy
+import GHC.Generics
+import Network.HTTP.Client qualified as HTTP
+import Options.Applicative
+import Servant.API
+import Servant.Client
 
+data Payload = Payload
+  { bom :: String,
+    projectName :: String,
+    projectVersion :: String,
+    autoCreate :: Bool
+  }
+  deriving (Generic, Show)
+
+instance ToJSON Payload
+
+data ApiResponse = ApiResponse
+  { token :: String
+  }
+  deriving (Generic, Show)
+
+instance FromJSON ApiResponse
+
+type DependenyTrackAPI =
+  "api"
+    :> "v1"
+    :> "bom"
+    :> ReqBody '[JSON] Payload
+    :> Header "X-Api-Key" String
+    :> Put '[JSON] ApiResponse
+
+api :: Proxy DependenyTrackAPI
+api = Proxy
+
+putBOM :: Payload -> Maybe String -> ClientM ApiResponse
+putBOM = client api
+
+data CliOptions = CliOptions
+  { opBomPath :: String,
+    opProjectName :: String,
+    opProjectVersion :: String,
+    opAutoCreate :: Bool,
+    opApiKey :: String
+  }
+  deriving (Show)
+
+cliParser :: Parser CliOptions
+cliParser =
+  CliOptions
+    <$> ( strOption
+            ( long "bom-filepath"
+                <> short 'f'
+                <> metavar "FILENAME"
+            )
+        )
+    <*> ( strOption
+            ( long "project-name"
+                <> short 'p'
+                <> metavar "PROJECT_NAME"
+                <> value "wire-server-ci"
+            )
+        )
+    <*> ( strOption
+            ( long "project-version"
+                <> short 'v'
+                <> metavar "PROJECT_VERSION"
+            )
+        )
+    <*> ( switch
+            ( long "auto-create"
+                <> short 'c'
+            )
+        )
+    <*> ( strOption
+            ( long "api-key"
+                <> short 'k'
+                <> metavar "API_KEY"
+            )
+        )
+
+fullCliParser :: ParserInfo CliOptions
+fullCliParser =
+  info
+    (cliParser <**> helper)
+    ( fullDesc
+        <> progDesc "Upload BOM files to deptrack"
+    )
+
+main :: IO ()
 main = do
-  (release, repo) <- options "Upload boms" do
-    (,)
-      <$> optText "release" 't' "Which release tag to upload the artifacts to"
-      <*> optText "repo" 'r' "Which repository to upload the artifacts to"
-  let bomName = "wire-server-bom-" <> release <> ".json"
-  ExitSuccess <- proc "nix" ["build", "-f", "nix", "wireServer.allLocalPackagesBom", "-o", bomName] mempty
-  printf ("uploading " % s % " to release " % s % "\n") bomName ("chart/" <> release)
-  proc "gh" ["-R", repo, "release", "upload", "chart/" <> release, bomName] mempty
+  options <- execParser fullCliParser
+  manager' <- HTTP.newManager HTTP.defaultManagerSettings
+  bom <- readFile $ opBomPath options
+  let payload =
+        Payload
+          { bom = bom,
+            projectName = opProjectName options,
+            projectVersion = opProjectVersion options,
+            autoCreate = opAutoCreate options
+          }
+  res <-
+    runClientM
+      (putBOM payload ((Just . opApiKey) options))
+      (mkClientEnv manager' (BaseUrl Https "deptrack.wire.link" 443 ""))
+  case res of
+    Left err -> print $ "Error: " ++ show err
+    Right res -> print res
