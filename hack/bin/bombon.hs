3af02894f8672125e7968069e3a9c178868d45fe
Sven Tennie <sven.tennie@wire.com>
2024-01-16 18:03:42 +0100

    deptrack needs its bom base64 encoded

--- hack/bin/bombon.hs
+++ hack/bin/bombon.hs
@@ -1,16 +1,21 @@
-#!/usr/bin/env -S nix -Lv run github:wireapp/ghc-flakr/a14de7ecd0c24192bc8d5cb534e8a0e21bbd91fa --extra-experimental-features flakes
+#!/usr/bin/env -S nix -Lv run github:wireapp/ghc-flakr/99fe5a331fdd37d52043f14e5c565ac29a30bcb4
 {-# LANGUAGE DataKinds #-}
 
 import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.ByteString.Base64.Lazy as Base64
 import Data.Proxy
 import GHC.Generics
 import Network.HTTP.Client qualified as HTTP
+import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Options.Applicative
 import Servant.API
 import Servant.Client
+import Data.Text.Lazy
+import Data.Text.Lazy.Encoding
 
 data Payload = Payload
-  { bom :: String,
+  { bom :: Text,
     projectName :: String,
     projectVersion :: String,
     autoCreate :: Bool
@@ -94,11 +99,11 @@ fullCliParser =
 main :: IO ()
 main = do
   options <- execParser fullCliParser
-  manager' <- HTTP.newManager HTTP.defaultManagerSettings
+  manager' <- HTTP.newManager tlsManagerSettings
   bom <- readFile $ opBomPath options
   let payload =
         Payload
-          { bom = bom,
+          { bom = toBase64Text bom,
             projectName = opProjectName options,
             projectVersion = opProjectVersion options,
             autoCreate = opAutoCreate options
@@ -110,3 +115,6 @@ main = do
   case res of
     Left err -> print $ "Error: " ++ show err
     Right res -> print res
+
+toBase64Text :: String -> Text
+toBase64Text = decodeUtf8 . Base64.encode . BL.pack
