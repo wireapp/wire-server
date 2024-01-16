ac5e1e49b5af0b78d189e52f57ebe61f65c77cb3
Marko Dimjašević <marko.dimjasevic@wire.com>
2024-01-16 09:29:37 +0100

    Fix a galley-types unit test

--- libs/galley-types/test/unit/Test/Galley/Types.hs
+++ libs/galley-types/test/unit/Test/Galley/Types.hs
@@ -100,7 +100,7 @@ instance Arbitrary FeatureFlags where
       <*> arbitrary
       <*> arbitrary
       <*> arbitrary
-      <*> arbitrary
+      <*> fmap (fmap unlocked) arbitrary
     where
       unlocked :: ImplicitLockStatus a -> ImplicitLockStatus a
       unlocked = ImplicitLockStatus . Public.setLockStatus Public.LockStatusUnlocked . _unImplicitLockStatus
