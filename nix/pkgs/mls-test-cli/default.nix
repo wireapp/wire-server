eeb6d670582d5368f427468161180f4b3db03819
Stefan Berthold <stefan.berthold@wire.com>
2024-01-18 10:08:24 +0000

    fixit! revert to mls-test-cli in branch sb/fix-client-id

--- nix/pkgs/mls-test-cli/default.nix
+++ nix/pkgs/mls-test-cli/default.nix
@@ -7,8 +7,8 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "wireapp";
     repo = "mls-test-cli";
-    rev = "93d42a76c5a5edfba4e6e1ef01830770fa1bc6bb";
-    sha256 = "sha256-cQULHeVejXAtyPrZiEAlPGTlkx5E9QJFjQIxPn7LXdI=";
+    rev = "30417661eb4e006972ffbdbac80fce6ec64fa90e";
+    sha256 = "sha256-VYSzPejf+Oc5goyxOqlZTwbPj6kqGM6eotefH4eskKE=";
   };
   pname = "mls-test-cli";
   version = "0.9.1";
@@ -16,8 +16,8 @@ rustPlatform.buildRustPackage rec {
     lockFile = "${src}/Cargo.lock";
     outputHashes = {
       "hpke-0.10.0" = "sha256-T1+BFwX6allljNZ/8T3mrWhOejnUU27BiWQetqU+0fY=";
-      "openmls-1.0.0" = "sha256-tAIm8+IgubNnU2M2A5cxHY5caiEQmisw73I9/cqfvUc=";
-      "safe_pqc_kyber-0.6.0" = "sha256-Ch1LA+by+ezf5RV0LDSQGC1o+IWKXk8IPvkwSrAos68=";
+      "openmls-1.0.0" = "sha256-nyIMAlTy7CTV0bVQ0ytamKHpERgtsVKTX4zv7aHzemo=";
+      "safe_pqc_kyber-0.6.2" = "sha256-9t+IIohCJcMIWRtqLA0idyMmjev82BtpST15Tthlge4=";
       "tls_codec-0.3.0" = "sha256-IO6tenXKkC14EoUDp/+DtFNOVzDfOlLu8K1EJI7sOzs=";
     };
   };
