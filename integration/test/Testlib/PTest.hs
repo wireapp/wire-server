125c2b8244f08a19d4e285b5ca0c00037f6ea9f1
Matthias Fischmann <mf@zerobuzz.net>
2024-01-24 10:30:17 +0100

    Use HasTests to save a few LOC.

--- integration/test/Testlib/PTest.hs
+++ integration/test/Testlib/PTest.hs
@@ -24,3 +24,37 @@ instance HasTests x => HasTests (Ciphersuite -> x) where
       [ mkTests m (n <> "[suite=" <> suite.code <> "]") s f (x suite)
         | suite <- allCiphersuites
       ]
+
+instance HasTests x => HasTests (Bool -> x) where
+  mkTests m n s f x =
+    mconcat
+      [ mkTests m (n <> "[" <> show b1 <> "]") s f (x b1)
+        | b1 <- [minBound ..]
+      ]
+
+instance HasTests x => HasTests ((Bool, Bool) -> x) where
+  mkTests m n s f x =
+    mconcat
+      [ mkTests m (n <> "[" <> show b1 <> "," <> show b2 <> "]") s f (x (b1, b2))
+        | b1 <- [minBound ..],
+          b2 <- [minBound ..]
+      ]
+
+instance HasTests x => HasTests ((Bool, Bool, Bool) -> x) where
+  mkTests m n s f x =
+    mconcat
+      [ mkTests m (n <> "[" <> show b1 <> "," <> show b2 <> "," <> show b3 <> "]") s f (x (b1, b2, b3))
+        | b1 <- [minBound ..],
+          b2 <- [minBound ..],
+          b3 <- [minBound ..]
+      ]
+
+instance HasTests x => HasTests ((Bool, Bool, Bool, Bool) -> x) where
+  mkTests m n s f x =
+    mconcat
+      [ mkTests m (n <> "[" <> show b1 <> "," <> show b2 <> "," <> show b3 <> "," <> show b4 <> "]") s f (x (b1, b2, b3, b4))
+        | b1 <- [minBound ..],
+          b2 <- [minBound ..],
+          b3 <- [minBound ..],
+          b4 <- [minBound ..]
+      ]
