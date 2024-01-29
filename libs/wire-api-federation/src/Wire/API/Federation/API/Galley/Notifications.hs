6772a75cb632e7eae89479b98732f4464d6ee5af
Matthias Fischmann <mf@zerobuzz.net>
2024-01-29 14:35:52 +0100

    Refactor VersionRange.

--- libs/wire-api-federation/src/Wire/API/Federation/API/Galley/Notifications.hs
+++ libs/wire-api-federation/src/Wire/API/Federation/API/Galley/Notifications.hs
@@ -54,31 +54,31 @@ instance IsNotificationTag GalleyNotificationTag where
 instance HasNotificationEndpoint 'OnClientRemovedTag where
   type Payload 'OnClientRemovedTag = ClientRemovedRequest
   type NotificationPath 'OnClientRemovedTag = "on-client-removed"
-  versionRange = AllVersions
+  versionRange = allVersions
 
 -- used to notify this backend that a new message has been posted to a
 -- remote conversation
 instance HasNotificationEndpoint 'OnMessageSentTag where
   type Payload 'OnMessageSentTag = RemoteMessage ConvId
   type NotificationPath 'OnMessageSentTag = "on-message-sent"
-  versionRange = AllVersions
+  versionRange = allVersions
 
 instance HasNotificationEndpoint 'OnMLSMessageSentTag where
   type Payload 'OnMLSMessageSentTag = RemoteMLSMessage
   type NotificationPath 'OnMLSMessageSentTag = "on-mls-message-sent"
-  versionRange = AllVersions
+  versionRange = allVersions
 
 -- used by the backend that owns a conversation to inform this backend of
 -- changes to the conversation
 instance HasNotificationEndpoint 'OnConversationUpdatedTag where
   type Payload 'OnConversationUpdatedTag = ConversationUpdate
   type NotificationPath 'OnConversationUpdatedTag = "on-conversation-updated"
-  versionRange = AllVersions
+  versionRange = allVersions
 
 instance HasNotificationEndpoint 'OnUserDeletedConversationsTag where
   type Payload 'OnUserDeletedConversationsTag = UserDeletedConversationsNotification
   type NotificationPath 'OnUserDeletedConversationsTag = "on-user-deleted-conversations"
-  versionRange = AllVersions
+  versionRange = allVersions
 
 -- | All the notification endpoints return an 'EmptyResponse'.
 type GalleyNotificationAPI =
