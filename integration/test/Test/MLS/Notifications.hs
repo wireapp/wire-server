62b01f2c231b4186de514f1dbe7dc0829761774d
Paolo Capriotti <paolo@capriotti.io>
2024-02-27 11:52:14 +0100

    Add failing test reproducing the issue

---
+++ integration/test/Test/MLS/Notifications.hs
@@ -0,0 +1,30 @@
+module Test.MLS.Notifications where
+
+import API.Gundeck
+import MLS.Util
+import Notifications
+import SetupHelpers
+import Testlib.Prelude
+
+testWelcomeNotification :: HasCallStack => App ()
+testWelcomeNotification = do
+  [alice, bob] <- createAndConnectUsers [OwnDomain, OtherDomain]
+  [alice1, alice2, bob1, bob2] <- traverse (createMLSClient def) [alice, alice, bob, bob]
+  traverse_ uploadNewKeyPackage [alice2, bob1, bob2]
+
+  void $ createNewGroup alice1
+  notif <- withWebSocket bob $ \ws -> do
+    void $ createAddCommit alice1 [alice, bob] >>= sendAndConsumeCommitBundle
+    awaitMatch isWelcomeNotif ws
+
+  notifId <- notif %. "id" & asString
+
+  void $
+    getNotifications
+      bob
+      def
+        { since = Just notifId,
+          client = Just bob2.client,
+          size = Just 10000
+        }
+      >>= getJSON 200
