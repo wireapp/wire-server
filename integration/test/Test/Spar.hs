9e9f613d36c13d234f313e7c5c2578d42271b48b
Leif Battermann <leif.battermann@wire.com>
2024-03-06 11:18:01 +0000

    test scim invitation timeout

--- integration/test/Test/Spar.hs
+++ integration/test/Test/Spar.hs
@@ -3,13 +3,26 @@
 module Test.Spar where
 
 import API.Spar
+import Control.Concurrent (threadDelay)
 import SetupHelpers
 import Testlib.Prelude
 
-testSparHappyUserCreation :: HasCallStack => App ()
-testSparHappyUserCreation = do
+testSparUserCreationInvitationTimeout :: HasCallStack => App ()
+testSparUserCreationInvitationTimeout = do
   (owner, _tid, _) <- createTeam OwnDomain 1
   tok <- createScimToken owner >>= \resp -> resp.json %. "token" >>= asString
   scimUser <- randomScimUser
   bindResponse (createScimUser OwnDomain tok scimUser) $ \res -> do
+    res.status `shouldMatchInt` 201
+
+  -- Trying to create the same user again right away should fail
+  bindResponse (createScimUser OwnDomain tok scimUser) $ \res -> do
+    res.status `shouldMatchInt` 409
+
+  -- However, if we wait until the invitation timeout has passed
+  -- (assuming it is configured to 10s locally and in CI)...
+  liftIO $ threadDelay (10_000_000)
+
+  -- ...we should be able to create the user again
+  retryT $ bindResponse (createScimUser OwnDomain tok scimUser) $ \res -> do
     res.status `shouldMatchInt` 201
