f132db1e4db389634f6bb603b2de7832e9a39d5c
Stefan Berthold <stefan.berthold@wire.com>
2024-03-26 16:08:16 +0000

    use multiSFT for is_federating

--- services/brig/src/Brig/Options.hs
+++ services/brig/src/Brig/Options.hs
@@ -424,8 +424,8 @@ data Opts = Opts
     cassandra :: !CassandraOpts,
     -- | ElasticSearch settings
     elasticsearch :: !ElasticSearchOpts,
-    -- | Federation
-    enableFederation :: !Bool,
+    -- | SFT Federation
+    multiSFT :: !(Maybe Bool),
     -- | RabbitMQ settings, required when federation is enabled.
     rabbitmq :: !(Maybe RabbitMqOpts),
     -- | AWS settings
@@ -934,7 +934,8 @@ Lens.makeLensesFor
   [ ("optSettings", "optionSettings"),
     ("elasticsearch", "elasticsearchL"),
     ("sft", "sftL"),
-    ("turn", "turnL")
+    ("turn", "turnL"),
+    ("multiSFT", "multiSFTL")
   ]
   ''Opts
 
