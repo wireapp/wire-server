f180542d0cfdc00df4d05cdbbeada181b6cef0b4
Magnus Viernickel <magnus.viernickel@wire.com>
2024-03-27 12:59:49 +0100

    [wip] some cleanups/ try knotting things togethre

--- services/spar/test-integration/Test/Spar/APISpec.hs
+++ services/spar/test-integration/Test/Spar/APISpec.hs
@@ -18,10 +18,7 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-module Test.Spar.APISpec
-  ( spec,
-  )
-where
+module Test.Spar.APISpec (spec) where
 
 import Bilge
 import Bilge.Assert
