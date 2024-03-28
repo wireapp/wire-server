1c4417e543b3f71e89d1e684d610f6ded0ad5d0c
Marko Dimjašević <marko.dimjasevic@wire.com>
2024-03-28 13:51:53 +0100

    WIP: First tests for GetUserProfile

--- libs/wire-subsystems/src/Wire/UserStore.hs
+++ libs/wire-subsystems/src/Wire/UserStore.hs
@@ -1,4 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Wire.UserStore where
 
+import Data.Id
+import Imports
+import Polysemy
+import Wire.API.User
+
 data UserStore m a where
-  GetUser :: UserId -> UserStore m User
+  GetUser :: UserId -> UserStore m (Maybe User)
+
+makeSem ''UserStore
