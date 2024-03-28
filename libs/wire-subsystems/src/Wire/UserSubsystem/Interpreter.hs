68b33125e1c4a188583d29491ad0ad5a91a92e8e
Marko Dimjašević <marko.dimjasevic@wire.com>
2024-03-28 13:51:53 +0100

    WIP: First tests for GetUserProfile

--- libs/wire-subsystems/src/Wire/UserSubsystem/Interpreter.hs
+++ libs/wire-subsystems/src/Wire/UserSubsystem/Interpreter.hs
@@ -1,10 +1,15 @@
+{-# OPTIONS_GHC -Wwarn #-}
+
 module Wire.UserSubsystem.Interpreter where
 
 import Data.Id
+import Data.LegalHold
 import Data.Qualified
 import Imports
 import Polysemy
 import Wire.API.User
+import Wire.UserStore
 
 getUserProfileImpl :: Member UserStore r => UserId -> Qualified UserId -> Sem r (Maybe UserProfile)
-getUserProfileImpl _profileViewer _quid = undefined
+getUserProfileImpl _profileViewer quid =
+  flip publicProfile UserLegalHoldDisabled <$$> getUser (qUnqualified quid)
--- libs/wire-subsystems/test/unit/Wire/UserSubsystem/InterpreterSpec.hs
+++ libs/wire-subsystems/test/unit/Wire/UserSubsystem/InterpreterSpec.hs
@@ -1,7 +1,39 @@
 module Wire.UserSubsystem.InterpreterSpec where
 
+import Data.LegalHold (UserLegalHoldStatus (UserLegalHoldDisabled))
+import Imports
+import Polysemy
+import Test.Hspec
+import Test.QuickCheck
+import Wire.API.User
+import Wire.UserStore
+import Wire.UserSubsystem.Interpreter
+
 spec :: Spec
 spec = describe "UserSubsystem.Interpreter" do
   describe "getUserProfileImpl" do
+    it "returns Nothing when the user doesn't exist" $ do
+      viewer <- generate arbitrary
+      targetUserId <- generate arbitrary
+      let retrievedProfile =
+            run $
+              staticUserStoreInterpreter [] $
+                getUserProfileImpl viewer targetUserId
+      retrievedProfile `shouldBe` Nothing
+
+    it "gets a local user profile as a non-connected user" do
+      viewer <- generate arbitrary
+      targetUser <- generate arbitrary
+      let retrievedProfile = run $ staticUserStoreInterpreter [targetUser] $ getUserProfileImpl viewer targetUser.userQualifiedId
+      retrievedProfile `shouldBe` Just (publicProfile targetUser UserLegalHoldDisabled)
+
     it "gets a local user profile as a connected user" do
-      undefined
+      viewer <- generate arbitrary
+      targetUser <- generate arbitrary
+      -- TODO: Mock the connection
+      let retrievedProfile = run $ staticUserStoreInterpreter [targetUser] $ getUserProfileImpl viewer targetUser.userQualifiedId
+      retrievedProfile `shouldBe` Just (connectedProfile targetUser UserLegalHoldDisabled)
+
+staticUserStoreInterpreter :: [User] -> InterpreterFor UserStore r
+staticUserStoreInterpreter allUsers = interpret $ \case
+  GetUser uid -> pure $ find (\user -> userId user == uid) allUsers
