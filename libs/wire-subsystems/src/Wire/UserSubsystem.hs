827df47f9cd4d70eaffcbecdc5cc80b03de6366e
Paolo Capriotti <paolo@capriotti.io>
2024-04-03 16:39:47 +0200

    WIP

--- libs/wire-subsystems/src/Wire/UserSubsystem.hs
+++ libs/wire-subsystems/src/Wire/UserSubsystem.hs
@@ -10,5 +10,6 @@ import Wire.API.User
 
 data UserSubsystem m a where
   GetUserProfile :: UserId -> Qualified UserId -> UserSubsystem m (Maybe UserProfile)
+  GetUserProfiles :: UserId -> Qualified UserId -> UserSubsystem m [UserProfile]
 
 makeSem ''UserSubsystem
--- libs/wire-subsystems/src/Wire/UserSubsystem/Interpreter.hs
+++ libs/wire-subsystems/src/Wire/UserSubsystem/Interpreter.hs
@@ -9,11 +9,16 @@ import Data.LegalHold
 import Data.Qualified
 import Imports
 import Polysemy
+import Polysemy.Error
 import Polysemy.Input
+import Wire.API.Federation.API
+import Wire.API.Federation.Error
 import Wire.API.Provider.Service
 import Wire.API.Team.Member
 import Wire.API.User
+import Wire.FederationAPIAccess
 import Wire.GalleyAPIAccess
+import Wire.Sem.Concurrency
 import Wire.UserStore
 
 data UserSubsystemConfig = UserSubsystemConfig
@@ -22,8 +27,54 @@ data UserSubsystemConfig = UserSubsystemConfig
     defaultLocale :: Locale
   }
 
-getRemoteUserProfile :: UserId -> Remote UserId -> Sem r (Maybe UserProfile)
-getRemoteUserProfile = undefined
+-- | Obtain user profiles for a list of users as they can be seen by
+-- a given user 'self'. If 'self' is an unknown 'UserId', return '[]'.
+getUserProfiles ::
+  ( Member GalleyAPIAccess r,
+    Member (Concurrency 'Unsafe) r,
+    Member (Error FederationError) r,
+    Member FederationAPIAccess r,
+    Member (Input UserSubsystemConfig) r,
+    Member UserStore r
+  ) =>
+  -- | User 'self' on whose behalf the profiles are requested.
+  Local UserId ->
+  -- | The users ('others') for which to obtain the profiles.
+  [Qualified UserId] ->
+  Sem r [UserProfile]
+getUserProfiles self others =
+  concat
+    <$> unsafePooledMapConcurrentlyN
+      8
+      (getUserProfilesFromDomain self)
+      (bucketQualified others)
+
+getUserProfilesFromDomain ::
+  ( Member (Concurrency 'Unsafe) r,
+    Member GalleyAPIAccess r,
+    Member (Error FederationError) r,
+    Member (Input UserSubsystemConfig) r,
+    Member FederationAPIAccess r,
+    Member UserStore r
+  ) =>
+  Local UserId ->
+  Qualified [UserId] ->
+  Sem r [UserProfile]
+getUserProfilesFromDomain self =
+  foldQualified
+    self
+    (getLocalUserProfiles (tUnqualified self) . tUnqualified)
+    getRemoteUserProfiles
+
+getRemoteUserProfiles ::
+  ( Member FederationAPIAccess r,
+    Member (Concurrency 'Unsafe) r,
+    Member (Error FederationError) r
+  ) =>
+  Remote [UserId] ->
+  Sem r [UserProfile]
+getRemoteUserProfiles ruids = do
+  runFederated ruids $ fedClient @'Brig @"get-users-by-ids" (tUnqualified ruids)
 
 getLocalUserProfiles ::
   forall r.
