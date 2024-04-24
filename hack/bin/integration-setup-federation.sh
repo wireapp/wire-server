863f5eb6eba9bc73bc631fe97050c52676e07a05
Akshay Mankar <akshay@wire.com>
2024-04-24 14:04:56 +0200

    Typo

--- hack/bin/integration-setup-federation.sh
+++ hack/bin/integration-setup-federation.sh
@@ -50,7 +50,7 @@ export FEDERATION_CA_CERTIFICATE
 echo "Installing charts..."
 
 set +e
-# This exists becasue we need to run `helmfile` with `--skip-deps`, without that it doesn't work.
+# This exists because we need to run `helmfile` with `--skip-deps`, without that it doesn't work.
 helm repo add bedag https://bedag.github.io/helm-charts/
 
 helmfile --environment "$HELMFILE_ENV" --file "${TOP_LEVEL}/hack/helmfile.yaml" sync --skip-deps --concurrency 0
