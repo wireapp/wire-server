fca3a13637a947408c37e2f0896cfabaa596a56c
Akshay Mankar <akshay@wire.com>
2024-04-29 16:17:27 +0200

    Give every redis node its own cert

--- hack/bin/selfsigned.sh
+++ hack/bin/selfsigned.sh
@@ -18,6 +18,10 @@ FEDERATION_CA="$TEMP/integration-ca"
 FEDERATION_LEAF_CERT="$TEMP/integration-leaf"
 ELASTICSEARCH_CA="$TEMP/elasticsearch-ca"
 ELASTICSEARCH_LEAF_CERT="$TEMP/elasticsearch-leaf"
+CSR_REDIS_CA="$TEMP/csr-redis-ca.json"
+CSR_REDIS="$TEMP/csr-redis.json"
+REDIS_CA="$TEMP/redis-ca"
+REDIS_LEAF_CERT="$TEMP/redis-leaf"
 
 command -v cfssl >/dev/null 2>&1 || { echo >&2 "cfssl is not installed, aborting. See https://github.com/cloudflare/cfssl"; exit 1; }
 command -v cfssljson >/dev/null 2>&1 || { echo >&2 "cfssljson is not installed, aborting. See https://github.com/cloudflare/cfssl"; exit 1; }
@@ -90,10 +94,39 @@ cp "$ELASTICSEARCH_LEAF_CERT-key.pem" "$ROOT_DIR/deploy/dockerephemeral/docker/e
 cp "$ELASTICSEARCH_CA.pem" "$ROOT_DIR/hack/helm_vars/elasticsearch-certs/elasticsearch-ca.pem"
 cp "$ELASTICSEARCH_CA-key.pem" "$ROOT_DIR/hack/helm_vars/elasticsearch-certs/elasticsearch-ca-key.pem"
 
-cp "$OUTPUTNAME_CA.pem" "$ROOT_DIR/deploy/dockerephemeral/docker/redis-ca.pem"
+echo '{
+    "CN": "redis.ca.example.com",
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    }
+}' > "$CSR_REDIS_CA"
+
+# generate CA key and cert
+cfssl gencert -initca "$CSR_REDIS_CA" | cfssljson -bare "$REDIS_CA"
+
+echo '{
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    }
+}' > "$CSR_REDIS"
+
+
+cp "$REDIS_CA.pem" "$ROOT_DIR/deploy/dockerephemeral/docker/redis-ca.pem"
+
 for redis_node in $(seq 1 6); do
-    cp "$OUTPUTNAME_LEAF_CERT.pem" "$ROOT_DIR/deploy/dockerephemeral/docker/redis-node-${redis_node}-cert.pem"
-    cp "$OUTPUTNAME_LEAF_CERT-key.pem" "$ROOT_DIR/deploy/dockerephemeral/docker/redis-node-${redis_node}-key.pem"
+    # generate cert and key based on CA given comma-separated hostnames as SANs
+    # TODO: Its not good to depend on nip.io for running integration tests locally
+    cfssl gencert \
+          -ca "$REDIS_CA.pem" \
+          -ca-key "$REDIS_CA-key.pem" \
+          -hostname="redis-${redis_node},172.20.0.3${redis_node}" \
+          "$CSR_REDIS" \
+        | cfssljson -bare "$REDIS_LEAF_CERT-${redis_node}"
+
+    cp "${REDIS_LEAF_CERT}-${redis_node}.pem" "$ROOT_DIR/deploy/dockerephemeral/docker/redis-node-${redis_node}-cert.pem"
+    cp "${REDIS_LEAF_CERT}-${redis_node}-key.pem" "$ROOT_DIR/deploy/dockerephemeral/docker/redis-node-${redis_node}-key.pem"
     chmod 0644 "$ROOT_DIR/deploy/dockerephemeral/docker/redis-node-${redis_node}-key.pem"
 done
 
