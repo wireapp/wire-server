ee762a0b0ba7fc35763b81d6a167c0d643aab4e0
Leif Battermann <leif.battermann@wire.com>
2024-05-02 18:37:39 +0200

    WPB-8988 Upgrade `rusty-jwt-tools` to support `ecdsa_secp256r1_sha256` (#4035)

--- libs/jwt-tools/test/Spec.hs
+++ libs/jwt-tools/test/Spec.hs
@@ -15,8 +15,6 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
-import Control.Monad.Trans.Except
-import Data.ByteString.Char8 (split)
 import Data.Jwt.Tools
 import Data.String.Conversions
 import Imports
@@ -24,18 +22,6 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "generateDpopToken FFI when passing valid inputs" $ do
-    it "should return an access token with the correct header" $ do
-      actual <- runExceptT $ generateDpopToken proof uid cid handle displayName tid domain nonce uri method maxSkewSecs expires now pem
-      -- The actual payload of the DPoP token is not deterministic as it depends on the current time.
-      -- We therefore only check the header, because if the header is correct, it means the token creation was successful.s
-      let expectedHeader = "eyJhbGciOiJFZERTQSIsInR5cCI6ImF0K2p3dCIsImp3ayI6eyJrdHkiOiJPS1AiLCJjcnYiOiJFZDI1NTE5IiwieCI6ImRZSTM4VWR4a3NDMEs0UXg2RTlKSzlZZkdtLWVoblkxOG9LbUhMMllzWmsifX0"
-      let actualHeader = either (const "") (head . split '.') actual
-      actualHeader `shouldBe` expectedHeader
-  describe "generateDpopToken FFI when passing a wrong nonce value" $ do
-    it "should return BackendNonceMismatchError" $ do
-      actual <- runExceptT $ generateDpopToken proof uid cid handle displayName tid domain (Nonce "foobar") uri method maxSkewSecs expires now pem
-      actual `shouldBe` Left BackendNonceMismatchError
   describe "toResult" $ do
     it "should convert to correct error" $ do
       toResult Nothing (Just token) `shouldBe` Right (cs token)
@@ -103,25 +89,3 @@ main = hspec $ do
       toResult Nothing Nothing `shouldBe` Left UnknownError
   where
     token = ""
-    proof = Proof "eyJhbGciOiJFZERTQSIsImp3ayI6eyJjcnYiOiJFZDI1NTE5Iiwia3R5IjoiT0tQIiwieCI6Im5MSkdOLU9hNkpzcTNLY2xaZ2dMbDdVdkFWZG1CMFE2QzNONUJDZ3BoSHcifSwidHlwIjoiZHBvcCtqd3QifQ.eyJhdWQiOiJodHRwczovL3dpcmUuY29tL2FjbWUvY2hhbGxlbmdlL2FiY2QiLCJjaGFsIjoid2EyVnJrQ3RXMXNhdUoyRDN1S1k4cmM3eTRrbDR1c0giLCJleHAiOjE3Mzk4ODA2NzQsImhhbmRsZSI6IndpcmVhcHA6Ly8lNDB5d2Z5ZG5pZ2Jud2h1b3pldGphZ3FAZXhhbXBsZS5jb20iLCJodG0iOiJQT1NUIiwiaHR1IjoiaHR0cHM6Ly9leGFtcGxlLmNvbS9jbGllbnRzL2NjNmU2NDBlMjk2ZThiYmEvYWNjZXNzLXRva2VuIiwiaWF0IjoxNzA4MzQ0Njc0LCJqdGkiOiI2ZmM1OWU3Zi1iNjY2LTRmZmMtYjczOC00ZjQ3NjBjODg0Y2EiLCJuYW1lIjoi5reB4qqu5KSq5rK255Kh4bKV6re14Y2q6omE6Jy16Iu17ICV54Kb66-v56qp5KqW766M6bGw6oOy6b6m57m15pWJ4LqH54et6rOj54KHIiwibmJmIjoxNzA4MzQ0Njc0LCJub25jZSI6IllWZ2dHdWlTUTZlamhQNTNFX0tPS3ciLCJzdWIiOiJ3aXJlYXBwOi8vSWZ0VzBLeFVSb2F1QWVockRremJiQSFjYzZlNjQwZTI5NmU4YmJhQGV4YW1wbGUuY29tIiwidGVhbSI6ImMxNTE5NzVlLWIxOTMtNDAwOS1hM2QyLTc0N2M5NjFmMjMzMyJ9.SHxpMzOe2yC3y6DP7lEH0l7_eOKrUZZI0OjgtnCKjO4OBD0XqKOi0y_z07-7FWc-KtThlsaZatnBNTB67GhQBw"
-    uid = UserId "21fb56d0-ac54-4686-ae01-e86b0e4cdb6c"
-    nonce = Nonce "YVggGuiSQ6ejhP53E_KOKw"
-    expires = ExpiryEpoch 1739967074
-    handle = Handle "ywfydnigbnwhuozetjagq"
-    displayName = DisplayName "\230\183\129\226\170\174\228\164\170\230\178\182\231\146\161\225\178\149\234\183\181\225\141\170\234\137\132\232\156\181\232\139\181\236\128\149\231\130\155\235\175\175\231\170\169\228\170\150\239\174\140\233\177\176\234\131\178\233\190\166\231\185\181\230\149\137\224\186\135\231\135\173\234\179\163\231\130\135"
-    tid = TeamId "c151975e-b193-4009-a3d2-747c961f2333"
-
-    now = NowEpoch 1704982162
-    cid = ClientId 14730821443162901434
-    domain = Domain "example.com"
-    uri = Uri "https://example.com/clients/cc6e640e296e8bba/access-token"
-    method = POST
-    maxSkewSecs = MaxSkewSecs 1
-    pem =
-      PemBundle $
-        "-----BEGIN PRIVATE KEY-----\n\
-        \MC4CAQAwBQYDK2VwBCIEIMkvahkqR9sHJSmFeCl3B7aJjsQGgwy++cccWTbuDyy+\n\
-        \-----END PRIVATE KEY-----\n\
-        \-----BEGIN PUBLIC KEY-----\n\
-        \MCowBQYDK2VwAyEAdYI38UdxksC0K4Qx6E9JK9YfGm+ehnY18oKmHL2YsZk=\n\
-        \-----END PUBLIC KEY-----\n"
