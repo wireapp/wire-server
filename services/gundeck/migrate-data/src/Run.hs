142299c43df773b661b977925a1b1a2aebd28138
Akshay Mankar <akshay@wire.com>
2024-05-13 14:16:27 +0200

    gundeck: Delete all APNS_VOIP and APNS_VOIP_SANDBOX push tokens (#4044)
    * gundeck: Introduce data migrations
    
    First one will delete any VOIP push tokens
    
    * nix: Add gundeck-migrate-data image
    
    * charts/cassandra-migrations: Run gundeck-migrate-data
    
    * changelog
    
    * format
    
    * charts/cassandra-migrations: Add missing image name for gundeckMigrateData
    
    * [gundeck] fixed reference to galley test
    
    * [gundeck] Added missing data_migration table.
    
    * cassandra-schema.cql: updated.
    
    ---------
    
    Co-authored-by: Igor Ranieri <igor@elland.me>

---
+++ services/gundeck/migrate-data/src/Run.hs
@@ -0,0 +1,32 @@
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2022 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Run where
+
+import Gundeck.DataMigration
+import Imports
+import Options.Applicative
+import System.Logger.Extended qualified as Log
+import V1_DeleteApnsVoipTokens qualified
+
+main :: IO ()
+main = do
+  o <- execParser (info (helper <*> cassandraSettingsParser) desc)
+  l <- Log.mkLogger Log.Debug Nothing Nothing
+  migrate l o [V1_DeleteApnsVoipTokens.migration]
+  where
+    desc = header "Gundeck Cassandra Data Migrations" <> fullDesc
