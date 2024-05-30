208590dcd14a1c0401eb774db453cecff16d36fa
Stefan Berthold <stefan.berthold@wire.com>
2024-05-30 13:53:01 +0000

    semantically no change

--- services/galley/exec/Main.hs
+++ services/galley/exec/Main.hs
@@ -15,6 +15,8 @@
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
 
+-- one more comment
+
 module Main
   ( main,
   )
