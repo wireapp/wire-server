ad972a27f980e6b6a2db7c2e6ab9642f594673f9
Paolo Capriotti <paolo@capriotti.io>
2024-06-05 14:47:48 +0200

    Restore public API

--- libs/wire-api/src/Wire/API/User/Identity.hs
+++ libs/wire-api/src/Wire/API/User/Identity.hs
@@ -101,26 +101,11 @@ data UserIdentity
   | PhoneIdentity Phone
   | SSOIdentity UserSSOId (Maybe Email) (Maybe Phone)
   deriving stock (Eq, Show, Generic)
+  deriving (Arbitrary) via (GenericUniform UserIdentity)
 
 isSSOIdentity :: UserIdentity -> Bool
 isSSOIdentity (SSOIdentity _ _ _) = True
 isSSOIdentity _ = False
-
--- | A tag for the 'UserIdentity' type. The tags for the 'PhoneIdentity' and
--- 'FullIdentity' cases are omitted because the 'Arbitrary' instance should not
--- generate a user identity with a phone number. Phone number-based user
--- accounts are no longer supported.
-data UserIdentityTag
-  = EmailIdentityTag
-  | SSOIdentityTag
-  deriving stock (Generic)
-  deriving (Arbitrary) via (GenericUniform UserIdentityTag)
-
-instance Arbitrary UserIdentity where
-  arbitrary =
-    arbitrary >>= \case
-      EmailIdentityTag -> EmailIdentity <$> arbitrary
-      SSOIdentityTag -> SSOIdentity <$> arbitrary <*> arbitrary <*> pure Nothing
 
 userIdentityObjectSchema :: ObjectSchema SwaggerDoc UserIdentity
 userIdentityObjectSchema =
