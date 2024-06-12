91b708ffc9c51ead6f36506e49d3a7b3051489a6
Marko Dimjašević <marko.dimjasevic@wire.com>
2024-06-12 11:12:50 +0200

    Drop the excluded_phones DB table

---
+++ services/brig/src/Brig/Schema/V82_DropPhoneBlacklistTable.hs
@@ -0,0 +1,33 @@
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2024 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Brig.Schema.V82_DropPhoneBlacklistTable
+  ( migration,
+  )
+where
+
+import Cassandra.Schema
+import Imports
+import Text.RawString.QQ
+
+-- | Phone based functionality is not supported any more
+migration :: Migration
+migration = Migration 82 "Remove a phone prefix blacklist table" $ do
+  schema'
+    [r|
+        DROP TABLE excluded_phones;
+    |]
