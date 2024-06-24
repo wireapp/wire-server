768eb20181f8056bb73a646bda988ee83512968f
Akshay Mankar <akshay@wire.com>
2024-06-24 09:55:11 +0200

    Reexport all MockInterpreters from one module

---
+++ libs/wire-subsystems/test/unit/Wire/MockInterpreters.hs
@@ -0,0 +1,12 @@
+module Wire.MockInterpreters (module MockInterpreters) where
+
+-- Run this from project root to generate the imports:
+-- ls libs/wire-subsystems/test/unit/Wire/MockInterpreters | sed 's|\(.*\)\.hs|import Wire.MockInterpreters.\1 as MockInterpreters|'                                                                                                  ─╯
+
+import Wire.MockInterpreters.Error as MockInterpreters
+import Wire.MockInterpreters.HashPassword as MockInterpreters
+import Wire.MockInterpreters.Now as MockInterpreters
+import Wire.MockInterpreters.PasswordResetCodeStore as MockInterpreters
+import Wire.MockInterpreters.PasswordStore as MockInterpreters
+import Wire.MockInterpreters.SessionStore as MockInterpreters
+import Wire.MockInterpreters.UserSubsystem as MockInterpreters
