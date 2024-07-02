b81c89278b3baf971bf1a4d34bafcca86cbd48ac
Akshay Mankar <akshay@wire.com>
2024-07-02 10:47:28 +0200

    group team operations together

--- libs/wire-subsystems/src/Wire/EmailSmsSubsystem.hs
+++ libs/wire-subsystems/src/Wire/EmailSmsSubsystem.hs
@@ -14,13 +14,13 @@ data EmailSmsSubsystem m a where
   SendPasswordResetMail :: Email -> PasswordResetPair -> Maybe Locale -> EmailSmsSubsystem m ()
   SendPasswordResetSms :: Phone -> PasswordResetPair -> Maybe Locale -> EmailSmsSubsystem m ()
   SendVerificationMail :: Email -> ActivationKey -> ActivationCode -> Maybe Locale -> EmailSmsSubsystem m ()
-  SendTeamDeletionVerificationMail :: Email -> Code.Value -> Maybe Locale -> EmailSmsSubsystem m ()
   SendCreateScimTokenVerificationMail :: Email -> Code.Value -> Maybe Locale -> EmailSmsSubsystem m ()
   SendLoginVerificationMail :: Email -> Code.Value -> Maybe Locale -> EmailSmsSubsystem m ()
   SendActivationMail :: Email -> Name -> ActivationKey -> ActivationCode -> Maybe Locale -> EmailSmsSubsystem m ()
   SendEmailAddressUpdateMail :: Email -> Name -> ActivationKey -> ActivationCode -> Maybe Locale -> EmailSmsSubsystem m ()
-  SendTeamActivationMail :: Email -> Name -> ActivationKey -> ActivationCode -> Maybe Locale -> Text -> EmailSmsSubsystem m ()
   SendNewClientEmail :: Email -> Name -> Client -> Locale -> EmailSmsSubsystem m ()
   SendDeletionEmail :: Email -> Name -> Code.Key -> Code.Value -> Locale -> EmailSmsSubsystem m ()
+  SendTeamActivationMail :: Email -> Name -> ActivationKey -> ActivationCode -> Maybe Locale -> Text -> EmailSmsSubsystem m ()
+  SendTeamDeletionVerificationMail :: Email -> Code.Value -> Maybe Locale -> EmailSmsSubsystem m ()
 
 makeSem ''EmailSmsSubsystem
