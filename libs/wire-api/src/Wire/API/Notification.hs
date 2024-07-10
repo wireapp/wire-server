afed7ebdeafd9b3f40435afc5a419b8f964712c3
Akshay Mankar <akshay@wire.com>
2024-07-10 11:09:33 +0200

    Make NotificationId a Text

--- libs/wire-api/src/Wire/API/Notification.hs
+++ libs/wire-api/src/Wire/API/Notification.hs
@@ -42,9 +42,7 @@ import Control.Lens (makeLenses, (.~))
 import Control.Lens.Operators ((?~))
 import Data.Aeson (FromJSON (..), ToJSON (..))
 import Data.Aeson.Types qualified as Aeson
-import Data.Bits
 import Data.HashMap.Strict.InsOrd qualified as InsOrdHashMap
-import Data.Id
 import Data.Json.Util
 import Data.List.NonEmpty (NonEmpty)
 import Data.OpenApi (ToParamSchema (..))
@@ -53,13 +51,12 @@ import Data.SOP
 import Data.Schema
 import Data.Text.Encoding
 import Data.Time.Clock (UTCTime)
-import Data.UUID qualified as UUID
 import Imports
 import Servant
 import Wire.API.Routes.MultiVerb
 import Wire.Arbitrary (Arbitrary, GenericUniform (..))
 
-type NotificationId = Id QueuedNotification
+type NotificationId = Text
 
 -- FUTUREWORK:
 -- This definition is very opaque, but we know some of the structure already
@@ -84,11 +81,9 @@ eventSchema = mkSchema sdoc Aeson.parseJSON (Just . Aeson.toJSON)
               )
             ]
 
+-- TODO: Delete
 isValidNotificationId :: NotificationId -> Bool
-isValidNotificationId (Id uuid) =
-  -- check that the version bits are set to 1
-  case UUID.toWords uuid of
-    (_, w, _, _) -> (w `shiftR` 12) .&. 0xf == 1
+isValidNotificationId _ = True
 
 --------------------------------------------------------------------------------
 -- QueuedNotification
