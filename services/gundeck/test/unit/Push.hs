503bf58f6e0b96fc045c7e82e9010c2ba0e7f605
Matthias Fischmann <mf@zerobuzz.net>
2024-07-11 14:47:10 +0200

    Fixup

--- services/gundeck/test/unit/Push.hs
+++ services/gundeck/test/unit/Push.hs
@@ -21,7 +21,7 @@
 module Push where
 
 import Data.Aeson qualified as Aeson
-import Gundeck.Push (pushAll, pushAny)
+import Gundeck.Push (pushAll)
 import Gundeck.Types
 import Imports
 import MockGundeck
