5e03504e465c91c80cb24ce5bc91bffad638052e
Igor Ranieri <igor@elland.me>
2024-07-25 11:41:02 +0200

    Weed+.

--- libs/imports/src/Imports.hs
+++ libs/imports/src/Imports.hs
@@ -60,19 +60,13 @@ module Imports
     module UnliftIO.Directory,
 
     -- ** Prelude
-    putChar,
     putStr,
     putStrLn,
     print,
-    getChar,
     getLine,
-    getContents,
-    interact,
     readFile,
     writeFile,
     appendFile,
-    readIO,
-    readLn,
 
     -- ** Environment
     getArgs,
@@ -241,9 +235,6 @@ type LByteString = Data.ByteString.Lazy.ByteString
 ----------------------------------------------------------------------------
 -- Lifted functions from Prelude
 
-putChar :: (MonadIO m) => Char -> m ()
-putChar = liftIO . P.putChar
-
 putStr :: (MonadIO m) => String -> m ()
 putStr = liftIO . P.putStr
 
@@ -253,17 +244,8 @@ putStrLn = liftIO . P.putStrLn
 print :: (Show a, MonadIO m) => a -> m ()
 print = liftIO . P.print
 
-getChar :: (MonadIO m) => m Char
-getChar = liftIO P.getChar
-
 getLine :: (MonadIO m) => m String
 getLine = liftIO P.getLine
-
-getContents :: (MonadIO m) => m String
-getContents = liftIO P.getContents
-
-interact :: (MonadIO m) => (String -> String) -> m ()
-interact = liftIO . P.interact
 
 readFile :: (MonadIO m) => FilePath -> m String
 readFile = liftIO . P.readFile
@@ -273,12 +255,6 @@ writeFile = fmap liftIO . P.writeFile
 
 appendFile :: (MonadIO m) => FilePath -> String -> m ()
 appendFile = fmap liftIO . P.appendFile
-
-readIO :: (Read a, MonadIO m) => String -> m a
-readIO = liftIO . P.readIO
-
-readLn :: (Read a, MonadIO m) => m a
-readLn = liftIO P.readLn
 
 ----------------------------------------------------------------------
 -- Functor
