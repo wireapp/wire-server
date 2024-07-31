0453493a7cc862f05b80bd66a5039c4924d8d598
Akshay Mankar <akshay@wire.com>
2024-07-31 09:52:06 +0200

    run-services: Add config tuned for manual usage and an option to turn it on

--- integration/test/Testlib/RunServices.hs
+++ integration/test/Testlib/RunServices.hs
@@ -1,10 +1,10 @@
-module Testlib.RunServices where
+module Testlib.RunServices (main) where
 
 import Control.Concurrent
 import Control.Monad.Codensity
+import Options.Applicative
 import System.Directory
-import System.Environment (getArgs)
-import System.Exit (exitWith)
+import System.Exit
 import System.FilePath
 import System.Posix (getWorkingDirectory)
 import System.Process
@@ -31,23 +31,43 @@ findProjectRoot path = do
       Nothing -> pure Nothing
       Just p -> findProjectRoot p
 
+data Opts = Opts
+  { withManualTestingOverrides :: Bool,
+    runSubprocess :: [String]
+  }
+  deriving (Show)
+
+optsParser :: Parser Opts
+optsParser =
+  Opts
+    <$> switch
+      ( long "with-manual-testing-overrides"
+          <> short 'm'
+          <> help "Run services with settings tuned for manual app usage (not recommended for running integration tests)"
+      )
+    <*> many
+      ( strArgument
+          ( metavar "COMMAND_WITH_ARGS"
+              <> help "When specified, the command will be run after services have started and service will be killed after the command exits"
+          )
+      )
+
 main :: IO ()
 main = do
   cwd <- getWorkingDirectory
   mbProjectRoot <- findProjectRoot cwd
+  opts <- execParser (info (optsParser <**> helper) fullDesc)
   cfg <- case mbProjectRoot of
     Nothing -> error "Could not find project root. Please make sure you call run-services from somewhere in wire-server."
     Just projectRoot ->
       pure $ joinPath [projectRoot, "services/integration.yaml"]
 
-  args <- getArgs
-
-  let run = case args of
+  let run = case opts.runSubprocess of
         [] -> do
           putStrLn "services started"
-          forever (threadDelay 1000000000)
+          forever (threadDelay maxBound)
         _ -> do
-          let cp = proc "sh" (["-c", "exec \"$@\"", "--"] <> args)
+          let cp = proc "sh" (["-c", "exec \"$@\"", "--"] <> opts.runSubprocess)
           (_, _, _, ph) <- createProcess cp
           exitWith =<< waitForProcess ph
 
@@ -57,6 +77,37 @@ main = do
       $ do
         _modifyEnv <-
           traverseConcurrentlyCodensity
-            (\r -> void $ startDynamicBackend r mempty)
+            ( \r ->
+                void
+                  $ if opts.withManualTestingOverrides
+                    then startDynamicBackend r manualTestingOverrides
+                    else startDynamicBackend r mempty
+            )
             [backendA, backendB]
         liftIO run
+
+manualTestingOverrides :: ServiceOverrides
+manualTestingOverrides =
+  let smtpEndpoint = object ["host" .= "localhost", "port" .= (2500 :: Int)]
+      authSettings =
+        object
+          [ "userTokenTimeout" .= (4838400 :: Int),
+            "sessionTokenTimeout" .= (86400 :: Int),
+            "accessTokenTimeout" .= (900 :: Int),
+            "providerTokenTimeout" .= (900 :: Int),
+            "legalHoldUserTokenTimeout" .= (4838400 :: Int),
+            "legalHoldAccessTokenTimeout" .= (900 :: Int)
+          ]
+   in def
+        { brigCfg =
+            mergeField "emailSMS.email.smtpEndpoint" smtpEndpoint
+              >=> setField "emailSMS.email.smtpConnType" "plain"
+              >=> removeField "emailSMS.email.sesQueue"
+              >=> removeField "emailSMS.email.sesEndpoint"
+              >=> mergeField "zauth.authSettings" authSettings
+              >=> setField @_ @Int "optSettings.setActivationTimeout" 3600
+              >=> setField @_ @Int "optSettings.setVerificationTimeout" 3600
+              >=> setField @_ @Int "optSettings.setTeamInvitationTimeout" 3600
+              >=> setField @_ @Int "optSettings.setUserCookieRenewAge" 1209600
+              >=> removeField "optSettings.setSuspendInactiveUsers"
+        }
