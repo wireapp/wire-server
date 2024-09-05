29c91229be30144a21a2e2a99833d77f876c296a
Leif Battermann <leif.battermann@wire.com>
2024-09-05 10:38:00 +0000

    failing test

---
+++ integration/test/Test/Teams.hs
@@ -0,0 +1,32 @@
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2024 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Test.Teams where
+
+import API.Brig
+import API.BrigInternal (createUser)
+import SetupHelpers
+import Testlib.JSON
+import Testlib.Prelude
+
+testInviteIndividualUserToTeam :: (HasCallStack) => App ()
+testInviteIndividualUserToTeam = do
+  (owner, _tid, _) <- createTeam OwnDomain 0
+  individualUser <- createUser OwnDomain def
+  email <- individualUser.json %. "email" >>= asString
+  inv <- postInvitation owner (PostInvitation $ Just email) >>= getJSON 201
+  printJSON inv
