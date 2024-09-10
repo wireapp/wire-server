77f6a8cbbba31dd35600a16e2b71f835b7991302
Magnus Viernickel <magnus.viernickel@wire.com>
2024-09-10 15:38:57 +0200

    [fix] drop broken rst2pdf package

--- nix/overlay-docs.nix
+++ nix/overlay-docs.nix
@@ -1,7 +1,6 @@
 self: super: rec {
   python312 = super.python312.override {
     packageOverrides = pself: psuper: {
-      rst2pdf = pself.callPackage ./pkgs/python-docs/rst2pdf.nix { };
       sphinx-multiversion = pself.callPackage ./pkgs/python-docs/sphinx-multiversion.nix { };
       sphinx_reredirects = pself.callPackage ./pkgs/python-docs/sphinx_reredirects.nix { };
       sphinxcontrib-kroki = pself.callPackage ./pkgs/python-docs/sphinxcontrib-kroki.nix { };
