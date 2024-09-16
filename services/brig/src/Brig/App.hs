8ff61f2c20320566897ae05ad41f5f2660f588ca
Matthias Fischmann <mf@zerobuzz.net>
2024-09-16 17:19:12 +0200

    Update services/brig/src/Brig/App.hs

--- services/brig/src/Brig/App.hs
+++ services/brig/src/Brig/App.hs
@@ -442,7 +442,8 @@ providerTemplates l = forLocale l <$> view provTemplates
 teamTemplates :: (MonadReader Env m) => Maybe Locale -> m (Locale, TeamTemplates)
 teamTemplates l = forLocale l <$> view tmTemplates
 
--- this works because team templates is not affected by `forLocale`
+-- this works because team templates is not affected by `forLocale`; it is useful where we
+-- use the `TeamTemplates` only for finding invitation url templates (those are not localized).
 teamTemplatesNoLocale :: (MonadReader Env m) => m TeamTemplates
 teamTemplatesNoLocale = snd <$> teamTemplates Nothing
 
