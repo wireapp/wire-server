abf771c0a67bb7a7196f9d7ee8c38689a25f1697
Matthias Fischmann <mf@zerobuzz.net>
2024-09-27 15:16:17 +0200

    rm internal routing table, ReqBodyHack in services/gundeck.

--- services/gundeck/src/Gundeck/API/Internal.hs
+++ services/gundeck/src/Gundeck/API/Internal.hs
@@ -43,61 +43,8 @@ import Servant.Server.Internal.Delayed
 import Servant.Server.Internal.DelayedIO
 import Servant.Server.Internal.ErrorFormatter
 import Wire.API.Push.Token qualified as PushTok
+import Wire.API.Routes.Internal.Gundeck
 import Wire.API.Routes.Public
-
--- | this can be replaced by `ReqBody '[JSON] Presence` once the fix in cannon from
--- https://github.com/wireapp/wire-server/pull/4246 has been deployed everywhere.
-data ReqBodyHack
-
--- | cloned from instance for ReqBody'.
-instance
-  ( HasServer api context,
-    HasContextEntry (MkContextWithErrorFormatter context) ErrorFormatters
-  ) =>
-  HasServer (ReqBodyHack :> api) context
-  where
-  type ServerT (ReqBodyHack :> api) m = Presence -> ServerT api m
-
-  hoistServerWithContext _ pc nt s = hoistServerWithContext (Proxy :: Proxy api) pc nt . s
-
-  route Proxy context subserver =
-    route (Proxy :: Proxy api) context $
-      addBodyCheck subserver ctCheck bodyCheck
-    where
-      rep = typeRep (Proxy :: Proxy ReqBodyHack)
-      formatError = bodyParserErrorFormatter $ getContextEntry (mkContextWithErrorFormatter context)
-
-      ctCheck = pure eitherDecode
-
-      -- Body check, we get a body parsing functions as the first argument.
-      bodyCheck f = withRequest $ \request -> do
-        mrqbody <- f <$> liftIO (lazyRequestBody request)
-        case mrqbody of
-          Left e -> delayedFailFatal $ formatError rep request e
-          Right v -> pure v
-
--- | cloned from instance for ReqBody'.
-instance
-  (RoutesToPaths rest) =>
-  RoutesToPaths (ReqBodyHack :> rest)
-  where
-  getRoutes = getRoutes @rest
-
-type GundeckInternalAPI =
-  "i"
-    :> ( ("status" :> Get '[JSON] NoContent)
-           :<|> ("push" :> "v2" :> ReqBody '[JSON] [Push] :> Post '[JSON] NoContent)
-           :<|> ( "presences"
-                    :> ( (QueryParam' [Required, Strict] "ids" (CommaSeparatedList UserId) :> Get '[JSON] [Presence])
-                           :<|> (Capture "uid" UserId :> Get '[JSON] [Presence])
-                           :<|> (ReqBodyHack :> Verb 'POST 201 '[JSON] (Headers '[Header "Location" GD.URI] NoContent))
-                           :<|> (Capture "uid" UserId :> "devices" :> Capture "did" ConnId :> "cannons" :> Capture "cannon" CannonId :> Delete '[JSON] NoContent)
-                       )
-                )
-           :<|> (ZUser :> "clients" :> Capture "cid" ClientId :> Delete '[JSON] NoContent)
-           :<|> (ZUser :> "user" :> Delete '[JSON] NoContent)
-           :<|> ("push-tokens" :> Capture "uid" UserId :> Get '[JSON] PushTokenList)
-       )
 
 servantSitemap :: ServerT GundeckInternalAPI Gundeck
 servantSitemap =
