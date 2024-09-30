a43a739c7b4c98171111621150bf7058d778adbc
Matthias Fischmann <mf@zerobuzz.net>
2024-09-30 08:42:44 +0200

    Add golden tests.

---
+++ libs/wire-api/test/golden/Test/Wire/API/Golden/Manual/CannonId.hs
@@ -0,0 +1,34 @@
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2022 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Test.Wire.API.Golden.Manual.CannonId
+  ( testObject_CannonId_1,
+    testObject_CannonId_2,
+    testObject_CannonId_3,
+  )
+where
+
+import Wire.API.CannonId
+
+testObject_CannonId_1 :: CannonId
+testObject_CannonId_1 = CannonId ""
+
+testObject_CannonId_2 :: CannonId
+testObject_CannonId_2 = CannonId "sdfiou"
+
+testObject_CannonId_3 :: CannonId
+testObject_CannonId_3 = CannonId "1!_*`'\""
---
+++ libs/wire-api/test/golden/Test/Wire/API/Golden/Manual/Presence.hs
@@ -0,0 +1,47 @@
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2022 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Test.Wire.API.Golden.Manual.Presence
+  ( testObject_Presence_1,
+    testObject_Presence_2,
+  )
+where
+
+import Data.Id
+import Data.UUID qualified as UUID
+import Imports
+import Wire.API.Presence
+
+testObject_Presence_1 :: Presence
+testObject_Presence_1 =
+  Presence
+    (Id . fromJust $ UUID.fromString "174ccaea-7f26-11ef-86cc-27bb6bf3b319")
+    (ConnId "wef")
+    (fromJust $ parse "http://example.com/")
+    Nothing
+    0
+    ""
+
+testObject_Presence_2 :: Presence
+testObject_Presence_2 =
+  Presence
+    (Id . fromJust $ UUID.fromString "174ccaea-7f26-11ef-86cc-37bb6bf3b319")
+    (ConnId "wef3")
+    (fromJust $ parse "http://example.com/3")
+    (Just (ClientId 1))
+    12323
+    "" -- __field always has to be "", see ToSchema instance.
