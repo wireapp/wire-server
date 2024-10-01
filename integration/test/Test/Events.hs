78cfda935e4ec291e8a86b0e3fe6c58bae73f971
Akshay Mankar <akshay@wire.com>
2024-10-01 16:12:34 +0200

    integration: Assert that acked events don't come back

--- integration/test/Test/Events.hs
+++ integration/test/Test/Events.hs
@@ -2,6 +2,7 @@ module Test.Events where
 
 import API.Brig
 import API.BrigCommon
+import API.Common
 import Control.Concurrent.STM (atomically)
 import Control.Concurrent.STM.TChan
 import Data.ByteString.Conversion (toByteString')
@@ -9,7 +10,7 @@ import qualified Network.WebSockets.Client as WS
 import qualified Network.WebSockets.Connection as WS
 import SetupHelpers
 import Testlib.Prelude
-import UnliftIO (Async, async, cancel, race)
+import UnliftIO (Async, async, cancel, race, waitAny)
 import UnliftIO.Concurrent (threadDelay)
 
 testConsumeEvents :: (HasCallStack) => App ()
@@ -17,34 +18,53 @@ testConsumeEvents = do
   alice <- randomUser OwnDomain def
   client <- addClient alice def {acapabilities = Just ["consumable-notifications"]} >>= getJSON 201
   clientId <- objId client
-  eventsChan <- liftIO newTChanIO
-  wsThread <- eventsWebSocket alice clientId eventsChan
-  mEvent <- race (threadDelay 1_000_000) (liftIO $ atomically (readTChan eventsChan))
-  case mEvent of
-    Left () -> assertFailure "No event recieved for 1s"
-    Right e -> do
-      e %. "payload.0.type" `shouldMatch` "user.client-add"
-      e %. "payload.0.client.id" `shouldMatch` clientId
-  cancel wsThread
+  do
+    eventsChan <- liftIO newTChanIO
+    ackChan <- liftIO newTChanIO
+    wsThread <- eventsWebSocket alice clientId eventsChan ackChan
+    mEvent <- race (threadDelay 1_000_000) (liftIO $ atomically (readTChan eventsChan))
+    case mEvent of
+      Left () -> assertFailure "No event recieved for 1s"
+      Right e -> do
+        e %. "payload.0.type" `shouldMatch` "user.client-add"
+        e %. "payload.0.client.id" `shouldMatch` clientId
+        deliveryTag <- e %. "delivery_tag"
+        liftIO $ atomically $ writeTChan ackChan $ object ["ack" .= deliveryTag]
+    cancel wsThread
 
-eventsWebSocket :: (MakesValue user) => user -> String -> TChan Value -> App (Async ())
-eventsWebSocket user clientId eventsChan = do
+  handle <- randomHandle
+  putHandle alice handle >>= assertSuccess
+  do
+    eventsChan <- liftIO newTChanIO
+    ackChan <- liftIO newTChanIO
+    wsThread <- eventsWebSocket alice clientId eventsChan ackChan
+    mEvent <- race (threadDelay 1_000_000) (liftIO $ atomically (readTChan eventsChan))
+    case mEvent of
+      Left () -> assertFailure "No event recieved for 1s"
+      Right e -> do
+        e %. "payload.0.type" `shouldMatch` "user.update"
+        e %. "payload.0.user.handle" `shouldMatch` handle
+    cancel wsThread
+
+eventsWebSocket :: (MakesValue user) => user -> String -> TChan Value -> TChan Value -> App (Async ())
+eventsWebSocket user clientId eventsChan ackChan = do
   serviceMap <- getServiceMap =<< objDomain user
   uid <- objId =<< objQidObject user
   let HostPort caHost caPort = serviceHostPort serviceMap Cannon
       path = "/events?client=" <> clientId
       caHdrs = [(fromString "Z-User", toByteString' uid)]
-      app = wsRead
-      -- r <- async wsRead
-      -- w <- async wsWrite
+      app conn = do
+        r <- async $ wsRead conn
+        w <- async $ wsWrite conn
+        void $ waitAny [r, w]
       wsRead conn = forever $ do
         bs <- WS.receiveData conn
         case decodeStrict' bs of
           Just n -> atomically $ writeTChan eventsChan n
           Nothing -> putStrLn $ "Failed to decode events: " ++ show bs
-  -- wsWrite = forever $ do
-  --   takeMVar latch
-  --   WS.sendClose conn ("close" :: ByteString)
+      wsWrite conn = forever $ do
+        ack <- atomically $ readTChan ackChan
+        WS.sendBinaryData conn (encode ack)
   liftIO
     $ async
     $ WS.runClientWith
