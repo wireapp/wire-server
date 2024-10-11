bfe5384b7d02aa0051d225852f0d5a2ba9889e67
Matthias Fischmann <mf@zerobuzz.net>
2024-10-11 10:18:26 +0200

    Fix: default parameters.

--- libs/wire-api/src/Wire/API/Password.hs
+++ libs/wire-api/src/Wire/API/Password.hs
@@ -125,8 +125,7 @@ defaultOptions :: Argon2.Options
 defaultOptions =
   Argon2.Options
     { iterations = 1,
-      -- TODO: fix this after meeting with Security
-      memory = 2 ^ (17 :: Int),
+      memory = 180224,
       parallelism = 32,
       variant = Argon2.Argon2id,
       version = Argon2.Version13
