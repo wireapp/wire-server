a23d6fab47192761cf11b68f91d2b45995728a9f
Leif Battermann <leif.battermann@wire.com>
2024-10-11 08:23:39 +0000

    getUserRecord implemented

--- services/galley/src/Galley/API/Teams/Export.hs
+++ services/galley/src/Galley/API/Teams/Export.hs
@@ -1,23 +1,42 @@
 module Galley.API.Teams.Export (getUserRecord) where
 
-import Control.Lens ((^.))
+import Control.Lens (view, (^.))
 import Galley.Effects
 import Galley.Effects.BrigAccess
+import Galley.Effects.SparAccess qualified as Spar
 import Imports
 import Polysemy
 import Wire.API.Error
 import Wire.API.Error.Galley
 import Wire.API.Team.Export
 import Wire.API.Team.Member
+import Wire.API.User (ScimUserInfo (suiCreatedOn), User (..))
 
 getUserRecord ::
   ( Member (ErrorS TeamMemberNotFound) r,
-    Member BrigAccess r
+    Member BrigAccess r,
+    Member Spar.SparAccess r
   ) =>
   TeamMember ->
   Sem r TeamExportUser
 getUserRecord member = do
   let uid = member ^. userId
   export <- getUserExportData uid >>= noteS @TeamMemberNotFound
-  -- TODO
-  pure export
+  mCreatedOn <- do
+    let mFromInvitation = snd <$> member ^. invitation
+    case mFromInvitation of
+      Just ts -> pure $ Just ts
+      Nothing -> do
+        -- TODO: make this a single user query
+        suis <- Spar.lookupScimUserInfos [uid]
+        pure $ listToMaybe suis >>= suiCreatedOn
+  -- TODO: optimize!
+  let mInviterId = fst <$> member ^. invitation
+  users <- getUsers (maybeToList mInviterId)
+  let invitedBy = listToMaybe users >>= userHandle
+  pure
+    export
+      { tExportInvitedBy = invitedBy,
+        tExportRole = permissionsRole . view permissions $ member,
+        tExportCreatedOn = mCreatedOn
+      }
