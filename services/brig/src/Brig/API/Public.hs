7b52d8b6994d82fde19eb32bbe2b9feb2fc8ecf8
Paolo Capriotti <paolo@capriotti.io>
2024-10-22 14:55:01 +0200

    Fix overlapping paths in API v7

--- services/brig/src/Brig/API/Public.hs
+++ services/brig/src/Brig/API/Public.hs
@@ -438,7 +438,9 @@ servantSitemap =
 
     userHandleAPI :: ServerT UserHandleAPI (Handler r)
     userHandleAPI =
-      Named @"check-user-handles" checkHandles
+      Named @"check-user-handles@v6" checkHandles
+        :<|> Named @"check-user-handles" checkHandles
+        :<|> Named @"check-user-handle@v6" checkHandle
         :<|> Named @"check-user-handle" checkHandle
 
     searchAPI :: ServerT SearchAPI (Handler r)
