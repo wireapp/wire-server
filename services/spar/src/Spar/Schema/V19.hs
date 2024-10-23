97ced4378daf5462338d4cd14f0849af736d9bd8
Leif Battermann <leif.battermann@wire.com>
2024-10-23 13:47:40 +0000

    db migration

---
+++ services/spar/src/Spar/Schema/V19.hs
@@ -0,0 +1,36 @@
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2022 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
+module Spar.Schema.V19
+  ( migration,
+  )
+where
+
+import Cassandra.Schema
+import Imports
+import Text.RawString.QQ
+
+migration :: Migration
+migration = Migration 19 "Add name column to scim token info" $ do
+  schema'
+    [r|
+        ALTER TABLE team_provisioning_by_team ADD (name text);
+      |]
+  schema'
+    [r|
+        ALTER TABLE team_provisioning_by_token  ADD (name text);
+      |]
