6d32c35b78e683bc1104a07b84149cda6baa2899
Leif Battermann <leif.battermann@wire.com>
2024-12-09 11:35:31 +0000

    test not-found and verify response body

--- integration/test/Test/EnterpriseLogin.hs
+++ integration/test/Test/EnterpriseLogin.hs
@@ -1,12 +1,18 @@
 module Test.EnterpriseLogin where
 
 import API.BrigInternal
+import API.Common
 import Testlib.Prelude
 
 testDomainRegistrationLock :: App ()
 testDomainRegistrationLock = do
-  let domain = "example.com"
+  domain <- randomDomain
+  bindResponse (getDomainRegistration OwnDomain domain) $ \resp -> do
+    resp.status `shouldMatchInt` 404
   bindResponse (domainRegistrationLock OwnDomain domain) $ \resp -> do
     resp.status `shouldMatchInt` 200
   bindResponse (getDomainRegistration OwnDomain domain) $ \resp -> do
     resp.status `shouldMatchInt` 200
+    resp.json %. "domain" `shouldMatch` domain
+    resp.json %. "domain_redirect" `shouldMatch` "locked"
+    resp.json %. "team_invite" `shouldMatch` "allowed"
