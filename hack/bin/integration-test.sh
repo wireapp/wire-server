89262afaf92b236c6359ddf610b6991f15bca0d0
Leif Battermann <leif.battermann@wire.com>
2024-12-13 13:46:18 +0000

    [tmp] revert this: only run some tests

--- hack/bin/integration-test.sh
+++ hack/bin/integration-test.sh
@@ -11,7 +11,7 @@ UPLOAD_LOGS=${UPLOAD_LOGS:-0}
 echo "Running integration tests on wire-server with parallelism=${HELM_PARALLELISM} ..."
 
 CHART=wire-server
-tests=(integration stern galley cargohold gundeck federator spar brig)
+tests=(integration) # stern galley cargohold gundeck federator spar brig)
 
 cleanup() {
     if ((CLEANUP_LOCAL_FILES > 0)); then
