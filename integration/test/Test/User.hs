acbdca880e514abfcf63f573bcb3e60cf4b63fa0
Akshay Mankar <akshay@wire.com>
2025-01-06 12:28:49 +0100

    integration: Run only the test we want to deflake

--- integration/test/Test/User.hs
+++ integration/test/Test/User.hs
@@ -179,8 +179,15 @@ testActivateAccountWithPhoneV5 = do
     resp.status `shouldMatchInt` 400
     resp.json %. "label" `shouldMatch` "bad-request"
 
-testMigratingPasswordHashingAlgorithm :: (HasCallStack) => App ()
-testMigratingPasswordHashingAlgorithm = do
+data Deflake = Deflake
+
+instance TestCases Deflake where
+  mkTestCases =
+    pure . flip map [(0 :: Int) .. 20] $ \n ->
+      MkTestCase {testCaseName = "Deflake" <> show n, testCase = Deflake}
+
+testMigratingPasswordHashingAlgorithm :: (HasCallStack) => Deflake -> App ()
+testMigratingPasswordHashingAlgorithm _ = do
   let argon2idOpts =
         object
           [ "algorithm" .= "argon2id",
