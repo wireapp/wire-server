5fb6da5b10a6d6b743482c982853b00e9b3380fc
Sven Tennie <sven.tennie@wire.com>
2025-01-17 11:17:43 +0100

    Fix linting issue

--- libs/wire-subsystems/test/unit/Wire/MockInterpreters/GalleyAPIAccess.hs
+++ libs/wire-subsystems/test/unit/Wire/MockInterpreters/GalleyAPIAccess.hs
@@ -18,11 +18,11 @@ miniGalleyAPIAccess ::
 miniGalleyAPIAccess member configs = interpret $ \case
   CreateSelfConv _ -> error "CreateSelfConv not implemented in miniGalleyAPIAccess"
   GetConv _ _ -> error "GetConv not implemented in miniGalleyAPIAccess"
-  GetTeamConv _ _ _ -> error "GetTeamConv not implemented in miniGalleyAPIAccess"
+  GetTeamConv {} -> error "GetTeamConv not implemented in miniGalleyAPIAccess"
   NewClient _ _ -> error "NewClient not implemented in miniGalleyAPIAccess"
   CheckUserCanJoinTeam _ -> error "CheckUserCanJoinTeam not implemented in miniGalleyAPIAccess"
-  AddTeamMember _ _ _ _ -> error "AddTeamMember not implemented in miniGalleyAPIAccess"
-  CreateTeam _ _ _ -> error "CreateTeam not implemented in miniGalleyAPIAccess"
+  AddTeamMember {} -> error "AddTeamMember not implemented in miniGalleyAPIAccess"
+  CreateTeam {} -> error "CreateTeam not implemented in miniGalleyAPIAccess"
   GetTeamMember _ _ -> pure member
   GetTeamMembers _ -> error "GetTeamMembers not implemented in miniGalleyAPIAccess"
   GetTeamId _ -> error "GetTeamId not implemented in miniGalleyAPIAccess"
@@ -31,14 +31,14 @@ miniGalleyAPIAccess member configs = interpret $ \case
   GetTeamLegalHoldStatus _ -> error "GetTeamLegalHoldStatus not implemented in miniGalleyAPIAccess"
   GetUserLegalholdStatus _ _ -> error "GetUserLegalholdStatus not implemented in miniGalleyAPIAccess"
   GetTeamSearchVisibility _ -> error "GetTeamSearchVisibility not implemented in miniGalleyAPIAccess"
-  ChangeTeamStatus _ _ _ -> error "ChangeTeamStatus not implemented in miniGalleyAPIAccess"
+  ChangeTeamStatus {} -> error "ChangeTeamStatus not implemented in miniGalleyAPIAccess"
   MemberIsTeamOwner _ _ -> error "MemberIsTeamOwner not implemented in miniGalleyAPIAccess"
   GetAllTeamFeaturesForUser _ -> pure configs
   GetFeatureConfigForTeam tid -> pure $ getFeatureConfigForTeamImpl configs tid
   GetVerificationCodeEnabled _ -> error "GetVerificationCodeEnabled not implemented in miniGalleyAPIAccess"
   GetExposeInvitationURLsToTeamAdmin _ -> error "GetExposeInvitationURLsToTeamAdmin not implemented in miniGalleyAPIAccess"
   IsMLSOne2OneEstablished _ _ -> error "IsMLSOne2OneEstablished not implemented in miniGalleyAPIAccess"
-  UnblockConversation _ _ _ -> error "UnblockConversation not implemented in miniGalleyAPIAccess"
+  UnblockConversation {} -> error "UnblockConversation not implemented in miniGalleyAPIAccess"
   GetEJPDConvInfo _ -> error "GetEJPDConvInfo not implemented in miniGalleyAPIAccess"
 
 getFeatureConfigForTeamImpl :: forall feature. (IsFeatureConfig feature) => AllTeamFeatures -> TeamId -> LockableFeature feature
