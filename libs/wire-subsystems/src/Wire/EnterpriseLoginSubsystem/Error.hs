3b39ebbc40ea4541fe356eade413261f05a346fc
Matthias Fischmann <mf@zerobuzz.net>
2025-01-20 09:26:06 +0100

    Polish error codes / messages.

--- libs/wire-subsystems/src/Wire/EnterpriseLoginSubsystem/Error.hs
+++ libs/wire-subsystems/src/Wire/EnterpriseLoginSubsystem/Error.hs
@@ -37,17 +37,12 @@ enterpriseLoginSubsystemErrorToHttpError =
     EnterpriseLoginSubsystemUnAuthorizeError -> Wai.mkError status409 "unauthorize-error" "Domain redirect can not bet set to unauthorized when locked or SSO"
     EnterpriseLoginSubsystemPreAuthorizeError -> Wai.mkError status409 "preauthorize-error" "Domain redirect must be 'none' to be pre-authorized"
     EnterpriseLoginSubsystemGuardFailed err ->
-      -- TODO: maybe we can show these wai errors to client devs and see if they have change
-      -- requests?  also, are we leaking too much info?
-      let e409 msg = Wai.mkError status409 "enterprise-login-guard-failed" ("condition failed: " <> msg)
-          e423 msg = Wai.mkError status423 "enterprise-login-guard-failed" ("could not parse domain: " <> LT.pack msg)
+      let e403 msg = Wai.mkError status403 "condition-failed" msg
+          e400 msg = Wai.mkError status400 "invalid-domain" (LT.pack msg)
        in case err of
-            DomRedirSetToSSO -> e409 "`domain_redirect` is set to `sso:{code}`"
-            DomRedirSetToBackend -> e409 "`domain_redirect` is set to `backend`" -- TODO: https://wearezeta.atlassian.net/wiki/spaces/ENGINEERIN/pages/1570832467/Email+domain+registration+and+configuration says this should be 403 with another label?
-            DomRedirSetToNoRegistration -> e409 "`domain_redirect` is set to `no-registration`"
-            TeamInviteSetToNotAllowed -> e409 "`teamInvite` is set to `not-allowed`"
-            TeamInviteRestrictedToOtherTeam -> e409 "`teamInvite` is restricted to another team."
-            InvalidDomain msg -> e423 msg
-  where
-    status423 :: Status
-    status423 = mkStatus 423 "Locked"
+            DomRedirSetToSSO -> e403 "`domain_redirect` is set to `sso:{code}`"
+            DomRedirSetToBackend -> e403 "`domain_redirect` is set to `backend`"
+            DomRedirSetToNoRegistration -> e403 "`domain_redirect` is set to `no-registration`"
+            TeamInviteSetToNotAllowed -> e403 "`teamInvite` is set to `not-allowed`"
+            TeamInviteRestrictedToOtherTeam -> e403 "`teamInvite` is restricted to another team."
+            InvalidDomain msg -> e400 msg -- probably impossible.
