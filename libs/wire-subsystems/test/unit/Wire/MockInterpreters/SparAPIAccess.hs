e5d00e5cc7f56bff2e245e938e04c6eedae50db8
Paolo Capriotti <paolo@capriotti.io>
2025-01-24 08:33:15 +0100

    Fix spacing

--- libs/wire-subsystems/test/unit/Wire/MockInterpreters/SparAPIAccess.hs
+++ libs/wire-subsystems/test/unit/Wire/MockInterpreters/SparAPIAccess.hs
@@ -7,4 +7,4 @@ import Wire.SparAPIAccess
 -- | interprets galley by statically returning the values passed
 miniSparAPIAccess :: InterpreterFor SparAPIAccess r
 miniSparAPIAccess = interpret $ \case
-  GetIdentityProviders _ -> error "GetIdentityProviders  not implemented in miniSparAPIAccess"
+  GetIdentityProviders _ -> error "GetIdentityProviders not implemented in miniSparAPIAccess"
