e21f6aad8f7faca2a73d3d18ebf9edf8cf7bf240
Akshay Mankar <akshay@wire.com>
2025-01-28 09:02:05 +0100

    integration: Move test for searching for whitelisted services

--- integration/test/API/Brig.hs
+++ integration/test/API/Brig.hs
@@ -609,6 +609,14 @@ newService dom providerId service = do
     resp.status `shouldMatchInt` 201
     resp.json
 
+getService :: (HasCallStack, MakesValue domain) => domain -> String -> String -> App Response
+getService domain pid sid = do
+  req <- baseRequest domain Brig Versioned $ joinHttpPath ["provider", "services", sid]
+  submit "GET" $
+    req
+      & addHeader "Z-Type" "provider"
+      & addHeader "Z-Provider" pid
+
 updateService ::
   ( HasCallStack,
     MakesValue dom,
@@ -636,7 +644,11 @@ updateService dom providerId serviceId mAcceptHeader newName = do
     $ req
 
 updateServiceConn ::
-  (MakesValue conn) =>
+  ( HasCallStack,
+    MakesValue domain,
+    MakesValue conn
+  ) =>
+  domain ->
   -- | providerId
   String ->
   -- | serviceId
@@ -644,8 +656,8 @@ updateServiceConn ::
   -- | connection update as a Json object, with an obligatory "password" field
   conn ->
   App Response
-updateServiceConn providerId serviceId connectionUpdate = do
-  req <- baseRequest OwnDomain Brig Versioned do
+updateServiceConn domain providerId serviceId connectionUpdate = do
+  req <- baseRequest domain Brig Versioned do
     joinHttpPath ["provider", "services", serviceId, "connection"]
   upd <- make connectionUpdate
   submit "PUT"
@@ -885,3 +897,16 @@ postServiceWhitelist user tid update = do
           "whitelist"
         ]
   submit "POST" (addJSON updateJson req)
+
+listTeamServiceProfilesByPrefix :: (MakesValue user) => user -> String -> Maybe String -> Bool -> Int -> App Response
+listTeamServiceProfilesByPrefix user tid mPrefix filterDisabled size = do
+  req <- baseRequest user Brig Versioned $ joinHttpPath ["teams", tid, "services", "whitelisted"]
+  submit "GET" $
+    req
+      & addQueryParams
+        ( catMaybes
+            [ ("prefix",) <$> mPrefix,
+              if filterDisabled then Nothing else Just ("filter_disabled", "false"),
+              Just ("size", show size)
+            ]
+        )
--- integration/test/Test/Provider.hs
+++ integration/test/Test/Provider.hs
@@ -22,3 +22,117 @@ testProviderUploadAsset = do
   -- test Nginz API
   bindResponse (Nginz.uploadProviderAsset OwnDomain (cs cookie) "another profile pic") $ \resp -> do
     resp.status `shouldMatchInt` 201
+
+testProviderSearchWhitelist :: (HasCallStack) => App ()
+testProviderSearchWhitelist =
+  withModifiedBackend
+    def
+      { brigCfg =
+          -- Disable password hashing rate limiting, so we can create enable services quickly
+          setField @_ @Int "optSettings.setPasswordHashingRateLimit.userLimit.inverseRate" 0
+      }
+    $ \domain -> do
+      (owner, tid, [user]) <- createTeam domain 2
+      provider <- setupProvider owner def {newProviderPassword = Just defPassword}
+      pid <- asString $ provider %. "id"
+      namePrefix <- randomString 10
+
+      services <-
+        forM (taggedServiceNames namePrefix) $ \(name, tags) -> do
+          createAndEnableService domain owner tid pid def {newServiceName = name, newServiceTags = tags}
+
+      allServiceIds <- traverse (%. "id") services
+
+      -- Searching with the common prefix shows all of them
+      listTeamServiceProfilesByPrefix user tid (Just namePrefix) False 20 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        asListOf (%. "id") (resp.json %. "services") `shouldMatch` allServiceIds
+
+      -- Searching without filtering returns all of them because all are enabled
+      listTeamServiceProfilesByPrefix user tid (Just namePrefix) True 20 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        asListOf (%. "id") (resp.json %. "services") `shouldMatch` allServiceIds
+
+      -- Search should yield services ordered by named
+      zerosPrefixedService <- do
+        serviceSuffix <- randomString 10
+        createAndEnableService domain owner tid pid def {newServiceName = "0000000000|" <> serviceSuffix, newServiceTags = ["social"]}
+      listTeamServiceProfilesByPrefix user tid Nothing True 20 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        firstServiceId <- (%. "id") . head =<< asList (resp.json %. "services")
+        firstServiceId `shouldMatch` (zerosPrefixedService %. "id")
+
+      -- Search by exact name yields only one service
+      forM_ (take 3 services) $ \service -> do
+        name <- asString $ service %. "name"
+        listTeamServiceProfilesByPrefix user tid (Just name) False 20 `bindResponse` \resp -> do
+          resp.status `shouldMatchInt` 200
+          resp.json %. "has_more" `shouldMatch` False
+          asListOf (%. "name") (resp.json %. "services") `shouldMatch` [name]
+
+      -- Search by prefix, case insensitve, doesn't asciify special characters
+      -- like name search
+      forM_ [("Bjø", "Bjørn"), ("Bjo", "bjorn"), ("chris", "CHRISTMAS")] $ \(searchTerm, hardcodedName) ->
+        listTeamServiceProfilesByPrefix user tid (Just $ namePrefix <> "|" <> searchTerm) False 20 `bindResponse` \resp -> do
+          resp.status `shouldMatchInt` 200
+          resp.json %. "has_more" `shouldMatch` False
+          asListOf (%. "name") (resp.json %. "services") `shouldMatch` [(namePrefix <> "|" <> hardcodedName)]
+
+      -- Search works even after changing name
+      let alphaService = head services
+          newAlphaName = namePrefix <> "|" <> "NotAlphaAnyMore"
+      alphaServiceId <- alphaService %. "id"
+      updateService domain pid alphaServiceId Nothing (Just newAlphaName)
+        >>= assertSuccess
+      listTeamServiceProfilesByPrefix user tid (Just newAlphaName) False 20 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.json %. "services.0.id" `shouldMatch` alphaServiceId
+        resp.json %. "services.0.name" `shouldMatch` newAlphaName
+
+createAndEnableService ::
+  (MakesValue domain, MakesValue user) =>
+  domain ->
+  user ->
+  String ->
+  String ->
+  NewService ->
+  App Value
+createAndEnableService domain teamAdmin tid pid newSvc = do
+  serviceId <- (newService domain pid newSvc) %. "id" & asString
+  -- serviceId <- asString $ service %. "id"
+  updateServiceConn domain pid serviceId (object ["password" .= defPassword, "enabled" .= True])
+    >>= assertSuccess
+  postServiceWhitelist teamAdmin tid (object ["id" .= serviceId, "provider" .= pid, "whitelisted" .= True])
+    >>= assertSuccess
+  getService domain pid serviceId >>= getJSON 200
+
+-- | A list of 20 services ordered alphabetically, all having names that begin
+-- with the given prefix.
+--
+-- NB: in some of the tests above, we depend on the fact that there are exactly
+-- 20 services here and the fact that they are ordered alphabetically.
+taggedServiceNames :: String -> [(String, [String])]
+taggedServiceNames prefix =
+  [ (prefixed "Alpha", ["social", "quiz", "business"]),
+    (prefixed "Beta", ["social", "music", "lifestyle"]),
+    (prefixed "bjorn", ["social", "quiz", "travel"]),
+    (prefixed "Bjørn", ["social", "music", "lifestyle"]),
+    (prefixed "CHRISTMAS", ["social", "quiz", "weather"]),
+    (prefixed "Delta", ["social", "music", "lifestyle"]),
+    (prefixed "Epsilon", ["social", "quiz", "business"]),
+    (prefixed "Freer", ["social", "music", "lifestyle"]),
+    (prefixed "Gamma", ["social", "quiz", "weather"]),
+    (prefixed "Gramma", ["social", "music", "lifestyle"]),
+    (prefixed "Hera", ["social", "quiz", "travel"]),
+    (prefixed "Io", ["social", "music", "lifestyle"]),
+    (prefixed "Jojo", ["social", "quiz", "weather"]),
+    (prefixed "Kuba", ["social", "music", "lifestyle"]),
+    (prefixed "Lawn", ["social", "quiz", "travel"]),
+    (prefixed "Mango", ["social", "music", "lifestyle"]),
+    (prefixed "North", ["social", "quiz", "weather"]),
+    (prefixed "Yak", ["social", "music", "lifestyle"]),
+    (prefixed "Zeta", ["social", "quiz", "travel"]),
+    (prefixed "Zulu", ["social", "music", "lifestyle"])
+  ]
+  where
+    prefixed n = (prefix <> "|" <> n)
