7ac0425e9421281e8cc45f05f46f2ab0ed4830e2
jschaul <jschaul@users.noreply.github.com>
2025-02-11 17:58:42 +0100

    Cleanup ingress classes after integration tests to not keep these objects lingering forever

--- hack/bin/integration-spring-cleaning.sh
+++ hack/bin/integration-spring-cleaning.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+"${DIR}"/integration-teardown-ingress-classes.sh
+
 IFS=$'\n'
 for NAMESPACE in $(kubectl get namespaces | grep "^test-" | awk '{print $1}'); do
     echo "$NAMESPACE"
