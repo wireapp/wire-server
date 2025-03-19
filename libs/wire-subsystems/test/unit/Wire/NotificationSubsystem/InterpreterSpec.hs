f975565c4444a988c85db8be53f0d4a2c6042cff
Paolo Capriotti <paolo@capriotti.io>
2025-03-19 10:39:39 +0100

    Fix toV2Push unit test

--- libs/wire-subsystems/test/unit/Wire/NotificationSubsystem/InterpreterSpec.hs
+++ libs/wire-subsystems/test/unit/Wire/NotificationSubsystem/InterpreterSpec.hs
@@ -233,7 +233,8 @@ spec = describe "NotificationSubsystem.Interpreter" do
             .&&. v2Push._pushTransient === pushToUser.transient
             .&&. v2Push._pushNativePriority === fromMaybe V2.HighPriority pushToUser.nativePriority
             .&&. v2Push._pushPayload === List1.singleton pushToUser.json
-            .&&. Set.map V2._recipientRoute v2Push._pushRecipients === Set.singleton pushToUser.route
+            .&&. Set.map V2._recipientRoute v2Push._pushRecipients
+              === Set.fromList (map (const pushToUser.route) (toList v2Push._pushRecipients))
             .&&. Set.map (\r -> Recipient r._recipientId r._recipientClients) v2Push._pushRecipients
               === Set.fromList (toList pushToUser.recipients)
 
