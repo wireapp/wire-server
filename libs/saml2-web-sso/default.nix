e2bcddcd03dfcca7047ca7cccc3ab3000a8c0d53
Matthias Fischmann <mf@zerobuzz.net>
2025-04-04 14:10:53 +0200

    Fixup

--- libs/saml2-web-sso/default.nix
+++ libs/saml2-web-sso/default.nix
@@ -69,7 +69,6 @@
 , uuid
 , wai
 , wai-extra
-, wai-utilities
 , warp
 , word8
 , xml-conduit
@@ -82,8 +81,6 @@ mkDerivation {
   pname = "saml2-web-sso";
   version = "0.20";
   src = gitignoreSource ./.;
-  isLibrary = true;
-  isExecutable = true;
   libraryHaskellDepends = [
     aeson
     asn1-encoding
@@ -147,74 +144,6 @@ mkDerivation {
     uuid
     wai
     wai-extra
-    warp
-    word8
-    xml-conduit
-    xml-conduit-writer
-    xml-hamlet
-    xml-types
-    yaml
-  ];
-  executableHaskellDepends = [
-    aeson
-    asn1-encoding
-    asn1-parse
-    asn1-types
-    base
-    base64-bytestring
-    binary
-    bytestring
-    case-insensitive
-    containers
-    cookie
-    crypton
-    crypton-x509
-    data-default
-    directory
-    dns
-    email-validate
-    errors
-    exceptions
-    extra
-    filepath
-    foundation
-    ghc-prim
-    hedgehog
-    hedgehog-quickcheck
-    hourglass
-    hsaml2
-    hspec
-    hspec-wai
-    http-media
-    http-types
-    hxt
-    imports
-    lens
-    lens-datetime
-    memory
-    mtl
-    network-uri
-    pretty-show
-    process
-    QuickCheck
-    quickcheck-instances
-    random
-    servant
-    servant-multipart
-    servant-server
-    shelly
-    silently
-    string-conversions
-    temporary
-    text
-    time
-    transformers
-    uniplate
-    uri-bytestring
-    uuid
-    wai
-    wai-extra
-    wai-utilities
     warp
     word8
     xml-conduit
@@ -296,5 +225,4 @@ mkDerivation {
   testToolDepends = [ hspec-discover ];
   description = "Library and example web app for the SAML Web-based SSO profile";
   license = lib.licenses.agpl3Only;
-  mainProgram = "toy-sp";
 }
