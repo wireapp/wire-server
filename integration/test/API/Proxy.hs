c36f36cf4ed15716988c2b4d92e43541ca2cc1eb
Matthias Fischmann <mf@zerobuzz.net>
2025-04-08 15:00:53 +0200

    Make test more exhaustive.

--- integration/test/API/Proxy.hs
+++ integration/test/API/Proxy.hs
@@ -2,17 +2,7 @@ module API.Proxy where
 
 import Testlib.Prelude
 
-getGiphy :: (HasCallStack, MakesValue caller) => caller -> App Response
-getGiphy caller = do
-  req <- baseRequest caller WireProxy Unversioned "/proxy/giphy/v1/gifs/search"
-  submit "GET" (req & addQueryParams [("q", "monday"), ("limit", "100"), ("offset", "0")])
-
-getGiphyNotFound :: (HasCallStack, MakesValue caller) => caller -> App Response
-getGiphyNotFound caller = do
-  req <- baseRequest caller WireProxy Unversioned "/proxy/giphy/v1/gifs/search/sorch"
-  submit "GET" req
-
-getGiphyBadRequest :: (HasCallStack, MakesValue caller) => caller -> App Response
-getGiphyBadRequest caller = do
-  req <- baseRequest caller WireProxy Unversioned "/proxy/giphy/v1/gifs/search?limit=true"
-  submit "GET" req
+getGiphy :: (HasCallStack, MakesValue caller) => caller -> String -> [(String, String)] -> App Response
+getGiphy caller path qparams = do
+  req <- baseRequest caller WireProxy Unversioned ("/proxy/giphy/v1/gifs/" <> path)
+  submit "GET" (req & addQueryParams qparams)
