6ee9b73563ed9c18a3d3080c75c05b3d7ebc8947
Matthias Fischmann <mf@zerobuzz.net>
2025-04-16 14:56:43 +0200

    Implement pagination interface.

--- libs/wire-subsystems/src/Wire/UserGroupSubsystem.hs
+++ libs/wire-subsystems/src/Wire/UserGroupSubsystem.hs
@@ -4,6 +4,7 @@
 module Wire.UserGroupSubsystem where
 
 import Data.Id
+import Data.UUID
 import Imports
 import Polysemy
 import Wire.API.UserGroup
@@ -13,8 +14,8 @@ data UserGroupSubsystem m a where
   CreateGroup :: NewUserGroup -> UserGroupSubsystem m UserGroup
   -- exposed as GET /user-groups/:d
   GetGroup :: UserGroupId -> UserGroupSubsystem m (Maybe UserGroup)
-  -- exposed as GET /user-groups?pagingState=?
-  GetGroups :: Text -> UserGroupSubsystem m UserGroupPage
+  -- exposed as GET /user-groups?limit=?&&last_key=?.  limit=0 means no limit.  default limit is 100.
+  GetGroups :: Maybe Int -> Maybe UUID -> UserGroupSubsystem m UserGroupPage
   -- exposed as PUT /user-groups/:id
   UpdateGroup :: UserGroupId -> UserGroupUpdate -> UserGroupSubsystem m UserGroup
   -- exposed as DELETE /user-groups/:id
