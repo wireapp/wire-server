c137fe4fc7f6791e07edbab09d381d732c422926
Matthias Fischmann <mf@zerobuzz.net>
2025-04-16 17:06:08 +0200

    Work-around mysteriously missing -XImportQualifiedPost.
    in wire-subsystems tests

--- libs/wire-subsystems/test/Main.hs
+++ libs/wire-subsystems/test/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
 import Imports
-import Spec qualified
-import Test.Hspec qualified as H
+import qualified Spec
+import qualified Test.Hspec as H
 
 main :: IO ()
 main = H.hspec Spec.spec
