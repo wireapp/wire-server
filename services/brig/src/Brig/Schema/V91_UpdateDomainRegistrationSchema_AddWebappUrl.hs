358499e4d6d2cd30fadae5f9a14391b47203ad7c
Sven Tennie <sven.tennie@wire.com>
2025-04-17 16:04:29 +0200

    Adjust (c)opyright

--- services/brig/src/Brig/Schema/V91_UpdateDomainRegistrationSchema_AddWebappUrl.hs
+++ services/brig/src/Brig/Schema/V91_UpdateDomainRegistrationSchema_AddWebappUrl.hs
@@ -2,7 +2,7 @@
 
 -- This file is part of the Wire Server implementation.
 --
--- Copyright (C) 2023 Wire Swiss GmbH <opensource@wire.com>
+-- Copyright (C) 2025 Wire Swiss GmbH <opensource@wire.com>
 --
 -- This program is free software: you can redistribute it and/or modify it under
 -- the terms of the GNU Affero General Public License as published by the Free
