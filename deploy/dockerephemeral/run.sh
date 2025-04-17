94b020f6c4c4667df56ad825d991cf8bd4fa171b
Leif Battermann <leif.battermann@wire.com>
2025-04-17 12:57:31 +0000

    fix fed v2 docker configs

--- deploy/dockerephemeral/run.sh
+++ deploy/dockerephemeral/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# To start the federation v0, v1 backends, set ENABLE_FEDERATION_V0=1, ENABLE_FEDERATION_V1=1
+# To start the federation v0, v1, v2 backends, set ENABLE_FEDERATION_V0=1, ENABLE_FEDERATION_V1=1, ENABLE_FEDERATION_V2=1
 # in the env where this script is run
 
 set -e
@@ -11,8 +11,8 @@ DOCKER_FILE="$SCRIPT_DIR/docker-compose.yaml"
 FED_VERSIONS=(0 1 2)
 
 if [ -e "${SCRIPT_DIR}/run.before.hook.local" ]; then
-    # shellcheck disable=SC1091
-    . "${SCRIPT_DIR}/run.before.hook.local"
+  # shellcheck disable=SC1091
+  . "${SCRIPT_DIR}/run.before.hook.local"
 fi
 
 opts=("--file" "$DOCKER_FILE")
@@ -34,8 +34,8 @@ cleanup() {
 if [ -z "$1" ]; then
   dc up -d
   if [ -e "${SCRIPT_DIR}/run.after.hook.local" ]; then
-      # shellcheck disable=SC1091
-      . "${SCRIPT_DIR}/run.after.hook.local"
+    # shellcheck disable=SC1091
+    . "${SCRIPT_DIR}/run.after.hook.local"
   fi
   trap cleanup EXIT
   echo "All Services started successfully, press Ctrl+C to stop them"
