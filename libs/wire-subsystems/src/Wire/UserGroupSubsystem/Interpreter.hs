1426cd4cc3bba6293fcd1fbc63ba25116709d243
Akshay Mankar <itsakshaymankar@gmail.com>
2025-04-29 10:45:54 +0200

    UserGroupSubsystem: Refine and keep constructors for getting and creating a group

--- libs/wire-subsystems/src/Wire/UserGroupSubsystem/Interpreter.hs
+++ libs/wire-subsystems/src/Wire/UserGroupSubsystem/Interpreter.hs
@@ -1,1 +1,10 @@
 module Wire.UserGroupSubsystem.Interpreter where
+
+import Imports
+import Polysemy
+import Wire.UserGroupSubsystem
+
+interpretUserGroupSubsystem :: InterpreterFor UserGroupSubsystem r
+interpretUserGroupSubsystem = interpret $ \case
+  CreateGroup creator newGroup -> undefined creator newGroup
+  GetGroup getter gid -> undefined getter gid
