a87e6af74a6790f95aeb9abab702f70e2bf27652
Akshay Mankar <itsakshaymankar@gmail.com>
2025-04-30 17:16:07 +0200

    Migrations: Make (team_id, id) the primary key of user_group table
    Since a group exists within bounds of a team, it will always be retrieved like
    that. So it makes sense to first index on the team id. Sticking to semantics for
    the sake of it might not always be the best thing to do, but here it will
    actually make the queries faster.

--- libs/wire-subsystems/postgres-migrations/20250423103417-create-user-group-table.sql
+++ libs/wire-subsystems/postgres-migrations/20250423103417-create-user-group-table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE user_group (
-  id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
   team_id uuid NOT NULL,
+  id uuid DEFAULT gen_random_uuid() UNIQUE,
   name text NOT NULL,
-  managed_by int NOT NULL
+  managed_by int NOT NULL,
+  PRIMARY KEY (team_id, id),
 )
