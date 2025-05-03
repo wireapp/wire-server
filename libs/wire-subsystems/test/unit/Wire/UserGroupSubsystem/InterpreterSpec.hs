bcfdf564f25cbb8c01be66b5ab38b8556c9543d5
Matthias Fischmann <mf@zerobuzz.net>
2025-05-03 22:26:03 +0200

    Unit tests for UserGroupSubsystem [WIP].

--- libs/wire-subsystems/test/unit/Wire/UserGroupSubsystem/InterpreterSpec.hs
+++ libs/wire-subsystems/test/unit/Wire/UserGroupSubsystem/InterpreterSpec.hs
@@ -24,7 +24,7 @@ import Wire.API.User as User
 import Wire.API.UserGroup
 import Wire.Arbitrary
 import Wire.GalleyAPIAccess
-import Wire.MockInterpreters
+import Wire.MockInterpreters as Mock
 import Wire.Sem.Random qualified as Rnd
 import Wire.UserGroupStore (UserGroupStore)
 import Wire.UserGroupSubsystem
@@ -68,6 +68,8 @@ unexpected =
 
 spec :: Spec
 spec = describe "UserGroupSubsystem.Interpreter" do
+  -- TODO: describe "CreateGroup :: UserId -> NewUserGroup -> UserGroupSubsystem m UserGroup"
+
   prop "team admins should be able to create and get groups" $ \team newUserGroupName ->
     expectRight
       . runDependencies (allUsers team) (galleyTeam team)
@@ -81,10 +83,10 @@ spec = describe "UserGroupSubsystem.Interpreter" do
         createdGroup <- createGroup (ownerId team) newUserGroup
         retrievedGroup <- getGroup (ownerId team) createdGroup.id_
         pure $
-          -- TODO: test createdAt when it's back.
           createdGroup.name === newUserGroupName
             .&&. createdGroup.members === newUserGroup.members
             .&&. createdGroup.managedBy === ManagedByWire
+            -- .&&. (ug.createdAt === now) -- TODO: test createdAt when it's back.
             .&&. Just createdGroup === retrievedGroup
 
   prop "only team admins should be able to create a group" $
@@ -116,6 +118,11 @@ spec = describe "UserGroupSubsystem.Interpreter" do
                     }
             void $ createGroup (ownerId team) newUserGroup
             unexpected
+
+  -- TODO: describe "GetGroup :: UserId -> UserGroupId -> UserGroupSubsystem m (Maybe UserGroup)"
+
+  it "key misses produce 404" $ do
+    pendingWith "TODO"
 
   prop "team admins can get all groups in their team; outsiders can see nothing" $ \team otherTeam userGroupName ->
     expectRight
@@ -158,6 +165,132 @@ spec = describe "UserGroupSubsystem.Interpreter" do
             pure $
               ((.id_) <$> view1memSet1) === Just group1.id_
                 .&&. ((.id_) <$> view2memSet1) === Nothing
+
+  describe "GetGroups :: UserId -> Maybe Int -> Maybe UUID -> UserGroupSubsystem m UserGroupPage" $ do
+    prop "team admins can get all groups in their team; outsiders can see nothing" $
+      \_team _otherTeam _userGroupName -> False === True
+
+    prop "team members can only get their own groups" $
+      \_team _userGroupName1 _userGroupName2 -> False === True
+
+    let -- now = unsafePerformIO getCurrentTime
+        nugs = [1 .. 15] <&> \(i :: Int) -> NewUserGroup (cs $ show i) []
+
+        check :: (HasCallStack) => UserGroupPage -> [UserGroupId] -> Bool -> Expectation
+        check have wantPage wantHasMore = do
+          (have.page <&> (.id_)) `shouldBe` wantPage
+          have.hasMore `shouldBe` wantHasMore
+
+        lastKeyOf :: UserGroupPage -> UUID
+        lastKeyOf = toUUID . (.id_) . last . (.page)
+
+    prop "pagination (static case): eventually gives you the entire data set (nothing gets removed or duplicated)" $
+      \team userGroups ->
+        Mock.runInMemoryUserGroupSubsystem now do
+          gids <- (sort . ((.id_) <$>)) <$> createGroup `mapM` nugs
+          allOfThem <- getGroups Nothing Nothing
+          first3 <- getGroups (Just 3) Nothing
+          next4 <- getGroups (Just 4) (Just $ lastKeyOf first3)
+          halfPage1 <- getGroups (Just 100) Nothing
+          halfPage2 <- getGroups (Just 100) (Just $ lastKeyOf next4)
+
+          pure do
+            check allOfThem gids False
+            check first3 (take 3 gids) True
+            check next4 (take 4 (drop 3 gids)) True
+            check halfPage1 gids False
+            check halfPage2 (drop 7 gids) False
+
+    it "paginates well under updates" $ do
+      Mock.runInMemoryUserGroupSubsystem now do
+        gids <- (sort . ((.id_) <$>)) <$> createGroup `mapM` nugs
+        beforeNewCreate <- getGroups (Just 8) Nothing
+        newGroup <- createGroup (NewUserGroup "the new one" [])
+        afterNewCreate <- getGroups (Just 8) (Just $ lastKeyOf beforeNewCreate)
+
+        pure do
+          check beforeNewCreate (take 8 gids) True
+
+          let afterGids =
+                if lastKeyOf beforeNewCreate < toUUID newGroup.id_
+                  then drop 8 (sort (newGroup.id_ : gids))
+                  else drop 8 gids
+          check afterNewCreate afterGids False
+
+    -- (...  or write this property?)
+    prop "pagination (dynamic case): when writing and removing between getting pages, result is as good as we can expect" $ \() ->
+      -- take the initial table, apply some removes and adds (which ones?), and you should get what you also get from paginating
+      False === True
+
+  describe "UpdateGroup :: UserId -> UserGroupId -> UserGroupUpdate -> UserGroupSubsystem m (Maybe UserGroup)" $ do
+    prop "updateGroup updates the name" $ \originalName userGroupUpdate ->
+      let now = unsafePerformIO getCurrentTime
+       in Mock.runInMemoryUserGroupSubsystem
+            now
+            do
+              ug0 <- createGroup (NewUserGroup originalName [])
+              ug1 <- getGroup ug0.id_
+              ug2 <- updateGroup ug0.id_ userGroupUpdate
+              ug3 <- getGroup ug0.id_
+              pure $
+                (ug1 === Just ug0)
+                  .&&. (ug2 === Just (ug0 {name = userGroupUpdate.name} :: UserGroup))
+                  .&&. (ug3 === ug2)
+            prop
+            "only team admins can update user groups"
+            $ \() -> False
+              === True
+                prop
+                "only team members are allowed in the group"
+              $ \() -> False === True
+
+  describe "DeleteGroup :: UserId -> UserGroupId -> UserGroupSubsystem m ()" $ do
+    prop "deleteGroup deletes" $ \newGroup1 newGroup2 -> do
+      let now = unsafePerformIO getCurrentTime
+       in Mock.runInMemoryUserGroupSubsystem now do
+            ug1 <- createGroup newGroup1
+            ug2 <- createGroup newGroup2
+            deleteGroup ug1.id_
+            allGroups <- (.page) <$> getGroups Nothing Nothing
+
+            deleteGroup (Id UUID.nil) -- idempotency
+            allGroups' <- (.page) <$> getGroups Nothing Nothing
+
+            pure $ do
+              allGroups `shouldBe` [ug2]
+              allGroups' `shouldBe` [ug2]
+
+      prop "only team admins can delete user groups" $ \() -> False === True
+
+  describe "AddUser, RemoveUser :: UserId -> UserGroupId -> UserId -> UserGroupSubsystem m ()" $ do
+    prop "addUser adds a user" $ \newGroup newUserId -> do
+      -- TODO: how do we feel about dangling user ids?  maybe that should be handled on another
+      -- level, and UserGroupSubsystem should be oblivious to what user ids point to?
+      let now = unsafePerformIO getCurrentTime
+       in Mock.runInMemoryUserGroupSubsystem now do
+            ug :: UserGroup <- createGroup newGroup
+            addUser ug.id_ newUserId
+            ug' :: Maybe UserGroup <- getGroup ug.id_
+            addUser ug.id_ newUserId -- idempotency
+            ug'' :: Maybe UserGroup <- getGroup ug.id_
+            pure $ do
+              ((sort . (.members)) <$> ug') `shouldBe` Just (sort (newUserId : ug.members))
+              ug'' `shouldBe` ug'
+
+    prop "removeUser removes a user" $ \newGroup -> do
+      let now = unsafePerformIO getCurrentTime
+       in Mock.runInMemoryUserGroupSubsystem now do
+            ug :: UserGroup <- createGroup newGroup
+            let removee :: UserId
+                removee = case ug.members of
+                  [] -> Id UUID.nil -- idempotency
+                  _ : _ -> ug.members !! (length ug.members `div` 2)
+            removeUser ug.id_ removee
+            ug' :: Maybe UserGroup <- getGroup ug.id_
+            pure $ do
+              ((sort . (.members)) <$> ug') `shouldBe` Just (sort (ug.members \\ [removee]))
+
+      prop "only team admins can and remove users to groups" $ \() -> False === True
 
 data TeamGenMod = AtLeastOneMember | AtLeastOneNonAdmin
 
