de7a7ea4f705c1062e36bb3089f64165a69ca357
Matthias Fischmann <mf@zerobuzz.net>
2025-05-05 12:50:24 +0200

    Re-add createdAt to UserGroup; return UserGroup from create.

--- libs/wire-subsystems/postgres-migrations/20250423103417-create-user-group-table.sql
+++ libs/wire-subsystems/postgres-migrations/20250423103417-create-user-group-table.sql
@@ -3,5 +3,6 @@ CREATE TABLE user_group (
   id uuid DEFAULT gen_random_uuid() UNIQUE,
   name text NOT NULL,
   managed_by int NOT NULL,
+  created_at timestamp with time zone DEFAULT now();
   PRIMARY KEY (team_id, id)
 )
