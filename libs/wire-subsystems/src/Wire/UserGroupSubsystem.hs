cfe4344c6af441c96c9b3c35a6032597c0137725
Matthias Fischmann <mf@zerobuzz.net>
2025-05-06 14:22:30 +0200

    Add rest of the UserGroup api.  [wip]

--- libs/wire-subsystems/src/Wire/UserGroupSubsystem.hs
+++ libs/wire-subsystems/src/Wire/UserGroupSubsystem.hs
@@ -11,5 +11,10 @@ import Wire.API.UserGroup
 data UserGroupSubsystem m a where
   CreateGroup :: UserId -> NewUserGroup -> UserGroupSubsystem m UserGroup
   GetGroup :: UserId -> UserGroupId -> UserGroupSubsystem m (Maybe UserGroup)
+  GetGroups :: UserId -> Maybe Int -> Maybe UserGroupId -> UserGroupSubsystem m UserGroupPage
+  UpdateGroup :: UserId -> UserGroupId -> UserGroupUpdate -> UserGroupSubsystem m (Maybe UserGroup)
+  DeleteGroup :: UserId -> UserGroupId -> UserGroupSubsystem m ()
+  AddUser :: UserId -> UserGroupId -> UserId -> UserGroupSubsystem m ()
+  RemoveUser :: UserId -> UserGroupId -> UserId -> UserGroupSubsystem m ()
 
 makeSem ''UserGroupSubsystem
