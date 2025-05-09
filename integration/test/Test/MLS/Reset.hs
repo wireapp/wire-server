c0e5d6bab591f3a92769e75eafe38c4fa7762282
Paolo Capriotti <paolo@capriotti.io>
2025-05-09 09:05:15 +0200

    fixup! Basic MLS reset test

--- integration/test/Test/MLS/Reset.hs
+++ integration/test/Test/MLS/Reset.hs
@@ -7,23 +7,18 @@ import Testlib.Prelude
 
 testResetGroupConversation :: (HasCallStack) => Domain -> App ()
 testResetGroupConversation domain = do
-  own <- make OwnDomain
-  other <- make domain
-  [alice, bob, charlie] <- createAndConnectUsers [own, other, own]
+  [alice, bob, charlie] <- createAndConnectUsers [make OwnDomain, make domain, make OwnDomain]
   [alice1, bob1] <- traverse (createMLSClient def) [alice, bob]
   void $ uploadNewKeyPackage def bob1
   conv <- createNewGroup def alice1
   void $ createAddCommit alice1 conv [bob] >>= sendAndConsumeCommitBundle
   mlsConv <- getMLSConv conv
 
-  bindResponse (resetConversation alice mlsConv.groupId 0) $ \resp -> do
-    resp.status `shouldMatchInt` 409
+  resetConversation alice mlsConv.groupId 0 >>= assertStatus 409
+  resetConversation bob mlsConv.groupId 0 >>= assertStatus 409
+  resetConversation charlie mlsConv.groupId mlsConv.epoch >>= assertStatus 403
+  resetConversation bob mlsConv.groupId mlsConv.epoch >>= assertStatus 200
 
-  bindResponse (resetConversation charlie mlsConv.groupId 0) $ \resp -> do
-    resp.status `shouldMatchInt` 403
-
-  bindResponse (resetConversation bob mlsConv.groupId mlsConv.epoch) $ \resp -> do
-    resp.status `shouldMatchInt` 200
   conv' <- getConversation alice conv >>= getJSON 200
   conv' %. "group_id" `shouldNotMatch` (mlsConv.groupId :: String)
   conv' %. "epoch" `shouldMatchInt` 0
