5923bdd5f3a527697b5af2586a9f81934a1501ab
Leif Battermann <leif.battermann@wire.com>
2025-05-09 10:44:39 +0000

    integration test for notifications

--- integration/test/Test/UserGroup.hs
+++ integration/test/Test/UserGroup.hs
@@ -3,23 +3,31 @@
 module Test.UserGroup where
 
 import API.Brig
+import API.Galley
 import SetupHelpers
 import Testlib.Prelude
+import Notifications (isUserGroupCreatedNotif)
 
 testUserGroupSmoke :: (HasCallStack) => App ()
 testUserGroupSmoke = do
-  (owner, _team, [mem1, mem2, mem3]) <- createTeam OwnDomain 4
+  (owner, team, [mem1, mem2, mem3, admin2]) <- createTeam OwnDomain 5
+  updateTeamMember team owner admin2 Admin >>= assertSuccess
   mem1id <- asString $ mem1 %. "id"
   mem2id <- asString $ mem2 %. "id"
   _mem3id <- asString $ mem3 %. "id"
 
   let badGid = "225c4d54-1ae7-11f0-8e9c-cbb31865d602"
 
-  gid <- bindResponse (createUserGroup owner (object ["name" .= "none", "members" .= ([mem1id, mem2id])])) $ \resp -> do
-    resp.status `shouldMatchInt` 200
-    resp.json %. "name" `shouldMatch` "none"
-    resp.json %. "members" `shouldMatch` [mem1id, mem2id]
-    asString $ (resp.json %. "id")
+  gid <- withWebSockets [owner, admin2] $ \wss -> do
+    gid <- bindResponse (createUserGroup owner (object ["name" .= "none", "members" .= ([mem1id, mem2id])])) $ \resp -> do
+      resp.status `shouldMatchInt` 200
+      resp.json %. "name" `shouldMatch` "none"
+      resp.json %. "members" `shouldMatch` [mem1id, mem2id]
+      asString $ (resp.json %. "id")
+    for_ wss $ \ws -> do
+      notif <- awaitMatch isUserGroupCreatedNotif ws
+      notif %. "payload.0.data.id" `shouldMatch` gid
+    pure gid
 
   bindResponse (getUserGroup owner badGid) $ \resp -> do
     resp.status `shouldMatchInt` 404
