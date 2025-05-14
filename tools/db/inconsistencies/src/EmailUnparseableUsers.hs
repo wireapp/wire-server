5f13a14c5ba699392fcc53ff42bb26de3cf0179f
jschaul <jschaul@users.noreply.github.com>
2025-05-14 17:01:15 +0200

    formatting

--- tools/db/inconsistencies/src/EmailUnparseableUsers.hs
+++ tools/db/inconsistencies/src/EmailUnparseableUsers.hs
@@ -1,28 +1,25 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module EmailUnparseableUsers  where
+module EmailUnparseableUsers where
 
 import Cassandra
 import Cassandra.Util
 import Conduit
-import Data.Aeson (ToJSON)
+import Data.Aeson (ToJSON, object, (.=))
 import Data.Aeson qualified as Aeson
 import Data.ByteString qualified as BS
 import Data.Conduit.Internal (zipSources)
 import Data.Conduit.List qualified as C
 import Data.Id
+import Data.Text.Encoding qualified as TE
 import Imports
 import System.Logger (Logger)
 import System.Logger qualified as Log
+import Text.Email.Validate qualified as Email
 import UnliftIO (pooledMapConcurrentlyN)
 import Wire.API.Team.Permission
 import Wire.API.User (AccountStatus)
 import Wire.API.User.EmailAddress (EmailAddress)
-
-import qualified Text.Email.Validate      as Email
-import qualified Data.Text.Encoding       as TE
-import           Data.Aeson               ((.=), object)
-import qualified Data.ByteString          as BS
 
 -- Problem statement:
 -- Upon brig re-index, we look up the user table and create a
@@ -57,21 +54,22 @@ runCommand l brig inconsistenciesFile = do
 toOffender :: UserDetailsRow -> Maybe Offender
 toOffender row@(_, _, _, _, Just rawEmail) =
   case Email.validate (TE.encodeUtf8 rawEmail) of
-    Left err -> Just (row, err)   -- keep error message
-    Right _  -> Nothing           -- good email → ignore
-toOffender _ = Nothing            -- no email stored
+    Left err -> Just (row, err) -- keep error message
+    Right _ -> Nothing -- good email → ignore
+toOffender _ = Nothing -- no email stored
 
 -- | Transform an offending row into JSON
 checkOffender :: Offender -> IO (Maybe Aeson.Value)
 checkOffender ((uid, activated, accStat, wt, Just rawEmail), err) =
-  pure . Just $ object
-    [ "userId"          .= uid
-    , "email"           .= rawEmail
-    , "activated"       .= activated
-    , "accountStatus"   .= accStat
-    , "statusWritetime" .= wt
-    , "parseError"      .= err
-    ]
+  pure . Just $
+    object
+      [ "userId" .= uid,
+        "email" .= rawEmail,
+        "activated" .= activated,
+        "accountStatus" .= accStat,
+        "statusWritetime" .= wt,
+        "parseError" .= err
+      ]
 checkOffender _ = pure Nothing -- impossible
 
 pageSize :: Int32
@@ -79,7 +77,9 @@ pageSize = 10000
 
 type UserDetailsRow = (UserId, Maybe Bool, Maybe AccountStatus, Maybe (Writetime AccountStatus), Maybe Text)
 
-type Offender = (UserDetailsRow, String) -- ^ row plus parse-error msg
+type Offender =
+  -- | row plus parse-error msg
+  (UserDetailsRow, String)
 
 getUsers :: ConduitM () [UserDetailsRow] Client ()
 getUsers = paginateC cql (paramsP LocalQuorum () pageSize) x5
