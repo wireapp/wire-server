69c232003474931f2832cd146def219a9cdaf070
Leif Battermann <leif.battermann@wire.com>
2025-06-05 11:20:17 +0000

    removed migration for linking user groups to channels

--- libs/wire-subsystems/postgres-migrations/20250512113021-create-channel-usergroup-assoc.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE channel_user_group (
-  channel_id uuid,
-  user_group_id uuid,
-  PRIMARY KEY (channel_id),
-  CONSTRAINT fk_user_group
-    FOREIGN KEY (user_group_id)
-      REFERENCES user_group(id)
-)
