88ea5be8c2cfcc539883e8fcd72bb68e7d06ba72
Sven Tennie <sven.tennie@wire.com>
2025-06-23 15:05:37 +0200

    Add check to team member email change

--- services/brig/src/Brig/API/Public.hs
+++ services/brig/src/Brig/API/Public.hs
@@ -1431,6 +1431,7 @@ updateUserEmail ::
   Public.EmailUpdate ->
   (Handler r) ()
 updateUserEmail zuserId emailOwnerId (Public.EmailUpdate email) = do
+  customerExtensionCheckBlockedDomains email
   maybeZuserTeamId <- lift $ wrapClient $ Data.lookupUserTeam zuserId
   whenM (not <$> assertHasPerm maybeZuserTeamId) $ throwStd insufficientTeamPermissions
   maybeEmailOwnerTeamId <- lift $ wrapClient $ Data.lookupUserTeam emailOwnerId
