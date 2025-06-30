5804a48a0dc8a9d9e3091d4101e24dcb8b0d17c4
Matthias Fischmann <mf@zerobuzz.net>
2025-06-30 14:04:42 +0200

    wip

--- libs/wire-subsystems/src/Wire/UserGroupSubsystem.hs
+++ libs/wire-subsystems/src/Wire/UserGroupSubsystem.hs
@@ -6,8 +6,8 @@ module Wire.UserGroupSubsystem where
 import Data.Id
 import Imports
 import Polysemy
-import Wire.API.Pagination
 import Wire.API.UserGroup
+import Wire.API.UserGroup.Pagination
 
 data UserGroupSubsystem m a where
   CreateGroup :: UserId -> NewUserGroup -> UserGroupSubsystem m UserGroup
@@ -18,8 +18,8 @@ data UserGroupSubsystem m a where
     Maybe SortBy ->
     Maybe SortOrder ->
     Maybe PageSize ->
-    Maybe (PaginationState UserGroupKey) ->
-    UserGroupSubsystem m (PaginationResult UserGroupKey UserGroup)
+    Maybe PaginationState ->
+    UserGroupSubsystem m PaginationResult
   UpdateGroup :: UserId -> UserGroupId -> UserGroupUpdate -> UserGroupSubsystem m ()
   DeleteGroup :: UserId -> UserGroupId -> UserGroupSubsystem m ()
   AddUser :: UserId -> UserGroupId -> UserId -> UserGroupSubsystem m ()
