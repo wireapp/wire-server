b90529fb9d8b23e2e1a3883980cd50dff9c5f8a1
Matthias Fischmann <mf@zerobuzz.net>
2025-07-10 16:08:51 +0200

    Revert "Fix database schema."
    This reverts commit 54e740461a6c60e96251faf9f4091843f444ee67.

--- libs/wire-subsystems/postgres-migrations/20250423103417-create-user-group-table.sql
+++ libs/wire-subsystems/postgres-migrations/20250423103417-create-user-group-table.sql
@@ -4,8 +4,5 @@ CREATE TABLE user_group (
   name text NOT NULL,
   managed_by int NOT NULL,
   created_at timestamptz DEFAULT now(),
-  PRIMARY KEY (team_id, id),
-  -- (0 :: int) is sometimes represented as four NULL bytes by
-  -- postgres, which can cause parser problems in hasql:
-  CONSTRAINT managed_by_nonzero
+  PRIMARY KEY (team_id, id)
 )
