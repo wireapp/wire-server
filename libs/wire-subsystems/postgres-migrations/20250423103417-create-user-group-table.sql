3077a8379cd9083b186d99db23d06a0aa0b9d57d
Matthias Fischmann <mf@zerobuzz.net>
2025-07-10 16:03:45 +0200

    Fix database schema.

--- libs/wire-subsystems/postgres-migrations/20250423103417-create-user-group-table.sql
+++ libs/wire-subsystems/postgres-migrations/20250423103417-create-user-group-table.sql
@@ -4,5 +4,8 @@ CREATE TABLE user_group (
   name text NOT NULL,
   managed_by int NOT NULL,
   created_at timestamptz DEFAULT now(),
-  PRIMARY KEY (team_id, id)
+  PRIMARY KEY (team_id, id),
+  -- (0 :: int) is sometimes represented as four NULL bytes by
+  -- postgres, which can cause parser problems in hasql:
+  CONSTRAINT managed_by_nonzero
 )
