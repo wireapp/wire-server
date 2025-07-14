31828a016b0594aa9b67431da5d0e3de888ba66e
Sven Tennie <sven.tennie@wire.com>
2025-07-14 19:24:11 +0200

    Use Now effect to get time

--- libs/wire-subsystems/src/Wire/TeamCollaboratorsSubsystem/Interpreter.hs
+++ libs/wire-subsystems/src/Wire/TeamCollaboratorsSubsystem/Interpreter.hs
@@ -6,11 +6,9 @@ import Data.Default
 import Data.Id
 import Data.Json.Util
 import Data.Qualified
-import Data.Time
 import Imports
 import Polysemy
 import Polysemy.Error
-import Polysemy.Input
 import Wire.API.Error
 import Wire.API.Error.Brig qualified as E
 import Wire.API.Event.Team
@@ -21,6 +19,7 @@ import Wire.API.Team.Member qualified as TeamMember
 import Wire.Error
 import Wire.GalleyAPIAccess
 import Wire.NotificationSubsystem
+import Wire.Sem.Now
 import Wire.TeamCollaboratorsStore qualified as Store
 import Wire.TeamCollaboratorsSubsystem
 
@@ -28,7 +27,7 @@ runTeamCollaboratorsSubsystem ::
   ( Member GalleyAPIAccess r,
     Member (Error TeamCollaboratorsError) r,
     Member Store.TeamCollaboratorsStore r,
-    Member (Input UTCTime) r,
+    Member Now r,
     Member NotificationSubsystem r
   ) =>
   InterpreterFor TeamCollaboratorsSubsystem r
@@ -40,7 +39,7 @@ createTeamCollaboratorImpl ::
   ( Member GalleyAPIAccess r,
     Member (Error TeamCollaboratorsError) r,
     Member Store.TeamCollaboratorsStore r,
-    Member (Input UTCTime) r,
+    Member Now r,
     Member NotificationSubsystem r
   ) =>
   Local UserId ->
@@ -53,7 +52,7 @@ createTeamCollaboratorImpl zUser user team perms = do
     throw InsufficientRights
   Store.createTeamCollaborator user team perms
 
-  now <- input
+  now <- get
   let event = newEvent team now (EdCollaboratorAdd user)
   teamMembersList <- getTeamMembers team
   let teamMembers :: [UserId] = view Team.userId <$> (teamMembersList ^. Team.teamMembers)
