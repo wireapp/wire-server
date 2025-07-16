a4b3518673b740b61261f2840d489f56a2bffbad
Sven Tennie <sven.tennie@wire.com>
2025-07-16 11:02:21 +0200

    Delete obsolete TODO

--- libs/wire-subsystems/src/Wire/TeamCollaboratorsSubsystem.hs
+++ libs/wire-subsystems/src/Wire/TeamCollaboratorsSubsystem.hs
@@ -10,8 +10,6 @@ import Wire.API.Team.Collaborator
 
 data TeamCollaboratorsSubsystem m a where
   CreateTeamCollaborator :: Local UserId -> UserId -> TeamId -> Set CollaboratorPermission -> TeamCollaboratorsSubsystem m ()
-  -- TODO: Create a data type for the response containing: UserID, TeamID and permissions
-  -- Only the UserId isn't enough
   GetAllTeamCollaborators :: Local UserId -> TeamId -> TeamCollaboratorsSubsystem m [GetTeamCollaborator]
 
 makeSem ''TeamCollaboratorsSubsystem
