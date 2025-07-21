e0216bb511f035cf42fe7291091403983238254c
Leif Battermann <leif.battermann@wire.com>
2025-07-21 13:48:45 +0000

    remove inginx-ingress-controller

--- hack/bin/integration-teardown-federation.sh
+++ hack/bin/integration-teardown-federation.sh
@@ -20,11 +20,7 @@ export ENTERPRISE_IMAGE_PULL_SECRET="."
 KUBERNETES_VERSION_MAJOR="$(kubectl version -o json | jq -r .serverVersion.major)"
 KUBERNETES_VERSION_MINOR="$(kubectl version -o json | jq -r .serverVersion.minor)"
 KUBERNETES_VERSION_MINOR="${KUBERNETES_VERSION_MINOR//[!0-9]/}" # some clusters report minor versions as a string like '27+'. Strip any non-digit characters.
-if (( KUBERNETES_VERSION_MAJOR > 1 || KUBERNETES_VERSION_MAJOR == 1 && KUBERNETES_VERSION_MINOR >= 23 )); then
-    export INGRESS_CHART="ingress-nginx-controller"
-else
-    export INGRESS_CHART="nginx-ingress-controller"
-fi
+export INGRESS_CHART="ingress-nginx-controller"
 
 # shellcheck disable=SC1091
 . "$DIR/helm_overrides.sh"
