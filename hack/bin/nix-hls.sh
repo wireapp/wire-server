f90102856858dff50fef6eff784aa45353e2e2a2
Akshay Mankar <itsakshaymankar@gmail.com>
2025-07-24 14:30:52 +0200

    nix-hls.sh: ALlow limiting memory used by HLS

--- hack/bin/nix-hls.sh
+++ hack/bin/nix-hls.sh
@@ -7,4 +7,11 @@ TOP_LEVEL="$(cd "$DIR/../.." && pwd)"
 
 direnv="$(nix-build --no-out-link "$TOP_LEVEL/nix" -A pkgs.direnv)/bin/direnv"
 
-"$direnv" exec "$TOP_LEVEL" haskell-language-server-wrapper "$@"
+# shellcheck disable=SC2016
+maxMemory=$("$direnv" exec "$TOP_LEVEL" bash -c 'echo "$HLS_MAX_MEMORY"')
+
+if [[ -z "$maxMemory" ]]; then
+    "$direnv" exec "$TOP_LEVEL" haskell-language-server-wrapper "$@"
+else
+    systemd-run --scope -p MemoryMax="$maxMemory" --user "$direnv" exec "$TOP_LEVEL" haskell-language-server-wrapper "$@"
+fi
