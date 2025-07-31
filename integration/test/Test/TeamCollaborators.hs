f72dc3cd7b5a21747f7245aba986c913358cbdfd
Gautier DI FOLCO <gautier.difolco.ext@wire.com>
2025-07-31 17:43:56 +0200

    WPB-18191: Add route to collaborator permissions from team

--- integration/test/Test/TeamCollaborators.hs
+++ integration/test/Test/TeamCollaborators.hs
@@ -182,3 +182,33 @@ testRemoveMemberInTeamConversation = do
 
   getConversation bob conv `bindResponse` \resp -> do
     resp.status `shouldMatchInt` 403
+
+testUpdateMember :: (HasCallStack) => App ()
+testUpdateMember = do
+  (owner, team, [alice]) <- createTeam OwnDomain 2
+
+  -- At the time of writing, it wasn't clear if this should be a bot instead.
+  bob <- randomUser OwnDomain def
+  addTeamCollaborator
+    owner
+    team
+    bob
+    ["implicit_connection"]
+    >>= assertSuccess
+  postOne2OneConversation bob alice team "chit-chat" >>= assertSuccess
+
+  updateTeamCollaborator
+    owner
+    team
+    bob
+    ["create_team_conversation", "implicit_connection"]
+    >>= assertSuccess
+  postOne2OneConversation bob alice team "chit-chat" >>= assertSuccess
+
+  updateTeamCollaborator
+    owner
+    team
+    bob
+    []
+    >>= assertSuccess
+  postOne2OneConversation bob alice team "chit-chat" >>= assertLabel 403 "operation-denied"
