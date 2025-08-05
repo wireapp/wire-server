c883788641281ab540ebc65a1a4db724798641fe
Gautier DI FOLCO <gautier.difolco.ext@wire.com>
2025-08-05 12:58:26 +0200

    filter then delete

--- services/galley/src/Galley/ConversationsSubsystem.hs
+++ services/galley/src/Galley/ConversationsSubsystem.hs
@@ -4,7 +4,10 @@ module Galley.ConversationsSubsystem
 where
 
 import Cassandra
+import Cassandra.Exec
+import Conduit
 import Data.Id
+import Galley.Cassandra.Queries qualified as Cql
 import Galley.Cassandra.Store (embedClient)
 import Galley.Cassandra.Util (logEffect)
 import Imports
@@ -12,6 +15,7 @@ import Polysemy
 import Polysemy.Input
 import Polysemy.TinyLog (TinyLog)
 import Wire.ConversationsSubsystem
+import Wire.Sem.Paging.Cassandra (InternalPage (InternalPage), mkInternalPage)
 
 interpretConversationsSubsystemCassandra ::
   ( Member (Embed IO) r,
@@ -27,7 +31,23 @@ interpretConversationsSubsystemCassandra =
         embedClient $ closeConversationsFromImpl tid uid
 
 closeConversationsFromImpl :: TeamId -> UserId -> Client ()
-closeConversationsFromImpl t u = retry x5 $ write conversationsUpdate (params LocalQuorum (t, Just u))
+closeConversationsFromImpl tid uid =
+  runConduit $
+    paginateWithStateC listConversationsIds
+      .| mapMC performFilter
+      .| mapM_C performUpdate
   where
-    conversationsUpdate :: PrepQuery W (TeamId, Maybe UserId) ()
-    conversationsUpdate = "update conversation set deleted = true where team = ? and creator = ?"
+    listConversationsIds pagingState =
+      fmap runIdentity <$> paginateWithState Cql.selectUserConvs (paramsPagingState LocalQuorum (Identity uid) 32 pagingState)
+    performFilter :: [ConvId] -> Client [ConvId]
+    performFilter convIds = do
+      page <- paginate conversationsFilter (params LocalQuorum (Identity convIds))
+      InternalPage (_, _, filteredConvIds) <- mkInternalPage page pure
+      pure $
+        map (\(convId, _team, _mCreator) -> convId) $
+          filter (\(_convId, team, mCreator) -> team == Just tid && mCreator == Just uid) filteredConvIds
+    conversationsFilter :: PrepQuery R (Identity [ConvId]) (ConvId, Maybe TeamId, Maybe UserId)
+    conversationsFilter = "select conv, team, creator from conversation where conv in ?"
+    performUpdate convIds = retry x5 $ write conversationsUpdate (params LocalQuorum (Identity convIds))
+    conversationsUpdate :: PrepQuery W (Identity [ConvId]) ()
+    conversationsUpdate = "update conversation set deleted = true where conv in ?"
