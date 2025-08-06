f77061fd0c89a538f0ef1eb414b22fe0093bec7d
Gautier DI FOLCO <gautier.difolco.ext@wire.com>
2025-08-06 17:31:48 +0200

    fix: remove O2O Conversations and remove users from others

--- services/galley/src/Galley/ConversationsSubsystem.hs
+++ services/galley/src/Galley/ConversationsSubsystem.hs
@@ -6,7 +6,9 @@ where
 import Cassandra
 import Cassandra.Exec
 import Conduit
+import Data.Bifoldable
 import Data.Id
+import Galley.Cassandra.Conversation (deleteConversation)
 import Galley.Cassandra.Queries qualified as Cql
 import Galley.Cassandra.Store (embedClient)
 import Galley.Cassandra.Util (logEffect)
@@ -14,6 +16,7 @@ import Imports
 import Polysemy
 import Polysemy.Input
 import Polysemy.TinyLog (TinyLog)
+import Wire.API.Conversation (ConvType (..))
 import Wire.ConversationsSubsystem
 import Wire.Sem.Paging.Cassandra (InternalPage (InternalPage), mkInternalPage)
 
@@ -35,19 +38,21 @@ closeConversationsFromImpl tid uid =
   runConduit $
     paginateWithStateC listConversationsIds
       .| mapMC performFilter
-      .| mapM_C performUpdate
+      .| mapM_C (bimapM_ (mapM_ deleteConversation) performConversationsRemoveUser)
   where
     listConversationsIds pagingState =
       fmap runIdentity <$> paginateWithState Cql.selectUserConvs (paramsPagingState LocalQuorum (Identity uid) 32 pagingState)
-    performFilter :: [ConvId] -> Client [ConvId]
+    performFilter :: [ConvId] -> Client ([ConvId], [ConvId])
     performFilter convIds = do
       page <- paginate conversationsFilter (params LocalQuorum (Identity convIds))
       InternalPage (_, _, filteredConvIds) <- mkInternalPage page pure
+      let extractConv = map (\(convId, _team, _convType) -> convId)
       pure $
-        map (\(convId, _team, _mCreator) -> convId) $
-          filter (\(_convId, team, mCreator) -> team == Just tid && mCreator == Just uid) filteredConvIds
-    conversationsFilter :: PrepQuery R (Identity [ConvId]) (ConvId, Maybe TeamId, Maybe UserId)
-    conversationsFilter = "select conv, team, creator from conversation where conv in ?"
-    performUpdate convIds = retry x5 $ write conversationsUpdate (params LocalQuorum (Identity convIds))
-    conversationsUpdate :: PrepQuery W (Identity [ConvId]) ()
-    conversationsUpdate = "update conversation set deleted = true where conv in ?"
+        bimap extractConv extractConv $
+          partition (\(_convId, _team, convType) -> convType == One2OneConv) $
+            filter (\(_convId, team, _convType) -> team == Just tid) filteredConvIds
+    conversationsFilter :: PrepQuery R (Identity [ConvId]) (ConvId, Maybe TeamId, ConvType)
+    conversationsFilter = "select conv, team, type from conversation where conv in ?"
+    performConversationsRemoveUser convIds = retry x5 $ write conversationsRemoveUser (params LocalQuorum (uid, convIds))
+    conversationsRemoveUser :: PrepQuery W (UserId, [ConvId]) ()
+    conversationsRemoveUser = "delete from user where user = ? and conv in ?"
