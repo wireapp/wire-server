38f55bccab88a422d45e76c78e882858fece4447
Gautier DI FOLCO <gautier.difolco.ext@wire.com>
2025-08-18 17:23:51 +0200

    test: check events

--- integration/test/Notifications.hs
+++ integration/test/Notifications.hs
@@ -230,6 +230,12 @@ isConnectionNotif status n =
 isUserGroupCreatedNotif :: (MakesValue a) => a -> App Bool
 isUserGroupCreatedNotif = notifTypeIsEqual "user-group.created"
 
+isUserGroupUpdatedNotif :: (MakesValue a) => a -> App Bool
+isUserGroupUpdatedNotif = notifTypeIsEqual "user-group.updated"
+
+isUserGroupUserAddedNotif :: (MakesValue a) => a -> App Bool
+isUserGroupUserAddedNotif = notifTypeIsEqual "user-group.member-added"
+
 isConvResetNotif :: (HasCallStack, MakesValue n) => n -> App Bool
 isConvResetNotif n =
   fieldEquals n "payload.0.type" "conversation.mls-reset"
