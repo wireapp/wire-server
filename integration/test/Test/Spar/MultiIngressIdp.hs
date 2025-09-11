c7966c83c278255fe1734a32739d64d1d574a838
Sven Tennie <sven.tennie@wire.com>
2025-09-11 13:54:02 +0200

    Only write the domain field for multi-ingress domains
    We have to set some constraints on this field that wouldn't fit in the
    general case.

--- integration/test/Test/Spar/MultiIngressIdp.hs
+++ integration/test/Test/Spar/MultiIngressIdp.hs
@@ -12,29 +12,88 @@ ernieZHost = "nginz-https.ernie.example.com"
 bertZHost :: String
 bertZHost = "nginz-https.bert.example.com"
 
+kermitZHost :: String
+kermitZHost = "nginz-https.kermit.example.com"
+
 testMultiIngressIdp :: (HasCallStack) => App ()
 testMultiIngressIdp = do
-  (owner, tid, _) <- createTeam OwnDomain 1
-  void $ setTeamFeatureStatus owner tid "sso" "enabled"
+  withModifiedBackend
+    def
+      { sparCfg =
+          removeField "saml.spSsoUri"
+            >=> removeField "saml.spAppUri"
+            >=> removeField "saml.contacts"
+            >=> setField
+              "saml.spDomainConfigs"
+              ( object
+                  [ ernieZHost
+                      .= object
+                        [ "spAppUri" .= "https://webapp.ernie.example.com",
+                          "spSsoUri" .= "https://nginz-https.ernie.example.com/sso",
+                          "contacts" .= [object ["type" .= "ContactTechnical"]]
+                        ],
+                    bertZHost
+                      .= object
+                        [ "spAppUri" .= "https://webapp.bert.example.com",
+                          "spSsoUri" .= "https://nginz-https.bert.example.com/sso",
+                          "contacts" .= [object ["type" .= "ContactTechnical"]]
+                        ]
+                  ]
+              )
+      }
+    $ \domain -> do
+      (owner, tid, _) <- createTeam domain 1
+      void $ setTeamFeatureStatus owner tid "sso" "enabled"
 
-  SampleIdP idpmeta _pCreds _ _ <- makeSampleIdPMetadata
-  idpId <-
-    createIdpWithZHost owner (Just ernieZHost) idpmeta `bindResponse` \resp -> do
-      resp.status `shouldMatchInt` 201
-      resp.jsonBody %. "extraInfo.domain" `shouldMatch` ernieZHost
-      resp.jsonBody %. "id" >>= asString
+      -- Requests with configured multi-ingress domains
+      SampleIdP idpmeta _pCreds _ _ <- makeSampleIdPMetadata
+      idpId <-
+        createIdpWithZHost owner (Just ernieZHost) idpmeta `bindResponse` \resp -> do
+          resp.status `shouldMatchInt` 201
+          resp.jsonBody %. "extraInfo.domain" `shouldMatch` ernieZHost
+          resp.jsonBody %. "id" >>= asString
 
-  getIdp owner idpId `bindResponse` \resp -> do
-    resp.status `shouldMatchInt` 200
-    resp.jsonBody %. "extraInfo.domain" `shouldMatch` ernieZHost
+      getIdp owner idpId `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.jsonBody %. "extraInfo.domain" `shouldMatch` ernieZHost
 
-  updateIdpWithZHost owner (Just bertZHost) idpId idpmeta `bindResponse` \resp -> do
-    resp.status `shouldMatchInt` 200
-    resp.jsonBody %. "extraInfo.domain" `shouldMatch` bertZHost
+      updateIdpWithZHost owner (Just bertZHost) idpId idpmeta `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.jsonBody %. "extraInfo.domain" `shouldMatch` bertZHost
 
-  getIdp owner idpId `bindResponse` \resp -> do
-    resp.status `shouldMatchInt` 200
-    resp.jsonBody %. "extraInfo.domain" `shouldMatch` bertZHost
+      getIdp owner idpId `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.jsonBody %. "extraInfo.domain" `shouldMatch` bertZHost
+
+      -- Requests with an unconfigured domain
+      SampleIdP idpmeta2 _pCreds _ _ <- makeSampleIdPMetadata
+      idpId2 <-
+        createIdpWithZHost owner (Just kermitZHost) idpmeta2 `bindResponse` \resp -> do
+          resp.status `shouldMatchInt` 201
+          resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
+          resp.jsonBody %. "id" >>= asString
+
+      getIdp owner idpId2 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
+
+      -- From unconfigured to configured domain
+      updateIdpWithZHost owner (Just bertZHost) idpId2 idpmeta2 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.jsonBody %. "extraInfo.domain" `shouldMatch` bertZHost
+
+      getIdp owner idpId2 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.jsonBody %. "extraInfo.domain" `shouldMatch` bertZHost
+
+      -- From configured domain to unconfigured
+      updateIdpWithZHost owner (Just kermitZHost) idpId2 idpmeta2 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
+
+      getIdp owner idpId2 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
 
 -- TODO: Test creation of two IDPs for the same domain -> should fail
 -- TODO: Test updating existing IDP such that two with the same domain would exist -> should fail
