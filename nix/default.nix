e8d545c184a5b0345ee9a233e589b20d42e01839
Akshay Mankar <itsakshaymankar@gmail.com>
2025-09-12 14:00:07 +0200

    Initailize flake

--- nix/default.nix
+++ nix/default.nix
@@ -1,17 +1,18 @@
-let
-  sources = import ./sources.nix;
+{
+  sources ? import ./sources.nix,
 
-  pkgs = import sources.nixpkgs {
+  pkgs ? import sources.nixpkgs {
     config.allowUnfree = true;
     overlays = [
       # All wire-server specific packages
       (import ./overlay.nix)
       (import ./overlay-docs.nix)
     ];
-  };
+  },
 
-  pkgs_24_11 = import sources.nixpkgs_24_11 { };
-
+  pkgs_24_11 ? import sources.nixpkgs_24_11 { }
+}:
+let
   profileEnv = pkgs.writeTextFile {
     name = "profile-env";
     destination = "/.profile";
