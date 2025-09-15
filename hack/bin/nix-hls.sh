efbb424f7a82f7f86425265728207d5d4c67875e
Akshay Mankar <itsakshaymankar@gmail.com>
2025-09-15 13:31:33 +0200

    WIP

--- hack/bin/nix-hls.sh
+++ hack/bin/nix-hls.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP_LEVEL="$(cd "$DIR/../.." && pwd)"
 
-direnv="$(nix-build --no-out-link "$TOP_LEVEL/nix" -A pkgs.direnv)/bin/direnv"
+direnv="$(nix build --no-link --print-out-paths "$TOP_LEVEL#pkgs.direnv")/bin/direnv"
 
 # shellcheck disable=SC2016
 maxMemory=$("$direnv" exec "$TOP_LEVEL" bash -c 'echo "$HLS_MAX_MEMORY"')
