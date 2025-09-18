282e7a9f37a7fc870ef0b8ec25a281d1b1163a37
Sven Tennie <sven.tennie@wire.com>
2025-09-18 15:24:56 +0200

    Improve test

--- integration/test/Test/Spar/MultiIngressIdp.hs
+++ integration/test/Test/Spar/MultiIngressIdp.hs
@@ -2,7 +2,9 @@ module Test.Spar.MultiIngressIdp where
 
 import API.GalleyInternal
 import API.Spar
-import SAML2.WebSSO.Test.Util
+import Control.Lens ((.~), (^.))
+import qualified SAML2.WebSSO.Test.Util as SAML
+import qualified SAML2.WebSSO.Types as SAML
 import SetupHelpers
 import Testlib.Prelude
 
@@ -46,7 +48,7 @@ testMultiIngressIdp = do
       void $ setTeamFeatureStatus owner tid "sso" "enabled"
 
       -- Requests with configured multi-ingress domains
-      SampleIdP idpmeta _pCreds _ _ <- makeSampleIdPMetadata
+      SAML.SampleIdP idpmeta _pCreds _ _ <- SAML.makeSampleIdPMetadata
       idpId <-
         createIdpWithZHost owner (Just ernieZHost) idpmeta `bindResponse` \resp -> do
           resp.status `shouldMatchInt` 201
@@ -66,7 +68,7 @@ testMultiIngressIdp = do
         resp.jsonBody %. "extraInfo.domain" `shouldMatch` bertZHost
 
       -- Requests with an unconfigured domain
-      SampleIdP idpmeta2 _pCreds _ _ <- makeSampleIdPMetadata
+      SAML.SampleIdP idpmeta2 _pCreds _ _ <- SAML.makeSampleIdPMetadata
       idpId2 <-
         createIdpWithZHost owner (Just kermitZHost) idpmeta2 `bindResponse` \resp -> do
           resp.status `shouldMatchInt` 201
@@ -132,22 +134,44 @@ testMultiIngressAtMostOneIdPPerDomain = do
       (owner, tid, _) <- createTeam domain 1
       void $ setTeamFeatureStatus owner tid "sso" "enabled"
 
-      SampleIdP idpmeta1 _pCreds _ _ <- makeSampleIdPMetadata
+      SAML.SampleIdP idpmeta1 _ _ _ <- SAML.makeSampleIdPMetadata
       idpId1 <-
         createIdpWithZHost owner (Just ernieZHost) idpmeta1 `bindResponse` \resp -> do
           resp.status `shouldMatchInt` 201
           resp.jsonBody %. "id" >>= asString
 
-      SampleIdP idpmeta2 _pCreds _ _ <- makeSampleIdPMetadata
+      SAML.SampleIdP idpmeta2 _ _ _ <- SAML.makeSampleIdPMetadata
       void $ createIdpWithZHost owner (Just ernieZHost) idpmeta2 `bindResponse` \resp -> do
         resp.status `shouldMatchInt` 409
         resp.jsonBody %. "label" `shouldMatch` "idp-duplicate-domain-for-team"
 
-      updateIdpWithZHost owner (Just ernieZHost) idpId1 idpmeta2 `bindResponse` \resp -> do
-        resp.status `shouldMatchInt` 200
-        resp.jsonBody %. "extraInfo.domain" `shouldMatch` ernieZHost
+      -- The edIssuer needs to stay unchanged. Otherwise, deletion will fail
+      -- with a 404 (see bug https://wearezeta.atlassian.net/browse/WPB-20407)
+      idpId2 <-
+        updateIdpWithZHost owner (Just ernieZHost) idpId1 (idpmeta2 & SAML.edIssuer .~ (idpmeta1 ^. SAML.edIssuer)) `bindResponse` \resp -> do
+          resp.status `shouldMatchInt` 200
+          resp.jsonBody %. "extraInfo.domain" `shouldMatch` ernieZHost
+          resp.jsonBody %. "id" >>= asString
 
--- TODO: Test updating existing IDP such that two with the same domain would exist -> should fail
+      deleteIdp owner idpId2 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 204
+
+      SAML.SampleIdP idpmeta3 _ _ _ <- SAML.makeSampleIdPMetadata
+      idpId3 <-
+        createIdpWithZHost owner (Just ernieZHost) idpmeta3 `bindResponse` \resp -> do
+          resp.status `shouldMatchInt` 201
+          resp.jsonBody %. "extraInfo.domain" `shouldMatch` ernieZHost
+          resp.jsonBody %. "id" >>= asString
+
+      SAML.SampleIdP idpmeta4 _ _ _ <- SAML.makeSampleIdPMetadata
+      void $ createIdpWithZHost owner (Just ernieZHost) idpmeta3 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 409
+        resp.jsonBody %. "label" `shouldMatch` "idp-duplicate-domain-for-team"
+
+      updateIdpWithZHost owner (Just ernieZHost) idpId3 idpmeta4 `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        printJSON resp.jsonBody
+        resp.jsonBody %. "extraInfo.domain" `shouldMatch` ernieZHost
 
 testNonMultiIngressSetupsCanHaveMoreIdPsPerDomain :: (HasCallStack) => App ()
 testNonMultiIngressSetupsCanHaveMoreIdPsPerDomain = do
@@ -155,51 +179,51 @@ testNonMultiIngressSetupsCanHaveMoreIdPsPerDomain = do
   void $ setTeamFeatureStatus owner tid "sso" "enabled"
 
   -- With Z-Host header
-  SampleIdP idpmeta1 _pCreds _ _ <- makeSampleIdPMetadata
+  SAML.SampleIdP idpmeta1 _pCreds _ _ <- SAML.makeSampleIdPMetadata
   idpId1 <-
     createIdpWithZHost owner (Just ernieZHost) idpmeta1 `bindResponse` \resp -> do
       resp.status `shouldMatchInt` 201
       resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
       resp.jsonBody %. "id" >>= asString
 
-  SampleIdP idpmeta2 _pCreds _ _ <- makeSampleIdPMetadata
+  SAML.SampleIdP idpmeta2 _pCreds _ _ <- SAML.makeSampleIdPMetadata
   idpId2 <-
     createIdpWithZHost owner (Just ernieZHost) idpmeta2 `bindResponse` \resp -> do
       resp.status `shouldMatchInt` 201
       resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
       resp.jsonBody %. "id" >>= asString
 
-  SampleIdP idpmeta3 _pCreds _ _ <- makeSampleIdPMetadata
+  SAML.SampleIdP idpmeta3 _pCreds _ _ <- SAML.makeSampleIdPMetadata
   updateIdpWithZHost owner (Just ernieZHost) idpId1 idpmeta3 `bindResponse` \resp -> do
     resp.status `shouldMatchInt` 200
     resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
 
-  SampleIdP idpmeta4 _pCreds _ _ <- makeSampleIdPMetadata
+  SAML.SampleIdP idpmeta4 _pCreds _ _ <- SAML.makeSampleIdPMetadata
   updateIdpWithZHost owner (Just ernieZHost) idpId2 idpmeta4 `bindResponse` \resp -> do
     resp.status `shouldMatchInt` 200
     resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
 
   -- Without Z-Host header
-  SampleIdP idpmeta5 _pCreds _ _ <- makeSampleIdPMetadata
+  SAML.SampleIdP idpmeta5 _pCreds _ _ <- SAML.makeSampleIdPMetadata
   idpId5 <-
     createIdpWithZHost owner Nothing idpmeta5 `bindResponse` \resp -> do
       resp.status `shouldMatchInt` 201
       resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
       resp.jsonBody %. "id" >>= asString
 
-  SampleIdP idpmeta6 _pCreds _ _ <- makeSampleIdPMetadata
+  SAML.SampleIdP idpmeta6 _pCreds _ _ <- SAML.makeSampleIdPMetadata
   idpId6 <-
     createIdpWithZHost owner Nothing idpmeta6 `bindResponse` \resp -> do
       resp.status `shouldMatchInt` 201
       resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
       resp.jsonBody %. "id" >>= asString
 
-  SampleIdP idpmeta7 _pCreds _ _ <- makeSampleIdPMetadata
+  SAML.SampleIdP idpmeta7 _pCreds _ _ <- SAML.makeSampleIdPMetadata
   updateIdpWithZHost owner Nothing idpId5 idpmeta7 `bindResponse` \resp -> do
     resp.status `shouldMatchInt` 200
     resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
 
-  SampleIdP idpmeta8 _pCreds _ _ <- makeSampleIdPMetadata
+  SAML.SampleIdP idpmeta8 _pCreds _ _ <- SAML.makeSampleIdPMetadata
   updateIdpWithZHost owner Nothing idpId6 idpmeta8 `bindResponse` \resp -> do
     resp.status `shouldMatchInt` 200
     resp.jsonBody %. "extraInfo.domain" `shouldMatch` Null
