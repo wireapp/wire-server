a012ce8ca7e2fa5da435c2bb8205eb9fcc9da2da
Sven Tennie <sven.tennie@wire.com>
2025-09-23 17:08:45 +0200

    Do not send events from the simulated future

--- integration/test/Test/NotificationsBenchmark.hs
+++ integration/test/Test/NotificationsBenchmark.hs
@@ -4,7 +4,9 @@ import API.Brig
 import API.BrigCommon
 import API.Common
 import API.GundeckInternal
+import Control.Concurrent
 import qualified Data.Map as Map
+import Data.Time
 import GHC.Conc (numCapabilities)
 import GHC.Stack
 import SetupHelpers
@@ -28,12 +30,31 @@ testBench = do
   -- Later, we only read from this map. Thus, it doesn't have to be thread-safe.
   userMap :: Map Word TestRecipient <- Stream.fromList [0 :: Word .. 1000] & Stream.parMapM parCfg (\i -> generateTestRecipient >>= \r -> pure (i, r)) & Stream.fold toMap
 
+  now <- liftIO getCurrentTime
+
   -- To be replaced with real data from the file. (See
   -- https://wearezeta.atlassian.net/wiki/spaces/PET/pages/2118680620/Simulating+production-like+data)
-  let fakeData = zip [0 :: Word ..] (cycle [0 .. 1000])
+  let fakeData = zip (plusDelta now <$> [0 :: Word ..]) (cycle [0 .. 1000])
 
-  -- TODO: Use the timestamp a calculate a delay
-  Stream.fromList fakeData & Stream.parMapM parCfg (\(t, uNo) -> sendAndReceive uNo userMap) & Stream.fold Fold.drain
+  Stream.fromList fakeData & Stream.parMapM parCfg (\(t, uNo) -> waitForTimeStamp t >> sendAndReceive uNo userMap) & Stream.fold Fold.drain
+
+-- TODO: Add a speed factor to the simulation as we want to simulate faster than real time
+waitForTimeStamp :: UTCTime -> App ()
+waitForTimeStamp timestamp = liftIO $ do
+  now <- getCurrentTime
+  print $ "(timestamp, now)" ++ show (timestamp, now)
+  when (now < timestamp)
+    $
+    -- Event comes from the simulated future: Wait here until now and timestamp are aligned.
+    let delta = diffTimeToMicroSeconds $ diffUTCTime timestamp now
+     in print ("Waiting " ++ show delta ++ " microseconds. (timestamp, now)" ++ show (timestamp, now))
+          >> threadDelay delta
+  where
+    diffTimeToMicroSeconds :: NominalDiffTime -> Int
+    diffTimeToMicroSeconds dt = floor @Double (realToFrac dt * 1_000_000)
+
+plusDelta :: UTCTime -> Word -> UTCTime
+plusDelta timestamp deltaMilliSeconds = addUTCTime (fromIntegral deltaMilliSeconds / 1000) timestamp
 
 sendAndReceive :: Int -> Map Word TestRecipient -> App ()
 sendAndReceive userNo userMap = do
