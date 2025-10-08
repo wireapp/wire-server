ad6fe1e476bdd679e2e8335f86f8ef88575d7e7d
Akshay Mankar <itsakshaymankar@gmail.com>
2025-10-08 11:56:30 +0200

    Delete unused column mls_group_member_client.key_pacakge_ref

--- libs/wire-subsystems/postgres-migrations/20250908142515-create-conversation-tables.sql
+++ libs/wire-subsystems/postgres-migrations/20250908142515-create-conversation-tables.sql
@@ -77,7 +77,6 @@ CREATE TABLE mls_group_member_client (
   user_domain text NOT NULL,
   "user" uuid NOT NULL,
   client text NOT NULL,
-  key_package_ref bytea,
   leaf_node_index integer NOT NULL,
   removal_pending boolean NOT NULL,
   PRIMARY KEY (group_id, user_domain, "user", client)
