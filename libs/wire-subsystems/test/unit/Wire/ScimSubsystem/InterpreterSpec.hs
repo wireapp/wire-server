f64499f73ebe5e74cf6ab94d614a1079062f712b
Matthias Fischmann <mf@zerobuzz.net>
2025-10-15 21:01:26 +0200

    Test works: create scim user group, get it back.

--- libs/wire-subsystems/test/unit/Wire/ScimSubsystem/InterpreterSpec.hs
+++ libs/wire-subsystems/test/unit/Wire/ScimSubsystem/InterpreterSpec.hs
@@ -4,6 +4,7 @@
 module Wire.ScimSubsystem.InterpreterSpec (spec) where
 
 import Data.Id
+import Data.Text qualified as Text
 import Imports
 import Network.URI
 import Polysemy
@@ -18,6 +19,7 @@ import Web.Scim.Schema.Common qualified as Common
 import Web.Scim.Schema.Meta qualified as Common
 import Wire.API.Team.Member as TM
 import Wire.API.User as User
+import Wire.API.User.Scim
 import Wire.API.UserGroup
 import Wire.ScimSubsystem
 import Wire.ScimSubsystem.Interpreter
@@ -57,86 +59,37 @@ runDependencies initialUsers initialTeams =
 
 instance Arbitrary Group.Group where
   arbitrary = do
-    mems <- listOf1 arbitrary
+    name <- Text.pack . take 4000 <$> ((:) <$> arbitrary <*> arbitrary)
     pure
       Group.Group
         { schemas = ["urn:ietf:params:scim:schemas:core:2.0:Group"],
-          displayName = "some scim user group",
-          members = mems
+          displayName = name,
+          members = []
         }
 
-instance Arbitrary Group.Member where
-  arbitrary = do
-    value <- arbitrary
-    let typ = "User"
-    let ref = "http://.../scim/vs/Users/463c0b40-a9d9-11f0-b091-181dea96dd7"
-    pure Group.Member {..}
+mkScimGroupMember :: User -> Group.Member
+mkScimGroupMember (idToText . User.userId -> value) =
+  let typ = "User"
+      ref = "$schema://$host.$domain/scim/vs/Users/$uuid" -- not a real URI, just a string for testing.
+   in Group.Member {..}
 
 spec :: Spec
 spec = UGS.timeoutHook $ describe "ScimSubsystem.Interpreter" $ do
-  focus . describe "createScimGroup" $ do
-    prop "creates a group returns it" $ \team newUserGroup ->
-      let resultOrError =
+  describe "createScimGroup" $ do
+    prop "creates a group returns it" $ \(team :: UGS.ArbitraryTeam) (newScimGroup_ :: Group.Group) ->
+      let newScimGroup =
+            newScimGroup_
+              { Group.members =
+                  let all_ = UGS.allUsers team
+                      nonscim_ = filter (\u -> u.userManagedBy /= ManagedByScim) all_
+                   in mkScimGroupMember <$> nonscim_
+              }
+          resultOrError = do
             runDependencies (UGS.allUsers team) (UGS.galleyTeam team) $ do
-              createdGroup <- createScimGroup team.tid newUserGroup
+              createdGroup :: Group.StoredGroup SparTag <- createScimGroup team.tid newScimGroup
               retrievedGroup :: Maybe UserGroup <- UGS.getGroup (UGS.ownerId team) createdGroup.thing.id False
               pure (createdGroup, retrievedGroup)
        in case resultOrError of
             Left err -> counterexample ("Left: " ++ show err) False
             Right (createdGroup, retrievedGroup) ->
               Just createdGroup.thing.id === ((.id_) <$> retrievedGroup)
-
-{-
-  let rndShuffle xs gen = map fst $ sortOn snd $ zip xs (Rand.randoms gen :: [Int])
-      -- ~half the users
-      n = length (allUsers team) `div` 2 + 1
-      members = take n $ rndShuffle (allUsers team) (Rand.mkStdGen seed)
-      resultOrError =
-        runDependencies (allUsers team) (galleyTeam team) scimBaseUrl
-          . interpretUserGroupSubsystem
-          $ do
-            let newUserGroup =
-                  NewUserGroup
-                    { name = newUserGroupName,
-                      members = User.userId <$> V.fromList members
-                    }
-            createdGroup <- createGroup (ownerId team) newUserGroup
-            retrievedGroup <- getGroup (ownerId team) createdGroup.id_ False
-            now <- toUTCTimeMillis <$> get
-            let assert =
-                  createdGroup.name
-                    === newUserGroupName
-                      .&&. createdGroup.members
-                    === Identity newUserGroup.members
-                      .&&. createdGroup.managedBy
-                    === ManagedByWire
-                      .&&. createdGroup.createdAt
-                    === now
-                      .&&. Just createdGroup
-                    === retrievedGroup
-            pure (createdGroup, assert)
-
-      assertPushEvents :: UserGroup -> [Push] -> Property
-      assertPushEvents ug pushes =
-        foldl
-          ( \acc push ->
-              acc .&&. case A.fromJSON @Event (A.Object push.json) of
-                A.Success (UserGroupEvent (UserGroupCreated ugid)) ->
-                  ugid
-                    === ug.id_
-                      .&&. push.origin
-                    === Just (ownerId team)
-                _ -> counterexample ("Failed to decode push: " <> show push) False
-          )
-          (length pushes === 1)
-          pushes
-   in case resultOrError of
-          Left err -> counterexample ("Unexpected error: " <> show err) False
-          Right (pushes, (ug, propertyCheck)) -> assertPushEvents ug pushes .&&. propertyCheck
-        prop
-        "created groups can be found in team-management, with managed_by == scim"
-        $ \(_ :: Bool) -> True
-          prop
-          "refuses to create scim groups with non-scim users"
-          $ \(_ :: Bool) -> True
--}
