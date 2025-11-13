be60c26969b46941c0c1cd6d580a4ac60646ab5c
Matthias Fischmann <mf@zerobuzz.net>
2025-11-13 12:09:24 +0100

    Fix email visibility for admin: add failing integration test.

--- integration/test/Test/Teams.hs
+++ integration/test/Test/Teams.hs
@@ -485,3 +485,22 @@ testSelectTeamMembersByIds = do
     resp.status `shouldMatchInt` 200
     tms <- resp.json %. "members" >>= asList
     length tms `shouldMatchInt` 5
+
+testListUsersEmailVisibility :: (HasCallStack) => App ()
+testListUsersEmailVisibility = do
+  let overrides :: ServiceOverrides =
+        def {brigCfg = setField "optSettings.setEmailVisibility" "visible_to_self"}
+
+  withModifiedBackend overrides $ \dom -> do
+    let userToQId mem = do
+          memId <- mem %. "id" >>= asString
+          pure $ object ["domain" .= dom, "id" .= memId]
+
+    (owner, _tid, mems) <- createTeam dom 5
+    memQIds <- for mems userToQId
+    memEmails <- for mems ((%. "email") >=> asString)
+    listUsers owner memQIds `bindResponse` \resp -> do
+      resp.status `shouldMatchInt` 200
+      returnedUsers <- resp.json %. "found" >>= asList
+      returnedEmails <- for returnedUsers ((%. "email") >=> asString)
+      returnedEmails `shouldMatch` memEmails
