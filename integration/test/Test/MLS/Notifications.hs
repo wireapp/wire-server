8c74da86a9f0edb1a6ab471a9a2ddfae101d75a6
Matthias Fischmann <mf@zerobuzz.net>
2025-11-24 11:24:27 +0100

    Failing integration test to reproduce ticket issue [WIP: not failing]

--- integration/test/Test/MLS/Notifications.hs
+++ integration/test/Test/MLS/Notifications.hs
@@ -17,7 +17,10 @@
 
 module Test.MLS.Notifications where
 
+import API.Galley
 import API.Gundeck
+import Control.Concurrent
+import Data.Timeout
 import MLS.Util
 import Notifications
 import SetupHelpers
@@ -45,3 +48,51 @@ testWelcomeNotification = do
           size = Just 10000
         }
       >>= getJSON 200
+
+testNotificationPagination :: (HasCallStack) => App ()
+testNotificationPagination = do
+  {-
+  Create a user with client1
+
+  Wait for all the notifications to expire (there should be no notifications which are sent to “All Clients”, i.e. there should be no notifications in the DB where clients field is null.
+
+  Send a lot of MLS messages to this client (so that the total payload size goes over 5 MB)
+
+  Create another client (say client2)
+
+  Fetch notifications for client 2
+  -}
+
+  let overrides = def {gundeckCfg = setField "settings.notificationTTL" (2 #> Second)}
+  withModifiedBackend overrides $ \dom -> do
+    -- create a user with client1
+    [alice, bob] <- createAndConnectUsers [dom, dom]
+    [alice1, bob1] <- traverse (createMLSClient def) [alice, bob]
+    traverse_ (uploadNewKeyPackage def) [alice1, bob1]
+
+    conv <- postConversation alice1 defMLS >>= getJSON 201
+    _convQid <- conv %. "qualified_id"
+    convId <- objConvId conv
+    createGroup def alice1 convId
+    void $ createAddCommit alice1 convId [bob] >>= sendAndConsumeCommitBundle
+
+    -- let notifs expire
+    liftIO $ threadDelay 2_300_000
+
+    -- bob sends alice > 5MB of MLS messages
+    let payload = replicate 20_000 '.'
+    forM_ [1 :: Int .. 260] $ \i -> do
+      print i
+      void $ createApplicationMessage convId bob1 payload >>= sendAndConsumeMessage
+
+    -- create new client for alice, fetch notifications, and check response consistency.
+    alice2 :: ClientIdentity <- createMLSClient def alice
+
+    getNotifications
+      alice
+      def {since = Nothing, size = Nothing, client = Just alice2.client}
+      `bindResponse` \resp -> do
+        resp.status `shouldMatchInt` 200
+        resp.json %. "has_more" `shouldMatch` False
+        nfs :: [Value] <- resp.json %. "notifications" >>= asList
+        length nfs `shouldMatchInt` 1
