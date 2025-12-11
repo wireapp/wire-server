a4b3a2e3f87da45effa4883684a595591939bf5c
Matthias Fischmann <mf@zerobuzz.net>
2025-12-11 13:01:38 +0100

    Implement validatePatchOp.

--- libs/hscim/src/Web/Scim/Schema/PatchOp.hs
+++ libs/hscim/src/Web/Scim/Schema/PatchOp.hs
@@ -17,61 +17,72 @@
 
 module Web.Scim.Schema.PatchOp where
 
-import Control.Monad (guard)
+import Control.Monad.Error.Class (MonadError, throwError)
+import Data.Aeson
 import qualified Data.Aeson.Diff as AD
 import qualified Data.Aeson.KeyMap as AK
-import Data.Aeson.Types
+import qualified Data.Aeson.Pointer as AD
 import Data.Bifunctor (first)
+import Imports
 import Web.Scim.Schema.Common (lowerKey)
-import Web.Scim.Schema.Schema (Schema (PatchOp20))
+import Web.Scim.Schema.Error
+import Web.Scim.Schema.Schema
 
--- | For scim patches (both user and group), we use the aeson-diff
 -- package.  This type provides the parser for the scim patch syntax.
 --
--- FUTUREWORK: some parts of the standard have been deemed exotic
+-- FUTUREWORK(fisx): some parts of the standard have been deemed exotic
 -- enough to skip implementation (sometimes, there are pending tests
 -- stubs for those things, sometimes not).  Example: value paths
 -- (filter on arrays to select some attributes, this is handled with
 -- indices in aeson-diff) are a scim thing and not supported here.
--- one could implement them by storing an extra `Map Text ValueOption`
--- in `PatchOp` and using the `AD.Path`s in `AD.Patch` to reference
--- those, but then we could't just call AD.patch as is any more.
 --
 -- https://datatracker.ietf.org/doc/html/rfc7644#section-3.5.2
+-- https://datatracker.ietf.org/doc/html/rfc6901
 newtype PatchOp = PatchOp AD.Patch
   deriving (Eq, Show)
 
-isLegalPatchOp :: PatchOp -> Bool
-isLegalPatchOp (PatchOp (AD.Patch ops)) = all ok ops
+-- | Allow only add, remove, replace, and disallow array indices (the
+-- latter need to be translated into value search expressions in
+-- scim).
+--
+-- Example:
+--
+--    { "schemas":
+--        ["urn:ietf:params:scim:api:messages:2.0:PatchOp"],
+--      "Operations":[
+--        {
+--         "op":"add",
+--         "path":"members",
+--         "value":[
+--          {
+--            "display": "Babs Jensen",
+--            "$ref": "https://example.com/v2/Users/2819c223...413861904646",
+--            "value": "2819c223-7f76-453a-919d-413861904646"
+--          }
+--         ]
+--        },
+--        ... + additional operations if needed ...
+--      ]
+--    }
+validatePatchOp :: forall m. (MonadError String m) => PatchOp -> m ()
+validatePatchOp (PatchOp (AD.Patch ops)) = opOk `mapM_` ops
   where
-    ok :: AD.Operation -> Bool
-    ok =
-      -- only add, remove, replace.  maybe we can also explicitly
-      -- scream about unsupposed value paths (array indices)?
-      undefined
+    opOk :: AD.Operation -> m ()
+    opOk = \case
+      AD.Add path _ -> pathOk path
+      AD.Rem path -> pathOk path
+      AD.Rep path _ -> pathOk path
+      AD.Mov {} -> throwError "unsupported patch operation: mov"
+      AD.Cpy {} -> throwError "unsupported patch operation: cpy"
+      AD.Tst {} -> throwError "unsupported patch operation: tst"
 
-{-
- { "schemas":
-       ["urn:ietf:params:scim:api:messages:2.0:PatchOp"],
-     "Operations":[
-       {
-        "op":"add",
-        "path":"members",
-        "value":[
-         {
-           "display": "Babs Jensen",
-           "$ref": "https://example.com/v2/Users/2819c223...413861904646",
-           "value": "2819c223-7f76-453a-919d-413861904646"
-         }
-        ]
-       },
-       ... + additional operations if needed ...
-     ]
-   }
+    pathOk :: AD.Pointer -> m ()
+    pathOk (AD.Pointer path) = keyOk `mapM_` path
 
-under "Operations" comes an AD.Patch.  we just need to forbid some operations.
-
--}
+    keyOk :: AD.Key -> m ()
+    keyOk = \case
+      AD.OKey {} -> pure ()
+      AD.AKey {} -> throwError "unsupported key type: index" -- TODO: make this work!
 
 instance FromJSON PatchOp where
   parseJSON = withObject "PatchOp" $ \obj_ -> do
@@ -79,19 +90,13 @@ instance FromJSON PatchOp where
     scms :: [Schema] <- obj .: "schemas"
     patchOp <- PatchOp <$> obj .: "operations"
     guard $ PatchOp20 `elem` scms
-    guard $ isLegalPatchOp patchOp
+    either fail pure (validatePatchOp patchOp)
     pure patchOp
 
 instance ToJSON PatchOp where
   toJSON (PatchOp operations) =
     object ["schemas" .= [PatchOp20], "operations" .= operations]
 
-operationIsLegal :: AD.Operation -> Bool
-operationIsLegal _op = undefined
-
-applyPatch {- (FromJSON a, ToJSON a, MonadError ScimError m) => -} :: PatchOp -> a -> m a
-applyPatch =
-  -- errors out when:
-  -- 1. parsing of patched value doesn't work
-  -- 2. ?
-  undefined
+applyPatch :: forall m a. (FromJSON a, ToJSON a, MonadError ScimError m) => PatchOp -> a -> m a
+applyPatch op a = do
+  throwError (todo op (eitherDecode @a (encode a)) :: ScimError)
