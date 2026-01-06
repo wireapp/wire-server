78d2e3da0709803bdc868bf89e5a664b5d6c9961
Akshay Mankar <itsakshaymankar@gmail.com>
2026-01-06 13:53:38 +0100

    HLS: Disable hlint
    https://github.com/haskell/haskell-language-server/issues/4674

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -29,4 +29,16 @@ self: super: {
   rabbitmqadmin = super.callPackage ./pkgs/rabbitmqadmin { };
 
   sbomqs = super.callPackage ./pkgs/sbomqs { };
+
+  # Disable hlint in HLS to get around this bug:
+  # https://github.com/haskell/haskell-language-server/issues/4674
+  haskell = super.haskell // {
+    packages = super.haskell.packages // {
+      ghc910 = super.haskell.packages.ghc910.override {
+        overrides = hfinal: hprev: {
+          haskell-language-server = self.haskell.lib.disableCabalFlag hprev.haskell-language-server "hlint";
+        };
+      };
+    };
+  };
 }
