f0b553ef0adcef09a5f207a7948187469d7fef20
Sven Tennie <sven.tennie@wire.com>
2026-01-12 18:16:06 +0100

    use zUser constant

--- services/spar/test/Test/Spar/Saml/IdPSpec.hs
+++ services/spar/test/Test/Spar/Saml/IdPSpec.hs
@@ -94,7 +94,9 @@ spec =
                   ( Info,
                     "IdP created, team="
                       <> (TL.encodeUtf8 . TL.fromStrict . idToText) tid
-                      <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://accounts.accesscontrol.windows.net/auth, domain=None, user=59128ccc-d38a-1d23-67d9-4f529ee7ca9f, certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37, replaces=None\n"
+                      <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://accounts.accesscontrol.windows.net/auth, domain=None, user="
+                      <> (TL.encodeUtf8 . TL.fromStrict . idToText . fromJust) zUser
+                      <> ", certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37, replaces=None\n"
                   )
 
             forM_ [(minBound :: WireIdPAPIVersion) .. maxBound] $ \apiVersion -> do
@@ -120,7 +122,9 @@ spec =
                       <> (TL.encodeUtf8 . TL.fromStrict . idToText) tid
                       <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://accounts.accesscontrol.windows.net/auth, domain="
                       <> domainPart
-                      <> ", user=59128ccc-d38a-1d23-67d9-4f529ee7ca9f, certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37, replaces=None\n"
+                      <> ", user="
+                      <> (TL.encodeUtf8 . TL.fromStrict . idToText . fromJust) zUser
+                      <> ", certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37, replaces=None\n"
                   )
                 expectedLogLineWithDomain = expectedLogLine . TL.encodeUtf8 . TL.fromStrict $ miHostAsText
                 expectedLogLineWithoutDomain = expectedLogLine "None"
@@ -149,7 +153,9 @@ spec =
                   ( Info,
                     "IdP deleted, team="
                       <> (TL.encodeUtf8 . TL.fromStrict . idToText) tid
-                      <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://accounts.accesscontrol.windows.net/auth, domain=None, user=59128ccc-d38a-1d23-67d9-4f529ee7ca9f, certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37\n"
+                      <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://accounts.accesscontrol.windows.net/auth, domain=None, user="
+                      <> (TL.encodeUtf8 . TL.fromStrict . idToText . fromJust) zUser
+                      <> ", certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37\n"
                   )
 
             (logs, _res) <- interpretWithLoggingMock (Just user) $ do
@@ -167,7 +173,9 @@ spec =
                       <> (TL.encodeUtf8 . TL.fromStrict . idToText) tid
                       <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://accounts.accesscontrol.windows.net/auth, domain="
                       <> (TL.encodeUtf8 . TL.fromStrict) miHostAsText
-                      <> ", user=59128ccc-d38a-1d23-67d9-4f529ee7ca9f, certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37\n"
+                      <> ", user="
+                      <> (TL.encodeUtf8 . TL.fromStrict . idToText . fromJust) zUser
+                      <> ", certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37\n"
                   )
 
             (logs, _res) <- interpretWithLoggingMock (Just user) $ do
@@ -184,7 +192,9 @@ spec =
                   ( Info,
                     "IdP updated, team="
                       <> (TL.encodeUtf8 . TL.fromStrict . idToText) tid
-                      <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://accounts.accesscontrol.windows.net/auth, domain=None, user=59128ccc-d38a-1d23-67d9-4f529ee7ca9f, new-certificates=, removed-certificates=\n"
+                      <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://accounts.accesscontrol.windows.net/auth, domain=None, user="
+                      <> (TL.encodeUtf8 . TL.fromStrict . idToText . fromJust) zUser
+                      <> ", new-certificates=, removed-certificates=\n"
                   )
 
             (logs, _res) <- interpretWithLoggingMock (Just user) $ do
@@ -202,7 +212,9 @@ spec =
                       <> (TL.encodeUtf8 . TL.fromStrict . idToText) tid
                       <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://accounts.accesscontrol.windows.net/auth, domain="
                       <> (TL.encodeUtf8 . TL.fromStrict) miHostAsText
-                      <> ", user=59128ccc-d38a-1d23-67d9-4f529ee7ca9f, new-certificates=, removed-certificates=\n"
+                      <> ", user="
+                      <> (TL.encodeUtf8 . TL.fromStrict . idToText . fromJust) zUser
+                      <> ", new-certificates=, removed-certificates=\n"
                   )
 
             (logs, _res) <- interpretWithLoggingMock (Just user) $ do
@@ -229,7 +241,9 @@ spec =
                   ( Info,
                     "IdP updated, team="
                       <> (TL.encodeUtf8 . TL.fromStrict . idToText) tid
-                      <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://new.idp.example.com/auth, domain=None, user=59128ccc-d38a-1d23-67d9-4f529ee7ca9f, new-certificates=Issuer: Country=US,O=Okta,OU=SSOProvider,CN=dev-500508,Email Address=info@okta.com; Subject: Country=US,O=Okta,OU=SSOProvider,CN=dev-500508,Email Address=info@okta.com; SHA1 Fingerprint: 5C:42:5B:27:B3:96:CC:9D:1B:1F:0E:4F:2B:8A:B8:E4:3C:9E:96:34, removed-certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37\n"
+                      <> ", idpId=00000000-0000-0000-0000-000000000000, issuer=https://new.idp.example.com/auth, domain=None, user="
+                      <> (TL.encodeUtf8 . TL.fromStrict . idToText . fromJust) zUser
+                      <> ", new-certificates=Issuer: Country=US,O=Okta,OU=SSOProvider,CN=dev-500508,Email Address=info@okta.com; Subject: Country=US,O=Okta,OU=SSOProvider,CN=dev-500508,Email Address=info@okta.com; SHA1 Fingerprint: 5C:42:5B:27:B3:96:CC:9D:1B:1F:0E:4F:2B:8A:B8:E4:3C:9E:96:34, removed-certificates=Issuer: CN=accounts.accesscontrol.windows.net; Subject: CN=accounts.accesscontrol.windows.net; SHA1 Fingerprint: 15:28:A6:B8:5A:C5:36:80:B4:B0:95:C6:9A:FD:77:9C:D6:5C:78:37\n"
                   )
 
             (logs, _res) <- interpretWithLoggingMock (Just user) $ do
