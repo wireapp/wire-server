e6ad9ee0b08d6e1a92d1915badfd351e92323607
Sven Tennie <sven.tennie@wire.com>
2026-01-14 09:22:53 +0100

    Simplify rusty-jwt-tools Nix setup
    Looks like `buildRustPackage` improved. Thus, we can simplify this Nix
    derivation and - because rusty-jwt-tool now has a `Cargo.lock` file on
    toplevel - drop cargo lock file related logic.

--- nix/pkgs/rusty_jwt_tools_ffi/default.nix
+++ nix/pkgs/rusty_jwt_tools_ffi/default.nix
@@ -6,39 +6,20 @@
 , gitMinimal
 }:
 
-# TODO: update to crate2nix once https://github.com/wireapp/rusty-jwt-tools as a
-# Cargo.lock file in its root (not at the ffi/ subpath).
-
 let
-  version = "0.9.0";
+  version = "0.14.0";
   src = fetchFromGitHub {
     owner = "wireapp";
     repo = "rusty-jwt-tools";
-    rev = "05441e98d9c7c5ec9bfcfba84e885988278f10e6";
-    sha256 = "sha256-HVq2BpPKp3cfdlKrS1AYWQ+a5VigFsYfSecZ60SFATI=";
+    rev = "b5de03a239d41b51e7cf9cd7c2674fa6be205134";
+    sha256 = "sha256-k7zxtdpLYWP+EYa+Vf6enViS+jIR2zt07yBZdozhKuE=";
   };
-  cargoLockFile = builtins.toFile "cargo.lock" (builtins.readFile "${src}/Cargo.lock");
-
 in
 rustPlatform.buildRustPackage {
   name = "rusty_jwt-tools_ffi-${version}";
   inherit version src;
-
-  cargoLock = {
-    lockFile = cargoLockFile;
-    outputHashes = {
-      # if any of these need updating, replace / create new key with
-      # lib.fakeSha256, rebuild, and replace with actual hash.
-      "certval-0.1.4" = "sha256-4BWvSzFZhlA+mKj+Y6GNEwNSKikNGVjDoPxyxiw9TFE=";
-      "biscuit-0.6.0-beta1" = "sha256-no7b4Un+7AES7EwWdZh/oeIa4w0caKLAUFsHWqgJOrg=";
-      "jwt-simple-0.13.0" = "sha256-QkVi7EGrU3nF+/32tNjTtAILo8sjasR27nyRgBH+xoA=";
-      "rcgen-0.9.2" = "sha256-3jFzInwdzFBot+L2Vm5NLF1ml33GH2+Iv3LqqGhLxFs=";
-      "ring-0.17.0-not-released-yet" = "sha256-TP8yZo64J/d1fw8l2J4+ol70EcHvpvHJBdpF3A+6Dgo=";
-    };
-  };
-
-  postPatch = ''
-    cp ${cargoLockFile} Cargo.lock
-  '';
+  # `buildRustPackage` requires `cargoHash`. So we have to update it as well
+    # when the Git `rev` is changed.
+  cargoHash = "sha256-gvFEwb+Cxnk7GhWrKs4hEhfyAI/QiE8Zqd2ZXXLbvuE=";
   doCheck = false;
 }
