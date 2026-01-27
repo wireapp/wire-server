5275938f9524be1ec59b58db75037781a0140091
Sven Tennie <sven.tennie@wire.com>
2026-01-27 17:18:14 +0100

    Update certs

--- libs/wire-subsystems/test/resources/saml/generate-certs.sh
+++ libs/wire-subsystems/test/resources/saml/generate-certs.sh
@@ -3,7 +3,7 @@
 # Generate a key and certs for testing
 openssl genpkey -algorithm RSA -out mykey.pem -pkeyopt rsa_keygen_bits:2048
 openssl req -new -x509 -key mykey.pem -out cert1.pem -days 3650 \
-  -subj "/C=US/O=ExampleOrg/OU=Dev1CN=CertOne/emailAddress=one@example.com"
+  -subj "/C=US/O=ExampleOrg/OU=Dev1/CN=CertOne/emailAddress=one@example.com"
 openssl req -new -x509 -key mykey.pem -out cert2.pem -days 3650 \
   -subj "/C=DE/O=ExampleOrg/OU=Dev2/CN=CertTwo/emailAddress=two@example.com"
 
