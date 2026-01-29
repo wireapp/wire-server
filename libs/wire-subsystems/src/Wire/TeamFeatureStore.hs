2961863f348dcfca6943a9e00cc566c26e317309
Leif Battermann <leif.battermann@wire.com>
2026-01-29 09:05:30 +0000

    revomved unused extensions

--- libs/wire-subsystems/src/Wire/TeamFeatureStore.hs
+++ libs/wire-subsystems/src/Wire/TeamFeatureStore.hs
@@ -14,9 +14,6 @@
 --
 -- You should have received a copy of the GNU Affero General Public License along
 -- with this program. If not, see <https://www.gnu.org/licenses/>.
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
-
 module Wire.TeamFeatureStore where
 
 import Data.Id
