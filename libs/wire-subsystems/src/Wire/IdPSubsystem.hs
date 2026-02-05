35d946d1de175e132096acfdc4e12d54e17abf01
Sven Tennie <sven.tennie@wire.com>
2026-02-05 08:37:11 +0100

    Prepare interpreter

--- libs/wire-subsystems/src/Wire/IdPSubsystem.hs
+++ libs/wire-subsystems/src/Wire/IdPSubsystem.hs
@@ -2,6 +2,7 @@ module Wire.IdPSubsystem where
 
 import Imports
 import Wire.API.Routes.Public (ZHostValue)
+import Wire.API.User (EmailAddress)
 
 data IdPSubsystem m a where
-  GetSsoCodeByEmail :: Maybe ZHostValue -> IdPSubsystem m ()
+  GetSsoCodeByEmail :: Maybe ZHostValue -> EmailAddress -> IdPSubsystem m ()
--- libs/wire-subsystems/src/Wire/IdPSubsystem/Interpreter.hs
+++ libs/wire-subsystems/src/Wire/IdPSubsystem/Interpreter.hs
@@ -1,1 +1,10 @@
-module Wire.IdPSubsystem.Interpreter where
+module Wire.IdPSubsystem.Interpreter (interpretIdPSubsystem) where
+
+import Imports
+import Polysemy
+import Wire.IdPSubsystem
+
+-- TODO: Use RateLimit effect for rate limiting
+interpretIdPSubsystem :: InterpreterFor IdPSubsystem r
+interpretIdPSubsystem = interpret $ \case
+  GetSsoCodeByEmail mbHost email -> undefined
