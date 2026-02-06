8bc336f0bf00ef4ceadfa7d9b93d7a32f8a84ee8
Sven Tennie <sven.tennie@wire.com>
2026-02-06 15:51:55 +0100

    Structure my work with TODOs

--- libs/wire-subsystems/src/Wire/IdPSubsystem/Interpreter.hs
+++ libs/wire-subsystems/src/Wire/IdPSubsystem/Interpreter.hs
@@ -7,4 +7,10 @@ import Wire.IdPSubsystem
 -- TODO: Use RateLimit effect for rate limiting
 interpretIdPSubsystem :: InterpreterFor IdPSubsystem r
 interpretIdPSubsystem = interpret $ \case
-  GetSsoCodeByEmail mbHost email -> undefined
+  GetSsoCodeByEmail mbHost email -> do
+    -- TODO: Rate limiting
+    -- TODO: Disable on cloud
+    -- TODO: The email belongs to a team -> lookup team
+    -- TODO: Lookup IdP for domain
+    -- TODO: Ensure: the user was created via SCIM or SSO (this means the user is managed_by: scim (and also has sso_id: {tenant: …, subject: …} properties))
+    undefined
