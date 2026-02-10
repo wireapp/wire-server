17c331bebd037d80975739740b63f0db3d58ae29
Sven Tennie <sven.tennie@wire.com>
2026-02-10 18:10:38 +0100

    Add test

--- libs/wire-subsystems/test/unit/Wire/IdPSubsystem/InterpreterSpec.hs
+++ libs/wire-subsystems/test/unit/Wire/IdPSubsystem/InterpreterSpec.hs
@@ -195,6 +195,44 @@ spec = describe "IdPSubsystem.Interpreter" $ do
       resultTail `shouldBe` Right (Just idp._idpId)
       filter (\(lvl, _msg) -> lvl > Log.Info) logsTail `shouldBe` mempty
 
+    prop "returns and IdP if there are multiple" $ \(teamMember :: TeamMember) user (idps :: NE.NonEmpty IdP) userRef email teamId dom -> do
+      -- This should not happen, because the IdP management API allows to
+      -- create only one IdP per domain. However, better not have undefined
+      -- behaviour - Just in case...
+      let userWithEmail =
+            user
+              { userIdentity = Just (SSOIdentity (UserSSOId userRef) (Just email)),
+                userEmailUnvalidated = Just email,
+                userTeam = Just teamId,
+                userManagedBy = ManagedByScim
+              }
+
+          idpsWithTeam =
+            idps
+              <&> SAML.idpExtraInfo . domain ?~ dom
+              <&> SAML.idpExtraInfo . team .~ teamId
+
+          teamMember' = teamMember & Wire.API.Team.Member.userId .~ (qUnqualified userWithEmail.userQualifiedId)
+
+          teams = Map.singleton teamId [teamMember']
+
+          (result, logs) =
+            runAllEffects
+              True
+              (NE.toList idpsWithTeam)
+              teams
+              (brigAPIAccessMockFn email userWithEmail)
+              (getSsoCodeByEmail (Just dom) email)
+
+          expectedIdPIds :: [SAML.IdPId] = NE.toList $ ((SAML._idpId) <$> idpsWithTeam)
+
+      result
+        `shouldSatisfy` ( \case
+                            Right (Just x) -> x `elem` expectedIdPIds
+                            e -> error $ "Unexpected result " <> show e
+                        )
+      filter (\(lvl, _msg) -> lvl > Log.Info) logs `shouldBe` mempty
+
     prop "returns Nothing for an unknown email" $ \(teamMember :: TeamMember) user idp userRef email anotherEmail teamId mbDomain -> do
       let userWithEmail =
             user
