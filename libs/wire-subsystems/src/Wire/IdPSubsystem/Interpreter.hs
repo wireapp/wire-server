bc985095ab471e93a5c06279a58b46d9866ebdf8
Sven Tennie <sven.tennie@wire.com>
2026-02-12 17:02:31 +0100

    Refactor

--- libs/wire-subsystems/src/Wire/IdPSubsystem/Interpreter.hs
+++ libs/wire-subsystems/src/Wire/IdPSubsystem/Interpreter.hs
@@ -34,7 +34,18 @@ interpretIdPSubsystem ::
   Bool ->
   InterpreterFor IdPSubsystem r
 interpretIdPSubsystem enableIdPByEmailDiscovery = interpret $ \case
-  GetSsoCodeByEmail mbHost email -> do
+  GetSsoCodeByEmail mbHost email -> getSsoCodeByEmailImpl enableIdPByEmailDiscovery mbHost email
+
+getSsoCodeByEmailImpl ::
+  ( Member (Logger (Log.Msg -> Log.Msg)) r,
+    Member (Error IdPSubsystemError) r,
+    Member BrigAPIAccess r,
+    Member GalleyAPIAccess r,
+    Member IdPConfigStore r
+  ) =>
+  Bool -> Maybe Text -> EmailAddress -> Sem r (Maybe SAML.IdPId)
+getSsoCodeByEmailImpl enableIdPByEmailDiscovery mbHost email =
+  do
     if not enableIdPByEmailDiscovery
       then pure Nothing
       else do
@@ -49,7 +60,7 @@ interpretIdPSubsystem enableIdPByEmailDiscovery = interpret $ \case
                 case mbTeam of
                   Just team -> do
                     idps <- getConfigsByTeam team
-                    selectIdP mbHost idps
+                    selectIdP idps
                   Nothing -> pure Nothing
           tooManyUsers -> do
             Logger.warn $
@@ -57,31 +68,31 @@ interpretIdPSubsystem enableIdPByEmailDiscovery = interpret $ \case
                 . Log.field "email" (fromEmail email)
                 . Log.field "user_ids" (Text.intercalate ", " (map (userIdToText . userQualifiedId) tooManyUsers))
             throw InconsistentUsers
+  where
+    userIdToText :: Qualified UserId -> Text
+    userIdToText uid = idToText (qUnqualified uid) <> "@" <> domainText (qDomain uid)
 
-userIdToText :: Qualified UserId -> Text
-userIdToText uid = idToText (qUnqualified uid) <> "@" <> domainText (qDomain uid)
+    selectIdP :: (Member (Logger (Log.Msg -> Log.Msg)) r) => [IP.IdP] -> Sem r (Maybe SAML.IdPId)
+    selectIdP idps = case idps of
+      -- No IdPs: no match
+      [] -> pure Nothing
+      -- Exactly one IdP: always return it
+      [idp] -> (pure . pure) (idp ^. SAML.idpId)
+      -- Multiple IdPs: find by domain if host provided
+      _idps' -> findIdPByDomain idps
 
-selectIdP :: (Member (Logger (Log.Msg -> Log.Msg)) r) => Maybe Text -> [IP.IdP] -> Sem r (Maybe SAML.IdPId)
-selectIdP mbHost idps = case idps of
-  -- No IdPs: no match
-  [] -> pure Nothing
-  -- Exactly one IdP: always return it
-  [idp] -> (pure . pure) (idp ^. SAML.idpId)
-  -- Multiple IdPs: find by domain if host provided
-  _idps' -> findIdPByDomain mbHost idps
+    isScimOrSsoUser :: User -> Bool
+    isScimOrSsoUser user =
+      userManagedBy user == ManagedByScim && isJust (userSSOId user)
 
-isScimOrSsoUser :: User -> Bool
-isScimOrSsoUser user =
-  userManagedBy user == ManagedByScim && isJust (userSSOId user)
-
-findIdPByDomain :: (Member (Logger (Log.Msg -> Log.Msg)) r) => Maybe Text -> [IP.IdP] -> Sem r (Maybe SAML.IdPId)
-findIdPByDomain mbHost idps = do
-  let matches :: [SAML.IdPId] =
-        SAML._idpId
-          <$> filter (\idp -> (idp ^. SAML.idpExtraInfo . IP.domain) == mbHost) idps
-  when (length matches > 1) $
-    Logger.warn $
-      Log.msg @Text "Found more than one IdP config for domain"
-        . Log.field "domain" (fromMaybe "None" mbHost)
-        . Log.field "idpIds" (intercalate "," $ (UUID.toString . SAML.fromIdPId) <$> matches)
-  pure $ listToMaybe matches
+    findIdPByDomain :: (Member (Logger (Log.Msg -> Log.Msg)) r) => [IP.IdP] -> Sem r (Maybe SAML.IdPId)
+    findIdPByDomain idps = do
+      let matches :: [SAML.IdPId] =
+            SAML._idpId
+              <$> filter (\idp -> (idp ^. SAML.idpExtraInfo . IP.domain) == mbHost) idps
+      when (length matches > 1) $
+        Logger.warn $
+          Log.msg @Text "Found more than one IdP config for domain"
+            . Log.field "domain" (fromMaybe "None" mbHost)
+            . Log.field "idpIds" (intercalate "," $ (UUID.toString . SAML.fromIdPId) <$> matches)
+      pure $ listToMaybe matches
