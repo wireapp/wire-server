5aed3d5324e7c6726a4653e200f9342ca8cd5a16
Sven Tennie <sven.tennie@wire.com>
2026-02-16 17:22:35 +0100

    Fix path to CA cert
    `curl` was complaining:
    
    The file '/certs/rabbitmq-ca/ca.pem' provided to --cacert does not exist

--- integration/scripts/integration-dynamic-backends-vhosts.sh
+++ integration/scripts/integration-dynamic-backends-vhosts.sh
@@ -7,6 +7,6 @@ DOMAIN=$2
 
 echo 'Creating RabbitMQ resources'
 
-curl --cacert /certs/rabbitmq-ca/ca.pem -u "$RABBITMQ_USERNAME:$RABBITMQ_PASSWORD" -X PUT "$ENDPOINT_URL/$DOMAIN"
+curl --cacert /certs/rabbitmq-ca/ca.crt -u "$RABBITMQ_USERNAME:$RABBITMQ_PASSWORD" -X PUT "$ENDPOINT_URL/$DOMAIN"
 
 echo "RabbitMQ vhost created successfully for $DOMAIN"
