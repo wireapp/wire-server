1bd69c03a7ba63de82b14c7c8e30e83f42a21da0
Sven Tennie <sven.tennie@wire.com>
2026-02-16 11:25:04 +0100

    Update Nix env
    We switch to the nixpkgs release branch (from unstable).

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     self.submodules = true;
-    nixpkgs.url = "github:nixos/nixpkgs?rev=09b8fda8959d761445f12b55f380d90375a1d6bb";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     nixpkgs_24_11.url = "github:nixos/nixpkgs?ref=nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     tom-bombadil = {
