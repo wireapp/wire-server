wireapp/wire-server

commit bd72d1e0482f49773bc94ddc954acd771693520c

WPB-22877: Allow filtering for type (regular, app) in /search/contacts (#4986)

Makefile
@@ -122,7 +122,7 @@ endif
 .PHONY: ci-safe
 ci-safe:
 	make c package=all
-	make ci-fast
+	./hack/bin/cabal-run-integration.sh integration
 
 .PHONY: ci
 ci:

integration/test/API/Brig.hs
@@ -257,6 +257,27 @@ listUsers usr qualifiedUserIds = do
   req <- baseRequest usr Brig Versioned $ joinHttpPath ["list-users"]
   submit "POST" (req & addJSONObject ["qualified_ids" .= qUsers])
 
+data SearchContactsCfg = SearchContactsCfg
+  { user :: Value,
+    searchTerm :: String,
+    domain :: String,
+    types :: Maybe [String]
+  }
+  deriving (Eq, Show)
+
+searchContactsWith ::
+  SearchContactsCfg ->
+  App Response
+searchContactsWith cfg = do
+  req <- baseRequest cfg.user Brig Versioned "/search/contacts"
+  q <- asString cfg.searchTerm
+  d <- objDomain cfg.domain
+  let typeFilter = maybe [] f cfg.types
+        where
+          f :: [String] -> [(String, String)]
+          f userTypes = [("type", intercalate "," userTypes)]
+  submit "GET" (req & addQueryParams ([("q", q), ("domain", d)] <> typeFilter))
+
 searchContacts ::
   ( MakesValue user,
     MakesValue searchTerm,
@@ -267,10 +288,10 @@ searchContacts ::
   domain ->
   App Response
 searchContacts user searchTerm domain = do
-  req <- baseRequest user Brig Versioned "/search/contacts"
-  q <- asString searchTerm
-  d <- objDomain domain
-  submit "GET" (req & addQueryParams [("q", q), ("domain", d)])
+  u <- make user
+  s <- make searchTerm & asString
+  d <- make domain & asString
+  searchContactsWith (SearchContactsCfg u s d Nothing)
 
 -- | https://staging-nginz-https.zinfra.io/v6/api/swagger-ui/#/default/get_teams__tid__search
 searchTeam :: (HasCallStack, MakesValue user) => user -> [(String, String)] -> App Response

integration/test/Test/Search.hs
@@ -142,6 +142,53 @@ testFederatedUserSearch = do
     void $ BrigI.createFedConn d1 (BrigI.FedConn d2 "full_search" Nothing)
     forM_ tcs (federatedUserSearch d1 d2)
 
+testFederatedUserSearchWithTypeOwnDomain :: (HasCallStack) => App ()
+testFederatedUserSearchWithTypeOwnDomain = do
+  d0 <- asString OwnDomain
+  checkUserSearch d0 d0 -- target OwnDomain locally
+
+testFederatedUserSearchWithType :: (HasCallStack) => App ()
+testFederatedUserSearchWithType =
+  startDynamicBackends [def, def] $ \[d1, d2] -> do
+    void $ BrigI.createFedConn d2 (BrigI.FedConn d1 "full_search" Nothing)
+    void $ BrigI.createFedConn d1 (BrigI.FedConn d2 "full_search" Nothing)
+    checkUserSearch d1 d1 -- target dynamic domain locally
+    checkUserSearch d1 d2 -- target one dynamic domain from another
+
+checkUserSearch :: (HasCallStack) => String -> String -> App ()
+checkUserSearch d1 d2 = do
+  (remoteSearcher, _, []) <- createTeam d1 1
+  (owner, tid, [mem]) <- createTeam d2 2
+  assertSuccess =<< GalleyI.setTeamFeatureStatus d2 tid "searchVisibilityInbound" "enabled"
+
+  -- create app with name "chappie" on d2
+  let newApp :: BrigP.NewApp
+      newApp = def {BrigP.name = "chappie"}
+   in BrigP.createApp owner tid newApp >>= assertSuccess
+
+  -- set name of d2 member to "chappie-also", so we can search for both with one prefix.
+  let nameUpd :: BrigP.PutSelf
+      nameUpd = def {BrigP.name = Just "chappie-also"}
+   in BrigP.putSelf mem nameUpd >>= assertSuccess
+
+  let filterByType :: (HasCallStack) => Value -> String -> Maybe [String] -> [String] -> App ()
+      filterByType user searchTerm types names = do
+        domain <- asString d2
+        BrigP.searchContactsWith BrigP.SearchContactsCfg {..}
+          `bindResponse` \resp -> do
+            resp.status `shouldMatchInt` 200
+            foundDocs :: [Value] <- resp.json %. "documents" >>= asList
+            foundNames :: [String] <- ((%. "name") >=> asString) `mapM` foundDocs
+            foundNames `shouldMatchSet` names
+
+  BrigI.refreshIndex d2
+  forM_ [owner, remoteSearcher] $ \searcher -> do
+    filterByType searcher "chappie" Nothing ["chappie", "chappie-also"]
+    filterByType searcher "chappie" (Just []) ["chappie", "chappie-also"]
+    filterByType searcher "chappie" (Just ["regular"]) ["chappie-also"]
+    filterByType searcher "chappie" (Just ["app"]) ["chappie"]
+    filterByType searcher "chappie" (Just ["regular", "app"]) ["chappie", "chappie-also"]
+
 federatedUserSearch :: (HasCallStack) => String -> String -> FedUserSearchTestCase -> App ()
 federatedUserSearch d1 d2 test = do
   void $ BrigI.updateFedConn d2 d1 (BrigI.FedConn d1 test.searchPolicy (restriction test.restrictionD2D1))

integration/test/Testlib/Assertions.hs
@@ -245,8 +245,8 @@ shouldMatchSet ::
   b ->
   App ()
 shouldMatchSet a b = do
-  la <- fmap sort (asList a)
-  lb <- fmap sort (asList b)
+  la <- asSet a
+  lb <- asSet b
   la `shouldMatch` lb
 
 shouldBeEmpty :: (MakesValue a, HasCallStack) => a -> App ()

libs/wire-api-federation/src/Wire/API/Federation/API/Brig.hs
@@ -37,7 +37,7 @@ import Wire.API.Federation.Version
 import Wire.API.MLS.CipherSuite
 import Wire.API.MLS.KeyPackage
 import Wire.API.Routes.SpecialiseToVersion
-import Wire.API.User (UserProfile)
+import Wire.API.User
 import Wire.API.User.Client
 import Wire.API.User.Client.Prekey (ClientPrekey, PrekeyBundle)
 import Wire.API.User.Search
@@ -51,7 +51,8 @@ data SearchRequest = SearchRequest
     -- | The searcher's team ID, used to matched against the remote backend's team federation policy.
     from :: Maybe TeamId,
     -- | The remote teams that the calling backend is allowed to federate with.
-    onlyInTeams :: Maybe [TeamId]
+    onlyInTeams :: Maybe [TeamId],
+    types :: Maybe [UserTypeFilter]
   }
   deriving (Show, Eq, Generic, Typeable)
   deriving (Arbitrary) via (GenericUniform SearchRequest)

libs/wire-api-federation/test/Test/Wire/API/Federation/API/BrigSpec.hs
@@ -30,7 +30,7 @@ spec = describe "Wire.API.Federation.API.Brig" $ do
   describe "RoundTripTests" $ do
     jsonRoundTrip @SearchRequest
   describe "JSON Golden Tests" $ do
-    jsonGoldenTest "SearchRequest" [aesonQQ|{"term": "searchedThing"}|] (SearchRequest "searchedThing" Nothing Nothing)
+    jsonGoldenTest "SearchRequest" [aesonQQ|{"term": "searchedThing"}|] (SearchRequest "searchedThing" Nothing Nothing Nothing)
 
 jsonGoldenTest :: (Eq a, Show a, FromJSON a) => String -> Value -> a -> Spec
 jsonGoldenTest name val expected =

libs/wire-api/src/Wire/API/Routes/Public/Brig.hs
@@ -1447,6 +1447,7 @@ type ConnectionAPI =
     :<|> Named
            "search-contacts"
            ( Summary "Search for users"
+               :> Description "Optional user-type filter semantics: omitted or empty (type=) means no filtering."
                :> ZLocalUser
                :> CanThrow 'InsufficientPermissions
                :> "search"
@@ -1454,6 +1455,7 @@ type ConnectionAPI =
                :> QueryParam' '[Required, Strict, Description "Search query"] "q" Text
                :> QueryParam' '[Optional, Strict, Description "Searched domain. Note: This is optional only for backwards compatibility, future versions will mandate this."] "domain" Domain
                :> QueryParam' '[Optional, Strict, Description "Number of results to return (min: 1, max: 500, default 15)"] "size" (Range 1 500 Int32)
+               :> QueryParam' '[Optional, Strict, Description "Only user types. Omitted or empty (type=) means no filtering."] "type" (CommaSeparatedList UserTypeFilter)
                :> Get '[Servant.JSON] (SearchResult Contact)
            )
 

libs/wire-api/src/Wire/API/User/Search.hs
@@ -32,6 +32,9 @@ module Wire.API.User.Search
     PagingState (..),
     EmailVerificationFilter (..),
     SetSearchable (..),
+    UserTypeFilter (..),
+    userTypeFilterToText,
+    userTypeFilterToUserType,
   )
 where
 
@@ -39,7 +42,9 @@ import Cassandra qualified as C
 import Control.Error
 import Control.Lens ((?~))
 import Data.Aeson hiding (object, (.=))
+import Data.Aeson qualified as A
 import Data.Aeson qualified as Aeson
+import Data.Attoparsec.ByteString qualified as AP
 import Data.Attoparsec.ByteString.Char8 (string)
 import Data.ByteString.Char8 qualified as C8
 import Data.ByteString.Conversion as BS
@@ -58,7 +63,7 @@ import Imports
 import Servant.API (FromHttpApiData, ToHttpApiData (..))
 import Web.Internal.HttpApiData (parseQueryParam)
 import Wire.API.Team.Role (Role)
-import Wire.API.User (ManagedBy, UserType)
+import Wire.API.User (ManagedBy, UserType (..))
 import Wire.API.User.Identity (EmailAddress)
 import Wire.Arbitrary (Arbitrary, GenericUniform (..))
 
@@ -310,6 +315,38 @@ instance FromByteString RoleFilter where
   parser = do
     parts <- C8.split ',' <$> parser
     RoleFilter <$> traverse (maybe (fail "Invalid role") pure . fromByteString) parts
+
+data UserTypeFilter = UserTypeFilterRegular | UserTypeFilterApp
+  deriving (Eq, Show, Generic)
+  deriving (Arbitrary) via (GenericUniform UserTypeFilter)
+  deriving (A.FromJSON, A.ToJSON, S.ToSchema) via (Schema UserTypeFilter)
+
+userTypeFilterToText :: UserTypeFilter -> Text
+userTypeFilterToText = TE.decodeUtf8 . toByteString'
+
+userTypeFilterToUserType :: UserTypeFilter -> UserType
+userTypeFilterToUserType UserTypeFilterRegular = UserTypeRegular
+userTypeFilterToUserType UserTypeFilterApp = UserTypeApp
+
+instance ToSchema UserTypeFilter where
+  schema =
+    enum @Text "UserTypeFilter" $
+      mconcat
+        [ element "regular" UserTypeFilterRegular,
+          element "app" UserTypeFilterApp
+        ]
+
+instance FromByteString UserTypeFilter where
+  parser =
+    AP.takeByteString
+      >>= \case
+        "regular" -> pure UserTypeFilterRegular
+        "app" -> pure UserTypeFilterApp
+        x -> fail $ "Invalid UserTypeFilter value: " <> show x
+
+instance ToByteString UserTypeFilter where
+  builder UserTypeFilterRegular = "regular"
+  builder UserTypeFilterApp = "app"
 
 instance FromHttpApiData RoleFilter where
   parseQueryParam = fmap RoleFilter . traverse parseQueryParam . T.split (== ',')

commit 1d594540c32cab9cf70a746eef6dcc509f8d1661

Makefile: Fix ci-safe target
It was broken in #4986

Makefile
@@ -122,7 +122,7 @@ endif
 .PHONY: ci-safe
 ci-safe:
 	make c package=all
-	./hack/bin/cabal-run-integration.sh integration
+	make ci-fast
 
 .PHONY: ci
 ci: