ba38e3b4216d24b5c3f54f4797e34778ef81238c
Leif Battermann <leif.battermann@wire.com>
2026-03-19 14:45:59 +0000

    readd comments

--- libs/wire-subsystems/src/Wire/TeamNotificationStore/Cassandra.hs
+++ libs/wire-subsystems/src/Wire/TeamNotificationStore/Cassandra.hs
@@ -88,12 +88,21 @@ notificationTTLSeconds = round $ nominalDiffTimeToSeconds $ 28 * nominalDay
 
 fetch :: TeamId -> Maybe NotificationId -> Range 1 10000 Int32 -> Client ResultPage
 fetch tid since (fromRange -> size) = do
+  -- We always need to look for one more than requested in order to correctly
+  -- report whether there are more results.
   let size' = bool (+ 1) (+ 2) (isJust since) size
   page1 <- case TimeUuid . toUUID <$> since of
     Nothing -> paginate cqlStart (paramsP LocalQuorum (Identity tid) size') & retry x1
     Just s -> paginate cqlSince (paramsP LocalQuorum (tid, s) size') & retry x1
+  -- Collect results, requesting more pages until we run out of data
+  -- or have found size + 1 notifications (not including the 'since').
   let isize = fromIntegral size' :: Int
   (ns, more) <- collect Seq.empty isize page1
+  -- Drop the extra element from the end as well. Keep the inclusive start
+  -- value in the response (if a 'since' was given and found).
+  -- This can probably simplified a lot further, but we need to understand
+  -- 'Seq' in order to do that. If you find a bug, this may be a good
+  -- place to start looking.
   pure $! case Seq.viewl (trim (isize - 1) ns) of
     EmptyL -> ResultPage Seq.empty False
     (x :< xs) -> ResultPage (x <| xs) more
@@ -139,6 +148,10 @@ toNotif (i, b) ns =
   maybe
     ns
     (\p1 -> queuedNotification notifId p1 : ns)
-    (JSON.decode' (fromBlob b))
+    ( JSON.decode' (fromBlob b)
+    -- FUTUREWORK: this is from the database, so it's slightly more ok to ignore parse
+    -- errors than if it's data provided by a client. it would still be better to have an
+    -- error entry in the log file and crash, rather than ignore the error and continue.
+    )
   where
     notifId = Id (fromTimeUuid i)
