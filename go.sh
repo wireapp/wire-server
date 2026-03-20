2b1fa537b5a7781d1eee69c04b611cf3a4a13102
Stefan Matting <stefan@wire.com>
2026-03-20 10:17:31 +0100

    add go.sh

---
+++ go.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -xe
+
+export HELM_DATA_HOME=$(mktemp -d)
+export HELM_CONFIG_HOME=$(mktemp -d)
+export HELM_CACHE_HOME=$(mktemp -d)
+
+make clean-charts
+
+NAMESPACE=test-stefan-2 DOCKER_TAG=5.27.0 make kube-integration-setup
+
+NAMESPACE=test-stefan-2 DOCKER_TAG=5.27.0 HELM_PARALLELISM=0 make kube-integration-test
