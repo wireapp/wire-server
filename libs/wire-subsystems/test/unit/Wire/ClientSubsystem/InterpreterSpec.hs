486566cfa4665bfcb565c92ed23d19e919aba6af
Leif Battermann <leif.battermann@wire.com>
2026-03-27 16:17:41 +0000

    improve tests

--- libs/wire-subsystems/test/unit/Wire/ClientSubsystem/InterpreterSpec.hs
+++ libs/wire-subsystems/test/unit/Wire/ClientSubsystem/InterpreterSpec.hs
@@ -1,8 +1,10 @@
 module Wire.ClientSubsystem.InterpreterSpec (spec) where
 
 import Data.Default
+import Data.Id
 import Data.Json.Util (toUTCTimeMillis)
 import Data.Qualified
+import Data.Set qualified as Set
 import Imports
 import Polysemy
 import Polysemy.Error
@@ -13,6 +15,7 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
 import Wire.API.Federation.Client (FederatorClient)
+import Wire.API.Push.V2 qualified as V2
 import Wire.API.Team.LegalHold.Internal
 import Wire.API.User.Client
 import Wire.API.User.Client.Prekey
@@ -101,6 +104,49 @@ runClientSubsystemTest users action =
           $ interpreted
    in ClientSubsystemTestResult {authState, result, deletions, events, pushes}
 
+expectRight ::
+  (Show e, Show a) =>
+  Either e a ->
+  (a -> Property) ->
+  Property
+expectRight result' assertRight =
+  counterexample ("unexpected result: " <> show result') $
+    case result' of
+      Left resultErr -> counterexample ("unexpected error: " <> show resultErr) False
+      Right value -> assertRight value
+
+assertSingle ::
+  (Show a) =>
+  String ->
+  [a] ->
+  (a -> Property) ->
+  Property
+assertSingle what xs assertItem = case xs of
+  [] -> counterexample ("expected one " <> what <> ", but got none") False
+  [x] -> assertItem x
+  _ : _ -> counterexample ("expected one " <> what <> ", but got many: " <> show xs) False
+
+assertClientEvent :: UserId -> Maybe ConnId -> Event -> MiniEvent -> Property
+assertClientEvent uid mConn expected miniEvent =
+  conjoin
+    [ miniEvent.userId === uid,
+      miniEvent.mConnId === mConn,
+      miniEvent.event === expected
+    ]
+
+assertClientPush :: UserId -> Maybe ConnId -> Event -> Push -> Property
+assertClientPush uid mConn expected push =
+  conjoin
+    [ push.origin === Just uid,
+      push.conn === mConn,
+      push.transient === False,
+      push.route === V2.RouteAny,
+      push.nativePriority === Nothing,
+      push.recipients === [Recipient uid V2.RecipientClientsAll],
+      push.apsData === toApsData expected,
+      push.isCellsEvent === False
+    ]
+
 spec :: Spec
 spec = describe "ClientSubsystem.Interpreter" do
   prop "adds and looks up a client" $ \user ->
@@ -120,32 +166,43 @@ spec = describe "ClientSubsystem.Interpreter" do
               clientMLSPublicKeys = mempty,
               clientLastActive = Nothing
             }
+        expectedPushEvent = ClientEvent (ClientAdded expectedClient)
         testResult =
           runClientSubsystemTest [user] do
             added <- addClient luid Nothing new
             stored <- lookupLocalClients user.id
             pure (added, stored)
         auth = testResult.authState
-     in counterexample ("unexpected result: " <> show testResult.result) $
-          case testResult.result of
-            Left clientErr ->
-              counterexample ("unexpected ClientError: " <> show clientErr) False
-            Right value ->
-              conjoin
-                [ value === (expectedClient, [expectedClient]),
-                  verificationCodeCalls auth === 1,
-                  reAuthCalls auth === 0,
-                  revokeCookiesCalls auth === 0,
-                  length testResult.deletions === 0,
-                  length testResult.events === 0,
-                  length testResult.pushes === 1
-                ]
+     in expectRight testResult.result $ \value ->
+          conjoin
+            [ value === (expectedClient, [expectedClient]),
+              auth.verificationCodeCalls === 1,
+              auth.reAuthCalls === 0,
+              auth.revokeCookiesCalls === 0,
+              testResult.deletions === [],
+              testResult.events === [],
+              assertSingle "push" testResult.pushes (assertClientPush user.id Nothing expectedPushEvent)
+            ]
 
   prop "removes client" $ \user conn ->
     let uid = user.id
         luid = toLocalUnsafe testDomain user.id
         new = newClient PermanentClientType validLastPrekey
         clientId = clientIdFromPrekey (unpackLastPrekey validLastPrekey)
+        expectedClient =
+          Client
+            { clientId,
+              clientType = PermanentClientType,
+              clientTime = toUTCTimeMillis defaultTime,
+              clientClass = Nothing,
+              clientLabel = Nothing,
+              clientCookie = Nothing,
+              clientModel = Nothing,
+              clientCapabilities = mempty,
+              clientMLSPublicKeys = mempty,
+              clientLastActive = Nothing
+            }
+        expectedPushEvent = ClientEvent (ClientAdded expectedClient)
         testResult =
           runClientSubsystemTest [user] do
             added <- addClient luid Nothing new
@@ -153,18 +210,17 @@ spec = describe "ClientSubsystem.Interpreter" do
             stored <- lookupLocalClients user.id
             pure (added, stored)
         auth = testResult.authState
-     in counterexample ("unexpected result: " <> show testResult.result) $
-          case testResult.result of
-            Left clientErr ->
-              counterexample ("unexpected ClientError: " <> show clientErr) False
-            Right (_, clients) ->
-              conjoin
-                [ length clients === 0,
-                  revokeCookiesCalls auth === 0,
-                  length testResult.deletions === 1,
-                  length testResult.events === 0,
-                  length testResult.pushes === 1
-                ]
+     in expectRight testResult.result $ \(added, clients) ->
+          conjoin
+            [ added === expectedClient,
+              clients === [],
+              auth.verificationCodeCalls === 1,
+              auth.reAuthCalls === 1,
+              auth.revokeCookiesCalls === 0,
+              testResult.deletions === [DeleteClient clientId uid (Just conn)],
+              testResult.events === [],
+              assertSingle "push" testResult.pushes (assertClientPush uid Nothing expectedPushEvent)
+            ]
 
   prop "legal hold client cannot be removed" $ \user conn ->
     let uid = user.id
@@ -177,7 +233,29 @@ spec = describe "ClientSubsystem.Interpreter" do
             removeClient uid conn clientId Nothing
      in counterexample ("unexpected result: " <> show testResult.result) $
           case testResult.result of
-            Left ClientLegalHoldCannotBeRemoved -> property True
+            Left ClientLegalHoldCannotBeRemoved ->
+              let expectedClient =
+                    Client
+                      { clientId,
+                        clientType = LegalHoldClientType,
+                        clientTime = toUTCTimeMillis defaultTime,
+                        clientClass = Just LegalHoldClient,
+                        clientLabel = Nothing,
+                        clientCookie = Nothing,
+                        clientModel = Nothing,
+                        clientCapabilities = ClientCapabilityList (Set.singleton ClientSupportsLegalholdImplicitConsent),
+                        clientMLSPublicKeys = mempty,
+                        clientLastActive = Nothing
+                      }
+                  expectedPushEvent = ClientEvent (ClientAdded expectedClient)
+               in conjoin
+                    [ testResult.authState.verificationCodeCalls === 1,
+                      testResult.authState.reAuthCalls === 0,
+                      testResult.authState.revokeCookiesCalls === 0,
+                      testResult.deletions === [],
+                      assertSingle "event" testResult.events (assertClientEvent uid Nothing (UserEvent (UserLegalHoldEnabled uid))),
+                      assertSingle "push" testResult.pushes (assertClientPush uid Nothing expectedPushEvent)
+                    ]
             Left clientErr ->
               counterexample ("unexpected ClientError: " <> show clientErr) False
             Right _ ->
@@ -187,6 +265,21 @@ spec = describe "ClientSubsystem.Interpreter" do
     let uid = user.id
         luid = toLocalUnsafe testDomain user.id
         new = newClient LegalHoldClientType validLastPrekey
+        clientId = clientIdFromPrekey (unpackLastPrekey validLastPrekey)
+        expectedClient =
+          Client
+            { clientId,
+              clientType = LegalHoldClientType,
+              clientTime = toUTCTimeMillis defaultTime,
+              clientClass = Just LegalHoldClient,
+              clientLabel = Nothing,
+              clientCookie = Nothing,
+              clientModel = Nothing,
+              clientCapabilities = ClientCapabilityList (Set.singleton ClientSupportsLegalholdImplicitConsent),
+              clientMLSPublicKeys = mempty,
+              clientLastActive = Nothing
+            }
+        expectedPushEvent = ClientEvent (ClientAdded expectedClient)
         testResult =
           runClientSubsystemTest [user] do
             added <- addClient luid Nothing new
@@ -194,45 +287,39 @@ spec = describe "ClientSubsystem.Interpreter" do
             stored <- lookupLocalClients user.id
             pure (added, stored)
         auth = testResult.authState
-     in counterexample ("unexpected result: " <> show testResult.result) $
-          case testResult.result of
-            Left clientErr ->
-              counterexample ("unexpected ClientError: " <> show clientErr) False
-            Right (_, clients) ->
-              conjoin
-                [ length clients === 0,
-                  revokeCookiesCalls auth === 0,
-                  length testResult.deletions === 1,
-                  length testResult.events === 2,
-                  length testResult.pushes === 1
-                ]
+     in expectRight testResult.result $ \(added, clients) ->
+          conjoin
+            [ added === expectedClient,
+              clients === [],
+              auth.verificationCodeCalls === 1,
+              auth.reAuthCalls === 0,
+              auth.revokeCookiesCalls === 0,
+              testResult.deletions === [DeleteClient clientId uid Nothing],
+              testResult.events
+                === [ MkMiniEvent uid Nothing (UserEvent (UserLegalHoldDisabled uid)),
+                      MkMiniEvent uid Nothing (UserEvent (UserLegalHoldEnabled uid))
+                    ],
+              assertSingle "push" testResult.pushes (assertClientPush uid Nothing expectedPushEvent)
+            ]
 
   prop "requests a legal hold client" $ \user ->
     let uid = user.id
         req = LegalHoldClientRequest uid validLastPrekey
+        clientId = clientIdFromPrekey (unpackLastPrekey validLastPrekey)
+        expectedEvent =
+          UserEvent
+            ( LegalHoldClientRequested
+                (LegalHoldClientRequestedData uid validLastPrekey clientId)
+            )
         testResult =
           runClientSubsystemTest [user] do
             publishLegalHoldClientRequested uid req
-        isLhRequested (UserEvent (LegalHoldClientRequested _)) = property True
-        isLhRequested e = counterexample ("unexpected event: " <> show e) False
-        isSingelLhEvent events = case events of
-          [] -> counterexample "expected one event, but got none" False
-          [event] ->
-            conjoin
-              [ event.userId === uid,
-                isLhRequested event.event
-              ]
-          _ -> counterexample "expected one event, but got many" False
-     in counterexample ("unexpected result: " <> show testResult.result) $
-          case testResult.result of
-            Left clientErr ->
-              counterexample ("unexpected ClientError: " <> show clientErr) False
-            Right () ->
-              conjoin
-                [ length testResult.deletions === 0,
-                  isSingelLhEvent testResult.events,
-                  length testResult.pushes === 0
-                ]
+     in expectRight testResult.result $ \() ->
+          conjoin
+            [ testResult.deletions === [],
+              assertSingle "event" testResult.events (assertClientEvent uid Nothing expectedEvent),
+              testResult.pushes === []
+            ]
 
 validLastPrekey :: LastPrekey
 validLastPrekey =
