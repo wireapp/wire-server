f92101e6bb60ca07eac61d6a20df7303111b2cc7
Matthias Fischmann <mf@zerobuzz.net>
2026-05-15 15:38:02 +0200

    Rewrite integration test: inline functions.

--- integration/test/Test/Apps.hs
+++ integration/test/Test/Apps.hs
@@ -603,44 +603,26 @@ testTeamSizeWithApps (TaggedBool testInternalApi) = do
 testZauthAndApps :: (HasCallStack) => App ()
 testZauthAndApps = do
   (owner, tid, []) <- createTeam OwnDomain 1
-  (app, cookie) <- createIt owner tid
-
-  refreshSucceeds app cookie
-  suspendApp app >> refreshFails app cookie
-  unsuspendApp app >> refreshSucceeds app cookie
-  where
-    createIt :: (HasCallStack, MakesValue owner) => owner -> String -> App (Value, String)
-    createIt owner tid =
-      createApp owner tid new `bindResponse` \resp -> do
-        resp.status `shouldMatchInt` 200
-        app <- resp.json %. "user"
-        cookie <- resp.json %. "cookie" & asString
-        pure (app, cookie)
-      where
-        new :: NewApp =
+  (app, cookie) <- do
+    let new :: NewApp =
           def
             { name = "chappie",
               description = "some description of this app",
               category = "ai"
             }
 
-    suspendApp :: (HasCallStack, MakesValue app) => app -> App ()
-    suspendApp app =
-      BrigI.setAccountStatus app "suspended" `bindResponse` \resp -> do
-        resp.status `shouldMatchInt` 200
+    createApp owner tid new `bindResponse` \resp -> do
+      resp.status `shouldMatchInt` 200
+      app <- resp.json %. "user"
+      cookie <- resp.json %. "cookie" & asString
+      pure (app, cookie)
 
-    unsuspendApp :: (HasCallStack, MakesValue app) => app -> App ()
-    unsuspendApp app =
-      BrigI.setAccountStatus app "active" `bindResponse` \resp -> do
-        resp.status `shouldMatchInt` 200
+  renewToken app cookie >>= assertSuccess
 
-    refreshSucceeds :: (HasCallStack, MakesValue app) => app -> String -> App ()
-    refreshSucceeds app cookie =
-      renewToken app cookie `bindResponse` \resp -> do
-        resp.status `shouldMatchInt` 200
+  BrigI.setAccountStatus app "suspended" >>= assertSuccess
+  renewToken app cookie `bindResponse` \resp -> do
+    resp.status `shouldMatchInt` 403
+    (resp.json %. "label") `shouldMatch` "invalid-credentials"
 
-    refreshFails :: (HasCallStack, MakesValue app) => app -> String -> App ()
-    refreshFails app cookie =
-      renewToken app cookie `bindResponse` \resp -> do
-        resp.status `shouldMatchInt` 403
-        (resp.json %. "label") `shouldMatch` "invalid-credentials"
+  BrigI.setAccountStatus app "active" >>= assertSuccess
+  renewToken app cookie >>= assertSuccess
