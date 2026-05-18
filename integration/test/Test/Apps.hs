84cd1bcf7c509fc39445d595250ed7c1fb0af7ed
Matthias Fischmann <mf@zerobuzz.net>
2026-05-18 09:28:11 +0200

    Simplify test.

--- integration/test/Test/Apps.hs
+++ integration/test/Test/Apps.hs
@@ -221,11 +221,9 @@ testPutApp = do
   domain <- make OwnDomain
   (owner, tid, [regularMember]) <- createTeam domain 2
   let new = def {name = "choppie"} :: NewApp
-  (app, appId) <- bindResponse (createApp owner tid new) $ \resp -> do
+  app <- bindResponse (createApp owner tid new) $ \resp -> do
     resp.status `shouldMatchInt` 200
-    (,)
-      <$> (resp.json %. "user")
-      <*> (resp.json %. "user.id")
+    resp.json %. "user"
 
   let Object appMetadata =
         [aesonQQ|
@@ -244,15 +242,15 @@ testPutApp = do
         }|]
 
   withWebSockets [owner, regularMember, app] \[wsOwner, wsRegularMember, wsApp] -> do
-    bindResponse (putAppMetadata tid owner appId (Object appMetadata)) $ \resp -> do
+    bindResponse (putAppMetadata tid owner app (Object appMetadata)) $ \resp -> do
       resp.status `shouldMatchInt` 200
     notifOwner <- awaitMatch isUserUpdatedNotif wsOwner
     notifMember <- awaitMatch isUserUpdatedNotif wsRegularMember
     notifApp <- awaitMatch isUserUpdatedNotif wsApp
-    notifOwner %. "payload.0.user.id" `shouldMatch` appId
+    notifOwner %. "payload.0.user.id" `shouldMatch` app
     length (nub [notifOwner, notifMember, notifApp]) `shouldMatchInt` 1
 
-  bindResponse (getApp owner tid appId) $ \resp -> do
+  bindResponse (getApp owner tid app) $ \resp -> do
     resp.status `shouldMatchInt` 200
     resp.json
       `shouldMatchShapeLenient` SObject
@@ -262,8 +260,8 @@ testPutApp = do
           ("app", SObject [("category", SString), ("description", SString)])
         ]
 
-  let badAppId = "5e002eca-114f-11f1-b5a3-7306b8837f91"
-  bindResponse (putAppMetadata tid owner badAppId (Object appMetadata)) $ \resp -> do
+  let badApp = "5e002eca-114f-11f1-b5a3-7306b8837f91"
+  bindResponse (putAppMetadata tid owner badApp (Object appMetadata)) $ \resp -> do
     resp.status `shouldMatchInt` 404
 
 -- | FUTUREWORK: 'Test.Apps.testFindApp',
