78c3c946e26bd03914d86bb1770f945418314ba5
Akshay Mankar <axeman@mailbox.org>
2026-07-02 16:37:02 +0200

    integration: Move test to target push to a specific client from gundeck-integration (#5304)
    The test in gundeck-integration is flaky.

--- integration/test/Test/Notifications.hs
+++ integration/test/Test/Notifications.hs
@@ -193,3 +193,32 @@ testGetServerTime = do
       r.status `shouldMatchInt` 200
       r.json %. "time" & asString
   void $ assertJust ("expected ISO 8601 format, but got: " <> formattedTimestampStr) $ iso8601ParseM @Maybe @UTCTime formattedTimestampStr
+
+testTargetClientPush :: (HasCallStack) => App ()
+testTargetClientPush = do
+  user <- randomUserId OwnDomain
+  userId <- user %. "id" & asString
+  cid1 <- randomClientId
+  connId1 <- randomConnId
+  cid2 <- randomClientId
+  connId2 <- randomConnId
+  let push c =
+        object
+          [ "recipients"
+              .= [ object
+                     [ "user_id" .= userId,
+                       "route" .= "any",
+                       "clients" .= [c]
+                     ]
+                 ],
+            "payload" .= [object ["client" .= c]]
+          ]
+  withWebSockets [(user, connId1, cid1), (user, connId2, cid2)] $ \[ws1, ws2] -> do
+    postPush user [push cid1, push cid2] >>= assertSuccess
+    forConcurrently_ [(cid1, ws1), (cid2, ws2)] $ \(cid, ws) -> do
+      ev <- awaitAnyEvent 2 ws
+      ev %. "payload" `shouldMatch` [object ["client" .= cid]]
+      assertNoEvent 2 ws
+      -- Also check the notification stream
+      lastNotif <- getLastNotification user def {client = Just cid} >>= getJSON 200
+      lastNotif %. "payload" `shouldMatch` [object ["client" .= cid]]
