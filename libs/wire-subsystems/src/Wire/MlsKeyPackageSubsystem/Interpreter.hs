8116b44334f64e28ba58aa6210146728858b6184
Leif Battermann <leif.battermann@wire.com>
2026-07-23 14:26:07 +0000

    licence header and small refactoring

--- libs/wire-subsystems/src/Wire/MlsKeyPackageSubsystem/Interpreter.hs
+++ libs/wire-subsystems/src/Wire/MlsKeyPackageSubsystem/Interpreter.hs
@@ -1,6 +1,24 @@
+-- This file is part of the Wire Server implementation.
+--
+-- Copyright (C) 2025 Wire Swiss GmbH <opensource@wire.com>
+--
+-- This program is free software: you can redistribute it and/or modify it under
+-- the terms of the GNU Affero General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option) any
+-- later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Affero General Public License along
+-- with this program. If not, see <https://www.gnu.org/licenses/>.
+
 module Wire.MlsKeyPackageSubsystem.Interpreter (interpretMlsKeyPackageSubsystem) where
 
 import Control.Concurrent qualified as C
+import Control.Error (atMay)
 import Control.Monad.Random (randomRIO)
 import Data.Id
 import Data.Time.Clock (NominalDiffTime)
@@ -13,29 +31,74 @@ import Wire.API.MLS.KeyPackage
 import Wire.API.MLS.LeafNode
 import Wire.API.MLS.Serialisation
 import Wire.MlsKeyPackageStore qualified as Store
-import Wire.MlsKeyPackageSubsystem
+import Wire.MlsKeyPackageSubsystem (MlsKeyPackageSubsystem (..), validateKeyPackageLifetime)
 
 interpretMlsKeyPackageSubsystem :: (Member Store.MlsKeyPackageStore r, Member (Embed IO) r, Member Resource r) => Maybe NominalDiffTime -> C.MVar () -> InterpreterFor MlsKeyPackageSubsystem r
 interpretMlsKeyPackageSubsystem configuredLifetime lock = interpret $ \case
-  InsertMlsKeyPackages u c ps -> Store.insertKeyPackages u c ps
-  ClaimMlsKeyPackage u c s -> bracket (embed (C.takeMVar lock)) (const $ embed (C.putMVar lock ())) (const $ claim configuredLifetime u c s)
-  CountMlsKeyPackages u c s -> fromIntegral . length <$> available configuredLifetime u c s
-  DeleteMlsKeyPackages u c s rs -> Store.deleteKeyPackages u c s rs
-  DeleteAllMlsKeyPackages u c ss -> Store.deleteAllKeyPackages u c ss
+  InsertMlsKeyPackages u c ps -> insertMlsKeyPackages u c ps
+  ClaimMlsKeyPackage u c s -> claimMlsKeyPackage configuredLifetime lock u c s
+  CountMlsKeyPackages u c s -> countMlsKeyPackages configuredLifetime u c s
+  DeleteMlsKeyPackages u c s rs -> deleteMlsKeyPackages u c s rs
+  DeleteAllMlsKeyPackages u c ss -> deleteAllMlsKeyPackages u c ss
 
-claim :: (Member Store.MlsKeyPackageStore r, Member (Embed IO) r) => Maybe NominalDiffTime -> UserId -> ClientId -> CipherSuiteTag -> Sem r (Maybe (KeyPackageRef, KeyPackageData))
-claim maxLifetime u c s = do
-  candidates <- available maxLifetime u c s
-  case candidates of
-    [] -> pure Nothing
-    _ -> do
-      index <- embed $ randomRIO (0, length candidates - 1)
-      let candidate = candidates !! index
-      Store.deleteKeyPackage u c s (fst candidate)
-      pure (Just candidate)
+insertMlsKeyPackages :: (Member Store.MlsKeyPackageStore r) => UserId -> ClientId -> [(KeyPackageRef, CipherSuiteTag, KeyPackageData)] -> Sem r ()
+insertMlsKeyPackages u c ps = Store.insertKeyPackages u c ps
 
-available :: (Member Store.MlsKeyPackageStore r, Member (Embed IO) r) => Maybe NominalDiffTime -> UserId -> ClientId -> CipherSuiteTag -> Sem r [(KeyPackageRef, KeyPackageData)]
-available maxLifetime u c s = do
+claimMlsKeyPackage ::
+  (Member Store.MlsKeyPackageStore r, Member (Embed IO) r, Member Resource r) =>
+  Maybe NominalDiffTime ->
+  MVar () ->
+  UserId ->
+  ClientId ->
+  CipherSuiteTag ->
+  Sem r (Maybe (KeyPackageRef, KeyPackageData))
+claimMlsKeyPackage maxLifetime lock u c s =
+  bracket (embed (C.takeMVar lock)) (const $ embed (C.putMVar lock ())) (const claim)
+  where
+    claim :: (Member Store.MlsKeyPackageStore r, Member (Embed IO) r) => Sem r (Maybe (KeyPackageRef, KeyPackageData))
+    claim = do
+      candidates <- getNonClaimedKeyPackages maxLifetime u c s
+      case candidates of
+        [] -> pure Nothing
+        _ -> do
+          mk <- embed (pick candidates)
+          for mk $ \candidate -> do
+            Store.deleteKeyPackage u c s (fst candidate)
+            pure candidate
+    pick :: [a] -> IO (Maybe a)
+    pick [] = pure Nothing
+    pick xs = do
+      i <- randomRIO (0, length xs - 1)
+      pure (atMay xs i)
+
+countMlsKeyPackages ::
+  ( Num b,
+    Member Store.MlsKeyPackageStore r,
+    Member (Embed IO) r
+  ) =>
+  Maybe NominalDiffTime ->
+  UserId ->
+  ClientId ->
+  CipherSuiteTag ->
+  Sem r b
+countMlsKeyPackages configuredLifetime u c s = fromIntegral . length <$> getNonClaimedKeyPackages configuredLifetime u c s
+
+deleteMlsKeyPackages ::
+  (Member Store.MlsKeyPackageStore r) =>
+  UserId ->
+  ClientId ->
+  CipherSuiteTag ->
+  [KeyPackageRef] ->
+  Sem r ()
+deleteMlsKeyPackages u c s rs = Store.deleteKeyPackages u c s rs
+
+deleteAllMlsKeyPackages :: (Member Store.MlsKeyPackageStore r) => UserId -> ClientId -> [CipherSuiteTag] -> Sem r ()
+deleteAllMlsKeyPackages u c ss = Store.deleteAllKeyPackages u c ss
+
+-- | Fetch all unclaimed non-expired key packages for a given client and delete
+-- from the database those that have expired.
+getNonClaimedKeyPackages :: (Member Store.MlsKeyPackageStore r, Member (Embed IO) r) => Maybe NominalDiffTime -> UserId -> ClientId -> CipherSuiteTag -> Sem r [(KeyPackageRef, KeyPackageData)]
+getNonClaimedKeyPackages maxLifetime u c s = do
   rows <- Store.lookupKeyPackages u c s
   now <- embed getPOSIXTime
   let decoded = mapMaybe decode rows
