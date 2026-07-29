76168ddae8c1ed32d388b6af70a23da88ee05698
Sven Tennie <sven.tennie@wire.com>
2026-07-29 18:06:16 +0200

    Connect Grafana LGTM to Postgresql

--- deploy/dockerephemeral/docker/postgres-initdb/setup.sql
+++ deploy/dockerephemeral/docker/postgres-initdb/setup.sql
@@ -7,3 +7,17 @@ GRANT ALL PRIVILEGES ON DATABASE "backendB" TO "wire-server";
 GRANT ALL PRIVILEGES ON DATABASE "dyn-1" TO "wire-server";
 GRANT ALL PRIVILEGES ON DATABASE "dyn-2" TO "wire-server";
 GRANT ALL PRIVILEGES ON DATABASE "dyn-3" TO "wire-server";
+
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+\connect "backendB"
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+\connect "dyn-1"
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+\connect "dyn-2"
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+\connect "dyn-3"
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
