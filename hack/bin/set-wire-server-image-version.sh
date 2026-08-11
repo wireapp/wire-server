d2d87f073b3c28ac7376f8dd53af8bbd0e137b0d
Stefan Matting <smatting@users.noreply.github.com>
2026-08-11 18:15:48 +0200

    hip-15 for onprem charts (#5438)
    ---------
    
    Co-authored-by: Gautier DI FOLCO <gautier.difolco@gmail.com>

--- hack/bin/set-wire-server-image-version.sh
+++ hack/bin/set-wire-server-image-version.sh
@@ -6,11 +6,23 @@ target_version=${1?$USAGE}
 TOP_LEVEL="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 CHARTS_DIR="$TOP_LEVEL/.local/charts"
 
-charts=(brig cannon galley gundeck spar cargohold proxy cassandra-migrations elasticsearch-index federator backoffice background-worker integration wire-server-enterprise)
+charts=(brig cannon galley gundeck spar cargohold proxy cassandra-migrations elasticsearch-index federator backoffice background-worker integration mlsstats nginz wire-server-enterprise)
 
 for chart in "${charts[@]}"; do
-    sed -i "s/^  tag: .*/  tag: $target_version/g" "$CHARTS_DIR/$chart/values.yaml"
-done
+    # Determine indentation for tag field
+    case "$chart" in
+        nginz)
+            indent="    "  # 4 spaces
+            ;;
+        *)
+            indent="  "    # 2 spaces
+            ;;
+    esac
 
-# special case nginz
-sed -i "s/^    tag: .*/    tag: $target_version/g" "$CHARTS_DIR/nginz/values.yaml"
+    if [ -f "$CHARTS_DIR/$chart/values.yaml" ]; then
+      sed -i "s|^${indent}tag: .*|${indent}tag: $target_version|g" "$CHARTS_DIR/$chart/values.yaml"
+    fi
+    if [ -f "$CHARTS_DIR/$chart/Chart.yaml" ]; then
+      sed -i "s|:do-not-use|:$target_version|g" "$CHARTS_DIR/$chart/Chart.yaml"
+    fi
+done
