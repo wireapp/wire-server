251969aba0da5867688502d835c3dc2642de0b81
Stefan Matting <stefan@wire.com>
2026-08-11 17:04:40 +0200

    fix bug in set-wire-server-image-version.sh

--- hack/bin/set-wire-server-image-version.sh
+++ hack/bin/set-wire-server-image-version.sh
@@ -9,18 +9,18 @@ CHARTS_DIR="$TOP_LEVEL/.local/charts"
 charts=(brig cannon galley gundeck spar cargohold proxy cassandra-migrations elasticsearch-index federator backoffice background-worker integration mlsstats nginz wire-server-enterprise)
 
 for chart in "${charts[@]}"; do
-    # Determine indentation length for tag field
+    # Determine indentation for tag field
     case "$chart" in
         nginz)
-            tag_indent="^    tag:"  # 4 spaces
+            indent="    "  # 4 spaces
             ;;
         *)
-            tag_indent="^  tag:"    # 2 spaces
+            indent="  "    # 2 spaces
             ;;
     esac
 
     if [ -f "$CHARTS_DIR/$chart/values.yaml" ]; then
-      sed -i "s|${tag_indent} .*|${tag_indent} $target_version|g" "$CHARTS_DIR/$chart/values.yaml"
+      sed -i "s|^${indent}tag: .*|${indent}tag: $target_version|g" "$CHARTS_DIR/$chart/values.yaml"
     fi
     if [ -f "$CHARTS_DIR/$chart/Chart.yaml" ]; then
       sed -i "s|:do-not-use|:$target_version|g" "$CHARTS_DIR/$chart/Chart.yaml"
