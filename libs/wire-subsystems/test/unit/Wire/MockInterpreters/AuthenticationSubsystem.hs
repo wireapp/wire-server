71568e0aaf720e6422d726c0ae24f15a5bda4435
Matthias Fischmann <mf@zerobuzz.net>
2026-08-19 13:44:44 +0200

    Flesh out authentication subsystem mock interpreter.

--- libs/wire-subsystems/test/unit/Wire/MockInterpreters/AuthenticationSubsystem.hs
+++ libs/wire-subsystems/test/unit/Wire/MockInterpreters/AuthenticationSubsystem.hs
@@ -32,4 +32,29 @@ mockAuthenticationSubsystemInterpreter = interpret \case
   EnforceVerificationCodeEither {} -> do
     modify \st -> st {verificationCodeCalls = st.verificationCodeCalls + 1}
     pure $ pure ()
-  _ -> error "mockAuthenticationSubsystemInterpreter: implement on demand"
+  VerifyUserPasswordError {} -> pure ()
+  NewCookie {} -> pure fakeCookie
+  NewCookieLimited {} -> pure (Right fakeCookie)
+  --
+  CreatePasswordResetCode {} -> error "AuthenticationSubsystem.CreatePasswordResetCode not implemented in mock interpreter"
+  ResetPassword {} -> error "AuthenticationSubsystem.ResetPassword not implemented in mock interpreter"
+  AuthenticateEither {} -> error "AuthenticationSubsystem.AuthenticateEither not implemented in mock interpreter"
+  VerifyUserPassword {} -> error "AuthenticationSubsystem.VerifyUserPassword not implemented in mock interpreter"
+  VerifyProviderPassword {} -> error "AuthenticationSubsystem.VerifyProviderPassword not implemented in mock interpreter"
+  InternalLookupPasswordResetCode {} -> error "AuthenticationSubsystem.InternalLookupPasswordResetCode not implemented in mock interpreter"
+
+fakeCookie :: forall t. (ZAuth.Body t ~ ZAuth.User) => Cookie (Token t)
+fakeCookie =
+  Cookie
+    { cookieId = CookieId 3,
+      cookieType = PersistentCookie,
+      cookieCreated = now1,
+      cookieExpires = now2,
+      cookieLabel = Nothing,
+      cookieSucc = Nothing,
+      cookieValue = val
+    }
+  where
+    val = Token (Signature "") (Header 0 0 0 Nothing) (ZAuth.User UUID.nil Nothing 0)
+    Just now1 = parseTimeM True defaultTimeLocale "%Y" "1983"
+    Just now2 = parseTimeM True defaultTimeLocale "%Y" "1987"
