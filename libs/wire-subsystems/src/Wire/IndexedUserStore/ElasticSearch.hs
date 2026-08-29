7fb124c668aa4696823381612cc8eb4294eedf1b
Matthias Fischmann <mf@zerobuzz.net>
2026-08-29 09:50:41 +0200

    [WPB-28089] Treat team collaborators like team members in contact search (second attempt). (#5488)
    * Include team collaborators in contact search.
    
    * Implement TeamCollaboratorsSubsystem interpreter with BrigAPIAccess.
    
    Was previously UserSubsystem, but since it TeamCollaboratorsSubsystem
    is also used outside of Brig, that is not always available.
    
    Further changes:
    
    - Support BrigAPIAccess locally in Brig.
    - Change collaborator field type in UserDoc to collapse `Nothing` and
      `Just []` (remove the Maybe).
    
    * Fix brig-index: do not bump index version in failure case.
    
    * Add release notes on required postgres setup steps.
    
    ---------
    
    Co-authored-by: Gautier DI FOLCO <gautier.difolco@wire.com>

--- libs/wire-subsystems/src/Wire/IndexedUserStore/ElasticSearch.hs
+++ libs/wire-subsystems/src/Wire/IndexedUserStore/ElasticSearch.hs
@@ -526,13 +526,17 @@ matchSelf :: UserId -> Maybe ES.Query
 matchSelf searcher = Just (termQ "_id" (idToText searcher))
 
 -- | Exclude apps from other teams.
--- Apps should only be searchable within their own team.
+-- Apps should only be searchable within their own team, or within a team they
+-- collaborate with.
 matchAppsFromOtherTeams :: Maybe TeamId -> Maybe ES.Query
 matchAppsFromOtherTeams mSearcherTeamId =
   Just $
     ES.QueryBoolQuery
       boolQuery
-        { ES.boolQueryMustMatch =
+        { -- Apps collaborating with the searcher's team are not excluded.
+          ES.boolQueryMustNotMatch =
+            maybeToList (termQ "collaborating_teams" . idToText <$> mSearcherTeamId),
+          ES.boolQueryMustMatch =
             [ -- Match apps (type = "app")
               termQ "type" "app",
               -- That are from a different team than the searcher
@@ -640,7 +644,16 @@ restrictSearchSpaceByUserType = \case
       else ES.TermsQuery "type" (userTypeFilterToText <$> (utsH :| utsT))
 
 matchTeamMembersOf :: TeamId -> ES.Query
-matchTeamMembersOf team = ES.TermQuery (ES.Term "team" $ idToText team) Nothing
+matchTeamMembersOf team =
+  ES.QueryBoolQuery
+    boolQuery
+      { ES.boolQueryShouldMatch =
+          [ -- Match users who are members of the team
+            ES.TermQuery (ES.Term "team" $ idToText team) Nothing,
+            -- Match users who are collaborators in the team
+            ES.TermQuery (ES.Term "collaborating_teams" $ idToText team) Nothing
+          ]
+      }
 
 matchTeamMembersSearchableByAllTeams :: ES.Query
 matchTeamMembersSearchableByAllTeams =
