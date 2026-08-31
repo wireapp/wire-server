cf204ab5e2504e8906473f6dd225241e98cfab0f
Stefan Matting <stefan@wire.com>
2026-08-31 16:33:40 +0200

    publish mlsstats image

--- nix/wire-server.nix
+++ nix/wire-server.nix
@@ -89,6 +89,7 @@ let
     team-info = [ "team-info" ];
     wire-server-enterprise = [ "wire-server-enterprise" ];
     migrate-features = [ "migrate-features" ];
+    mls-stats = [ "mls-stats" ];
   };
 
   inherit (lib) attrsets;
