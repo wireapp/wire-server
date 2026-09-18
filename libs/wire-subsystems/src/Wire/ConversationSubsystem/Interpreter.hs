435d3439ecd5e66af93c1b50570e74dc0d49546d
Leif Battermann <leif.battermann@wire.com>
2026-09-18 13:13:53 +0200

    WPB-28709 additional logging on failed mls commits (#5544)

--- libs/wire-subsystems/src/Wire/ConversationSubsystem/Interpreter.hs
+++ libs/wire-subsystems/src/Wire/ConversationSubsystem/Interpreter.hs
@@ -25,15 +25,18 @@ module Wire.ConversationSubsystem.Interpreter
   )
 where
 
+import Data.Aeson qualified as A
+import Data.Aeson.Types qualified as AT
 import Data.Qualified
+import Data.Text qualified as Text
 import Imports
-import Network.Wai.Utilities.JSONResponse (JSONResponse)
+import Network.Wai.Utilities.JSONResponse (JSONResponse (..))
 import Polysemy
 import Polysemy.Async (Async)
 import Polysemy.Error
 import Polysemy.Input
 import Polysemy.Resource (Resource)
-import Polysemy.TinyLog (TinyLog)
+import Polysemy.TinyLog (TinyLog, logErrors)
 import Wire.API.Conversation.Config
 import Wire.API.Error
 import Wire.API.Federation.Client (FederatorClient)
@@ -86,6 +89,14 @@ import Wire.TeamStore (TeamStore)
 import Wire.TeamSubsystem (TeamSubsystem)
 import Wire.UserClientIndexStore (UserClientIndexStore)
 import Wire.UserGroupStore (UserGroupStore)
+
+renderConversationSubsystemError :: ConversationSubsystemError -> Text
+renderConversationSubsystemError errorValue =
+  let response = toResponse errorValue
+      label = case response.value of
+        A.Object object -> fromMaybe "unknown" (AT.parseMaybe (A..: "label") object)
+        _ -> "unknown"
+   in "status=" <> Text.pack (show response.status) <> " label=" <> label
 
 interpretConversationSubsystem ::
   ( Member MeetingNotifier r,
@@ -152,9 +163,15 @@ interpretConversationSubsystem = interpret $ \case
   InternalGetLocalMember cid uid ->
     mapErrors $ ConvStore.getLocalMember cid uid
   PostMLSCommitBundle loc qusr c ctype qConvOrSub conn oosCheck bundle ->
-    mapErrors $ MLSMessage.postMLSCommitBundle loc qusr c ctype qConvOrSub conn oosCheck bundle
+    logErrors @_ @ConversationSubsystemError
+      renderConversationSubsystemError
+      "MLS commit bundle failed"
+      (mapErrors $ MLSMessage.postMLSCommitBundle loc qusr c ctype qConvOrSub conn oosCheck bundle)
   PostMLSCommitBundleFromLocalUser v lusr c conn bundle ->
-    mapErrors $ MLSMessage.postMLSCommitBundleFromLocalUser v lusr c conn bundle
+    logErrors @_ @ConversationSubsystemError
+      renderConversationSubsystemError
+      "MLS commit bundle failed"
+      (mapErrors $ MLSMessage.postMLSCommitBundleFromLocalUser v lusr c conn bundle)
   PostMLSMessage loc qusr c ctype qconvOrSub con oosCheck msg ->
     mapErrors $ MLSMessage.postMLSMessage loc qusr c ctype qconvOrSub con oosCheck msg
   PostMLSMessageFromLocalUser v lusr c conn smsg ->
